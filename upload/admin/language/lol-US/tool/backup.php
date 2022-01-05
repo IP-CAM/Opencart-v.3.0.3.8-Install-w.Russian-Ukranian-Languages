464b872e787b08305728f333cc26f80857be5d44
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:02 +0200

    New translations backup.php (LOLCAT)

---
+++ upload/admin/language/lol-US/tool/backup.php
@@ -0,0 +1,19 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns95535:0crwdne95535:0';
+
+// Text
+$_['text_success']     = 'crwdns95537:0crwdne95537:0';
+
+// Entry
+$_['entry_progress']   = 'crwdns95539:0crwdne95539:0';
+$_['entry_export']     = 'crwdns95541:0crwdne95541:0';
+
+// Tab
+$_['tab_backup']       = 'crwdns95543:0crwdne95543:0';
+$_['tab_restore']      = 'crwdns95545:0crwdne95545:0';
+
+// Error
+$_['error_permission'] = 'crwdns95547:0crwdne95547:0';
+$_['error_export']     = 'crwdns95549:0crwdne95549:0';
+$_['error_file']       = 'crwdns95551:0crwdne95551:0';
