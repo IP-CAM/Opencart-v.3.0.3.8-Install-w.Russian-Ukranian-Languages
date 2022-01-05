18c9333d898aca018cdcb8e987241796caea6826
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:33 +0200

    New translations marketing.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/extension/marketing.php
@@ -0,0 +1,15 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns83927:0crwdne83927:0';
+
+// Text
+$_['text_success']     = 'crwdns83929:0crwdne83929:0';
+$_['text_list']        = 'crwdns83931:0crwdne83931:0';
+
+// Column
+$_['column_name']      = 'crwdns83933:0crwdne83933:0';
+$_['column_status']    = 'crwdns83935:0crwdne83935:0';
+$_['column_action']    = 'crwdns83937:0crwdne83937:0';
+
+// Error
+$_['error_permission'] = 'crwdns83939:0crwdne83939:0';
