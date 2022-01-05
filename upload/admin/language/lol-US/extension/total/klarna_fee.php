4849f93a65a3eb8a598788b99879350c18270cb3
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:26 +0200

    New translations klarna_fee.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/total/klarna_fee.php
@@ -0,0 +1,24 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns93085:0crwdne93085:0';
+
+// Text
+$_['text_extension']   = 'crwdns93087:0crwdne93087:0';
+$_['text_success']     = 'crwdns93089:0crwdne93089:0';
+$_['text_edit']        = 'crwdns93091:0crwdne93091:0';
+$_['text_sweden']      = 'crwdns93093:0crwdne93093:0';
+$_['text_norway']      = 'crwdns93095:0crwdne93095:0';
+$_['text_finland']     = 'crwdns93097:0crwdne93097:0';
+$_['text_denmark']     = 'crwdns93099:0crwdne93099:0';
+$_['text_germany']     = 'crwdns93101:0crwdne93101:0';
+$_['text_netherlands'] = 'crwdns93103:0crwdne93103:0';
+
+// Entry
+$_['entry_total']      = 'crwdns93105:0crwdne93105:0';
+$_['entry_fee']        = 'crwdns93107:0crwdne93107:0';
+$_['entry_tax_class']  = 'crwdns93109:0crwdne93109:0';
+$_['entry_status']     = 'crwdns93111:0crwdne93111:0';
+$_['entry_sort_order'] = 'crwdns93113:0crwdne93113:0';
+
+// Error
+$_['error_permission'] = 'crwdns93115:0crwdne93115:0';
