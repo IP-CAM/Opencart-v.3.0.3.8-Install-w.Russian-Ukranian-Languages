20decb9c608ac1e0ee0fb8a65b81de09a8ba92b0
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:13 +0200

    New translations stock_status.php (LOLCAT)

---
+++ upload/admin/language/lol-US/localisation/stock_status.php
@@ -0,0 +1,21 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns93593:0crwdne93593:0';
+
+// Text
+$_['text_success']     = 'crwdns93595:0crwdne93595:0';
+$_['text_list']        = 'crwdns93597:0crwdne93597:0';
+$_['text_add']         = 'crwdns93599:0crwdne93599:0';
+$_['text_edit']        = 'crwdns93601:0crwdne93601:0';
+
+// Column
+$_['column_name']      = 'crwdns93603:0crwdne93603:0';
+$_['column_action']    = 'crwdns93605:0crwdne93605:0';
+
+// Entry
+$_['entry_name']       = 'crwdns93607:0crwdne93607:0';
+
+// Error
+$_['error_permission'] = 'crwdns93609:0crwdne93609:0';
+$_['error_name']       = 'crwdns93611:0crwdne93611:0';
+$_['error_product']    = 'crwdns93613:0%scrwdne93613:0';
