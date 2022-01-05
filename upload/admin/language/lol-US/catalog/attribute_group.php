4accb0b698cf6cd0345d90958fe2ed859fd3e12e
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:20:55 +0200

    New translations attribute_group.php (LOLCAT)

---
+++ upload/admin/language/lol-US/catalog/attribute_group.php
@@ -0,0 +1,24 @@
+<?php
+// Heading
+$_['heading_title']     = 'crwdns81089:0crwdne81089:0';
+
+// Text
+$_['text_success']      = 'crwdns81091:0crwdne81091:0';
+$_['text_list']         = 'crwdns81093:0crwdne81093:0';
+$_['text_add']          = 'crwdns81095:0crwdne81095:0';
+$_['text_edit']         = 'crwdns81097:0crwdne81097:0';
+
+// Column
+$_['column_name']       = 'crwdns81099:0crwdne81099:0';
+$_['column_sort_order'] = 'crwdns81101:0crwdne81101:0';
+$_['column_action']     = 'crwdns81103:0crwdne81103:0';
+
+// Entry
+$_['entry_name']        = 'crwdns81105:0crwdne81105:0';
+$_['entry_sort_order']  = 'crwdns81107:0crwdne81107:0';
+
+// Error
+$_['error_permission']  = 'crwdns81109:0crwdne81109:0';
+$_['error_name']        = 'crwdns81111:0crwdne81111:0';
+$_['error_attribute']   = 'crwdns81113:0%scrwdne81113:0';
+$_['error_product']     = 'crwdns81115:0%scrwdne81115:0';
