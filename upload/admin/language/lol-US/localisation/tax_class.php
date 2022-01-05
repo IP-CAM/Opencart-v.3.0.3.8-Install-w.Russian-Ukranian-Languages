2bbbbfc962e66460c4d18fd78fcc228fed36f720
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:14 +0200

    New translations tax_class.php (LOLCAT)

---
+++ upload/admin/language/lol-US/localisation/tax_class.php
@@ -0,0 +1,32 @@
+<?php
+// Heading
+$_['heading_title']     = 'crwdns93615:0crwdne93615:0';
+
+// Text
+$_['text_success']      = 'crwdns93617:0crwdne93617:0';
+$_['text_list']         = 'crwdns93619:0crwdne93619:0';
+$_['text_add']          = 'crwdns93621:0crwdne93621:0';
+$_['text_edit']         = 'crwdns93623:0crwdne93623:0';
+$_['text_tax_class']    = 'crwdns93625:0crwdne93625:0';
+$_['text_tax_rate']     = 'crwdns93627:0crwdne93627:0';
+$_['text_shipping']     = 'crwdns93629:0crwdne93629:0';
+$_['text_payment']      = 'crwdns93631:0crwdne93631:0';
+$_['text_store']        = 'crwdns93633:0crwdne93633:0';
+
+// Column
+$_['column_title']      = 'crwdns93635:0crwdne93635:0';
+$_['column_action']     = 'crwdns93637:0crwdne93637:0';
+
+// Entry
+$_['entry_title']       = 'crwdns93639:0crwdne93639:0';
+$_['entry_description'] = 'crwdns93641:0crwdne93641:0';
+$_['entry_rate']        = 'crwdns93643:0crwdne93643:0';
+$_['entry_based']       = 'crwdns93645:0crwdne93645:0';
+$_['entry_geo_zone']    = 'crwdns93647:0crwdne93647:0';
+$_['entry_priority']    = 'crwdns93649:0crwdne93649:0';
+
+// Error
+$_['error_permission']  = 'crwdns93651:0crwdne93651:0';
+$_['error_title']       = 'crwdns93653:0crwdne93653:0';
+$_['error_description'] = 'crwdns93655:0crwdne93655:0';
+$_['error_product']     = 'crwdns93657:0%scrwdne93657:0';
