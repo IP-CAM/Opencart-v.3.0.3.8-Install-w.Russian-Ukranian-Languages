5adfe9c13a2b21e6fa53ef5edbc61ec48074cdd8
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:29 +0200

    New translations tax.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/total/tax.php
@@ -0,0 +1,15 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns93183:0crwdne93183:0';
+
+// Text
+$_['text_extension']   = 'crwdns93185:0crwdne93185:0';
+$_['text_success']     = 'crwdns93187:0crwdne93187:0';
+$_['text_edit']        = 'crwdns93189:0crwdne93189:0';
+
+// Entry
+$_['entry_status']     = 'crwdns93191:0crwdne93191:0';
+$_['entry_sort_order'] = 'crwdns93193:0crwdne93193:0';
+
+// Error
+$_['error_permission'] = 'crwdns93195:0crwdne93195:0';
