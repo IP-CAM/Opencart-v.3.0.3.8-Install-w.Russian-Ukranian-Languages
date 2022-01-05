6ad95df4e699adf2fb7a9d3db148883757c2f19a
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:20:34 +0200

    New translations customer_approval.php (LOLCAT)

---
+++ upload/admin/language/lol-US/customer/customer_approval.php
@@ -0,0 +1,28 @@
+<?php
+// Heading
+$_['heading_title']         = 'crwdns82407:0crwdne82407:0';
+
+// Text
+$_['text_success']          = 'crwdns82409:0crwdne82409:0';
+$_['text_list']             = 'crwdns82411:0crwdne82411:0';
+$_['text_default']          = 'crwdns82413:0crwdne82413:0';
+$_['text_customer']         = 'crwdns82415:0crwdne82415:0';
+$_['text_affiliate']        = 'crwdns82417:0crwdne82417:0';
+
+// Column
+$_['column_name']           = 'crwdns82419:0crwdne82419:0';
+$_['column_email']          = 'crwdns82421:0crwdne82421:0';
+$_['column_customer_group'] = 'crwdns82423:0crwdne82423:0';
+$_['column_type']           = 'crwdns82425:0crwdne82425:0';
+$_['column_date_added']     = 'crwdns82427:0crwdne82427:0';
+$_['column_action']         = 'crwdns82429:0crwdne82429:0';
+
+// Entry
+$_['entry_name']            = 'crwdns82431:0crwdne82431:0';
+$_['entry_email']           = 'crwdns82433:0crwdne82433:0';
+$_['entry_customer_group']  = 'crwdns82435:0crwdne82435:0';
+$_['entry_type']            = 'crwdns82437:0crwdne82437:0';
+$_['entry_date_added']      = 'crwdns82439:0crwdne82439:0';
+
+// Error
+$_['error_permission']      = 'crwdns82441:0crwdne82441:0';
