efc087646d1bcd51d7a596c99dafd06e5d64b8d0
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:12 +0200

    New translations laybuy.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/payment/laybuy.php
@@ -0,0 +1,29 @@
+<?php
+// Heading
+$_['heading_title']			= 'crwdns98967:0crwdne98967:0';
+
+// Text
+$_['text_title']			= 'crwdns98969:0crwdne98969:0';
+$_['text_plan_preview']		= 'crwdns98971:0crwdne98971:0';
+$_['text_payment']			= 'crwdns98973:0crwdne98973:0';
+$_['text_due_date']			= 'crwdns98975:0crwdne98975:0';
+$_['text_amount']			= 'crwdns98977:0crwdne98977:0';
+$_['text_downpayment']		= 'crwdns98979:0crwdne98979:0';
+$_['text_today']			= 'crwdns98981:0crwdne98981:0';
+$_['text_delivery_msg']		= 'crwdns98983:0crwdne98983:0';
+$_['text_fee_msg']			= 'crwdns98985:0crwdne98985:0';
+$_['text_month']			= 'crwdns98987:0crwdne98987:0';
+$_['text_months']			= 'crwdns98989:0crwdne98989:0';
+$_['text_status_1']			= 'crwdns98991:0crwdne98991:0';
+$_['text_status_5']			= 'crwdns98993:0crwdne98993:0';
+$_['text_status_7']			= 'crwdns98995:0crwdne98995:0';
+$_['text_status_50']		= 'crwdns98997:0crwdne98997:0';
+$_['text_status_51']		= 'crwdns98999:0crwdne98999:0';
+$_['text_comment']			= 'crwdns99001:0crwdne99001:0';
+
+// Entry
+$_['entry_initial']			= 'crwdns99003:0crwdne99003:0';
+$_['entry_months']			= 'crwdns99005:0crwdne99005:0';
+
+// Button
+$_['button_confirm']		= 'crwdns99007:0crwdne99007:0';
