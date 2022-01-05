4716e42c1d48463d4bf2ed7f80ade15892284838
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:21 +0200

    New translations firstdata.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/payment/firstdata.php
@@ -0,0 +1,42 @@
+<?php
+// Heading
+$_['text_title']				= 'crwdns98479:0crwdne98479:0';
+
+// Button
+$_['button_confirm']			= 'crwdns98481:0crwdne98481:0';
+
+// Text
+$_['text_new_card']				= 'crwdns98483:0crwdne98483:0';
+$_['text_store_card']			= 'crwdns98485:0crwdne98485:0';
+$_['text_address_response']		= 'crwdns98487:0crwdne98487:0';
+$_['text_address_ppx']			= 'crwdns98489:0crwdne98489:0';
+$_['text_address_yyy']			= 'crwdns98491:0crwdne98491:0';
+$_['text_address_yna']			= 'crwdns98493:0crwdne98493:0';
+$_['text_address_nyz']			= 'crwdns98495:0crwdne98495:0';
+$_['text_address_nnn']			= 'crwdns98497:0crwdne98497:0';
+$_['text_address_ypx']			= 'crwdns98499:0crwdne98499:0';
+$_['text_address_pyx']			= 'crwdns98501:0crwdne98501:0';
+$_['text_address_xxu']			= 'crwdns98503:0crwdne98503:0';
+$_['text_card_code_verify']		= 'crwdns98505:0crwdne98505:0';
+$_['text_card_code_m']			= 'crwdns98507:0crwdne98507:0';
+$_['text_card_code_n']			= 'crwdns98509:0crwdne98509:0';
+$_['text_card_code_p']			= 'crwdns98511:0crwdne98511:0';
+$_['text_card_code_s']			= 'crwdns98513:0crwdne98513:0';
+$_['text_card_code_u']			= 'crwdns98515:0crwdne98515:0';
+$_['text_card_code_x']			= 'crwdns98517:0crwdne98517:0';
+$_['text_card_code_blank']		= 'crwdns98519:0crwdne98519:0';
+$_['text_card_type_m']			= 'crwdns98521:0crwdne98521:0';
+$_['text_card_type_v']			= 'crwdns98523:0crwdne98523:0';
+$_['text_card_type_c']			= 'crwdns98525:0crwdne98525:0';
+$_['text_card_type_a']			= 'crwdns98527:0crwdne98527:0';
+$_['text_card_type_ma']			= 'crwdns98529:0crwdne98529:0';
+$_['text_card_type_mauk']		= 'crwdns98531:0crwdne98531:0';
+$_['text_response_code_full']	= 'crwdns98533:0crwdne98533:0';
+$_['text_response_code']		= 'crwdns98535:0crwdne98535:0';
+$_['text_response_card']		= 'crwdns98537:0crwdne98537:0';
+$_['text_response_card_type']	= 'crwdns98539:0crwdne98539:0';
+$_['text_response_proc_code']	= 'crwdns98541:0crwdne98541:0';
+$_['text_response_ref']			= 'crwdns98543:0crwdne98543:0';
+
+// Error
+$_['error_failed']				= 'crwdns98545:0crwdne98545:0';
