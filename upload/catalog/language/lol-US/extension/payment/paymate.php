d8c9b88e2db9e9170f16d9966eadc1c3c7407465
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:14 +0200

    New translations paymate.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/payment/paymate.php
@@ -0,0 +1,10 @@
+<?php
+// Text
+$_['text_title']				= 'crwdns99013:0crwdne99013:0';
+$_['text_unable']				= 'crwdns99015:0crwdne99015:0';
+$_['text_declined']				= 'crwdns99017:0crwdne99017:0';
+$_['text_failed']				= 'crwdns99019:0crwdne99019:0';
+$_['text_failed_message']		= 'crwdns99021:0%scrwdnd99021:0%scrwdne99021:0';
+$_['text_basket']				= 'crwdns99023:0crwdne99023:0';
+$_['text_checkout']				= 'crwdns99025:0crwdne99025:0';
+$_['text_success']				= 'crwdns99027:0crwdne99027:0';
