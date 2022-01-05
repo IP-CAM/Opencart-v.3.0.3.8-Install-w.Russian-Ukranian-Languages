eadceba28ef05228cf00d2855349b4699e7f8486
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:10 +0200

    New translations globalpay_remote.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/payment/globalpay_remote.php
@@ -0,0 +1,55 @@
+<?php
+// Text
+$_['text_title']				= 'crwdns98727:0crwdne98727:0';
+$_['text_credit_card']			= 'crwdns98729:0crwdne98729:0';
+$_['text_wait']					= 'crwdns98731:0crwdne98731:0';
+$_['text_result']				= 'crwdns98733:0crwdne98733:0';
+$_['text_message']				= 'crwdns98735:0crwdne98735:0';
+$_['text_cvn_result']			= 'crwdns98737:0crwdne98737:0';
+$_['text_avs_postcode']			= 'crwdns98739:0crwdne98739:0';
+$_['text_avs_address']			= 'crwdns98741:0crwdne98741:0';
+$_['text_eci']					= 'crwdns98743:0crwdne98743:0';
+$_['text_tss']					= 'crwdns98745:0crwdne98745:0';
+$_['text_card_bank']			= 'crwdns98747:0crwdne98747:0';
+$_['text_card_country']			= 'crwdns98749:0crwdne98749:0';
+$_['text_card_region']			= 'crwdns98751:0crwdne98751:0';
+$_['text_last_digits']			= 'crwdns98753:0crwdne98753:0';
+$_['text_order_ref']			= 'crwdns98755:0crwdne98755:0';
+$_['text_timestamp']			= 'crwdns98757:0crwdne98757:0';
+$_['text_card_visa']			= 'crwdns98759:0crwdne98759:0';
+$_['text_card_mc']				= 'crwdns98761:0crwdne98761:0';
+$_['text_card_amex']			= 'crwdns98763:0crwdne98763:0';
+$_['text_card_switch']			= 'crwdns98765:0crwdne98765:0';
+$_['text_card_laser']			= 'crwdns98767:0crwdne98767:0';
+$_['text_card_diners']			= 'crwdns98769:0crwdne98769:0';
+$_['text_auth_code']			= 'crwdns98771:0crwdne98771:0';
+$_['text_3d_s1']				= 'crwdns98773:0crwdne98773:0';
+$_['text_3d_s2']				= 'crwdns98775:0crwdne98775:0';
+$_['text_3d_s3']				= 'crwdns98777:0crwdne98777:0';
+$_['text_3d_s4']				= 'crwdns98779:0crwdne98779:0';
+$_['text_3d_s5']				= 'crwdns98781:0crwdne98781:0';
+$_['text_3d_s6']				= 'crwdns98783:0crwdne98783:0';
+$_['text_3d_s7']				= 'crwdns98785:0crwdne98785:0';
+$_['text_3d_s8']				= 'crwdns98787:0crwdne98787:0';
+$_['text_3d_s9']				= 'crwdns98789:0crwdne98789:0';
+$_['text_3d_s10']				= 'crwdns98791:0crwdne98791:0';
+
+// Entry
+$_['entry_cc_type']				= 'crwdns98793:0crwdne98793:0';
+$_['entry_cc_number']			= 'crwdns98795:0crwdne98795:0';
+$_['entry_cc_name']				= 'crwdns98797:0crwdne98797:0';
+$_['entry_cc_expire_date']		= 'crwdns98799:0crwdne98799:0';
+$_['entry_cc_cvv2']				= 'crwdns98801:0crwdne98801:0';
+$_['entry_cc_issue']			= 'crwdns98803:0crwdne98803:0';
+
+// Help
+$_['help_start_date']			= 'crwdns98805:0crwdne98805:0';
+$_['help_issue']				= 'crwdns98807:0crwdne98807:0';
+
+// Error
+$_['error_card_number']			= 'crwdns98809:0crwdne98809:0';
+$_['error_card_name']			= 'crwdns98811:0crwdne98811:0';
+$_['error_card_cvv']			= 'crwdns98813:0crwdne98813:0';
+$_['error_3d_unable']			= 'crwdns98815:0crwdne98815:0';
+$_['error_3d_500_response_no_payment'] = 'crwdns98817:0crwdne98817:0';
+$_['error_3d_unsuccessful']		= 'crwdns98819:0crwdne98819:0';
