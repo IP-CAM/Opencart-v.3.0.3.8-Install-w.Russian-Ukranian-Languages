94e9251cc9149921e04736ddcd9e9fe41e34459a
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:20:45 +0200

    New translations activity.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/dashboard/activity.php
@@ -0,0 +1,32 @@
+<?php
+// Heading
+$_['heading_title']                = 'crwdns83645:0crwdne83645:0';
+
+// Text
+$_['text_extension']               = 'crwdns83647:0crwdne83647:0';
+$_['text_success']                 = 'crwdns83649:0crwdne83649:0';
+$_['text_edit']                    = 'crwdns83651:0crwdne83651:0';
+$_['text_activity_register']       = 'crwdns83653:0%dcrwdnd83653:0%scrwdne83653:0';
+$_['text_activity_edit']           = 'crwdns83655:0%dcrwdnd83655:0%scrwdne83655:0';
+$_['text_activity_password']       = 'crwdns83657:0%dcrwdnd83657:0%scrwdne83657:0';
+$_['text_activity_reset']          = 'crwdns83659:0%dcrwdnd83659:0%scrwdne83659:0';
+$_['text_activity_login']          = 'crwdns83661:0%dcrwdnd83661:0%scrwdne83661:0';
+$_['text_activity_forgotten']      = 'crwdns83663:0%dcrwdnd83663:0%scrwdne83663:0';
+$_['text_activity_address_add']    = 'crwdns83665:0%dcrwdnd83665:0%scrwdne83665:0';
+$_['text_activity_address_edit']   = 'crwdns83667:0%dcrwdnd83667:0%scrwdne83667:0';
+$_['text_activity_address_delete'] = 'crwdns83669:0%dcrwdnd83669:0%scrwdne83669:0';
+$_['text_activity_return_account'] = 'crwdns83671:0%dcrwdnd83671:0%scrwdnd83671:0%dcrwdne83671:0';
+$_['text_activity_return_guest']   = 'crwdns83673:0%scrwdnd83673:0%dcrwdne83673:0';
+$_['text_activity_order_account']  = 'crwdns83675:0%dcrwdnd83675:0%scrwdnd83675:0%dcrwdne83675:0';
+$_['text_activity_order_guest']    = 'crwdns83677:0%scrwdnd83677:0%dcrwdne83677:0';
+$_['text_activity_affiliate_add']  = 'crwdns83679:0%dcrwdnd83679:0%scrwdne83679:0';
+$_['text_activity_affiliate_edit'] = 'crwdns83681:0%dcrwdnd83681:0%scrwdne83681:0';
+$_['text_activity_transaction']    = 'crwdns83683:0%dcrwdnd83683:0%scrwdnd83683:0%dcrwdne83683:0';
+
+// Entry
+$_['entry_status']                 = 'crwdns83685:0crwdne83685:0';
+$_['entry_sort_order']             = 'crwdns83687:0crwdne83687:0';
+$_['entry_width']                  = 'crwdns83689:0crwdne83689:0';
+
+// Error
+$_['error_permission']             = 'crwdns83691:0crwdne83691:0';
