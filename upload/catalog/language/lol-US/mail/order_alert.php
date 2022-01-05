787b6e34c255573c95074dec751c85d665e0d158
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:53 +0200

    New translations order_alert.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/mail/order_alert.php
@@ -0,0 +1,10 @@
+<?php
+// Text
+$_['text_subject']      = 'crwdns100309:0%scrwdnd100309:0%scrwdne100309:0';
+$_['text_received']     = 'crwdns100311:0crwdne100311:0';
+$_['text_order_id']     = 'crwdns100313:0crwdne100313:0';
+$_['text_date_added']   = 'crwdns100315:0crwdne100315:0';
+$_['text_order_status'] = 'crwdns100317:0crwdne100317:0';
+$_['text_product']      = 'crwdns100319:0crwdne100319:0';
+$_['text_total']        = 'crwdns100321:0crwdne100321:0';
+$_['text_comment']      = 'crwdns100323:0crwdne100323:0';
