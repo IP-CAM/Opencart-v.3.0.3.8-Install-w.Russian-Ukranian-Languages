d94b49e705642d793c41259de9e12540cb01d847
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:07 +0200

    New translations affiliate.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/account/affiliate.php
@@ -0,0 +1,35 @@
+<?php
+// Heading
+$_['heading_title']             = 'crwdns95817:0crwdne95817:0';
+
+// Text
+$_['text_account']              = 'crwdns95819:0crwdne95819:0';
+$_['text_affiliate']            = 'crwdns95821:0crwdne95821:0';
+$_['text_my_affiliate']         = 'crwdns95823:0crwdne95823:0';
+$_['text_payment']              = 'crwdns95825:0crwdne95825:0';
+$_['text_cheque']               = 'crwdns95827:0crwdne95827:0';
+$_['text_paypal']               = 'crwdns95829:0crwdne95829:0';
+$_['text_bank']                 = 'crwdns95831:0crwdne95831:0';
+$_['text_success']              = 'crwdns95833:0crwdne95833:0';
+$_['text_agree']                = 'crwdns95835:0%scrwdnd95835:0%scrwdne95835:0';
+
+// Entry
+$_['entry_company']             = 'crwdns95837:0crwdne95837:0';
+$_['entry_website']             = 'crwdns95839:0crwdne95839:0';
+$_['entry_tax']                 = 'crwdns95841:0crwdne95841:0';
+$_['entry_payment']             = 'crwdns95843:0crwdne95843:0';
+$_['entry_cheque']              = 'crwdns95845:0crwdne95845:0';
+$_['entry_paypal']              = 'crwdns95847:0crwdne95847:0';
+$_['entry_bank_name']           = 'crwdns95849:0crwdne95849:0';
+$_['entry_bank_branch_number']  = 'crwdns95851:0crwdne95851:0';
+$_['entry_bank_swift_code']     = 'crwdns95853:0crwdne95853:0';
+$_['entry_bank_account_name']   = 'crwdns95855:0crwdne95855:0';
+$_['entry_bank_account_number'] = 'crwdns95857:0crwdne95857:0';
+
+// Error
+$_['error_agree']               = 'crwdns95859:0%scrwdne95859:0';
+$_['error_cheque']              = 'crwdns95861:0crwdne95861:0';
+$_['error_paypal']              = 'crwdns95863:0crwdne95863:0';
+$_['error_bank_account_name']   = 'crwdns95865:0crwdne95865:0';
+$_['error_bank_account_number'] = 'crwdns95867:0crwdne95867:0';
+$_['error_custom_field']        = 'crwdns95869:0%scrwdne95869:0';
