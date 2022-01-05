62fdc1891bfc1b8895dbafca0301b4d8de0a9b66
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:03 +0200

    New translations order.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/api/order.php
@@ -0,0 +1,16 @@
+<?php
+// Text
+$_['text_success']           = 'crwdns96621:0crwdne96621:0';
+
+// Error
+$_['error_permission']       = 'crwdns96623:0crwdne96623:0';
+$_['error_customer']         = 'crwdns96625:0crwdne96625:0';
+$_['error_payment_address']  = 'crwdns96627:0crwdne96627:0';
+$_['error_payment_method']   = 'crwdns96629:0crwdne96629:0';
+$_['error_no_payment']       = 'crwdns96631:0crwdne96631:0';
+$_['error_shipping_address'] = 'crwdns96633:0crwdne96633:0';
+$_['error_shipping_method']  = 'crwdns96635:0crwdne96635:0';
+$_['error_no_shipping']      = 'crwdns96637:0crwdne96637:0';
+$_['error_stock']            = 'crwdns96639:0crwdne96639:0';
+$_['error_minimum']          = 'crwdns96641:0%scrwdnd96641:0%scrwdne96641:0';
+$_['error_not_found']        = 'crwdns96643:0crwdne96643:0';
