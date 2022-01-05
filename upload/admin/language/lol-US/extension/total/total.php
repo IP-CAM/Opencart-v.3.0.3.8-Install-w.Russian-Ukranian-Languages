bc2d9d4f560024e662b5462a8d6116685e059501
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:30 +0200

    New translations total.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/total/total.php
@@ -0,0 +1,15 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns93197:0crwdne93197:0';
+
+// Text
+$_['text_extension']   = 'crwdns93199:0crwdne93199:0';
+$_['text_success']     = 'crwdns93201:0crwdne93201:0';
+$_['text_edit']        = 'crwdns93203:0crwdne93203:0';
+
+// Entry
+$_['entry_status']     = 'crwdns93205:0crwdne93205:0';
+$_['entry_sort_order'] = 'crwdns93207:0crwdne93207:0';
+
+// Error
+$_['error_permission'] = 'crwdns93209:0crwdne93209:0';
