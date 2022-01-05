750914e378b9ca7900592b07916f3461dd3020f3
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:14 +0200

    New translations filter.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/module/filter.php
@@ -0,0 +1,14 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns84797:0crwdne84797:0';
+
+// Text
+$_['text_extension']   = 'crwdns84799:0crwdne84799:0';
+$_['text_success']     = 'crwdns84801:0crwdne84801:0';
+$_['text_edit']        = 'crwdns84803:0crwdne84803:0';
+
+// Entry
+$_['entry_status']     = 'crwdns84805:0crwdne84805:0';
+
+// Error
+$_['error_permission'] = 'crwdns84807:0crwdne84807:0';
