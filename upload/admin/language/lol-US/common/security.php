bc2e58d257a06dfba223332bc49a21cbcca8e675
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:20:32 +0200

    New translations security.php (LOLCAT)

---
+++ upload/admin/language/lol-US/common/security.php
@@ -0,0 +1,29 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns82079:0crwdne82079:0';
+
+// Text
+$_['text_success']     = 'crwdns82081:0crwdne82081:0';
+$_['text_admin']       = 'crwdns82083:0crwdne82083:0';
+$_['text_security']    = 'crwdns82085:0crwdne82085:0';
+$_['text_choose']      = 'crwdns82087:0crwdne82087:0';
+$_['text_automatic']   = 'crwdns82089:0crwdne82089:0';
+$_['text_manual']      = 'crwdns82091:0crwdne82091:0';
+$_['text_move']        = 'crwdns82093:0crwdne82093:0';
+$_['text_to']          = 'crwdns82095:0crwdne82095:0';
+$_['text_config']      = 'crwdns82097:0crwdne82097:0';
+$_['text_admin']       = 'crwdns82099:0crwdne82099:0';
+
+// Entry
+$_['entry_directory']  = 'crwdns82101:0crwdne82101:0';
+
+// Button
+$_['button_move']      = 'crwdns82103:0crwdne82103:0';
+$_['button_manual']    = 'crwdns82105:0crwdne82105:0';
+
+// Error
+$_['error_permission'] = 'crwdns82107:0crwdne82107:0';
+$_['error_path']       = 'crwdns82109:0crwdne82109:0';
+$_['error_directory']  = 'crwdns82111:0crwdne82111:0';
+$_['error_exists']     = 'crwdns82113:0crwdne82113:0';
+$_['error_writable']   = 'crwdns82115:0crwdne82115:0';
