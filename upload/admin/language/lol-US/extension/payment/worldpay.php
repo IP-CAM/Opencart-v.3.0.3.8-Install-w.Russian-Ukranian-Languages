e8cabd2109f563b3c02fd2cc4aa0c9a1a6f17742
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:40 +0200

    New translations worldpay.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/payment/worldpay.php
@@ -0,0 +1,78 @@
+<?php
+// Heading
+$_['heading_title']				         = 'crwdns91519:0crwdne91519:0';
+
+// Text
+$_['text_extension']				     = 'crwdns91521:0crwdne91521:0';
+$_['text_success']				         = 'crwdns91523:0crwdne91523:0';
+$_['text_worldpay']				         = 'crwdns91525:0crwdne91525:0';
+$_['text_test']					         = 'crwdns91527:0crwdne91527:0';
+$_['text_live']					         = 'crwdns91529:0crwdne91529:0';
+$_['text_authenticate']			         = 'crwdns91531:0crwdne91531:0';
+$_['text_release_ok']		 	         = 'crwdns91533:0crwdne91533:0';
+$_['text_release_ok_order']		         = 'crwdns91535:0crwdne91535:0';
+$_['text_refund_ok']			         = 'crwdns91537:0crwdne91537:0';
+$_['text_refund_ok_order']		         = 'crwdns91539:0crwdne91539:0';
+$_['text_void_ok']				         = 'crwdns91541:0crwdne91541:0';
+$_['text_refund']                        = 'crwdns91543:0crwdne91543:0';
+$_['text_edit']                          = 'crwdns91545:0crwdne91545:0';
+
+// Entry
+$_['entry_service_key']			         = 'crwdns91547:0crwdne91547:0';
+$_['entry_client_key']			         = 'crwdns91549:0crwdne91549:0';
+$_['entry_total']				         = 'crwdns91551:0crwdne91551:0';
+$_['entry_order_status']		         = 'crwdns91553:0crwdne91553:0';
+$_['entry_geo_zone']			         = 'crwdns91555:0crwdne91555:0';
+$_['entry_status']				         = 'crwdns91557:0crwdne91557:0';
+$_['entry_sort_order']			         = 'crwdns91559:0crwdne91559:0';
+$_['entry_debug']				         = 'crwdns91561:0crwdne91561:0';
+$_['entry_card']				         = 'crwdns91563:0crwdne91563:0';
+$_['entry_secret_token']		         = 'crwdns91565:0crwdne91565:0';
+$_['entry_webhook_url']			         = 'crwdns91567:0crwdne91567:0';
+$_['entry_cron_job_url']		         = 'crwdns91569:0crwdne91569:0';
+$_['entry_last_cron_job_run']	         = 'crwdns91571:0crwdne91571:0';
+$_['entry_success_status']		         = 'crwdns91573:0crwdne91573:0';
+$_['entry_failed_status']		         = 'crwdns91575:0crwdne91575:0';
+$_['entry_settled_status']			     = 'crwdns91577:0crwdne91577:0';
+$_['entry_refunded_status']			     = 'crwdns91579:0crwdne91579:0';
+$_['entry_partially_refunded_status']	 = 'crwdns91581:0crwdne91581:0';
+$_['entry_charged_back_status']			 = 'crwdns91583:0crwdne91583:0';
+$_['entry_information_requested_status'] = 'crwdns91585:0crwdne91585:0';
+$_['entry_information_supplied_status']	 = 'crwdns91587:0crwdne91587:0';
+$_['entry_chargeback_reversed_status']	 = 'crwdns91589:0crwdne91589:0';
+
+
+$_['entry_reversed_status']			     = 'crwdns91591:0crwdne91591:0';
+$_['entry_voided_status']			     = 'crwdns91593:0crwdne91593:0';
+
+// Help
+$_['help_total']				         = 'crwdns91595:0crwdne91595:0';
+$_['help_debug']				         = 'crwdns91597:0crwdne91597:0';
+$_['help_secret_token']			         = 'crwdns91599:0crwdne91599:0';
+$_['help_webhook_url']			         = 'crwdns91601:0crwdne91601:0';
+$_['help_cron_job_url']			         = 'crwdns91603:0crwdne91603:0';
+
+// Tab
+$_['tab_settings']				         = 'crwdns91605:0crwdne91605:0';
+$_['tab_order_status']			         = 'crwdns91607:0crwdne91607:0';
+
+// Error
+$_['error_permission']			         = 'crwdns91609:0crwdne91609:0';
+$_['error_service_key']			         = 'crwdns91611:0crwdne91611:0';
+$_['error_client_key']			         = 'crwdns91613:0crwdne91613:0';
+
+// Order page - payment tab
+$_['text_payment_info']			         = 'crwdns91615:0crwdne91615:0';
+$_['text_refund_status']		         = 'crwdns91617:0crwdne91617:0';
+$_['text_order_ref']			         = 'crwdns91619:0crwdne91619:0';
+$_['text_order_total']			         = 'crwdns91621:0crwdne91621:0';
+$_['text_total_released']		         = 'crwdns91623:0crwdne91623:0';
+$_['text_transactions']			         = 'crwdns91625:0crwdne91625:0';
+$_['text_column_amount']		         = 'crwdns91627:0crwdne91627:0';
+$_['text_column_type']			         = 'crwdns91629:0crwdne91629:0';
+$_['text_column_date_added']	         = 'crwdns91631:0crwdne91631:0';
+
+$_['text_confirm_refund']		         = 'crwdns91633:0crwdne91633:0';
+
+$_['button_refund']				         = 'crwdns91635:0crwdne91635:0';
+
