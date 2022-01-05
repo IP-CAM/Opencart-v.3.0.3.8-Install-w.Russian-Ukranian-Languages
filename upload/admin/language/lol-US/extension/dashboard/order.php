028333cccb84066bb1ab880c9df466abe4036f0b
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:26 +0200

    New translations order.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/dashboard/order.php
@@ -0,0 +1,17 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns83777:0crwdne83777:0';
+
+// Text
+$_['text_extension']   = 'crwdns83779:0crwdne83779:0';
+$_['text_success']     = 'crwdns83781:0crwdne83781:0';
+$_['text_edit']        = 'crwdns83783:0crwdne83783:0';
+$_['text_view']        = 'crwdns83785:0crwdne83785:0';
+
+// Entry
+$_['entry_status']     = 'crwdns83787:0crwdne83787:0';
+$_['entry_sort_order'] = 'crwdns83789:0crwdne83789:0';
+$_['entry_width']      = 'crwdns83791:0crwdne83791:0';
+
+// Error
+$_['error_permission'] = 'crwdns83793:0crwdne83793:0';
