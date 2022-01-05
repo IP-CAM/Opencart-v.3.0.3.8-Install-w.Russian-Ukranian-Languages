a60c41415b364cbf7747cb2ec718462d5b4a00e9
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:19 +0200

    New translations forgotten.php (LOLCAT)

---
+++ upload/admin/language/lol-US/mail/forgotten.php
@@ -0,0 +1,6 @@
+<?php
+// Text
+$_['text_subject']  = 'crwdns93817:0%scrwdne93817:0';
+$_['text_greeting'] = 'crwdns93819:0%scrwdne93819:0';
+$_['text_change']   = 'crwdns93821:0crwdne93821:0';
+$_['text_ip']       = 'crwdns93823:0crwdne93823:0';
