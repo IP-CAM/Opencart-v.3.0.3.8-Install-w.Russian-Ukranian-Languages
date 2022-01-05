56676f1f6e009a1614cadb537ae23b2ee94336ea
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:37 +0200

    New translations cheque.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/payment/cheque.php
@@ -0,0 +1,7 @@
+<?php
+// Text
+$_['text_title']       = 'crwdns97985:0crwdne97985:0';
+$_['text_instruction'] = 'crwdns97987:0crwdne97987:0';
+$_['text_payable']     = 'crwdns97989:0crwdne97989:0';
+$_['text_address']     = 'crwdns97991:0crwdne97991:0';
+$_['text_payment']     = 'crwdns97993:0crwdne97993:0';
