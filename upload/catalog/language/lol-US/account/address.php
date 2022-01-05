5cf1d197079388095ed21d6eaee77c1a26c1709c
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:07 +0200

    New translations address.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/account/address.php
@@ -0,0 +1,38 @@
+<?php
+// Heading
+$_['heading_title']      = 'crwdns95757:0crwdne95757:0';
+
+// Text
+$_['text_account']       = 'crwdns95759:0crwdne95759:0';
+$_['text_address_book']  = 'crwdns95761:0crwdne95761:0';
+$_['text_address_add']   = 'crwdns95763:0crwdne95763:0';
+$_['text_address_edit']  = 'crwdns95765:0crwdne95765:0';
+$_['text_add']           = 'crwdns95767:0crwdne95767:0';
+$_['text_edit']          = 'crwdns95769:0crwdne95769:0';
+$_['text_delete']        = 'crwdns95771:0crwdne95771:0';
+$_['text_empty']         = 'crwdns95773:0crwdne95773:0';
+$_['text_confirm']       = 'crwdns95775:0crwdne95775:0';
+
+// Entry
+$_['entry_firstname']    = 'crwdns95777:0crwdne95777:0';
+$_['entry_lastname']     = 'crwdns95779:0crwdne95779:0';
+$_['entry_company']      = 'crwdns95781:0crwdne95781:0';
+$_['entry_address_1']    = 'crwdns95783:0crwdne95783:0';
+$_['entry_address_2']    = 'crwdns95785:0crwdne95785:0';
+$_['entry_postcode']     = 'crwdns95787:0crwdne95787:0';
+$_['entry_city']         = 'crwdns95789:0crwdne95789:0';
+$_['entry_country']      = 'crwdns95791:0crwdne95791:0';
+$_['entry_zone']         = 'crwdns95793:0crwdne95793:0';
+$_['entry_default']      = 'crwdns95795:0crwdne95795:0';
+
+// Error
+$_['error_delete']       = 'crwdns95797:0crwdne95797:0';
+$_['error_default']      = 'crwdns95799:0crwdne95799:0';
+$_['error_firstname']    = 'crwdns95801:0crwdne95801:0';
+$_['error_lastname']     = 'crwdns95803:0crwdne95803:0';
+$_['error_address_1']    = 'crwdns95805:0crwdne95805:0';
+$_['error_postcode']     = 'crwdns95807:0crwdne95807:0';
+$_['error_city']         = 'crwdns95809:0crwdne95809:0';
+$_['error_country']      = 'crwdns95811:0crwdne95811:0';
+$_['error_zone']         = 'crwdns95813:0crwdne95813:0';
+$_['error_custom_field'] = 'crwdns95815:0%scrwdne95815:0';
