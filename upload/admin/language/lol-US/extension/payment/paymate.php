51a6e67e994a79afe50324307046ce61352aa327
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:01 +0200

    New translations paymate.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/payment/paymate.php
@@ -0,0 +1,28 @@
+<?php
+// Heading
+$_['heading_title']		 = 'crwdns87977:0crwdne87977:0';
+
+// Text
+$_['text_extension']	 = 'crwdns87979:0crwdne87979:0';
+$_['text_success']		 = 'crwdns87981:0crwdne87981:0';
+$_['text_edit']          = 'crwdns87983:0crwdne87983:0';
+$_['text_paymate']		 = 'crwdns87985:0crwdne87985:0';
+
+// Entry
+$_['entry_username']	 = 'crwdns87987:0crwdne87987:0';
+$_['entry_password']	 = 'crwdns87989:0crwdne87989:0';
+$_['entry_test']		 = 'crwdns87991:0crwdne87991:0';
+$_['entry_total']		 = 'crwdns87993:0crwdne87993:0';
+$_['entry_order_status'] = 'crwdns87995:0crwdne87995:0';
+$_['entry_geo_zone']	 = 'crwdns87997:0crwdne87997:0';
+$_['entry_status']		 = 'crwdns87999:0crwdne87999:0';
+$_['entry_sort_order']	 = 'crwdns88001:0crwdne88001:0';
+
+// Help
+$_['help_password']		 = 'crwdns88003:0crwdne88003:0';
+$_['help_total']		 = 'crwdns88005:0crwdne88005:0';
+
+// Error
+$_['error_permission']	 = 'crwdns88007:0crwdne88007:0';
+$_['error_username']	 = 'crwdns88009:0crwdne88009:0';
+$_['error_password']	 = 'crwdns88011:0crwdne88011:0';
