f179f1714030f11b4476cce11f49a8fb02f9d081
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:20:36 +0200

    New translations banner.php (LOLCAT)

---
+++ upload/admin/language/lol-US/design/banner.php
@@ -0,0 +1,28 @@
+<?php
+// Heading
+$_['heading_title']     = 'crwdns82479:0crwdne82479:0';
+
+// Text
+$_['text_success']      = 'crwdns82481:0crwdne82481:0';
+$_['text_list']         = 'crwdns82483:0crwdne82483:0';
+$_['text_add']          = 'crwdns82485:0crwdne82485:0';
+$_['text_edit']         = 'crwdns82487:0crwdne82487:0';
+$_['text_default']      = 'crwdns82489:0crwdne82489:0';
+
+// Column
+$_['column_name']       = 'crwdns82491:0crwdne82491:0';
+$_['column_status']     = 'crwdns82493:0crwdne82493:0';
+$_['column_action']     = 'crwdns82495:0crwdne82495:0';
+
+// Entry
+$_['entry_name']        = 'crwdns82497:0crwdne82497:0';
+$_['entry_title']       = 'crwdns82499:0crwdne82499:0';
+$_['entry_link']        = 'crwdns82501:0crwdne82501:0';
+$_['entry_image']       = 'crwdns82503:0crwdne82503:0';
+$_['entry_status']      = 'crwdns82505:0crwdne82505:0';
+$_['entry_sort_order']  = 'crwdns82507:0crwdne82507:0';
+
+// Error
+$_['error_permission'] = 'crwdns82509:0crwdne82509:0';
+$_['error_name']       = 'crwdns82511:0crwdne82511:0';
+$_['error_title']      = 'crwdns82513:0crwdne82513:0';
