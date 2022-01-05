e5d94662c1584df8aaea35466b014f45a9586874
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:07 +0200

    New translations forgotten.php (LOLCAT)

---
+++ upload/admin/language/lol-US/common/forgotten.php
@@ -0,0 +1,19 @@
+<?php
+// Heading
+$_['heading_title']   = 'crwdns81961:0crwdne81961:0';
+
+// Text
+$_['text_forgotten']  = 'crwdns81963:0crwdne81963:0';
+$_['text_your_email'] = 'crwdns81965:0crwdne81965:0';
+$_['text_email']      = 'crwdns81967:0crwdne81967:0';
+$_['text_success']    = 'crwdns81969:0crwdne81969:0';
+
+// Entry
+$_['entry_email']     = 'crwdns81971:0crwdne81971:0';
+$_['entry_password']  = 'crwdns81973:0crwdne81973:0';
+$_['entry_confirm']   = 'crwdns81975:0crwdne81975:0';
+
+// Error
+$_['error_email']     = 'crwdns81977:0crwdne81977:0';
+$_['error_password']  = 'crwdns81979:0crwdne81979:0';
+$_['error_confirm']   = 'crwdns81981:0crwdne81981:0';
