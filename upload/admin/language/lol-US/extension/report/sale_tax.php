36c043e7292cb2b00f8f9b98270916d4617c13f9
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:50 +0200

    New translations sale_tax.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/report/sale_tax.php
@@ -0,0 +1,31 @@
+<?php
+// Heading
+$_['heading_title']     = 'crwdns92101:0crwdne92101:0';
+
+// Text
+$_['text_extension']    = 'crwdns92103:0crwdne92103:0';
+$_['text_edit']         = 'crwdns92105:0crwdne92105:0';
+$_['text_success']      = 'crwdns92107:0crwdne92107:0';
+$_['text_filter']       = 'crwdns92109:0crwdne92109:0';
+$_['text_year']         = 'crwdns92111:0crwdne92111:0';
+$_['text_month']        = 'crwdns92113:0crwdne92113:0';
+$_['text_week']         = 'crwdns92115:0crwdne92115:0';
+$_['text_day']          = 'crwdns92117:0crwdne92117:0';
+$_['text_all_status']   = 'crwdns92119:0crwdne92119:0';
+
+// Column
+$_['column_date_start'] = 'crwdns92121:0crwdne92121:0';
+$_['column_date_end']   = 'crwdns92123:0crwdne92123:0';
+$_['column_title']      = 'crwdns92125:0crwdne92125:0';
+$_['column_orders']     = 'crwdns92127:0crwdne92127:0';
+$_['column_total']      = 'crwdns92129:0crwdne92129:0';
+
+// Entry
+$_['entry_date_start']  = 'crwdns92131:0crwdne92131:0';
+$_['entry_date_end']    = 'crwdns92133:0crwdne92133:0';
+$_['entry_group']       = 'crwdns92135:0crwdne92135:0';
+$_['entry_status']      = 'crwdns92137:0crwdne92137:0';
+$_['entry_sort_order']  = 'crwdns92139:0crwdne92139:0';
+
+// Error
+$_['error_permission']  = 'crwdns92141:0crwdne92141:0';
