d4f54d14a0f854fb2bf0a16aa2cb8e8445f18fd8
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:20 +0200

    New translations pilibaba_button.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/module/pilibaba_button.php
@@ -0,0 +1,14 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns85017:0crwdne85017:0';
+
+// Text
+$_['text_extension']   = 'crwdns85019:0crwdne85019:0';
+$_['text_success']     = 'crwdns85021:0crwdne85021:0';
+$_['text_edit']        = 'crwdns85023:0crwdne85023:0';
+
+// Entry
+$_['entry_status']     = 'crwdns85025:0crwdne85025:0';
+
+// Error
+$_['error_permission'] = 'crwdns85027:0crwdne85027:0';
