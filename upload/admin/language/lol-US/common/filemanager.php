c6ff2edf55a8af92fefc583bbf996eccc92a2678
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:05 +0200

    New translations filemanager.php (LOLCAT)

---
+++ upload/admin/language/lol-US/common/filemanager.php
@@ -0,0 +1,23 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns81925:0crwdne81925:0';
+
+// Text
+$_['text_uploaded']    = 'crwdns81927:0crwdne81927:0';
+$_['text_directory']   = 'crwdns81929:0crwdne81929:0';
+$_['text_delete']      = 'crwdns81931:0crwdne81931:0';
+
+// Entry
+$_['entry_search']     = 'crwdns81933:0crwdne81933:0';
+$_['entry_folder']     = 'crwdns81935:0crwdne81935:0';
+
+// Error
+$_['error_permission'] = 'crwdns81937:0crwdne81937:0';
+$_['error_filename']   = 'crwdns81939:0crwdne81939:0';
+$_['error_folder']     = 'crwdns81941:0crwdne81941:0';
+$_['error_exists']     = 'crwdns81943:0crwdne81943:0';
+$_['error_directory']  = 'crwdns81945:0crwdne81945:0';
+$_['error_filesize']   = 'crwdns81947:0crwdne81947:0';
+$_['error_filetype']   = 'crwdns81949:0crwdne81949:0';
+$_['error_upload']     = 'crwdns81951:0crwdne81951:0';
+$_['error_delete']     = 'crwdns81953:0crwdne81953:0';
