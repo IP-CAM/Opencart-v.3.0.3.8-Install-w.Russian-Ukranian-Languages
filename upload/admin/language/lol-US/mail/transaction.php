fc9701a11eef474b080a5dd3d22a8f0e37c874c8
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:21 +0200

    New translations transaction.php (LOLCAT)

---
+++ upload/admin/language/lol-US/mail/transaction.php
@@ -0,0 +1,6 @@
+<?php
+// Text
+$_['text_subject']  = 'crwdns93843:0%scrwdne93843:0';
+$_['text_received'] = 'crwdns93845:0%scrwdne93845:0';
+$_['text_total']    = 'crwdns93847:0%scrwdne93847:0';
+$_['text_credit']   = 'crwdns93849:0crwdne93849:0';
