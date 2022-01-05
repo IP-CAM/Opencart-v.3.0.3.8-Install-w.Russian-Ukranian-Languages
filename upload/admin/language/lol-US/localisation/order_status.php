970b3aed54476af3d7a886ff90a7ab9488a32c75
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:35 +0200

    New translations order_status.php (LOLCAT)

---
+++ upload/admin/language/lol-US/localisation/order_status.php
@@ -0,0 +1,24 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns93497:0crwdne93497:0';
+
+// Text
+$_['text_success']     = 'crwdns93499:0crwdne93499:0';
+$_['text_list']        = 'crwdns93501:0crwdne93501:0';
+$_['text_add']         = 'crwdns93503:0crwdne93503:0';
+$_['text_edit']        = 'crwdns93505:0crwdne93505:0';
+
+// Column
+$_['column_name']      = 'crwdns93507:0crwdne93507:0';
+$_['column_action']    = 'crwdns93509:0crwdne93509:0';
+
+// Entry
+$_['entry_name']       = 'crwdns93511:0crwdne93511:0';
+
+// Error
+$_['error_permission'] = 'crwdns93513:0crwdne93513:0';
+$_['error_name']       = 'crwdns93515:0crwdne93515:0';
+$_['error_default']    = 'crwdns93517:0crwdne93517:0';
+$_['error_download']   = 'crwdns93519:0crwdne93519:0';
+$_['error_store']      = 'crwdns93521:0%scrwdne93521:0';
+$_['error_order']      = 'crwdns93523:0%scrwdne93523:0';
