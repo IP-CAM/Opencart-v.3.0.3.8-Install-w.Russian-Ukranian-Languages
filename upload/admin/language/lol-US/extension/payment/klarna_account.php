71ee1f1c66e6dc0c3671d6b115ffabdde7087442
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:16 +0200

    New translations klarna_account.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/payment/klarna_account.php
@@ -0,0 +1,39 @@
+<?php
+// Heading
+$_['heading_title']			= 'crwdns87225:0crwdne87225:0';
+
+// Text
+$_['text_extension']		= 'crwdns87227:0crwdne87227:0';
+$_['text_success']			= 'crwdns87229:0crwdne87229:0';
+$_['text_edit']             = 'crwdns87231:0crwdne87231:0';
+$_['text_klarna_account']	= 'crwdns87233:0crwdne87233:0';
+$_['text_live']				= 'crwdns87235:0crwdne87235:0';
+$_['text_beta']				= 'crwdns87237:0crwdne87237:0';
+$_['text_sweden']			= 'crwdns87239:0crwdne87239:0';
+$_['text_norway']			= 'crwdns87241:0crwdne87241:0';
+$_['text_finland']			= 'crwdns87243:0crwdne87243:0';
+$_['text_denmark']			= 'crwdns87245:0crwdne87245:0';
+$_['text_germany']			= 'crwdns87247:0crwdne87247:0';
+$_['text_netherlands']		= 'crwdns87249:0crwdne87249:0';
+
+// Entry
+$_['entry_merchant']		= 'crwdns87251:0crwdne87251:0';
+$_['entry_secret']			= 'crwdns87253:0crwdne87253:0';
+$_['entry_server']			= 'crwdns87255:0crwdne87255:0';
+$_['entry_total']			= 'crwdns87257:0crwdne87257:0';
+$_['entry_pending_status']	= 'crwdns87259:0crwdne87259:0';
+$_['entry_accepted_status'] = 'crwdns87261:0crwdne87261:0';
+$_['entry_geo_zone']		= 'crwdns87263:0crwdne87263:0';
+$_['entry_status']			= 'crwdns87265:0crwdne87265:0';
+$_['entry_sort_order']		= 'crwdns87267:0crwdne87267:0';
+
+// Help
+$_['help_merchant']			= 'crwdns87269:0crwdne87269:0';
+$_['help_secret']			= 'crwdns87271:0crwdne87271:0';
+$_['help_total']			= 'crwdns87273:0crwdne87273:0';
+
+// Error
+$_['error_permission']		= 'crwdns87275:0crwdne87275:0';
+$_['error_pclass']			= 'crwdns87277:0%scrwdnd87277:0%scrwdnd87277:0%scrwdne87277:0';
+$_['error_curl']			= 'crwdns87279:0%dcrwdnd87279:0%scrwdne87279:0';
+$_['error_log']				= 'crwdns87281:0crwdne87281:0';
