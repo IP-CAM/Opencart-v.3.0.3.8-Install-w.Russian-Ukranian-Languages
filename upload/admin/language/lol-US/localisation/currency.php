a68ebd72fbb6b9e794f36216170f063e4aa85060
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:32 +0200

    New translations currency.php (LOLCAT)

---
+++ upload/admin/language/lol-US/localisation/currency.php
@@ -0,0 +1,38 @@
+<?php
+// Heading
+$_['heading_title']        = 'crwdns93273:0crwdne93273:0';
+
+// Text
+$_['text_success']         = 'crwdns93275:0crwdne93275:0';
+$_['text_list']            = 'crwdns93277:0crwdne93277:0';
+$_['text_add']             = 'crwdns93279:0crwdne93279:0';
+$_['text_edit']            = 'crwdns93281:0crwdne93281:0';
+$_['text_iso']             = 'crwdns93283:0crwdne93283:0';
+
+// Column
+$_['column_title']         = 'crwdns93285:0crwdne93285:0';
+$_['column_code']          = 'crwdns93287:0crwdne93287:0';
+$_['column_value']         = 'crwdns93289:0crwdne93289:0';
+$_['column_date_modified'] = 'crwdns93291:0crwdne93291:0';
+$_['column_action']        = 'crwdns93293:0crwdne93293:0';
+
+// Entry
+$_['entry_title']          = 'crwdns93295:0crwdne93295:0';
+$_['entry_code']           = 'crwdns93297:0crwdne93297:0';
+$_['entry_value']          = 'crwdns93299:0crwdne93299:0';
+$_['entry_symbol_left']    = 'crwdns93301:0crwdne93301:0';
+$_['entry_symbol_right']   = 'crwdns93303:0crwdne93303:0';
+$_['entry_decimal_place']  = 'crwdns93305:0crwdne93305:0';
+$_['entry_status']         = 'crwdns93307:0crwdne93307:0';
+
+// Help
+$_['help_code']            = 'crwdns93309:0crwdne93309:0';
+$_['help_value']           = 'crwdns93311:0crwdne93311:0';
+
+// Error
+$_['error_permission']     = 'crwdns93313:0crwdne93313:0';
+$_['error_title']          = 'crwdns93315:0crwdne93315:0';
+$_['error_code']           = 'crwdns93317:0crwdne93317:0';
+$_['error_default']        = 'crwdns93319:0crwdne93319:0';
+$_['error_store']          = 'crwdns93321:0%scrwdne93321:0';
+$_['error_order']          = 'crwdns93323:0%scrwdne93323:0';
