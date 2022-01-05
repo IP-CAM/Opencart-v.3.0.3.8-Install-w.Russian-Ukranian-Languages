f24e0cb586996278a720d19dd24f10f781cea631
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:32 +0200

    New translations fraud.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/extension/fraud.php
@@ -0,0 +1,15 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns83913:0crwdne83913:0';
+
+// Text
+$_['text_success']     = 'crwdns83915:0crwdne83915:0';
+$_['text_list']        = 'crwdns83917:0crwdne83917:0';
+
+// Column
+$_['column_name']      = 'crwdns83919:0crwdne83919:0';
+$_['column_status']    = 'crwdns83921:0crwdne83921:0';
+$_['column_action']    = 'crwdns83923:0crwdne83923:0';
+
+// Error
+$_['error_permission'] = 'crwdns83925:0crwdne83925:0';
