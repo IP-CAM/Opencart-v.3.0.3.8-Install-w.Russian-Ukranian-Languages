b841b8175fa6c04778e8cf38d2d62fecca6a3aeb
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:24 +0200

    New translations credit.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/total/credit.php
@@ -0,0 +1,15 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns93049:0crwdne93049:0';
+
+// Text
+$_['text_extension']   = 'crwdns93051:0crwdne93051:0';
+$_['text_success']     = 'crwdns93053:0crwdne93053:0';
+$_['text_edit']        = 'crwdns93055:0crwdne93055:0';
+
+// Entry
+$_['entry_status']     = 'crwdns93057:0crwdne93057:0';
+$_['entry_sort_order'] = 'crwdns93059:0crwdne93059:0';
+
+// Error
+$_['error_permission'] = 'crwdns93061:0crwdne93061:0';
