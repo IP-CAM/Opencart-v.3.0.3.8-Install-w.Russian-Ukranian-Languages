bef95afb8fc387ae3febe5bc6e04545c2cc71dc7
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:54 +0200

    New translations item.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/shipping/item.php
@@ -0,0 +1,18 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns92427:0crwdne92427:0';
+
+// Text
+$_['text_extension']   = 'crwdns92429:0crwdne92429:0';
+$_['text_success']     = 'crwdns92431:0crwdne92431:0';
+$_['text_edit']        = 'crwdns92433:0crwdne92433:0';
+
+// Entry
+$_['entry_cost']       = 'crwdns92435:0crwdne92435:0';
+$_['entry_tax_class']  = 'crwdns92437:0crwdne92437:0';
+$_['entry_geo_zone']   = 'crwdns92439:0crwdne92439:0';
+$_['entry_status']     = 'crwdns92441:0crwdne92441:0';
+$_['entry_sort_order'] = 'crwdns92443:0crwdne92443:0';
+
+// Error
+$_['error_permission'] = 'crwdns92445:0crwdne92445:0';
