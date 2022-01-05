1c509373581e8b5f3e325211e6718ba9d3aabacb
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:59 +0200

    New translations voucher.php (LOLCAT)

---
+++ upload/admin/language/lol-US/sale/voucher.php
@@ -0,0 +1,46 @@
+<?php
+// Heading
+$_['heading_title']     = 'crwdns94875:0crwdne94875:0';
+
+// Text
+$_['text_success']      = 'crwdns94877:0crwdne94877:0';
+$_['text_list']         = 'crwdns94879:0crwdne94879:0';
+$_['text_add']          = 'crwdns94881:0crwdne94881:0';
+$_['text_edit']         = 'crwdns94883:0crwdne94883:0';
+
+// Column
+$_['column_name']       = 'crwdns94885:0crwdne94885:0';
+$_['column_code']       = 'crwdns94887:0crwdne94887:0';
+$_['column_from']       = 'crwdns94889:0crwdne94889:0';
+$_['column_to']         = 'crwdns94891:0crwdne94891:0';
+$_['column_theme']      = 'crwdns94893:0crwdne94893:0';
+$_['column_amount']     = 'crwdns94895:0crwdne94895:0';
+$_['column_status']     = 'crwdns94897:0crwdne94897:0';
+$_['column_order_id']   = 'crwdns94899:0crwdne94899:0';
+$_['column_customer']   = 'crwdns94901:0crwdne94901:0';
+$_['column_date_added'] = 'crwdns94903:0crwdne94903:0';
+$_['column_action']     = 'crwdns94905:0crwdne94905:0';
+
+// Entry
+$_['entry_code']        = 'crwdns94907:0crwdne94907:0';
+$_['entry_from_name']   = 'crwdns94909:0crwdne94909:0';
+$_['entry_from_email']  = 'crwdns94911:0crwdne94911:0';
+$_['entry_to_name']     = 'crwdns94913:0crwdne94913:0';
+$_['entry_to_email']    = 'crwdns94915:0crwdne94915:0';
+$_['entry_theme']       = 'crwdns94917:0crwdne94917:0';
+$_['entry_message']     = 'crwdns94919:0crwdne94919:0';
+$_['entry_amount']      = 'crwdns94921:0crwdne94921:0';
+$_['entry_status']      = 'crwdns94923:0crwdne94923:0';
+
+// Help
+$_['help_code']         = 'crwdns94925:0crwdne94925:0';
+
+// Error
+$_['error_permission']  = 'crwdns94927:0crwdne94927:0';
+$_['error_exists']      = 'crwdns94929:0crwdne94929:0';
+$_['error_code']        = 'crwdns94931:0crwdne94931:0';
+$_['error_to_name']     = 'crwdns94933:0crwdne94933:0';
+$_['error_from_name']   = 'crwdns94935:0crwdne94935:0';
+$_['error_email']       = 'crwdns94937:0crwdne94937:0';
+$_['error_amount']      = 'crwdns94939:0crwdne94939:0';
+$_['error_order']       = 'crwdns94941:0%scrwdne94941:0';
