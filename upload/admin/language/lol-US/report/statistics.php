a1fbcad8feb719294996c67e16a53c0567c8d74b
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:56 +0200

    New translations statistics.php (LOLCAT)

---
+++ upload/admin/language/lol-US/report/statistics.php
@@ -0,0 +1,24 @@
+<?php
+// Heading
+$_['heading_title']         = 'crwdns94359:0crwdne94359:0';
+
+// Text
+$_['text_success']          = 'crwdns94361:0crwdne94361:0';
+$_['text_list']             = 'crwdns94363:0crwdne94363:0';
+$_['text_order_sale']       = 'crwdns94365:0crwdne94365:0';
+$_['text_order_processing'] = 'crwdns94367:0crwdne94367:0';
+$_['text_order_complete']   = 'crwdns94369:0crwdne94369:0';
+$_['text_order_other']      = 'crwdns94371:0crwdne94371:0';
+$_['text_returns']          = 'crwdns94373:0crwdne94373:0';
+$_['text_customer']         = 'crwdns94375:0crwdne94375:0';
+$_['text_affiliate']        = 'crwdns94377:0crwdne94377:0';
+$_['text_product']          = 'crwdns94379:0crwdne94379:0';
+$_['text_review']           = 'crwdns94381:0crwdne94381:0';
+
+// Column
+$_['column_name']           = 'crwdns94383:0crwdne94383:0';
+$_['column_value']	        = 'crwdns94385:0crwdne94385:0';
+$_['column_action']         = 'crwdns94387:0crwdne94387:0';
+
+// Error
+$_['error_permission']      = 'crwdns94389:0crwdne94389:0';
