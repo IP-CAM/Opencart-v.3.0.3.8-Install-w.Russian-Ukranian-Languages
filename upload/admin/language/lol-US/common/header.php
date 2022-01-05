588f72cd2a78ca49e6475575867c0de9008acfe1
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:20:59 +0200

    New translations header.php (LOLCAT)

---
+++ upload/admin/language/lol-US/common/header.php
@@ -0,0 +1,12 @@
+<?php
+// Heading
+$_['heading_title']      = 'crwdns81983:0crwdne81983:0';
+
+// Text
+$_['text_profile']       = 'crwdns81985:0crwdne81985:0';
+$_['text_store']         = 'crwdns81987:0crwdne81987:0';
+$_['text_help']          = 'crwdns81989:0crwdne81989:0';
+$_['text_homepage']      = 'crwdns81991:0crwdne81991:0';
+$_['text_support']       = 'crwdns81993:0crwdne81993:0';
+$_['text_documentation'] = 'crwdns81995:0crwdne81995:0';
+$_['text_logout']        = 'crwdns81997:0crwdne81997:0';
