19764b4cf0d4465841ccd12855fb9cd7ad0b8529
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:20:57 +0200

    New translations filter.php (LOLCAT)

---
+++ upload/admin/language/lol-US/catalog/filter.php
@@ -0,0 +1,26 @@
+<?php
+// Heading
+$_['heading_title']     = 'crwdns81231:0crwdne81231:0';
+
+// Text
+$_['text_success']      = 'crwdns81233:0crwdne81233:0';
+$_['text_list']         = 'crwdns81235:0crwdne81235:0';
+$_['text_add']          = 'crwdns81237:0crwdne81237:0';
+$_['text_edit']         = 'crwdns81239:0crwdne81239:0';
+$_['text_group']        = 'crwdns81241:0crwdne81241:0';
+$_['text_value']        = 'crwdns81243:0crwdne81243:0';
+
+// Column
+$_['column_group']      = 'crwdns81245:0crwdne81245:0';
+$_['column_sort_order'] = 'crwdns81247:0crwdne81247:0';
+$_['column_action']     = 'crwdns81249:0crwdne81249:0';
+
+// Entry
+$_['entry_group']       = 'crwdns81251:0crwdne81251:0';
+$_['entry_name']        = 'crwdns81253:0crwdne81253:0';
+$_['entry_sort_order']  = 'crwdns81255:0crwdne81255:0';
+
+// Error
+$_['error_permission']  = 'crwdns81257:0crwdne81257:0';
+$_['error_group']       = 'crwdns81259:0crwdne81259:0';
+$_['error_name']        = 'crwdns81261:0crwdne81261:0';
