2fb76184111fd51a3adc61628805a81b74a19518
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:45 +0200

    New translations reward.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/total/reward.php
@@ -0,0 +1,16 @@
+<?php
+// Heading
+$_['heading_title'] = 'crwdns100103:0%scrwdne100103:0';
+
+// Text
+$_['text_reward']   = 'crwdns100105:0%scrwdne100105:0';
+$_['text_order_id'] = 'crwdns100107:0%scrwdne100107:0';
+$_['text_success']  = 'crwdns100109:0crwdne100109:0';
+
+// Entry
+$_['entry_reward']  = 'crwdns100111:0%scrwdne100111:0';
+
+// Error
+$_['error_reward']  = 'crwdns100113:0crwdne100113:0';
+$_['error_points']  = 'crwdns100115:0%scrwdne100115:0';
+$_['error_maximum'] = 'crwdns100117:0%scrwdne100117:0';
