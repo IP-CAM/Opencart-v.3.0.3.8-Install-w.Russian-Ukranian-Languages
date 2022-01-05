ba6db7052f08450aa1892a3ca0d1827ddae5888e
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:25:16 +0200

    New translations footer.php (LOLCAT)

---
+++ upload/install/language/lol-US/common/footer.php
@@ -0,0 +1,5 @@
+<?php
+// Text
+$_['text_project']       = 'crwdns100725:0crwdne100725:0';
+$_['text_documentation'] = 'crwdns100727:0crwdne100727:0';
+$_['text_support']       = 'crwdns100729:0crwdne100729:0';
