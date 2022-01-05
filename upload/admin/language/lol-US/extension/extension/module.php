5a06bb7aa3315e9dcde6f7cc61bc236b80bb0cbf
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:27 +0200

    New translations module.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/extension/module.php
@@ -0,0 +1,24 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns83955:0crwdne83955:0';
+
+// Text
+$_['text_success']     = 'crwdns83957:0crwdne83957:0';
+$_['text_layout']      = 'crwdns83959:0%scrwdne83959:0';
+$_['text_add']         = 'crwdns83961:0crwdne83961:0';
+$_['text_list']        = 'crwdns83963:0crwdne83963:0';
+
+// Column
+$_['column_name']      = 'crwdns83965:0crwdne83965:0';
+$_['column_status']    = 'crwdns83967:0crwdne83967:0';
+$_['column_action']    = 'crwdns83969:0crwdne83969:0';
+
+// Entry
+$_['entry_code']       = 'crwdns83971:0crwdne83971:0';
+$_['entry_name']       = 'crwdns83973:0crwdne83973:0';
+
+// Error
+$_['error_permission'] = 'crwdns83975:0crwdne83975:0';
+$_['error_name']       = 'crwdns83977:0crwdne83977:0';
+$_['error_code_name']  = 'crwdns83979:0crwdne83979:0';
+$_['error_code']       = 'crwdns83981:0crwdne83981:0';
