ab99313bb6ffa9d17e1bbbc17e4fe6a48140a16c
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:12 +0200

    New translations divido_calculator.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/module/divido_calculator.php
@@ -0,0 +1,14 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns84755:0crwdne84755:0';
+
+// Text
+$_['text_extension']   = 'crwdns84757:0crwdne84757:0';
+$_['text_success']     = 'crwdns84759:0crwdne84759:0';
+$_['text_edit']        = 'crwdns84761:0crwdne84761:0';
+
+// Entry
+$_['entry_status']     = 'crwdns84763:0crwdne84763:0';
+
+// Error
+$_['error_permission'] = 'crwdns84765:0crwdne84765:0';
