34ecd97e24471d8238bf268446060843bec87c2b
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:35 +0200

    New translations default.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/menu/default.php
@@ -0,0 +1,25 @@
+<?php
+// Heading
+$_['heading_title']          = 'crwdns84515:0crwdne84515:0';
+
+// Text
+$_['text_extension']         = 'crwdns84517:0crwdne84517:0';
+$_['text_success']           = 'crwdns84519:0crwdne84519:0';
+$_['text_edit']              = 'crwdns84521:0crwdne84521:0';
+$_['text_import']            = 'crwdns84523:0crwdne84523:0';
+
+// Column
+$_['column_google_category'] = 'crwdns84525:0crwdne84525:0';
+$_['column_category']        = 'crwdns84527:0crwdne84527:0';
+$_['column_action']          = 'crwdns84529:0crwdne84529:0';
+
+// Entry
+$_['entry_google_category']  = 'crwdns84531:0crwdne84531:0';
+$_['entry_category']         = 'crwdns84533:0crwdne84533:0';
+$_['entry_data_feed']        = 'crwdns84535:0crwdne84535:0';
+$_['entry_status']           = 'crwdns84537:0crwdne84537:0';
+
+// Error
+$_['error_permission']       = 'crwdns84539:0crwdne84539:0';
+$_['error_upload']           = 'crwdns84541:0crwdne84541:0';
+$_['error_filetype']         = 'crwdns84543:0crwdne84543:0';
