dd9cf90f86a14ec1d505f61eb5eb040549d8ad74
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:43 +0200

    New translations sagepay_server.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/credit_card/sagepay_server.php
@@ -0,0 +1,31 @@
+<?php
+// Heading
+$_['heading_title']         = 'crwdns97253:0crwdne97253:0';
+
+// Text
+$_['text_empty']		    = 'crwdns97255:0crwdne97255:0';
+$_['text_account']          = 'crwdns97257:0crwdne97257:0';
+$_['text_card']			    = 'crwdns97259:0crwdne97259:0';
+$_['text_fail_card']	    = 'crwdns97261:0crwdne97261:0';
+$_['text_success_card']     = 'crwdns97263:0crwdne97263:0';
+$_['text_success_add_card'] = 'crwdns97265:0crwdne97265:0';
+
+// Column
+$_['column_type']		    = 'crwdns97267:0crwdne97267:0';
+$_['column_digits']	        = 'crwdns97269:0crwdne97269:0';
+$_['column_expiry']	     	= 'crwdns97271:0crwdne97271:0';
+
+// Entry
+$_['entry_cc_owner']        = 'crwdns97273:0crwdne97273:0';
+$_['entry_cc_type']         = 'crwdns97275:0crwdne97275:0';
+$_['entry_cc_number']       = 'crwdns97277:0crwdne97277:0';
+$_['entry_cc_expire_date']  = 'crwdns97279:0crwdne97279:0';
+$_['entry_cc_cvv2']         = 'crwdns97281:0crwdne97281:0';
+$_['entry_cc_choice']       = 'crwdns97283:0crwdne97283:0';
+
+// Button
+$_['button_add_card']       = 'crwdns97285:0crwdne97285:0';
+$_['button_new_card']       = 'crwdns97287:0crwdne97287:0';
+
+
+
