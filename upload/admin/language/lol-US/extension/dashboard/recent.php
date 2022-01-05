1b339861a4d731dcbd6a13f7fa10235c68623df6
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:27 +0200

    New translations recent.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/dashboard/recent.php
@@ -0,0 +1,25 @@
+<?php
+// Heading
+$_['heading_title']     = 'crwdns83795:0crwdne83795:0';
+
+// Text
+$_['text_extension']    = 'crwdns83797:0crwdne83797:0';
+$_['text_success']      = 'crwdns83799:0crwdne83799:0';
+$_['text_edit']         = 'crwdns83801:0crwdne83801:0';
+
+// Column
+$_['column_order_id']   = 'crwdns83803:0crwdne83803:0';
+$_['column_customer']   = 'crwdns83805:0crwdne83805:0';
+$_['column_status']     = 'crwdns83807:0crwdne83807:0';
+$_['column_total']      = 'crwdns83809:0crwdne83809:0';
+$_['column_date_added'] = 'crwdns83811:0crwdne83811:0';
+$_['column_action']     = 'crwdns83813:0crwdne83813:0';
+
+
+// Entry
+$_['entry_status']      = 'crwdns83815:0crwdne83815:0';
+$_['entry_sort_order']  = 'crwdns83817:0crwdne83817:0';
+$_['entry_width']       = 'crwdns83819:0crwdne83819:0';
+
+// Error
+$_['error_permission']  = 'crwdns83821:0crwdne83821:0';
