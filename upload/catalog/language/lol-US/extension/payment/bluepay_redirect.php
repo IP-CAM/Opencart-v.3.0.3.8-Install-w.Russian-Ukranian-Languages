00b242a5e979a8c9fbd94737e5687d946a5c9017
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:36 +0200

    New translations bluepay_redirect.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/payment/bluepay_redirect.php
@@ -0,0 +1,30 @@
+<?php
+// Text
+$_['text_title']				= 'crwdns97833:0crwdne97833:0';
+$_['text_credit_card']			= 'crwdns97835:0crwdne97835:0';
+$_['text_description']			= 'crwdns97837:0%scrwdnd97837:0%scrwdne97837:0';
+$_['text_card_type']			= 'crwdns97839:0crwdne97839:0';
+$_['text_card_name']			= 'crwdns97841:0crwdne97841:0';
+$_['text_card_digits']			= 'crwdns97843:0crwdne97843:0';
+$_['text_card_expiry']			= 'crwdns97845:0crwdne97845:0';
+
+// Returned text
+$_['text_transaction_error']	= 'crwdns97847:0crwdne97847:0';
+
+// Entry
+$_['entry_card']				= 'crwdns97849:0crwdne97849:0';
+$_['entry_card_existing']		= 'crwdns97851:0crwdne97851:0';
+$_['entry_card_new']			= 'crwdns97853:0crwdne97853:0';
+$_['entry_card_save']			= 'crwdns97855:0crwdne97855:0';
+$_['entry_cc_owner']			= 'crwdns97857:0crwdne97857:0';
+$_['entry_cc_number']			= 'crwdns97859:0crwdne97859:0';
+$_['entry_cc_start_date']		= 'crwdns97861:0crwdne97861:0';
+$_['entry_cc_expire_date']		= 'crwdns97863:0crwdne97863:0';
+$_['entry_cc_cvv2']				= 'crwdns97865:0crwdne97865:0';
+$_['entry_cc_address']			= 'crwdns97867:0crwdne97867:0';
+$_['entry_cc_city']				= 'crwdns97869:0crwdne97869:0';
+$_['entry_cc_state']			= 'crwdns97871:0crwdne97871:0';
+$_['entry_cc_zipcode']			= 'crwdns97873:0crwdne97873:0';
+$_['entry_cc_phone']			= 'crwdns97875:0crwdne97875:0';
+$_['entry_cc_email']			= 'crwdns97877:0crwdne97877:0';
+$_['entry_cc_choice']			= 'crwdns97879:0crwdne97879:0';
