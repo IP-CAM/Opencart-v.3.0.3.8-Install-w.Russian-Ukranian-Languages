be050cd4fff181ca533bd061860eb677ed0ad2c0
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:33 +0200

    New translations header.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/common/header.php
@@ -0,0 +1,16 @@
+<?php
+// Text
+$_['text_home']          = 'crwdns97025:0crwdne97025:0';
+$_['text_wishlist']      = 'crwdns97027:0%scrwdne97027:0';
+$_['text_shopping_cart'] = 'crwdns97029:0crwdne97029:0';
+$_['text_category']      = 'crwdns97031:0crwdne97031:0';
+$_['text_account']       = 'crwdns97033:0crwdne97033:0';
+$_['text_register']      = 'crwdns97035:0crwdne97035:0';
+$_['text_login']         = 'crwdns97037:0crwdne97037:0';
+$_['text_order']         = 'crwdns97039:0crwdne97039:0';
+$_['text_transaction']   = 'crwdns97041:0crwdne97041:0';
+$_['text_download']      = 'crwdns97043:0crwdne97043:0';
+$_['text_logout']        = 'crwdns97045:0crwdne97045:0';
+$_['text_checkout']      = 'crwdns97047:0crwdne97047:0';
+$_['text_search']        = 'crwdns97049:0crwdne97049:0';
+$_['text_all']           = 'crwdns97051:0crwdne97051:0';
