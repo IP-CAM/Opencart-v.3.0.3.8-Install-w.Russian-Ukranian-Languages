8785f9c1d24db3526e712bf881a8b0c05d22aa6c
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:25:00 +0200

    New translations sagepay_us.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/payment/sagepay_us.php
@@ -0,0 +1,10 @@
+<?php
+// Text
+$_['text_title']				= 'crwdns99573:0crwdne99573:0';
+$_['text_credit_card']			= 'crwdns99575:0crwdne99575:0';
+
+// Entry
+$_['entry_cc_owner']			= 'crwdns99577:0crwdne99577:0';
+$_['entry_cc_number']			= 'crwdns99579:0crwdne99579:0';
+$_['entry_cc_expire_date']		= 'crwdns99581:0crwdne99581:0';
+$_['entry_cc_cvv2']				= 'crwdns99583:0crwdne99583:0';
