e8f062ff9fff7dc102f12f721e3887d82fe0e832
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:47 +0200

    New translations royal_mail.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/shipping/royal_mail.php
@@ -0,0 +1,15 @@
+<?php
+// Text
+$_['text_title']                        = 'crwdns99951:0crwdne99951:0';
+$_['text_weight']                       = 'crwdns99953:0crwdne99953:0';
+$_['text_insurance']                    = 'crwdns99955:0crwdne99955:0';
+$_['text_special_delivery']             = 'crwdns99957:0crwdne99957:0';
+$_['text_1st_class_signed']             = 'crwdns99959:0crwdne99959:0';
+$_['text_2nd_class_signed']             = 'crwdns99961:0crwdne99961:0';
+$_['text_1st_class_standard']           = 'crwdns99963:0crwdne99963:0';
+$_['text_2nd_class_standard']           = 'crwdns99965:0crwdne99965:0';
+$_['text_international_standard']       = 'crwdns99967:0crwdne99967:0';
+$_['text_international_tracked_signed'] = 'crwdns99969:0crwdne99969:0';
+$_['text_international_tracked']        = 'crwdns99971:0crwdne99971:0';
+$_['text_international_signed']         = 'crwdns99973:0crwdne99973:0';
+$_['text_international_economy']        = 'crwdns99975:0crwdne99975:0';
