ad96452abe299b4027fb0d9967e41183432cb8f9
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:23 +0200

    New translations store.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/module/store.php
@@ -0,0 +1,7 @@
+<?php
+// Heading
+$_['heading_title'] = 'crwdns97665:0crwdne97665:0';
+
+// Text
+$_['text_default']  = 'crwdns97667:0crwdne97667:0';
+$_['text_store']    = 'crwdns97669:0crwdne97669:0';
