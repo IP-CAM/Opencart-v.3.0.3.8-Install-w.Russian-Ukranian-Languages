9e308f22e1097ed6d6b6f6a60479ab9e1b626c89
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:43 +0200

    New translations alipay.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/payment/alipay.php
@@ -0,0 +1,32 @@
+<?php
+// Heading
+$_['heading_title']                  = 'crwdns85145:0crwdne85145:0';
+
+// Text
+$_['text_extension']                 = 'crwdns85147:0crwdne85147:0';
+$_['text_success']                   = 'crwdns85149:0crwdne85149:0';
+$_['text_edit']                      = 'crwdns85151:0crwdne85151:0';
+$_['text_alipay']                    = 'crwdns85153:0crwdne85153:0';
+$_['text_live']                      = 'crwdns85155:0crwdne85155:0';
+$_['text_sandbox']                   = 'crwdns85157:0crwdne85157:0';
+
+// Entry
+$_['entry_app_id']                   = 'crwdns85159:0crwdne85159:0';
+$_['entry_merchant_private_key']     = 'crwdns85161:0crwdne85161:0';
+$_['entry_alipay_public_key']        = 'crwdns85163:0crwdne85163:0';
+$_['entry_test']                     = 'crwdns85165:0crwdne85165:0';
+$_['entry_total']                    = 'crwdns85167:0crwdne85167:0';
+$_['entry_order_status']             = 'crwdns85169:0crwdne85169:0';
+$_['entry_geo_zone']                 = 'crwdns85171:0crwdne85171:0';
+$_['entry_status']                   = 'crwdns85173:0crwdne85173:0';
+$_['entry_sort_order']               = 'crwdns85175:0crwdne85175:0';
+
+// Help
+$_['help_total']                     = 'crwdns85177:0crwdne85177:0';
+$_['help_alipay_setup']              = 'crwdns85179:0crwdne85179:0';
+
+// Error
+$_['error_permission']               = 'crwdns85181:0crwdne85181:0';
+$_['error_app_id']                   = 'crwdns85183:0crwdne85183:0';
+$_['error_merchant_private_key']     = 'crwdns85185:0crwdne85185:0';
+$_['error_alipay_public_key']        = 'crwdns85187:0crwdne85187:0';
