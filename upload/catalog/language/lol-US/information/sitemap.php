7d4f3a03494b0394815dd884c0983900fa149f92
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:50 +0200

    New translations sitemap.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/information/sitemap.php
@@ -0,0 +1,17 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns100195:0crwdne100195:0';
+
+// Text
+$_['text_special']     = 'crwdns100197:0crwdne100197:0';
+$_['text_account']     = 'crwdns100199:0crwdne100199:0';
+$_['text_edit']        = 'crwdns100201:0crwdne100201:0';
+$_['text_password']    = 'crwdns100203:0crwdne100203:0';
+$_['text_address']     = 'crwdns100205:0crwdne100205:0';
+$_['text_history']     = 'crwdns100207:0crwdne100207:0';
+$_['text_download']    = 'crwdns100209:0crwdne100209:0';
+$_['text_cart']        = 'crwdns100211:0crwdne100211:0';
+$_['text_checkout']    = 'crwdns100213:0crwdne100213:0';
+$_['text_search']      = 'crwdns100215:0crwdne100215:0';
+$_['text_information'] = 'crwdns100217:0crwdne100217:0';
+$_['text_contact']     = 'crwdns100219:0crwdne100219:0';
