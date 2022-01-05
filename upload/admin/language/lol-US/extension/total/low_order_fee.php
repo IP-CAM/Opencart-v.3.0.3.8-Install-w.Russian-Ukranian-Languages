a794833844dc44575e57ffd4e4ebfb3e9f7a5ade
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:26 +0200

    New translations low_order_fee.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/total/low_order_fee.php
@@ -0,0 +1,21 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns93117:0crwdne93117:0';
+
+// Text
+$_['text_extension']   = 'crwdns93119:0crwdne93119:0';
+$_['text_success']     = 'crwdns93121:0crwdne93121:0';
+$_['text_edit']        = 'crwdns93123:0crwdne93123:0';
+
+// Entry
+$_['entry_total']      = 'crwdns93125:0crwdne93125:0';
+$_['entry_fee']        = 'crwdns93127:0crwdne93127:0';
+$_['entry_tax_class']  = 'crwdns93129:0crwdne93129:0';
+$_['entry_status']     = 'crwdns93131:0crwdne93131:0';
+$_['entry_sort_order'] = 'crwdns93133:0crwdne93133:0';
+
+// Help
+$_['help_total']       = 'crwdns93135:0crwdne93135:0';
+
+// Error
+$_['error_permission'] = 'crwdns93137:0crwdne93137:0';
