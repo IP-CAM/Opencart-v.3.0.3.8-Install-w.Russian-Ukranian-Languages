d1b67107bced9e3fa11e56e2a988454c70262540
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:27 +0200

    New translations installer.php (LOLCAT)

---
+++ upload/admin/language/lol-US/marketplace/installer.php
@@ -0,0 +1,29 @@
+<?php
+// Heading
+$_['heading_title']     = 'crwdns94135:0crwdne94135:0';
+
+// Text
+$_['text_progress']     = 'crwdns94137:0crwdne94137:0';
+$_['text_upload']       = 'crwdns94139:0crwdne94139:0';
+$_['text_history']      = 'crwdns94141:0crwdne94141:0';
+$_['text_success']      = 'crwdns94143:0crwdne94143:0';
+$_['text_install']      = 'crwdns94145:0crwdne94145:0';
+
+// Column
+$_['column_filename']   = 'crwdns94147:0crwdne94147:0';
+$_['column_date_added'] = 'crwdns94149:0crwdne94149:0';
+$_['column_action']     = 'crwdns94151:0crwdne94151:0';
+
+// Entry
+$_['entry_upload']      = 'crwdns94153:0crwdne94153:0';
+$_['entry_progress']    = 'crwdns94155:0crwdne94155:0';
+
+// Help
+$_['help_upload']       = 'crwdns94157:0crwdne94157:0';
+
+// Error
+$_['error_permission']  = 'crwdns94159:0crwdne94159:0';
+$_['error_install']     = 'crwdns94161:0crwdne94161:0';
+$_['error_upload']      = 'crwdns94163:0crwdne94163:0';
+$_['error_filetype']    = 'crwdns94165:0crwdne94165:0';
+$_['error_file']        = 'crwdns94167:0crwdne94167:0';
