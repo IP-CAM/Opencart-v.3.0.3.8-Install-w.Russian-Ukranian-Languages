cdc7504e4eedea51f0b15ca5a93d650f8db7b7c7
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:37 +0200

    New translations pickup.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/shipping/pickup.php
@@ -0,0 +1,16 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns92485:0crwdne92485:0';
+
+// Text
+$_['text_extension']   = 'crwdns92487:0crwdne92487:0';
+$_['text_success']     = 'crwdns92489:0crwdne92489:0';
+$_['text_edit']        = 'crwdns92491:0crwdne92491:0';
+
+// Entry
+$_['entry_geo_zone']   = 'crwdns92493:0crwdne92493:0';
+$_['entry_status']     = 'crwdns92495:0crwdne92495:0';
+$_['entry_sort_order'] = 'crwdns92497:0crwdne92497:0';
+
+// Error
+$_['error_permission'] = 'crwdns92499:0crwdne92499:0';
