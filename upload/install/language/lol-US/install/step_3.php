43660a5f66796a6a96d81b9c92413c64e0b8a956
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:25:16 +0200

    New translations step_3.php (LOLCAT)

---
+++ upload/install/language/lol-US/install/step_3.php
@@ -0,0 +1,36 @@
+<?php
+// Heading
+$_['heading_title']          = 'crwdns100855:0crwdne100855:0';
+
+// Text
+$_['text_step_3']            = 'crwdns100857:0crwdne100857:0';
+$_['text_db_connection']     = 'crwdns100859:0crwdne100859:0';
+$_['text_db_administration'] = 'crwdns100861:0crwdne100861:0';
+$_['text_mysqli']            = 'crwdns100863:0crwdne100863:0';
+$_['text_pdo']               = 'crwdns100865:0crwdne100865:0';
+$_['text_pgsql']             = 'crwdns100867:0crwdne100867:0';
+
+// Entry
+$_['entry_db_driver']        = 'crwdns100869:0crwdne100869:0';
+$_['entry_db_hostname']      = 'crwdns100871:0crwdne100871:0';
+$_['entry_db_username']      = 'crwdns100873:0crwdne100873:0';
+$_['entry_db_password']      = 'crwdns100875:0crwdne100875:0';
+$_['entry_db_database']      = 'crwdns100877:0crwdne100877:0';
+$_['entry_db_port']          = 'crwdns100879:0crwdne100879:0';
+$_['entry_db_prefix']        = 'crwdns100881:0crwdne100881:0';
+$_['entry_username']         = 'crwdns100883:0crwdne100883:0';
+$_['entry_password']         = 'crwdns100885:0crwdne100885:0';
+$_['entry_email']            = 'crwdns100887:0crwdne100887:0';
+
+// Error
+$_['error_db_driver']        = 'crwdns100889:0crwdne100889:0';
+$_['error_db_hostname']      = 'crwdns100891:0crwdne100891:0';
+$_['error_db_username']      = 'crwdns100893:0crwdne100893:0';
+$_['error_db_database']      = 'crwdns100895:0crwdne100895:0';
+$_['error_db_port']          = 'crwdns100897:0crwdne100897:0';
+$_['error_db_prefix']        = 'crwdns100899:0crwdne100899:0';
+$_['error_db_connect']       = 'crwdns100901:0crwdne100901:0';
+$_['error_username']         = 'crwdns100903:0crwdne100903:0';
+$_['error_password']         = 'crwdns100905:0crwdne100905:0';
+$_['error_email']            = 'crwdns100907:0crwdne100907:0';
+$_['error_config']           = 'crwdns100909:0crwdne100909:0';
