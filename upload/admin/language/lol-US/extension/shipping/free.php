01b5547c920893c815354be2a3d89c9a72b50ab4
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:53 +0200

    New translations free.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/shipping/free.php
@@ -0,0 +1,20 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns92407:0crwdne92407:0';
+
+// Text
+$_['text_extension']   = 'crwdns92409:0crwdne92409:0';
+$_['text_success']     = 'crwdns92411:0crwdne92411:0';
+$_['text_edit']        = 'crwdns92413:0crwdne92413:0';
+
+// Entry
+$_['entry_total']      = 'crwdns92415:0crwdne92415:0';
+$_['entry_geo_zone']   = 'crwdns92417:0crwdne92417:0';
+$_['entry_status']     = 'crwdns92419:0crwdne92419:0';
+$_['entry_sort_order'] = 'crwdns92421:0crwdne92421:0';
+
+// Help
+$_['help_total']       = 'crwdns92423:0crwdne92423:0';
+
+// Error
+$_['error_permission'] = 'crwdns92425:0crwdne92425:0';
