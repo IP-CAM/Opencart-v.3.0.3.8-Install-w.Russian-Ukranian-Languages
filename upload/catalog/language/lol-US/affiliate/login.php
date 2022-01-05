3c953f6081e907b4c16a61b58c4d9ef0140bd222
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:56 +0200

    New translations login.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/affiliate/login.php
@@ -0,0 +1,22 @@
+<?php
+// Heading
+$_['heading_title']                 = 'crwdns96449:0crwdne96449:0';
+
+// Text
+$_['text_account']                  = 'crwdns96451:0crwdne96451:0';
+$_['text_login']                    = 'crwdns96453:0crwdne96453:0';
+$_['text_description']              = 'crwdns96455:0%scrwdnd96455:0%scrwdnd96455:0%scrwdne96455:0';
+$_['text_new_affiliate']            = 'crwdns96457:0crwdne96457:0';
+$_['text_register_account']         = 'crwdns96459:0crwdne96459:0';
+$_['text_returning_affiliate']      = 'crwdns96461:0crwdne96461:0';
+$_['text_i_am_returning_affiliate'] = 'crwdns96463:0crwdne96463:0';
+$_['text_forgotten']                = 'crwdns96465:0crwdne96465:0';
+
+// Entry
+$_['entry_email']                   = 'crwdns96467:0crwdne96467:0';
+$_['entry_password']                = 'crwdns96469:0crwdne96469:0';
+
+// Error
+$_['error_login']                   = 'crwdns96471:0crwdne96471:0';
+$_['error_attempts']                = 'crwdns96473:0crwdne96473:0';
+$_['error_approved']                = 'crwdns96475:0crwdne96475:0';
