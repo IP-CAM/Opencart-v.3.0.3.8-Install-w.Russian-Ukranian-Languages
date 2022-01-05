bbc7770f8c78e68862567d6d4ef5f39a397851fe
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:08 +0200

    New translations online.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/dashboard/online.php
@@ -0,0 +1,17 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns83759:0crwdne83759:0';
+
+// Text
+$_['text_extension']   = 'crwdns83761:0crwdne83761:0';
+$_['text_success']     = 'crwdns83763:0crwdne83763:0';
+$_['text_edit']        = 'crwdns83765:0crwdne83765:0';
+$_['text_view']        = 'crwdns83767:0crwdne83767:0';
+
+// Entry
+$_['entry_status']     = 'crwdns83769:0crwdne83769:0';
+$_['entry_sort_order'] = 'crwdns83771:0crwdne83771:0';
+$_['entry_width']      = 'crwdns83773:0crwdne83773:0';
+
+// Error
+$_['error_permission'] = 'crwdns83775:0crwdne83775:0';
