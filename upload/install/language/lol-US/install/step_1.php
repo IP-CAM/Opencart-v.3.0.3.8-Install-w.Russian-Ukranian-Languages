0c20a3b9018b702f2c771de19132d68363ec5d01
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:25:18 +0200

    New translations step_1.php (LOLCAT)

---
+++ upload/install/language/lol-US/install/step_1.php
@@ -0,0 +1,7 @@
+<?php
+// Heading
+$_['heading_title'] = 'crwdns100739:0crwdne100739:0';
+
+// Text
+$_['text_step_1']   = 'crwdns100741:0crwdne100741:0';
+$_['text_terms']    = 'crwdns100743:0crwdne100743:0';
