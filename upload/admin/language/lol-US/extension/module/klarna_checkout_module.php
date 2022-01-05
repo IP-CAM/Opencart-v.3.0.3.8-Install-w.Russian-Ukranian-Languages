be6c7cb1c8ef564dde3844b6eb51d105ebd1ebd2
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:16 +0200

    New translations klarna_checkout_module.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/module/klarna_checkout_module.php
@@ -0,0 +1,13 @@
+<?php
+// Heading
+$_['heading_title']	   = 'crwdns84859:0crwdne84859:0';
+
+// Text
+$_['text_extension']   = 'crwdns84861:0crwdne84861:0';
+$_['text_success']	   = 'crwdns84863:0crwdne84863:0';
+
+//Entry
+$_['entry_status']	   = 'crwdns84865:0crwdne84865:0';
+
+//Error
+$_['error_permission'] = 'crwdns84867:0crwdne84867:0';
