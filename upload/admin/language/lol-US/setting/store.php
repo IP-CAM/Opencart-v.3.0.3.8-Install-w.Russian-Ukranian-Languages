1c437e90e1281ad7176845ea43c41afc4a7d0e5b
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:01 +0200

    New translations store.php (LOLCAT)

---
+++ upload/admin/language/lol-US/setting/store.php
@@ -0,0 +1,98 @@
+<?php
+// Heading
+$_['heading_title']                = 'crwdns95363:0crwdne95363:0';
+
+// Text
+$_['text_settings']                = 'crwdns95365:0crwdne95365:0';
+$_['text_success']                 = 'crwdns95367:0crwdne95367:0';
+$_['text_list']                    = 'crwdns95369:0crwdne95369:0';
+$_['text_add']                     = 'crwdns95371:0crwdne95371:0';
+$_['text_edit']                    = 'crwdns95373:0crwdne95373:0';
+$_['text_items']                   = 'crwdns95375:0crwdne95375:0';
+$_['text_tax']                     = 'crwdns95377:0crwdne95377:0';
+$_['text_account']                 = 'crwdns95379:0crwdne95379:0';
+$_['text_checkout']                = 'crwdns95381:0crwdne95381:0';
+$_['text_stock']                   = 'crwdns95383:0crwdne95383:0';
+$_['text_shipping']                = 'crwdns95385:0crwdne95385:0';
+$_['text_payment']                 = 'crwdns95387:0crwdne95387:0';
+
+// Column
+$_['column_name']                  = 'crwdns95389:0crwdne95389:0';
+$_['column_url']	               = 'crwdns95391:0crwdne95391:0';
+$_['column_action']                = 'crwdns95393:0crwdne95393:0';
+
+// Entry
+$_['entry_url']                    = 'crwdns95395:0crwdne95395:0';
+$_['entry_ssl']                    = 'crwdns95397:0crwdne95397:0';
+$_['entry_meta_title']             = 'crwdns95399:0crwdne95399:0';
+$_['entry_meta_description']       = 'crwdns95401:0crwdne95401:0';
+$_['entry_meta_keyword']           = 'crwdns95403:0crwdne95403:0';
+$_['entry_layout']                 = 'crwdns95405:0crwdne95405:0';
+$_['entry_theme']                  = 'crwdns95407:0crwdne95407:0';
+$_['entry_name']                   = 'crwdns95409:0crwdne95409:0';
+$_['entry_owner']                  = 'crwdns95411:0crwdne95411:0';
+$_['entry_address']                = 'crwdns95413:0crwdne95413:0';
+$_['entry_geocode']                = 'crwdns95415:0crwdne95415:0';
+$_['entry_email']                  = 'crwdns95417:0crwdne95417:0';
+$_['entry_telephone']              = 'crwdns95419:0crwdne95419:0';
+$_['entry_fax']                    = 'crwdns95421:0crwdne95421:0';
+$_['entry_image']                  = 'crwdns95423:0crwdne95423:0';
+$_['entry_open']                   = 'crwdns95425:0crwdne95425:0';
+$_['entry_comment']                = 'crwdns95427:0crwdne95427:0';
+$_['entry_location']               = 'crwdns95429:0crwdne95429:0';
+$_['entry_country']                = 'crwdns95431:0crwdne95431:0';
+$_['entry_zone']                   = 'crwdns95433:0crwdne95433:0';
+$_['entry_language']               = 'crwdns95435:0crwdne95435:0';
+$_['entry_currency']               = 'crwdns95437:0crwdne95437:0';
+$_['entry_tax']                    = 'crwdns95439:0crwdne95439:0';
+$_['entry_tax_default']            = 'crwdns95441:0crwdne95441:0';
+$_['entry_tax_customer']           = 'crwdns95443:0crwdne95443:0';
+$_['entry_customer_group']         = 'crwdns95445:0crwdne95445:0';
+$_['entry_customer_group_display'] = 'crwdns95447:0crwdne95447:0';
+$_['entry_customer_price']         = 'crwdns95449:0crwdne95449:0';
+$_['entry_account']                = 'crwdns95451:0crwdne95451:0';
+$_['entry_cart_weight']            = 'crwdns95453:0crwdne95453:0';
+$_['entry_checkout_guest']         = 'crwdns95455:0crwdne95455:0';
+$_['entry_checkout']               = 'crwdns95457:0crwdne95457:0';
+$_['entry_order_status']           = 'crwdns95459:0crwdne95459:0';
+$_['entry_stock_display']          = 'crwdns95461:0crwdne95461:0';
+$_['entry_stock_checkout']         = 'crwdns95463:0crwdne95463:0';
+$_['entry_logo']                   = 'crwdns95465:0crwdne95465:0';
+$_['entry_icon']                   = 'crwdns95467:0crwdne95467:0';
+$_['entry_secure']                 = 'crwdns95469:0crwdne95469:0';
+
+// Help
+$_['help_url']                     = 'crwdns95471:0crwdne95471:0';
+$_['help_ssl']                     = 'crwdns95473:0crwdne95473:0';
+$_['help_geocode']                 = 'crwdns95475:0crwdne95475:0';
+$_['help_open']                    = 'crwdns95477:0crwdne95477:0';
+$_['help_comment']                 = 'crwdns95479:0crwdne95479:0';
+$_['help_location']                = 'crwdns95481:0crwdne95481:0';
+$_['help_currency']                = 'crwdns95483:0crwdne95483:0';
+$_['help_tax_default']             = 'crwdns95485:0crwdne95485:0';
+$_['help_tax_customer']            = 'crwdns95487:0crwdne95487:0';
+$_['help_customer_group']          = 'crwdns95489:0crwdne95489:0';
+$_['help_customer_group_display']  = 'crwdns95491:0crwdne95491:0';
+$_['help_customer_price']          = 'crwdns95493:0crwdne95493:0';
+$_['help_account']                 = 'crwdns95495:0crwdne95495:0';
+$_['help_checkout_guest']          = 'crwdns95497:0crwdne95497:0';
+$_['help_checkout']                = 'crwdns95499:0crwdne95499:0';
+$_['help_order_status']            = 'crwdns95501:0crwdne95501:0';
+$_['help_stock_display']           = 'crwdns95503:0crwdne95503:0';
+$_['help_stock_checkout']          = 'crwdns95505:0crwdne95505:0';
+$_['help_icon']                    = 'crwdns95507:0crwdne95507:0';
+$_['help_secure']                  = 'crwdns95509:0crwdne95509:0';
+
+// Error
+$_['error_warning']                = 'crwdns95511:0crwdne95511:0';
+$_['error_permission']             = 'crwdns95513:0crwdne95513:0';
+$_['error_url']                    = 'crwdns95515:0crwdne95515:0';
+$_['error_meta_title']             = 'crwdns95517:0crwdne95517:0';
+$_['error_name']                   = 'crwdns95519:0crwdne95519:0';
+$_['error_owner']                  = 'crwdns95521:0crwdne95521:0';
+$_['error_address']                = 'crwdns95523:0crwdne95523:0';
+$_['error_email']                  = 'crwdns95525:0crwdne95525:0';
+$_['error_telephone']              = 'crwdns95527:0crwdne95527:0';
+$_['error_customer_group_display'] = 'crwdns95529:0crwdne95529:0';
+$_['error_default']                = 'crwdns95531:0crwdne95531:0';
+$_['error_store']                  = 'crwdns95533:0%scrwdne95533:0';
