f687693ec46449d1e83db971225b511b1d29d7a3
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:21 +0200

    New translations pp_braintree_button.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/module/pp_braintree_button.php
@@ -0,0 +1,20 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns85029:0crwdne85029:0';
+
+// Text
+$_['text_extension']   = 'crwdns85031:0crwdne85031:0';
+$_['text_success']     = 'crwdns85033:0crwdne85033:0';
+$_['text_edit']        = 'crwdns85035:0crwdne85035:0';
+$_['text_info']        = 'crwdns85037:0crwdne85037:0';
+$_['text_info_li1']    = 'crwdns85039:0crwdne85039:0';
+$_['text_info_li2']    = 'crwdns85041:0crwdne85041:0';
+$_['text_info_li3']    = 'crwdns85043:0crwdne85043:0';
+$_['text_layouts']     = 'crwdns85045:0crwdne85045:0';
+$_['text_layout_link'] = 'crwdns85047:0crwdne85047:0';
+
+// Entry
+$_['entry_status']     = 'crwdns85049:0crwdne85049:0';
+
+// Error
+$_['error_permission'] = 'crwdns85051:0crwdne85051:0';
