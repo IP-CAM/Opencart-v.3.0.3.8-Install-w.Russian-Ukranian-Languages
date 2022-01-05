e0b1ffcf5400f00b9daa1e8ef4306709ac103bb7
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:34 +0200

    New translations location.php (LOLCAT)

---
+++ upload/admin/language/lol-US/localisation/location.php
@@ -0,0 +1,38 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns93445:0crwdne93445:0';
+
+// Text
+$_['text_success']     = 'crwdns93447:0crwdne93447:0';
+$_['text_list']        = 'crwdns93449:0crwdne93449:0';
+$_['text_add']         = 'crwdns93451:0crwdne93451:0';
+$_['text_edit']        = 'crwdns93453:0crwdne93453:0';
+$_['text_default']     = 'crwdns93455:0crwdne93455:0';
+$_['text_time']        = 'crwdns93457:0crwdne93457:0';
+$_['text_geocode']     = 'crwdns93459:0crwdne93459:0';
+
+// Column
+$_['column_name']      = 'crwdns93461:0crwdne93461:0';
+$_['column_address']   = 'crwdns93463:0crwdne93463:0';
+$_['column_action']    = 'crwdns93465:0crwdne93465:0';
+
+// Entry
+$_['entry_name']       = 'crwdns93467:0crwdne93467:0';
+$_['entry_address']    = 'crwdns93469:0crwdne93469:0';
+$_['entry_geocode']    = 'crwdns93471:0crwdne93471:0';
+$_['entry_telephone']  = 'crwdns93473:0crwdne93473:0';
+$_['entry_fax']        = 'crwdns93475:0crwdne93475:0';
+$_['entry_image']      = 'crwdns93477:0crwdne93477:0';
+$_['entry_open']       = 'crwdns93479:0crwdne93479:0';
+$_['entry_comment']    = 'crwdns93481:0crwdne93481:0';
+
+// Help
+$_['help_geocode']     = 'crwdns93483:0crwdne93483:0';
+$_['help_open']        = 'crwdns93485:0crwdne93485:0';
+$_['help_comment']     = 'crwdns93487:0crwdne93487:0';
+
+// Error
+$_['error_permission'] = 'crwdns93489:0crwdne93489:0';
+$_['error_name']       = 'crwdns93491:0crwdne93491:0';
+$_['error_address']    = 'crwdns93493:0crwdne93493:0';
+$_['error_telephone']  = 'crwdns93495:0crwdne93495:0';
