184ca4e834744c7c400b6a5a208f2ca2aa7d0f57
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:58 +0200

    New translations cart.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/api/cart.php
@@ -0,0 +1,10 @@
+<?php
+// Text
+$_['text_success']     = 'crwdns96575:0crwdne96575:0';
+
+// Error
+$_['error_permission'] = 'crwdns96577:0crwdne96577:0';
+$_['error_stock']      = 'crwdns96579:0crwdne96579:0';
+$_['error_minimum']    = 'crwdns96581:0%scrwdnd96581:0%scrwdne96581:0';
+$_['error_store']      = 'crwdns96583:0crwdne96583:0';
+$_['error_required']   = 'crwdns96585:0%scrwdne96585:0';
