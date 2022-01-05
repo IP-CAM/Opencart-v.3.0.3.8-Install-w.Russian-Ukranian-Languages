8ce3b445c8f94d12a683b71036b341deaeaa6354
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:21 +0200

    New translations divido.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/payment/divido.php
@@ -0,0 +1,18 @@
+<?php
+$_['text_checkout_title']      = 'crwdns97997:0crwdne97997:0';
+$_['text_choose_plan']         = 'crwdns97999:0crwdne97999:0';
+$_['text_choose_deposit']      = 'crwdns98001:0crwdne98001:0';
+$_['text_monthly_payments']    = 'crwdns98003:0crwdne98003:0';
+$_['text_months']              = 'crwdns98005:0crwdne98005:0';
+$_['text_term']                = 'crwdns98007:0crwdne98007:0';
+$_['text_deposit']             = 'crwdns98009:0crwdne98009:0';
+$_['text_credit_amount']       = 'crwdns98011:0crwdne98011:0';
+$_['text_amount_payable']      = 'crwdns98013:0crwdne98013:0';
+$_['text_total_interest']      = 'crwdns98015:0crwdne98015:0';
+$_['text_monthly_installment'] = 'crwdns98017:0crwdne98017:0';
+$_['text_redirection']         = 'crwdns98019:0crwdne98019:0';
+$_['divido_checkout']          = 'crwdns98021:0crwdne98021:0';
+$_['deposit_to_low']           = 'crwdns98023:0crwdne98023:0';
+$_['credit_amount_to_low']     = 'crwdns98025:0crwdne98025:0';
+$_['no_country']               = 'crwdns98027:0crwdne98027:0';
+$_['text_divido']              = 'crwdns98029:0crwdne98029:0';
