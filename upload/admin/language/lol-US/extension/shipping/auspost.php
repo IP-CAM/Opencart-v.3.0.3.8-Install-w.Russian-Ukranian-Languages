2888e665e5a1f2c3259405990d9e0ea83db375ad
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:50 +0200

    New translations auspost.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/shipping/auspost.php
@@ -0,0 +1,24 @@
+<?php
+// Heading
+$_['heading_title']      = 'crwdns92143:0crwdne92143:0';
+
+// Text
+$_['text_extension']     = 'crwdns92145:0crwdne92145:0';
+$_['text_success']       = 'crwdns92147:0crwdne92147:0';
+$_['text_edit']          = 'crwdns92149:0crwdne92149:0';
+
+// Entry
+$_['entry_api']          = 'crwdns92151:0crwdne92151:0';
+$_['entry_postcode']     = 'crwdns92153:0crwdne92153:0';
+$_['entry_weight_class'] = 'crwdns92155:0crwdne92155:0';
+$_['entry_tax_class']    = 'crwdns92157:0crwdne92157:0';
+$_['entry_geo_zone']     = 'crwdns92159:0crwdne92159:0';
+$_['entry_status']       = 'crwdns92161:0crwdne92161:0';
+$_['entry_sort_order']   = 'crwdns92163:0crwdne92163:0';
+
+// Help
+$_['help_weight_class']  = 'crwdns92165:0crwdne92165:0';
+
+// Error
+$_['error_permission']   = 'crwdns92167:0crwdne92167:0';
+$_['error_postcode']     = 'crwdns92169:0crwdne92169:0';
