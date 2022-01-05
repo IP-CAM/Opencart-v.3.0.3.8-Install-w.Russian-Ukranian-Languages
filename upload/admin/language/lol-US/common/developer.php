c387c992e175213a9ad17574a5d01c18b399f5de
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:04 +0200

    New translations developer.php (LOLCAT)

---
+++ upload/admin/language/lol-US/common/developer.php
@@ -0,0 +1,25 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns81899:0crwdne81899:0';
+
+// Text
+$_['text_success']     = 'crwdns81901:0crwdne81901:0';
+$_['text_theme']       = 'crwdns81903:0crwdne81903:0';
+$_['text_sass']        = 'crwdns81905:0crwdne81905:0';
+$_['text_cache']       = 'crwdns81907:0%scrwdne81907:0';
+
+// Column
+$_['column_component'] = 'crwdns81909:0crwdne81909:0';
+$_['column_action']    = 'crwdns81911:0crwdne81911:0';
+
+// Entry
+$_['entry_theme']      = 'crwdns81913:0crwdne81913:0';
+$_['entry_sass']       = 'crwdns81915:0crwdne81915:0';
+$_['entry_cache']      = 'crwdns81917:0crwdne81917:0';
+
+// Button
+$_['button_on']        = 'crwdns81919:0crwdne81919:0';
+$_['button_off']       = 'crwdns81921:0crwdne81921:0';
+
+// Error
+$_['error_permission'] = 'crwdns81923:0crwdne81923:0';
