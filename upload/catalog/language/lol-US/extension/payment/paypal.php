a5632fd8d74fbb45a3c27ba8e2d9c1801bb2c85f
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:15 +0200

    New translations paypal.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/payment/paypal.php
@@ -0,0 +1,28 @@
+<?php
+// Text
+$_['text_title']						= 'crwdns99029:0crwdne99029:0';
+$_['text_paypal_express']				= 'crwdns99031:0crwdne99031:0';
+$_['text_paypal_card']					= 'crwdns99033:0crwdne99033:0';
+$_['text_wait']							= 'crwdns99035:0crwdne99035:0';
+$_['text_order_message']				= 'crwdns99037:0%scrwdne99037:0';
+
+// Entry
+$_['entry_card_number']					= 'crwdns99039:0crwdne99039:0';
+$_['entry_expiration_date']				= 'crwdns99041:0crwdne99041:0';
+$_['entry_cvv']							= 'crwdns99043:0crwdne99043:0';
+
+// Button
+$_['button_pay']						= 'crwdns99045:0crwdne99045:0';
+
+// Error
+$_['error_warning']						= 'crwdns99047:0crwdne99047:0';
+$_['error_3ds_error']					= 'crwdns99049:0crwdne99049:0';
+$_['error_3ds_skipped_by_buyer']		= 'crwdns99051:0crwdne99051:0';
+$_['error_3ds_failure']					= 'crwdns99053:0crwdne99053:0';
+$_['error_3ds_undefined']				= 'crwdns99055:0crwdne99055:0';
+$_['error_3ds_bypassed']				= 'crwdns99057:0crwdne99057:0';
+$_['error_3ds_unavailable']				= 'crwdns99059:0crwdne99059:0';
+$_['error_3ds_attempted']				= 'crwdns99061:0crwdne99061:0';
+$_['error_3ds_card_ineligible']			= 'crwdns99063:0crwdne99063:0';
+$_['error_payment']						= 'crwdns99065:0%scrwdne99065:0';
+$_['error_timeout'] 	  				= 'crwdns99067:0crwdne99067:0';
