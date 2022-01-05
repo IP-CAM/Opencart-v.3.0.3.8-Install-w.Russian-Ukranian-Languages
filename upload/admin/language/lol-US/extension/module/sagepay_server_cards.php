6bfcae69543c2d269ef4ce6b5218ba57a5310ca2
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:22 +0200

    New translations sagepay_server_cards.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/module/sagepay_server_cards.php
@@ -0,0 +1,13 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns85065:0crwdne85065:0';
+
+$_['text_extension']   = 'crwdns85067:0crwdne85067:0';
+$_['text_success']     = 'crwdns85069:0crwdne85069:0';
+$_['text_edit']        = 'crwdns85071:0crwdne85071:0';
+
+// Entry
+$_['entry_status']     = 'crwdns85073:0crwdne85073:0';
+
+// Error
+$_['error_permission'] = 'crwdns85075:0crwdne85075:0';
