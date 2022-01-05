3d49fcf944ff44cabe284b96b6da9cd090aaecce
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:05 +0200

    New translations user_group.php (LOLCAT)

---
+++ upload/admin/language/lol-US/user/user_group.php
@@ -0,0 +1,23 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns95687:0crwdne95687:0';
+
+// Text
+$_['text_success']     = 'crwdns95689:0crwdne95689:0';
+$_['text_list']        = 'crwdns95691:0crwdne95691:0';
+$_['text_add']         = 'crwdns95693:0crwdne95693:0';
+$_['text_edit']        = 'crwdns95695:0crwdne95695:0';
+
+// Column
+$_['column_name']      = 'crwdns95697:0crwdne95697:0';
+$_['column_action']    = 'crwdns95699:0crwdne95699:0';
+
+// Entry
+$_['entry_name']       = 'crwdns95701:0crwdne95701:0';
+$_['entry_access']     = 'crwdns95703:0crwdne95703:0';
+$_['entry_modify']     = 'crwdns95705:0crwdne95705:0';
+
+// Error
+$_['error_permission'] = 'crwdns95707:0crwdne95707:0';
+$_['error_name']       = 'crwdns95709:0crwdne95709:0';
+$_['error_user']       = 'crwdns95711:0%scrwdne95711:0';
