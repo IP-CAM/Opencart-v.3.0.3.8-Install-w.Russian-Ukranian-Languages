eb898d8e885b819bc1ec3ec080417e4fad7d2cb5
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:25:01 +0200

    New translations securetrading_pp.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/payment/securetrading_pp.php
@@ -0,0 +1,15 @@
+<?php
+
+$_['text_title'] = 'crwdns99585:0crwdne99585:0';
+$_['button_confirm'] = 'crwdns99587:0crwdne99587:0';
+
+$_['text_postcode_check'] = 'crwdns99589:0%scrwdne99589:0';
+$_['text_security_code_check'] = 'crwdns99591:0%scrwdne99591:0';
+$_['text_address_check'] = 'crwdns99593:0%scrwdne99593:0';
+$_['text_not_given'] = 'crwdns99595:0crwdne99595:0';
+$_['text_not_checked'] = 'crwdns99597:0crwdne99597:0';
+$_['text_match'] = 'crwdns99599:0crwdne99599:0';
+$_['text_not_match'] = 'crwdns99601:0crwdne99601:0';
+$_['text_payment_details'] = 'crwdns99603:0crwdne99603:0';
+
+$_['entry_card_type'] = 'crwdns99605:0crwdne99605:0';
