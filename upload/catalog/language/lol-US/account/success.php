091dc0145cd67bd8294297573aa13578cea7dc98
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:54 +0200

    New translations success.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/account/success.php
@@ -0,0 +1,9 @@
+<?php
+// Heading
+$_['heading_title'] = 'crwdns96337:0crwdne96337:0';
+
+// Text
+$_['text_message']  = 'crwdns96339:0%scrwdne96339:0';
+$_['text_approval'] = 'crwdns96341:0%scrwdnd96341:0%scrwdne96341:0';
+$_['text_account']  = 'crwdns96343:0crwdne96343:0';
+$_['text_success']  = 'crwdns96345:0crwdne96345:0';
