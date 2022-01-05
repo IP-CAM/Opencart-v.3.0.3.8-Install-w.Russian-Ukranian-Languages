9c09e635f584c5d8b02875891e417f789547e530
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:25:19 +0200

    New translations en-gb.php (LOLCAT)

---
+++ upload/install/language/lol-US/en-gb.php
@@ -0,0 +1,7 @@
+<?php
+// Button
+$_['button_continue'] = 'crwdns100731:0crwdne100731:0';
+$_['button_back']     = 'crwdns100733:0crwdne100733:0';
+
+// Error
+$_['error_exception'] = 'crwdns100735:0%scrwdnd100735:0%scrwdnd100735:0%scrwdnd100735:0%scrwdne100735:0';
