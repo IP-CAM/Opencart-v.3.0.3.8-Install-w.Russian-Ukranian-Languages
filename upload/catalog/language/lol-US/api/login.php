a9b00d9a83209bf3a6b4f46b14727e7c97733086
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:02 +0200

    New translations login.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/api/login.php
@@ -0,0 +1,7 @@
+<?php
+// Text
+$_['text_success'] = 'crwdns96615:0crwdne96615:0';
+
+// Error
+$_['error_key']    = 'crwdns96617:0crwdne96617:0';
+$_['error_ip']     = 'crwdns96619:0%scrwdne96619:0';
