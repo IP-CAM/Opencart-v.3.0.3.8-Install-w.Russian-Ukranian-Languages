11b2de2e51e4964c4ecb372e7eca95d0bb7f5c0b
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:10 +0200

    New translations banner.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/module/banner.php
@@ -0,0 +1,22 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns84661:0crwdne84661:0';
+
+// Text
+$_['text_extension']   = 'crwdns84663:0crwdne84663:0';
+$_['text_success']     = 'crwdns84665:0crwdne84665:0';
+$_['text_edit']        = 'crwdns84667:0crwdne84667:0';
+
+// Entry
+$_['entry_name']       = 'crwdns84669:0crwdne84669:0';
+$_['entry_banner']     = 'crwdns84671:0crwdne84671:0';
+$_['entry_dimension']  = 'crwdns84673:0crwdne84673:0';
+$_['entry_width']      = 'crwdns84675:0crwdne84675:0';
+$_['entry_height']     = 'crwdns84677:0crwdne84677:0';
+$_['entry_status']     = 'crwdns84679:0crwdne84679:0';
+
+// Error
+$_['error_permission'] = 'crwdns84681:0crwdne84681:0';
+$_['error_name']       = 'crwdns84683:0crwdne84683:0';
+$_['error_width']      = 'crwdns84685:0crwdne84685:0';
+$_['error_height']     = 'crwdns84687:0crwdne84687:0';
