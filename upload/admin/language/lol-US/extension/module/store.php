097437a90545c8dd513ee2b521a620b78cbd5e3b
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:24 +0200

    New translations store.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/module/store.php
@@ -0,0 +1,18 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns85129:0crwdne85129:0';
+
+// Text
+$_['text_extension']   = 'crwdns85131:0crwdne85131:0';
+$_['text_success']     = 'crwdns85133:0crwdne85133:0';
+$_['text_edit']        = 'crwdns85135:0crwdne85135:0';
+
+// Entry
+$_['entry_admin']      = 'crwdns85137:0crwdne85137:0';
+$_['entry_status']     = 'crwdns85139:0crwdne85139:0';
+
+//Help
+$_['help_admin']       = 'crwdns85141:0crwdne85141:0';
+
+// Error
+$_['error_permission'] = 'crwdns85143:0crwdne85143:0';
