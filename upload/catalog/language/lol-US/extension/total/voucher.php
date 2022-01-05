965d894b9a96fc377ab7cf353cffbe5cce8e8cff
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:48 +0200

    New translations voucher.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/total/voucher.php
@@ -0,0 +1,14 @@
+<?php
+// Heading
+$_['heading_title'] = 'crwdns100147:0crwdne100147:0';
+
+// Text
+$_['text_voucher']  = 'crwdns100149:0%scrwdne100149:0';
+$_['text_success']  = 'crwdns100151:0crwdne100151:0';
+
+// Entry
+$_['entry_voucher'] = 'crwdns100153:0crwdne100153:0';
+
+// Error
+$_['error_voucher'] = 'crwdns100155:0crwdne100155:0';
+$_['error_empty']   = 'crwdns100157:0crwdne100157:0';
