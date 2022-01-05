58c2192897e18091eb57c80e510d4a8083896e21
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:46 +0200

    New translations shipping.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/total/shipping.php
@@ -0,0 +1,20 @@
+<?php
+// Heading
+$_['heading_title']        = 'crwdns100119:0crwdne100119:0';
+
+// Text
+$_['text_success']         = 'crwdns100121:0crwdne100121:0';
+$_['text_shipping']        = 'crwdns100123:0crwdne100123:0';
+$_['text_shipping_method'] = 'crwdns100125:0crwdne100125:0';
+
+// Entry
+$_['entry_country']        = 'crwdns100127:0crwdne100127:0';
+$_['entry_zone']           = 'crwdns100129:0crwdne100129:0';
+$_['entry_postcode']       = 'crwdns100131:0crwdne100131:0';
+
+// Error
+$_['error_postcode']       = 'crwdns100133:0crwdne100133:0';
+$_['error_country']        = 'crwdns100135:0crwdne100135:0';
+$_['error_zone']           = 'crwdns100137:0crwdne100137:0';
+$_['error_shipping']       = 'crwdns100139:0crwdne100139:0';
+$_['error_no_shipping']    = 'crwdns100141:0%scrwdne100141:0';
