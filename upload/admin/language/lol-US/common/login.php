27f7e29f91ef1a6caf18703042c15055e0ac1929
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:20:48 +0200

    New translations login.php (LOLCAT)

---
+++ upload/admin/language/lol-US/common/login.php
@@ -0,0 +1,20 @@
+<?php
+// Heading
+$_['heading_title']  = 'crwdns81999:0crwdne81999:0';
+
+// Text
+$_['text_heading']   = 'crwdns82001:0crwdne82001:0';
+$_['text_login']     = 'crwdns82003:0crwdne82003:0';
+$_['text_forgotten'] = 'crwdns82005:0crwdne82005:0';
+
+// Entry
+$_['entry_username'] = 'crwdns82007:0crwdne82007:0';
+$_['entry_password'] = 'crwdns82009:0crwdne82009:0';
+
+// Button
+$_['button_login']   = 'crwdns82011:0crwdne82011:0';
+
+// Error
+$_['error_login']    = 'crwdns82013:0crwdne82013:0';
+$_['error_token']    = 'crwdns82015:0crwdne82015:0';
+$_['error_attempts'] = 'crwdns82017:0crwdne82017:0';
