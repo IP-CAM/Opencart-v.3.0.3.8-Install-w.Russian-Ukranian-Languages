e9f4e132151ce78918c885a29934ed7586fcb02e
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:48 +0200

    New translations password.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/account/password.php
@@ -0,0 +1,16 @@
+<?php
+// Heading
+$_['heading_title']  = 'crwdns96047:0crwdne96047:0';
+
+// Text
+$_['text_account']   = 'crwdns96049:0crwdne96049:0';
+$_['text_password']  = 'crwdns96051:0crwdne96051:0';
+$_['text_success']   = 'crwdns96053:0crwdne96053:0';
+
+// Entry
+$_['entry_password'] = 'crwdns96055:0crwdne96055:0';
+$_['entry_confirm']  = 'crwdns96057:0crwdne96057:0';
+
+// Error
+$_['error_password'] = 'crwdns96059:0crwdne96059:0';
+$_['error_confirm']  = 'crwdns96061:0crwdne96061:0';
