243b6e435371ad718a7c97518f0656d5bd99d934
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:11 +0200

    New translations modification.php (LOLCAT)

---
+++ upload/admin/language/lol-US/marketplace/modification.php
@@ -0,0 +1,19 @@
+<?php
+// Heading
+$_['heading_title']     = 'crwdns94293:0crwdne94293:0';
+
+// Text
+$_['text_success']      = 'crwdns94295:0crwdne94295:0';
+$_['text_refresh']      = 'crwdns94297:0crwdne94297:0';
+$_['text_list']         = 'crwdns94299:0crwdne94299:0';
+
+// Column
+$_['column_name']       = 'crwdns94301:0crwdne94301:0';
+$_['column_author']     = 'crwdns94303:0crwdne94303:0';
+$_['column_version']    = 'crwdns94305:0crwdne94305:0';
+$_['column_status']     = 'crwdns94307:0crwdne94307:0';
+$_['column_date_added'] = 'crwdns94309:0crwdne94309:0';
+$_['column_action']     = 'crwdns94311:0crwdne94311:0';
+
+// Error
+$_['error_permission']  = 'crwdns94313:0crwdne94313:0';
