5413975c01f88c415c9993617d4c567288ec969e
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:29 +0200

    New translations newsletter.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/account/newsletter.php
@@ -0,0 +1,11 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns95979:0crwdne95979:0';
+
+// Text
+$_['text_account']     = 'crwdns95981:0crwdne95981:0';
+$_['text_newsletter']  = 'crwdns95983:0crwdne95983:0';
+$_['text_success']     = 'crwdns95985:0crwdne95985:0';
+
+// Entry
+$_['entry_newsletter'] = 'crwdns95987:0crwdne95987:0';
