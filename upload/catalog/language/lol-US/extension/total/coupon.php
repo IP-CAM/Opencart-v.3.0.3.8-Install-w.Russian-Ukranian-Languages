631880aa4c4096325570b8d9d74c2a9a09893cf1
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:42 +0200

    New translations coupon.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/total/coupon.php
@@ -0,0 +1,14 @@
+<?php
+// Heading
+$_['heading_title'] = 'crwdns100081:0crwdne100081:0';
+
+// Text
+$_['text_coupon']   = 'crwdns100083:0%scrwdne100083:0';
+$_['text_success']  = 'crwdns100085:0crwdne100085:0';
+
+// Entry
+$_['entry_coupon']  = 'crwdns100087:0crwdne100087:0';
+
+// Error
+$_['error_coupon']  = 'crwdns100089:0crwdne100089:0';
+$_['error_empty']   = 'crwdns100091:0crwdne100091:0';
