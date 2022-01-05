d91380c108abfc584c478f45c4b7cde6074b6356
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:47 +0200

    New translations perpetual_payments.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/payment/perpetual_payments.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']		 = 'crwdns88335:0crwdne88335:0';
+
+// Text
+$_['text_extension']	 = 'crwdns88337:0crwdne88337:0';
+$_['text_success']		 = 'crwdns88339:0crwdne88339:0';
+$_['text_edit']          = 'crwdns88341:0crwdne88341:0';
+
+// Entry
+$_['entry_auth_id']		 = 'crwdns88343:0crwdne88343:0';
+$_['entry_auth_pass']	 = 'crwdns88345:0crwdne88345:0';
+$_['entry_test']		 = 'crwdns88347:0crwdne88347:0';
+$_['entry_total']		 = 'crwdns88349:0crwdne88349:0';
+$_['entry_order_status'] = 'crwdns88351:0crwdne88351:0';
+$_['entry_geo_zone']	 = 'crwdns88353:0crwdne88353:0';
+$_['entry_status']		 = 'crwdns88355:0crwdne88355:0';
+$_['entry_sort_order']	 = 'crwdns88357:0crwdne88357:0';
+
+// Help
+$_['help_test']			 = 'crwdns88359:0crwdne88359:0';
+$_['help_total']		 = 'crwdns88361:0crwdne88361:0';
+
+// Error
+$_['error_permission']	 = 'crwdns88363:0crwdne88363:0';
+$_['error_auth_id']		 = 'crwdns88365:0crwdne88365:0';
+$_['error_auth_pass']	 = 'crwdns88367:0crwdne88367:0';
