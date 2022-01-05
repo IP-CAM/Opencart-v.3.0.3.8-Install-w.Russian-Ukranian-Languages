344277d79e0c8eefc2d9f66a8250b84f0e37b721
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:38 +0200

    New translations twocheckout.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/payment/twocheckout.php
@@ -0,0 +1,29 @@
+<?php
+// Heading
+$_['heading_title']		 = 'crwdns91389:0crwdne91389:0';
+
+// Text
+$_['text_extension']	 = 'crwdns91391:0crwdne91391:0';
+$_['text_success']		 = 'crwdns91393:0crwdne91393:0';
+$_['text_edit']          = 'crwdns91395:0crwdne91395:0';
+$_['text_twocheckout']	 = 'crwdns91397:0crwdne91397:0';
+
+// Entry
+$_['entry_account']		 = 'crwdns91399:0crwdne91399:0';
+$_['entry_secret']		 = 'crwdns91401:0crwdne91401:0';
+$_['entry_display']		 = 'crwdns91403:0crwdne91403:0';
+$_['entry_test']		 = 'crwdns91405:0crwdne91405:0';
+$_['entry_total']		 = 'crwdns91407:0crwdne91407:0';
+$_['entry_order_status'] = 'crwdns91409:0crwdne91409:0';
+$_['entry_geo_zone']	 = 'crwdns91411:0crwdne91411:0';
+$_['entry_status']		 = 'crwdns91413:0crwdne91413:0';
+$_['entry_sort_order']	 = 'crwdns91415:0crwdne91415:0';
+
+// Help
+$_['help_secret']		 = 'crwdns91417:0crwdne91417:0';
+$_['help_total']		 = 'crwdns91419:0crwdne91419:0';
+
+// Error
+$_['error_permission']	 = 'crwdns91421:0crwdne91421:0';
+$_['error_account']		 = 'crwdns91423:0crwdne91423:0';
+$_['error_secret']		 = 'crwdns91425:0crwdne91425:0';
