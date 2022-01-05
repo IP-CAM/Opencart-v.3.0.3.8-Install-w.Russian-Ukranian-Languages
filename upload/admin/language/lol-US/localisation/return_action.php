a6e82ea75948d4b8a29a4cc2c971a7870f24b744
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:54 +0200

    New translations return_action.php (LOLCAT)

---
+++ upload/admin/language/lol-US/localisation/return_action.php
@@ -0,0 +1,21 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns93525:0crwdne93525:0';
+
+// Text
+$_['text_success']     = 'crwdns93527:0crwdne93527:0';
+$_['text_list']        = 'crwdns93529:0crwdne93529:0';
+$_['text_add']         = 'crwdns93531:0crwdne93531:0';
+$_['text_edit']        = 'crwdns93533:0crwdne93533:0';
+
+// Column
+$_['column_name']      = 'crwdns93535:0crwdne93535:0';
+$_['column_action']    = 'crwdns93537:0crwdne93537:0';
+
+// Entry
+$_['entry_name']       = 'crwdns93539:0crwdne93539:0';
+
+// Error
+$_['error_permission'] = 'crwdns93541:0crwdne93541:0';
+$_['error_name']       = 'crwdns93543:0crwdne93543:0';
+$_['error_return']     = 'crwdns93545:0%scrwdne93545:0';
