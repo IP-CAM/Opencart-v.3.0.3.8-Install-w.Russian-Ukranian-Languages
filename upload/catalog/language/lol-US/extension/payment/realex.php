e78a7b4152dff894ee9b18cdbfbb63dd4f48d7cb
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:57 +0200

    New translations realex.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/payment/realex.php
@@ -0,0 +1,48 @@
+<?php
+// Heading
+$_['text_title']				= 'crwdns99307:0crwdne99307:0';
+
+// Button
+$_['button_confirm']			= 'crwdns99309:0crwdne99309:0';
+
+// Entry
+$_['entry_cc_type']				= 'crwdns99311:0crwdne99311:0';
+
+// Text
+$_['text_success']				= 'crwdns99313:0crwdne99313:0';
+$_['text_decline']				= 'crwdns99315:0crwdne99315:0';
+$_['text_bank_error']			= 'crwdns99317:0crwdne99317:0';
+$_['text_generic_error']		= 'crwdns99319:0crwdne99319:0';
+$_['text_hash_failed']			= 'crwdns99321:0crwdne99321:0';
+$_['text_link']					= 'crwdns99323:0%scrwdne99323:0';
+$_['text_select_card']			= 'crwdns99325:0crwdne99325:0';
+$_['text_result']				= 'crwdns99327:0crwdne99327:0';
+$_['text_message']				= 'crwdns99329:0crwdne99329:0';
+$_['text_cvn_result']			= 'crwdns99331:0crwdne99331:0';
+$_['text_avs_postcode']			= 'crwdns99333:0crwdne99333:0';
+$_['text_avs_address']			= 'crwdns99335:0crwdne99335:0';
+$_['text_eci']					= 'crwdns99337:0crwdne99337:0';
+$_['text_tss']					= 'crwdns99339:0crwdne99339:0';
+$_['text_order_ref']			= 'crwdns99341:0crwdne99341:0';
+$_['text_timestamp']			= 'crwdns99343:0crwdne99343:0';
+$_['text_card_type']			= 'crwdns99345:0crwdne99345:0';
+$_['text_card_digits']			= 'crwdns99347:0crwdne99347:0';
+$_['text_card_exp']				= 'crwdns99349:0crwdne99349:0';
+$_['text_card_name']			= 'crwdns99351:0crwdne99351:0';
+$_['text_3d_s1']				= 'crwdns99353:0crwdne99353:0';
+$_['text_3d_s2']				= 'crwdns99355:0crwdne99355:0';
+$_['text_3d_s3']				= 'crwdns99357:0crwdne99357:0';
+$_['text_3d_s4']				= 'crwdns99359:0crwdne99359:0';
+$_['text_3d_s5']				= 'crwdns99361:0crwdne99361:0';
+$_['text_3d_s6']				= 'crwdns99363:0crwdne99363:0';
+$_['text_3d_s7']				= 'crwdns99365:0crwdne99365:0';
+$_['text_3d_s8']				= 'crwdns99367:0crwdne99367:0';
+$_['text_3d_s9']				= 'crwdns99369:0crwdne99369:0';
+$_['text_3d_s10']				= 'crwdns99371:0crwdne99371:0';
+$_['text_3d_liability']     	= 'crwdns99373:0crwdne99373:0';
+$_['text_card_visa']			= 'crwdns99375:0crwdne99375:0';
+$_['text_card_mc']				= 'crwdns99377:0crwdne99377:0';
+$_['text_card_amex']			= 'crwdns99379:0crwdne99379:0';
+$_['text_card_switch']			= 'crwdns99381:0crwdne99381:0';
+$_['text_card_laser']			= 'crwdns99383:0crwdne99383:0';
+$_['text_card_diners']			= 'crwdns99385:0crwdne99385:0';
