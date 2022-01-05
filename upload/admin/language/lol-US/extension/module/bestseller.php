d4faee59b3ae90c839c6405c0058cd54c56668cb
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:10 +0200

    New translations bestseller.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/module/bestseller.php
@@ -0,0 +1,22 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns84689:0crwdne84689:0';
+
+// Text
+$_['text_extension']   = 'crwdns84691:0crwdne84691:0';
+$_['text_success']     = 'crwdns84693:0crwdne84693:0';
+$_['text_edit']        = 'crwdns84695:0crwdne84695:0';
+
+// Entry
+$_['entry_name']       = 'crwdns84697:0crwdne84697:0';
+$_['entry_limit']      = 'crwdns84699:0crwdne84699:0';
+$_['entry_image']      = 'crwdns84701:0crwdne84701:0';
+$_['entry_width']      = 'crwdns84703:0crwdne84703:0';
+$_['entry_height']     = 'crwdns84705:0crwdne84705:0';
+$_['entry_status']     = 'crwdns84707:0crwdne84707:0';
+
+// Error
+$_['error_permission'] = 'crwdns84709:0crwdne84709:0';
+$_['error_name']       = 'crwdns84711:0crwdne84711:0';
+$_['error_width']      = 'crwdns84713:0crwdne84713:0';
+$_['error_height']     = 'crwdns84715:0crwdne84715:0';
