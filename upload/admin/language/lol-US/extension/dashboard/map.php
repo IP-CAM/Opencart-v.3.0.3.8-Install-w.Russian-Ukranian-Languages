1e4732f945e66d8bcb44dd28a3a5e15fd94e6820
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:07 +0200

    New translations map.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/dashboard/map.php
@@ -0,0 +1,18 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns83739:0crwdne83739:0';
+
+// Text
+$_['text_extension']   = 'crwdns83741:0crwdne83741:0';
+$_['text_success']     = 'crwdns83743:0crwdne83743:0';
+$_['text_edit']        = 'crwdns83745:0crwdne83745:0';
+$_['text_order']       = 'crwdns83747:0crwdne83747:0';
+$_['text_sale']        = 'crwdns83749:0crwdne83749:0';
+
+// Entry
+$_['entry_status']     = 'crwdns83751:0crwdne83751:0';
+$_['entry_sort_order'] = 'crwdns83753:0crwdne83753:0';
+$_['entry_width']      = 'crwdns83755:0crwdne83755:0';
+
+// Error
+$_['error_permission'] = 'crwdns83757:0crwdne83757:0';
