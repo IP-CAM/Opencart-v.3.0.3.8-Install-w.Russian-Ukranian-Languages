8d31d3f6e68e2280b04054f83021fff1ad2f9f7e
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:05 +0200

    New translations bank_transfer.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/payment/bank_transfer.php
@@ -0,0 +1,23 @@
+<?php
+// Heading
+$_['heading_title']      = 'crwdns85563:0crwdne85563:0';
+
+// Text
+$_['text_extension']     = 'crwdns85565:0crwdne85565:0';
+$_['text_success']       = 'crwdns85567:0crwdne85567:0';
+$_['text_edit']          = 'crwdns85569:0crwdne85569:0';
+
+// Entry
+$_['entry_bank']         = 'crwdns85571:0crwdne85571:0';
+$_['entry_total']        = 'crwdns85573:0crwdne85573:0';
+$_['entry_order_status'] = 'crwdns85575:0crwdne85575:0';
+$_['entry_geo_zone']     = 'crwdns85577:0crwdne85577:0';
+$_['entry_status']       = 'crwdns85579:0crwdne85579:0';
+$_['entry_sort_order']   = 'crwdns85581:0crwdne85581:0';
+
+// Help
+$_['help_total']         = 'crwdns85583:0crwdne85583:0'; 
+
+// Error 
+$_['error_permission']   = 'crwdns85585:0crwdne85585:0';
+$_['error_bank']         = 'crwdns85587:0crwdne85587:0';
