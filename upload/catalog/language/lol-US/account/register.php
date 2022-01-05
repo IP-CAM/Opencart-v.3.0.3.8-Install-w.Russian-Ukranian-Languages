0bef1b19643aac1f877f4fe8ad4fc3ac741b4758
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:50 +0200

    New translations register.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/account/register.php
@@ -0,0 +1,33 @@
+<?php
+// Heading
+$_['heading_title']        = 'crwdns96147:0crwdne96147:0';
+
+// Text
+$_['text_account']         = 'crwdns96149:0crwdne96149:0';
+$_['text_register']        = 'crwdns96151:0crwdne96151:0';
+$_['text_account_already'] = 'crwdns96153:0%scrwdne96153:0';
+$_['text_your_details']    = 'crwdns96155:0crwdne96155:0';
+$_['text_newsletter']      = 'crwdns96157:0crwdne96157:0';
+$_['text_your_password']   = 'crwdns96159:0crwdne96159:0';
+$_['text_agree']           = 'crwdns96161:0%scrwdnd96161:0%scrwdne96161:0';
+
+// Entry
+$_['entry_customer_group'] = 'crwdns96163:0crwdne96163:0';
+$_['entry_firstname']      = 'crwdns96165:0crwdne96165:0';
+$_['entry_lastname']       = 'crwdns96167:0crwdne96167:0';
+$_['entry_email']          = 'crwdns96169:0crwdne96169:0';
+$_['entry_telephone']      = 'crwdns96171:0crwdne96171:0';
+$_['entry_newsletter']     = 'crwdns96173:0crwdne96173:0';
+$_['entry_password']       = 'crwdns96175:0crwdne96175:0';
+$_['entry_confirm']        = 'crwdns96177:0crwdne96177:0';
+
+// Error
+$_['error_exists']         = 'crwdns96179:0crwdne96179:0';
+$_['error_firstname']      = 'crwdns96181:0crwdne96181:0';
+$_['error_lastname']       = 'crwdns96183:0crwdne96183:0';
+$_['error_email']          = 'crwdns96185:0crwdne96185:0';
+$_['error_telephone']      = 'crwdns96187:0crwdne96187:0';
+$_['error_custom_field']   = 'crwdns96189:0%scrwdne96189:0';
+$_['error_password']       = 'crwdns96191:0crwdne96191:0';
+$_['error_confirm']        = 'crwdns96193:0crwdne96193:0';
+$_['error_agree']          = 'crwdns96195:0%scrwdne96195:0';
