19fa916b14439d4889a15aa3a2eebdce6eefc458
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:10 +0200

    New translations klarna_account.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/payment/klarna_account.php
@@ -0,0 +1,39 @@
+<?php
+// Text
+$_['text_title']				= 'crwdns98821:0%scrwdne98821:0';
+$_['text_terms']				= 'crwdns98823:0%scrwdnd98823:0%scrwdne98823:0';
+$_['text_information']			= 'crwdns98825:0crwdne98825:0';
+$_['text_additional']			= 'crwdns98827:0crwdne98827:0';
+$_['text_male']					= 'crwdns98829:0crwdne98829:0';
+$_['text_female']				= 'crwdns98831:0crwdne98831:0';
+$_['text_year']					= 'crwdns98833:0crwdne98833:0';
+$_['text_month']				= 'crwdns98835:0crwdne98835:0';
+$_['text_day']					= 'crwdns98837:0crwdne98837:0';
+$_['text_payment_option']		= 'crwdns98839:0crwdne98839:0';
+$_['text_single_payment']		= 'crwdns98841:0crwdne98841:0';
+$_['text_monthly_payment']		= 'crwdns98843:0%scrwdnd98843:0%scrwdne98843:0';
+$_['text_comment']				= 'crwdns98845:0%scrwdne98845:0' . "crwdns98847:0crwdne98847:0" . 'crwdns98849:0%scrwdnd98849:0%scrwdnd98849:0%.4fcrwdne98849:0';
+$_['text_terms_description']    = 'crwdns98851:0crwdne98851:0';
+
+// Entry
+$_['entry_gender']				= 'crwdns98853:0crwdne98853:0';
+$_['entry_pno']					= 'crwdns98855:0crwdne98855:0';
+$_['entry_dob']					= 'crwdns98857:0crwdne98857:0';
+$_['entry_phone_no']			= 'crwdns98859:0crwdne98859:0';
+$_['entry_street']				= 'crwdns98861:0crwdne98861:0';
+$_['entry_house_no']			= 'crwdns98863:0crwdne98863:0';
+$_['entry_house_ext']			= 'crwdns98865:0crwdne98865:0';
+$_['entry_company']				= 'crwdns98867:0crwdne98867:0';
+
+// Help
+$_['help_pno']					= 'crwdns98869:0crwdne98869:0';
+$_['help_phone_no']				= 'crwdns98871:0crwdne98871:0';
+$_['help_street']				= 'crwdns98873:0crwdne98873:0';
+$_['help_house_no']				= 'crwdns98875:0crwdne98875:0';
+$_['help_house_ext']			= 'crwdns98877:0crwdne98877:0';
+$_['help_company']				= 'crwdns98879:0crwdne98879:0';
+
+// Error
+$_['error_deu_terms']			= 'crwdns98881:0crwdne98881:0';
+$_['error_address_match']		= 'crwdns98883:0crwdne98883:0';
+$_['error_network']				= 'crwdns98885:0crwdne98885:0';
