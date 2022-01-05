09d6fcb04f846abcd961e61342e6a01da14ec7ab
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:25:20 +0200

    New translations column_left.php (LOLCAT)

---
+++ upload/install/language/lol-US/common/column_left.php
@@ -0,0 +1,8 @@
+<?php
+// Text
+$_['text_license']       = 'crwdns100713:0crwdne100713:0';
+$_['text_installation']  = 'crwdns100715:0crwdne100715:0';
+$_['text_configuration'] = 'crwdns100717:0crwdne100717:0';
+$_['text_upgrade']       = 'crwdns100719:0crwdne100719:0';
+$_['text_finished']      = 'crwdns100721:0crwdne100721:0';
+$_['text_language']      = 'crwdns100723:0crwdne100723:0';
