adedd5ccc08d54762f4f906f184f4dc4871830e0
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:12 +0200

    New translations nochex.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/payment/nochex.php
@@ -0,0 +1,31 @@
+<?php
+// Heading
+$_['heading_title']		 = 'crwdns87935:0crwdne87935:0';
+
+// Text
+$_['text_extension']	 = 'crwdns87937:0crwdne87937:0';
+$_['text_success']		 = 'crwdns87939:0crwdne87939:0';
+$_['text_edit']          = 'crwdns87941:0crwdne87941:0';
+$_['text_nochex']		 = 'crwdns87943:0crwdne87943:0';
+$_['text_seller']		 = 'crwdns87945:0crwdne87945:0';
+$_['text_merchant']		 = 'crwdns87947:0crwdne87947:0';
+
+// Entry
+$_['entry_email']		 = 'crwdns87949:0crwdne87949:0';
+$_['entry_account']		 = 'crwdns87951:0crwdne87951:0';
+$_['entry_merchant']	 = 'crwdns87953:0crwdne87953:0';
+$_['entry_template']	 = 'crwdns87955:0crwdne87955:0';
+$_['entry_test']		 = 'crwdns87957:0crwdne87957:0';
+$_['entry_total']		 = 'crwdns87959:0crwdne87959:0';
+$_['entry_order_status'] = 'crwdns87961:0crwdne87961:0';
+$_['entry_geo_zone']     = 'crwdns87963:0crwdne87963:0';
+$_['entry_status']	     = 'crwdns87965:0crwdne87965:0';
+$_['entry_sort_order']   = 'crwdns87967:0crwdne87967:0';
+
+// Help
+$_['help_total']	     = 'crwdns87969:0crwdne87969:0';
+
+// Error
+$_['error_permission']   = 'crwdns87971:0crwdne87971:0';
+$_['error_email']	     = 'crwdns87973:0crwdne87973:0';
+$_['error_merchant']     = 'crwdns87975:0crwdne87975:0';
