1e46a1e392a7c4832c36dfd08e9d4d639affdfb8
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:36 +0200

    New translations cardconnect.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/payment/cardconnect.php
@@ -0,0 +1,44 @@
+<?php
+// Text
+$_['text_title']            = 'crwdns97881:0crwdne97881:0';
+$_['text_card_details']     = 'crwdns97883:0crwdne97883:0';
+$_['text_echeck_details']   = 'crwdns97885:0crwdne97885:0';
+$_['text_card']             = 'crwdns97887:0crwdne97887:0';
+$_['text_echeck']           = 'crwdns97889:0crwdne97889:0';
+$_['text_wait']             = 'crwdns97891:0crwdne97891:0';
+$_['text_confirm_delete']   = 'crwdns97893:0crwdne97893:0';
+$_['text_no_cards']         = 'crwdns97895:0crwdne97895:0';
+$_['text_select_card']      = 'crwdns97897:0crwdne97897:0';
+
+// Entry
+$_['entry_method']          = 'crwdns97899:0crwdne97899:0';
+$_['entry_card_new_or_old'] = 'crwdns97901:0crwdne97901:0';
+$_['entry_card_new']        = 'crwdns97903:0crwdne97903:0';
+$_['entry_card_old']        = 'crwdns97905:0crwdne97905:0';
+$_['entry_card_type']       = 'crwdns97907:0crwdne97907:0';
+$_['entry_card_number']     = 'crwdns97909:0crwdne97909:0';
+$_['entry_card_expiry']     = 'crwdns97911:0crwdne97911:0';
+$_['entry_card_cvv2']       = 'crwdns97913:0crwdne97913:0';
+$_['entry_card_save']       = 'crwdns97915:0crwdne97915:0';
+$_['entry_card_choice']     = 'crwdns97917:0crwdne97917:0';
+$_['entry_account_number']  = 'crwdns97919:0crwdne97919:0';
+$_['entry_routing_number']  = 'crwdns97921:0crwdne97921:0';
+
+// Button
+$_['button_confirm']        = 'crwdns97923:0crwdne97923:0';
+$_['button_delete']         = 'crwdns97925:0crwdne97925:0';
+
+// Error
+$_['error_card_number']     = 'crwdns97927:0crwdne97927:0';
+$_['error_card_type']       = 'crwdns97929:0crwdne97929:0';
+$_['error_card_cvv2']       = 'crwdns97931:0crwdne97931:0';
+$_['error_data_missing']    = 'crwdns97933:0crwdne97933:0';
+$_['error_not_logged_in']   = 'crwdns97935:0crwdne97935:0';
+$_['error_no_order']        = 'crwdns97937:0crwdne97937:0';
+$_['error_no_post_data']    = 'crwdns97939:0$_POSTcrwdne97939:0';
+$_['error_select_card']     = 'crwdns97941:0crwdne97941:0';
+$_['error_no_card']         = 'crwdns97943:0crwdne97943:0';
+$_['error_no_echeck']       = 'crwdns97945:0crwdne97945:0';
+$_['error_account_number']  = 'crwdns97947:0crwdne97947:0';
+$_['error_routing_number']  = 'crwdns97949:0crwdne97949:0';
+$_['error_not_enabled']     = 'crwdns97951:0crwdne97951:0';
