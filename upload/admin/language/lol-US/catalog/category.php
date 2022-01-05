18142e17d5d4422705cd3ad8f4576b25b9f9488a
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:20:56 +0200

    New translations category.php (LOLCAT)

---
+++ upload/admin/language/lol-US/catalog/category.php
@@ -0,0 +1,47 @@
+<?php
+// Heading
+$_['heading_title']          = 'crwdns81117:0crwdne81117:0';
+
+// Text
+$_['text_success']           = 'crwdns81119:0crwdne81119:0';
+$_['text_list']              = 'crwdns81121:0crwdne81121:0';
+$_['text_add']               = 'crwdns81123:0crwdne81123:0';
+$_['text_edit']              = 'crwdns81125:0crwdne81125:0';
+$_['text_default']           = 'crwdns81127:0crwdne81127:0';
+$_['text_keyword']           = 'crwdns81129:0crwdne81129:0';
+
+// Column
+$_['column_name']            = 'crwdns81131:0crwdne81131:0';
+$_['column_sort_order']      = 'crwdns81133:0crwdne81133:0';
+$_['column_action']          = 'crwdns81135:0crwdne81135:0';
+
+// Entry
+$_['entry_name']             = 'crwdns81137:0crwdne81137:0';
+$_['entry_description']      = 'crwdns81139:0crwdne81139:0';
+$_['entry_meta_title'] 	     = 'crwdns81141:0crwdne81141:0';
+$_['entry_meta_keyword']     = 'crwdns81143:0crwdne81143:0';
+$_['entry_meta_description'] = 'crwdns81145:0crwdne81145:0';
+$_['entry_store']            = 'crwdns81147:0crwdne81147:0';
+$_['entry_keyword']          = 'crwdns81149:0crwdne81149:0';
+$_['entry_parent']           = 'crwdns81151:0crwdne81151:0';
+$_['entry_filter']           = 'crwdns81153:0crwdne81153:0';
+$_['entry_image']            = 'crwdns81155:0crwdne81155:0';
+$_['entry_top']              = 'crwdns81157:0crwdne81157:0';
+$_['entry_column']           = 'crwdns81159:0crwdne81159:0';
+$_['entry_sort_order']       = 'crwdns81161:0crwdne81161:0';
+$_['entry_status']           = 'crwdns81163:0crwdne81163:0';
+$_['entry_layout']           = 'crwdns81165:0crwdne81165:0';
+
+// Help
+$_['help_filter']            = 'crwdns81167:0crwdne81167:0';
+$_['help_top']               = 'crwdns81169:0crwdne81169:0';
+$_['help_column']            = 'crwdns81171:0crwdne81171:0';
+
+// Error
+$_['error_warning']          = 'crwdns81173:0crwdne81173:0';
+$_['error_permission']       = 'crwdns81175:0crwdne81175:0';
+$_['error_name']             = 'crwdns81177:0crwdne81177:0';
+$_['error_meta_title']       = 'crwdns81179:0crwdne81179:0';
+$_['error_keyword']          = 'crwdns81181:0crwdne81181:0';
+$_['error_unique']           = 'crwdns81183:0crwdne81183:0';
+$_['error_parent']           = 'crwdns81185:0crwdne81185:0';
