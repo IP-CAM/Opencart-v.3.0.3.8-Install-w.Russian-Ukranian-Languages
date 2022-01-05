536579eaf53769cf8a25ce232d1ddbc2c0b59054
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:40 +0200

    New translations wechat_pay.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/payment/wechat_pay.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @package		OpenCart
+ * @author		Meng Wenbin
+ * @copyright	Copyright (c) 2010 - 2017, Chengdu Guangda Network Technology Co. Ltd. (https://www.opencart.cn/)
+ * @license		https://opensource.org/licenses/GPL-3.0
+ * @link		https://www.opencart.cn
+ */
+
+// Heading
+$_['heading_title']                  = 'crwdns91471:0crwdne91471:0';
+
+// Text
+$_['text_extension']                 = 'crwdns91473:0crwdne91473:0';
+$_['text_success']                   = 'crwdns91475:0crwdne91475:0';
+$_['text_edit']                      = 'crwdns91477:0crwdne91477:0';
+$_['text_wechat_pay']                = 'crwdns91479:0crwdne91479:0';
+
+// Entry
+$_['entry_app_id']                   = 'crwdns91481:0crwdne91481:0';
+$_['entry_app_secret']               = 'crwdns91483:0crwdne91483:0';
+$_['entry_mch_id']                   = 'crwdns91485:0crwdne91485:0';
+$_['entry_api_secret']               = 'crwdns91487:0crwdne91487:0';
+$_['entry_debug']                    = 'crwdns91489:0crwdne91489:0';
+$_['entry_total']                    = 'crwdns91491:0crwdne91491:0';
+$_['entry_currency']                 = 'crwdns91493:0crwdne91493:0';
+$_['entry_completed_status']         = 'crwdns91495:0crwdne91495:0';
+$_['entry_geo_zone']                 = 'crwdns91497:0crwdne91497:0';
+$_['entry_status']                   = 'crwdns91499:0crwdne91499:0';
+$_['entry_sort_order']               = 'crwdns91501:0crwdne91501:0';
+
+// Help
+$_['help_total']                     = 'crwdns91503:0crwdne91503:0';
+$_['help_currency']                  = 'crwdns91505:0crwdne91505:0';
+$_['help_wechat_pay_setup']          = 'crwdns91507:0crwdne91507:0';
+
+// Error
+$_['error_permission']               = 'crwdns91509:0crwdne91509:0';
+$_['error_app_id']                   = 'crwdns91511:0crwdne91511:0';
+$_['error_app_secret']               = 'crwdns91513:0crwdne91513:0';
+$_['error_mch_id']                   = 'crwdns91515:0crwdne91515:0';
+$_['error_api_secret']               = 'crwdns91517:0crwdne91517:0';
