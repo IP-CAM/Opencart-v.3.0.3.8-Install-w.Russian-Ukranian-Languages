ab9930675e9264a0332e2dce989b625a5c0c6774
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:25:27 +0200

    New translations upgrade.php (LOLCAT)

---
+++ upload/install/language/lol-US/upgrade/upgrade.php
@@ -0,0 +1,20 @@
+<?php
+// Heading
+$_['heading_title']   = 'crwdns100959:0crwdne100959:0';
+$_['heading_success'] = 'crwdns100961:0crwdne100961:0';
+
+// Text
+$_['text_upgrade']    = 'crwdns100963:0crwdne100963:0';
+$_['text_server']     = 'crwdns100965:0crwdne100965:0';
+$_['text_steps']      = 'crwdns100967:0crwdne100967:0';
+$_['text_error']      = 'crwdns100969:0crwdne100969:0';
+$_['text_clear']      = 'crwdns100971:0crwdne100971:0';
+$_['text_admin']      = 'crwdns100973:0crwdne100973:0';
+$_['text_user']       = 'crwdns100975:0crwdne100975:0';
+$_['text_setting']    = 'crwdns100977:0crwdne100977:0';
+$_['text_store']      = 'crwdns100979:0crwdne100979:0';
+$_['text_progress']   = 'crwdns100981:0%scrwdnd100981:0%scrwdnd100981:0%scrwdne100981:0';
+$_['text_success']    = 'crwdns100983:0crwdne100983:0';
+
+// Entry
+$_['entry_progress']  = 'crwdns100985:0crwdne100985:0';
