d6a18b857dade81b5718eecc4c8212bf136fbd98
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:46 +0200

    New translations sale_coupon.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/report/sale_coupon.php
@@ -0,0 +1,25 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns91947:0crwdne91947:0';
+
+// Text
+$_['text_extension']   = 'crwdns91949:0crwdne91949:0';
+$_['text_edit']        = 'crwdns91951:0crwdne91951:0';
+$_['text_success']     = 'crwdns91953:0crwdne91953:0';
+$_['text_filter']      = 'crwdns91955:0crwdne91955:0';
+
+// Column
+$_['column_name']      = 'crwdns91957:0crwdne91957:0';
+$_['column_code']      = 'crwdns91959:0crwdne91959:0';
+$_['column_orders']    = 'crwdns91961:0crwdne91961:0';
+$_['column_total']     = 'crwdns91963:0crwdne91963:0';
+$_['column_action']    = 'crwdns91965:0crwdne91965:0';
+
+// Entry
+$_['entry_date_start'] = 'crwdns91967:0crwdne91967:0';
+$_['entry_date_end']   = 'crwdns91969:0crwdne91969:0';
+$_['entry_status']     = 'crwdns91971:0crwdne91971:0';
+$_['entry_sort_order'] = 'crwdns91973:0crwdne91973:0';
+
+// Error
+$_['error_permission']  = 'crwdns91975:0crwdne91975:0';
