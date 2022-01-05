be4bb1e4f071c8cc8c0701c8fe5959008283e1cb
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:16 +0200

    New translations paypoint.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/payment/paypoint.php
@@ -0,0 +1,11 @@
+<?php
+// Heading
+$_['heading_title']				= 'crwdns99069:0%scrwdne99069:0';
+
+// Text
+$_['text_title']				= 'crwdns99071:0crwdne99071:0';
+$_['text_response']				= 'crwdns99073:0crwdne99073:0';
+$_['text_success']				= 'crwdns99075:0crwdne99075:0';
+$_['text_success_wait']			= 'crwdns99077:0%scrwdne99077:0';
+$_['text_failure']				= 'crwdns99079:0crwdne99079:0';
+$_['text_failure_wait']			= 'crwdns99081:0%scrwdne99081:0';
