5b919125fac7c10a719a0e4369ed11d11b1d0074
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:15 +0200

    New translations google_hangouts.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/module/google_hangouts.php
@@ -0,0 +1,19 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns84809:0crwdne84809:0';
+
+// Text
+$_['text_extension']   = 'crwdns84811:0crwdne84811:0';
+$_['text_success']     = 'crwdns84813:0crwdne84813:0';
+$_['text_edit']        = 'crwdns84815:0crwdne84815:0';
+
+// Entry
+$_['entry_code']       = 'crwdns84817:0crwdne84817:0';
+$_['entry_status']     = 'crwdns84819:0crwdne84819:0';
+
+// Help
+$_['help_code']        = 'crwdns84821:0crwdne84821:0';
+
+// Error
+$_['error_permission'] = 'crwdns84823:0crwdne84823:0';
+$_['error_code']       = 'crwdns84825:0crwdne84825:0';
