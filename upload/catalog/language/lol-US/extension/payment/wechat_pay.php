7fa6e5e9388fa2133bdae6a4b95137b6523d9671
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:25:05 +0200

    New translations wechat_pay.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/payment/wechat_pay.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package		OpenCart
+ * @author		Meng Wenbin
+ * @copyright	Copyright (c) 2010 - 2017, Chengdu Guangda Network Technology Co. Ltd. (https://www.opencart.cn/)
+ * @license		https://opensource.org/licenses/GPL-3.0
+ * @link		https://www.opencart.cn
+ */
+
+// Heading
+$_['heading_title']              = 'crwdns99767:0crwdne99767:0';
+
+// Text
+$_['text_title']                 = 'crwdns99769:0crwdne99769:0';
+$_['text_checkout']              = 'crwdns99771:0crwdne99771:0';
+$_['text_qrcode']                = 'crwdns99773:0crwdne99773:0';
+$_['text_qrcode_description']    = 'crwdns99775:0crwdne99775:0';
