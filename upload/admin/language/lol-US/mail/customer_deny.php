4936aef629a9d0f9431f2dcdd3b13204931e9749
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:19 +0200

    New translations customer_deny.php (LOLCAT)

---
+++ upload/admin/language/lol-US/mail/customer_deny.php
@@ -0,0 +1,6 @@
+<?php
+// Text
+$_['text_subject'] = 'crwdns93809:0%scrwdne93809:0';
+$_['text_welcome'] = 'crwdns93811:0%scrwdne93811:0';
+$_['text_denied']  = 'crwdns93813:0crwdne93813:0';
+$_['text_thanks']  = 'crwdns93815:0crwdne93815:0';
