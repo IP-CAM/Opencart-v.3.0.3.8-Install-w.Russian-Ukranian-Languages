fef4c5f6412229a38e67ddd412770514ae9a9055
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:32 +0200

    New translations feed.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/extension/feed.php
@@ -0,0 +1,15 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns83899:0crwdne83899:0';
+
+// Text
+$_['text_success']     = 'crwdns83901:0crwdne83901:0';
+$_['text_list']        = 'crwdns83903:0crwdne83903:0';
+
+// Column
+$_['column_name']      = 'crwdns83905:0crwdne83905:0';
+$_['column_status']    = 'crwdns83907:0crwdne83907:0';
+$_['column_action']    = 'crwdns83909:0crwdne83909:0';
+
+// Error
+$_['error_permission'] = 'crwdns83911:0crwdne83911:0';
