dfc84673ce4de029a1187f652f7d36be6f690dcc
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:25:10 +0200

    New translations ec_ship.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/shipping/ec_ship.php
@@ -0,0 +1,19 @@
+<?php
+// Text
+$_['text_title']                               = 'crwdns99843:0crwdne99843:0';
+$_['text_weight']                              = 'crwdns99845:0crwdne99845:0';
+$_['text_air_registered_mail']                 = 'crwdns99847:0crwdne99847:0';
+$_['text_air_parcel']                          = 'crwdns99849:0crwdne99849:0';
+$_['text_e_express_service_to_us']             = 'crwdns99851:0crwdne99851:0';
+$_['text_e_express_service_to_canada']         = 'crwdns99853:0crwdne99853:0';
+$_['text_e_express_service_to_united_kingdom'] = 'crwdns99855:0crwdne99855:0';
+$_['text_e_express_service_to_russia']         = 'crwdns99857:0crwdne99857:0';
+$_['text_e_express_service_one']               = 'crwdns99859:0crwdne99859:0';
+$_['text_e_express_service_two']               = 'crwdns99861:0crwdne99861:0';
+$_['text_speed_post']                          = 'crwdns99863:0crwdne99863:0';
+$_['text_smart_post']                          = 'crwdns99865:0crwdne99865:0';
+$_['text_local_courier_post']                  = 'crwdns99867:0crwdne99867:0';
+$_['text_local_parcel']                        = 'crwdns99869:0crwdne99869:0';
+
+//error
+$_['text_unavailable']                         = 'crwdns99871:0crwdne99871:0';
