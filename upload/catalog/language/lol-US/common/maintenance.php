6ef5800aad54091dfaa9926cbccf02c6ef4bc9e5
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:36 +0200

    New translations maintenance.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/common/maintenance.php
@@ -0,0 +1,7 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns97055:0crwdne97055:0';
+
+// Text
+$_['text_maintenance'] = 'crwdns97057:0crwdne97057:0';
+$_['text_message']     = 'crwdns97059:0crwdne97059:0';
