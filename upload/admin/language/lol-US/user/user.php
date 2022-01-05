d11b1ef6abed923b7cf836c64461c63d5c43796a
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:04 +0200

    New translations user.php (LOLCAT)

---
+++ upload/admin/language/lol-US/user/user.php
@@ -0,0 +1,38 @@
+<?php
+// Heading
+$_['heading_title']         = 'crwdns95631:0crwdne95631:0';
+
+// Text
+$_['text_success']          = 'crwdns95633:0crwdne95633:0';
+$_['text_list']             = 'crwdns95635:0crwdne95635:0';
+$_['text_add']              = 'crwdns95637:0crwdne95637:0';
+$_['text_edit']             = 'crwdns95639:0crwdne95639:0';
+
+// Column
+$_['column_username']       = 'crwdns95641:0crwdne95641:0';
+$_['column_status']         = 'crwdns95643:0crwdne95643:0';
+$_['column_date_added']     = 'crwdns95645:0crwdne95645:0';
+$_['column_action']         = 'crwdns95647:0crwdne95647:0';
+
+// Entry
+$_['entry_username']        = 'crwdns95649:0crwdne95649:0';
+$_['entry_user_group']      = 'crwdns95651:0crwdne95651:0';
+$_['entry_password']        = 'crwdns95653:0crwdne95653:0';
+$_['entry_confirm']         = 'crwdns95655:0crwdne95655:0';
+$_['entry_firstname']       = 'crwdns95657:0crwdne95657:0';
+$_['entry_lastname']        = 'crwdns95659:0crwdne95659:0';
+$_['entry_email']           = 'crwdns95661:0crwdne95661:0';
+$_['entry_image']           = 'crwdns95663:0crwdne95663:0';
+$_['entry_status']          = 'crwdns95665:0crwdne95665:0';
+
+// Error
+$_['error_permission']      = 'crwdns95667:0crwdne95667:0';
+$_['error_account']         = 'crwdns95669:0crwdne95669:0';
+$_['error_exists_username'] = 'crwdns95671:0crwdne95671:0';
+$_['error_username']        = 'crwdns95673:0crwdne95673:0';
+$_['error_password']        = 'crwdns95675:0crwdne95675:0';
+$_['error_confirm']         = 'crwdns95677:0crwdne95677:0';
+$_['error_firstname']       = 'crwdns95679:0crwdne95679:0';
+$_['error_lastname']        = 'crwdns95681:0crwdne95681:0';
+$_['error_email']           = 'crwdns95683:0crwdne95683:0';
+$_['error_exists_email']    = 'crwdns95685:0crwdne95685:0';
