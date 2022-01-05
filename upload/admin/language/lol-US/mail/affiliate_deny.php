7c79b986eb445ca485e8a36030561cfe456d5eda
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:17 +0200

    New translations affiliate_deny.php (LOLCAT)

---
+++ upload/admin/language/lol-US/mail/affiliate_deny.php
@@ -0,0 +1,6 @@
+<?php
+// Text
+$_['text_subject'] = 'crwdns93791:0%scrwdne93791:0';
+$_['text_welcome'] = 'crwdns93793:0%scrwdne93793:0';
+$_['text_denied']  = 'crwdns93795:0crwdne93795:0';
+$_['text_thanks']  = 'crwdns93797:0crwdne93797:0';
