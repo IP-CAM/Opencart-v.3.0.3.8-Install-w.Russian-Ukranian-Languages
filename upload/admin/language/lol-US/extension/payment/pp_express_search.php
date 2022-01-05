408c04c9f68bafbcb54b35af02234151af28ff0f
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:51 +0200

    New translations pp_express_search.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/payment/pp_express_search.php
@@ -0,0 +1,73 @@
+<?php
+// Heading
+$_['heading_title']			   = 'crwdns89093:0crwdne89093:0';
+
+// Text
+$_['text_pp_express']		   = 'crwdns89095:0crwdne89095:0';
+$_['text_date_search']		   = 'crwdns89097:0crwdne89097:0';
+$_['text_searching']		   = 'crwdns89099:0crwdne89099:0';
+$_['text_name']				   = 'crwdns89101:0crwdne89101:0';
+$_['text_buyer_info']		   = 'crwdns89103:0crwdne89103:0';
+$_['text_view']				   = 'crwdns89105:0crwdne89105:0';
+$_['text_format']			   = 'crwdns89107:0crwdne89107:0';
+
+// Column
+$_['column_date']			   = 'crwdns89109:0crwdne89109:0';
+$_['column_type']			   = 'crwdns89111:0crwdne89111:0';
+$_['column_email']			   = 'crwdns89113:0crwdne89113:0';
+$_['column_name']			   = 'crwdns89115:0crwdne89115:0';
+$_['column_transid']		   = 'crwdns89117:0crwdne89117:0';
+$_['column_status']			   = 'crwdns89119:0crwdne89119:0';
+$_['column_currency']		   = 'crwdns89121:0crwdne89121:0';
+$_['column_amount']			   = 'crwdns89123:0crwdne89123:0';
+$_['column_fee']			   = 'crwdns89125:0crwdne89125:0';
+$_['column_netamt']		       = 'crwdns89127:0crwdne89127:0';
+$_['column_action']		       = 'crwdns89129:0crwdne89129:0';
+
+// Entry
+$_['entry_trans_all']		   = 'crwdns89131:0crwdne89131:0';
+$_['entry_trans_sent']		   = 'crwdns89133:0crwdne89133:0';
+$_['entry_trans_received']     = 'crwdns89135:0crwdne89135:0';
+$_['entry_trans_masspay']	   = 'crwdns89137:0crwdne89137:0';
+$_['entry_trans_money_req']	   = 'crwdns89139:0crwdne89139:0';
+$_['entry_trans_funds_add']	   = 'crwdns89141:0crwdne89141:0';
+$_['entry_trans_funds_with']   = 'crwdns89143:0crwdne89143:0';
+$_['entry_trans_referral']	   = 'crwdns89145:0crwdne89145:0';
+$_['entry_trans_fee']		   = 'crwdns89147:0crwdne89147:0';
+$_['entry_trans_subscription'] = 'crwdns89149:0crwdne89149:0';
+$_['entry_trans_dividend']     = 'crwdns89151:0crwdne89151:0';
+$_['entry_trans_billpay']      = 'crwdns89153:0crwdne89153:0';
+$_['entry_trans_refund']       = 'crwdns89155:0crwdne89155:0';
+$_['entry_trans_conv']         = 'crwdns89157:0crwdne89157:0';
+$_['entry_trans_bal_trans']	   = 'crwdns89159:0crwdne89159:0';
+$_['entry_trans_reversal']	   = 'crwdns89161:0crwdne89161:0';
+$_['entry_trans_shipping']	   = 'crwdns89163:0crwdne89163:0';
+$_['entry_trans_bal_affect']   = 'crwdns89165:0crwdne89165:0';
+$_['entry_trans_echeque']	   = 'crwdns89167:0crwdne89167:0';
+$_['entry_date']			   = 'crwdns89169:0crwdne89169:0';
+$_['entry_date_start']		   = 'crwdns89171:0crwdne89171:0';
+$_['entry_date_end']		   = 'crwdns89173:0crwdne89173:0';
+$_['entry_date_to']			   = 'crwdns89175:0crwdne89175:0';
+$_['entry_transaction']		   = 'crwdns89177:0crwdne89177:0';
+$_['entry_transaction_type']   = 'crwdns89179:0crwdne89179:0';
+$_['entry_transaction_status'] = 'crwdns89181:0crwdne89181:0';
+$_['entry_email']			   = 'crwdns89183:0crwdne89183:0';
+$_['entry_email_buyer']		   = 'crwdns89185:0crwdne89185:0';
+$_['entry_email_merchant']	   = 'crwdns89187:0crwdne89187:0';
+$_['entry_receipt']			   = 'crwdns89189:0crwdne89189:0';
+$_['entry_transaction_id']	   = 'crwdns89191:0crwdne89191:0';
+$_['entry_invoice_no']		   = 'crwdns89193:0crwdne89193:0';
+$_['entry_auction']			   = 'crwdns89195:0crwdne89195:0';
+$_['entry_amount']			   = 'crwdns89197:0crwdne89197:0';
+$_['entry_recurring_id']	   = 'crwdns89199:0crwdne89199:0';
+$_['entry_salutation']		   = 'crwdns89201:0crwdne89201:0';
+$_['entry_firstname']		   = 'crwdns89203:0crwdne89203:0';
+$_['entry_middlename']		   = 'crwdns89205:0crwdne89205:0';
+$_['entry_lastname']		   = 'crwdns89207:0crwdne89207:0';
+$_['entry_suffix']			   = 'crwdns89209:0crwdne89209:0';
+$_['entry_status_all']		   = 'crwdns89211:0crwdne89211:0';
+$_['entry_status_pending']	   = 'crwdns89213:0crwdne89213:0';
+$_['entry_status_processing']  = 'crwdns89215:0crwdne89215:0';
+$_['entry_status_success']	   = 'crwdns89217:0crwdne89217:0';
+$_['entry_status_denied']	   = 'crwdns89219:0crwdne89219:0';
+$_['entry_status_reversed']	   = 'crwdns89221:0crwdne89221:0';
