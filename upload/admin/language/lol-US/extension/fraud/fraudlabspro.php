54bb359fd807790ee1d4211304093f338f4505f2
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:41 +0200

    New translations fraudlabspro.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/fraud/fraudlabspro.php
@@ -0,0 +1,81 @@
+<?php
+// Heading
+$_['heading_title']              = 'crwdns84119:0crwdne84119:0';
+
+// Text
+$_['text_extension']             = 'crwdns84121:0crwdne84121:0';
+$_['text_success']               = 'crwdns84123:0crwdne84123:0';
+$_['text_edit']                  = 'crwdns84125:0crwdne84125:0';
+$_['text_signup']                = 'crwdns84127:0crwdne84127:0';
+$_['text_id']                    = 'crwdns84129:0crwdne84129:0';
+$_['text_ip_address']            = 'crwdns84131:0crwdne84131:0';
+$_['text_ip_net_speed']          = 'crwdns84133:0crwdne84133:0';
+$_['text_ip_isp_name']           = 'crwdns84135:0crwdne84135:0';
+$_['text_ip_usage_type']         = 'crwdns84137:0crwdne84137:0';
+$_['text_ip_domain']             = 'crwdns84139:0crwdne84139:0';
+$_['text_ip_time_zone']          = 'crwdns84141:0crwdne84141:0';
+$_['text_ip_location']           = 'crwdns84143:0crwdne84143:0';
+$_['text_ip_distance']           = 'crwdns84145:0crwdne84145:0';
+$_['text_ip_latitude']           = 'crwdns84147:0crwdne84147:0';
+$_['text_ip_longitude']          = 'crwdns84149:0crwdne84149:0';
+$_['text_risk_country']          = 'crwdns84151:0crwdne84151:0';
+$_['text_free_email']            = 'crwdns84153:0crwdne84153:0';
+$_['text_ship_forward']          = 'crwdns84155:0crwdne84155:0';
+$_['text_using_proxy']           = 'crwdns84157:0crwdne84157:0';
+$_['text_bin_found']             = 'crwdns84159:0crwdne84159:0';
+$_['text_email_blacklist']       = 'crwdns84161:0crwdne84161:0';
+$_['text_credit_card_blacklist'] = 'crwdns84163:0crwdne84163:0';
+$_['text_score']                 = 'crwdns84165:0crwdne84165:0';
+$_['text_status']                = 'crwdns84167:0crwdne84167:0';
+$_['text_message']               = 'crwdns84169:0crwdne84169:0';
+$_['text_transaction_id']        = 'crwdns84171:0crwdne84171:0';
+$_['text_credits']               = 'crwdns84173:0crwdne84173:0';
+$_['text_error']                 = 'crwdns84175:0crwdne84175:0';
+$_['text_flp_upgrade']           = 'crwdns84177:0[Upgrade]crwdne84177:0';
+$_['text_flp_merchant_area']     = 'crwdns84179:0crwdne84179:0';
+$_['text_rule_validation']       = 'crwdns84181:0crwdne84181:0';
+$_['text_testing']               = 'crwdns84183:0crwdne84183:0';
+
+// Entry
+$_['entry_status']               = 'crwdns84185:0crwdne84185:0';
+$_['entry_key']                  = 'crwdns84187:0crwdne84187:0';
+$_['entry_score']                = 'crwdns84189:0crwdne84189:0';
+$_['entry_order_status']         = 'crwdns84191:0crwdne84191:0';
+$_['entry_review_status']        = 'crwdns84193:0crwdne84193:0';
+$_['entry_approve_status']       = 'crwdns84195:0crwdne84195:0';
+$_['entry_reject_status']        = 'crwdns84197:0crwdne84197:0';
+$_['entry_simulate_ip']          = 'crwdns84199:0crwdne84199:0';
+
+// Help
+$_['help_order_status']          = 'crwdns84201:0crwdne84201:0';
+$_['help_review_status']         = 'crwdns84203:0crwdne84203:0';
+$_['help_approve_status']        = 'crwdns84205:0crwdne84205:0';
+$_['help_reject_status']         = 'crwdns84207:0crwdne84207:0';
+$_['help_simulate_ip']           = 'crwdns84209:0crwdne84209:0';
+$_['help_fraudlabspro_id']       = 'crwdns84211:0crwdne84211:0';
+$_['help_ip_address']            = 'crwdns84213:0crwdne84213:0';
+$_['help_ip_net_speed']          = 'crwdns84215:0crwdne84215:0';
+$_['help_ip_isp_name']           = 'crwdns84217:0crwdne84217:0';
+$_['help_ip_usage_type']         = 'crwdns84219:0crwdne84219:0';
+$_['help_ip_domain']             = 'crwdns84221:0crwdne84221:0';
+$_['help_ip_time_zone']          = 'crwdns84223:0crwdne84223:0';
+$_['help_ip_location']           = 'crwdns84225:0crwdne84225:0';
+$_['help_ip_distance']           = 'crwdns84227:0crwdne84227:0';
+$_['help_ip_latitude']           = 'crwdns84229:0crwdne84229:0';
+$_['help_ip_longitude']          = 'crwdns84231:0crwdne84231:0';
+$_['help_risk_country']          = 'crwdns84233:0crwdne84233:0';
+$_['help_free_email']            = 'crwdns84235:0crwdne84235:0';
+$_['help_ship_forward']          = 'crwdns84237:0crwdne84237:0';
+$_['help_using_proxy']           = 'crwdns84239:0crwdne84239:0';
+$_['help_bin_found']             = 'crwdns84241:0crwdne84241:0';
+$_['help_email_blacklist']       = 'crwdns84243:0crwdne84243:0';
+$_['help_credit_card_blacklist'] = 'crwdns84245:0crwdne84245:0';
+$_['help_score']                 = 'crwdns84247:0crwdne84247:0';
+$_['help_status']                = 'crwdns84249:0crwdne84249:0';
+$_['help_message']               = 'crwdns84251:0crwdne84251:0';
+$_['help_transaction_id']        = 'crwdns84253:0crwdne84253:0';
+$_['help_credits']               = 'crwdns84255:0crwdne84255:0';
+
+// Error
+$_['error_permission']           = 'crwdns84257:0crwdne84257:0';
+$_['error_key']                  = 'crwdns84259:0crwdne84259:0';
