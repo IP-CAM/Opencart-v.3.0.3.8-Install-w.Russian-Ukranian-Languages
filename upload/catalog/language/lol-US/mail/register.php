3c62f69c5ed7b9ec91d7c849fc09bc73dbb26747
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:54 +0200

    New translations register.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/mail/register.php
@@ -0,0 +1,15 @@
+<?php
+// Text
+$_['text_subject']        = 'crwdns100339:0%scrwdne100339:0';
+$_['text_welcome']        = 'crwdns100341:0%scrwdne100341:0';
+$_['text_login']          = 'crwdns100343:0crwdne100343:0';
+$_['text_approval']       = 'crwdns100345:0crwdne100345:0';
+$_['text_service']        = 'crwdns100347:0crwdne100347:0';
+$_['text_thanks']         = 'crwdns100349:0crwdne100349:0';
+$_['text_new_customer']   = 'crwdns100351:0crwdne100351:0';
+$_['text_signup']         = 'crwdns100353:0crwdne100353:0';
+$_['text_customer_group'] = 'crwdns100355:0crwdne100355:0';
+$_['text_firstname']      = 'crwdns100357:0crwdne100357:0';
+$_['text_lastname']       = 'crwdns100359:0crwdne100359:0';
+$_['text_email']          = 'crwdns100361:0crwdne100361:0';
+$_['text_telephone']      = 'crwdns100363:0crwdne100363:0';
