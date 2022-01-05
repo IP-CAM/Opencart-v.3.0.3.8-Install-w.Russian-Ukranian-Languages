a0e0590b5452b909d074423a22f98ca31369c43f
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:25 +0200

    New translations event.php (LOLCAT)

---
+++ upload/admin/language/lol-US/marketplace/event.php
@@ -0,0 +1,20 @@
+<?php
+// Heading
+$_['heading_title']     = 'crwdns94073:0crwdne94073:0';
+
+// Text
+$_['text_success']      = 'crwdns94075:0crwdne94075:0';
+$_['text_list']         = 'crwdns94077:0crwdne94077:0';
+$_['text_event']        = 'crwdns94079:0crwdne94079:0';
+$_['text_info']         = 'crwdns94081:0crwdne94081:0';
+$_['text_trigger']      = 'crwdns94083:0crwdne94083:0';
+$_['text_action']       = 'crwdns94085:0crwdne94085:0';
+
+// Column
+$_['column_code']       = 'crwdns94087:0crwdne94087:0';
+$_['column_status']     = 'crwdns94089:0crwdne94089:0';
+$_['column_sort_order'] = 'crwdns94091:0crwdne94091:0';
+$_['column_action']     = 'crwdns94093:0crwdne94093:0';
+
+// Error
+$_['error_permission']  = 'crwdns94095:0crwdne94095:0';
