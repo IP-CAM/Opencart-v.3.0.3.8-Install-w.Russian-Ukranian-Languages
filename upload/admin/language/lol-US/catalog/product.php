781f7b7a43cac8a0d41c3844fb79790f4c8a8fc7
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:00 +0200

    New translations product.php (LOLCAT)

---
+++ upload/admin/language/lol-US/catalog/product.php
@@ -0,0 +1,110 @@
+<?php
+// Heading
+$_['heading_title']          = 'crwdns81439:0crwdne81439:0';
+
+// Text
+$_['text_success']           = 'crwdns81441:0crwdne81441:0';
+$_['text_list']              = 'crwdns81443:0crwdne81443:0';
+$_['text_add']               = 'crwdns81445:0crwdne81445:0';
+$_['text_edit']              = 'crwdns81447:0crwdne81447:0';
+$_['text_filter']            = 'crwdns81449:0crwdne81449:0';
+$_['text_plus']              = 'crwdns81451:0crwdne81451:0';
+$_['text_minus']             = 'crwdns81453:0crwdne81453:0';
+$_['text_default']           = 'crwdns81455:0crwdne81455:0';
+$_['text_option']            = 'crwdns81457:0crwdne81457:0';
+$_['text_option_value']      = 'crwdns81459:0crwdne81459:0';
+$_['text_percent']           = 'crwdns81461:0crwdne81461:0';
+$_['text_amount']            = 'crwdns81463:0crwdne81463:0';
+$_['text_keyword']           = 'crwdns81465:0crwdne81465:0';
+
+// Column
+$_['column_name']            = 'crwdns81467:0crwdne81467:0';
+$_['column_model']           = 'crwdns81469:0crwdne81469:0';
+$_['column_image']           = 'crwdns81471:0crwdne81471:0';
+$_['column_price']           = 'crwdns81473:0crwdne81473:0';
+$_['column_quantity']        = 'crwdns81475:0crwdne81475:0';
+$_['column_status']          = 'crwdns81477:0crwdne81477:0';
+$_['column_action']          = 'crwdns81479:0crwdne81479:0';
+
+// Entry
+$_['entry_name']             = 'crwdns81481:0crwdne81481:0';
+$_['entry_description']      = 'crwdns81483:0crwdne81483:0';
+$_['entry_meta_title']       = 'crwdns81485:0crwdne81485:0';
+$_['entry_meta_keyword']     = 'crwdns81487:0crwdne81487:0';
+$_['entry_meta_description'] = 'crwdns81489:0crwdne81489:0';
+$_['entry_store']            = 'crwdns81491:0crwdne81491:0';
+$_['entry_keyword']          = 'crwdns81493:0crwdne81493:0';
+$_['entry_model']            = 'crwdns81495:0crwdne81495:0';
+$_['entry_sku']              = 'crwdns81497:0crwdne81497:0';
+$_['entry_upc']              = 'crwdns81499:0crwdne81499:0';
+$_['entry_ean']              = 'crwdns81501:0crwdne81501:0';
+$_['entry_jan']              = 'crwdns81503:0crwdne81503:0';
+$_['entry_isbn']             = 'crwdns81505:0crwdne81505:0';
+$_['entry_mpn']              = 'crwdns81507:0crwdne81507:0';
+$_['entry_location']         = 'crwdns81509:0crwdne81509:0';
+$_['entry_shipping']         = 'crwdns81511:0crwdne81511:0';
+$_['entry_manufacturer']     = 'crwdns81513:0crwdne81513:0';
+$_['entry_date_available']   = 'crwdns81515:0crwdne81515:0';
+$_['entry_quantity']         = 'crwdns81517:0crwdne81517:0';
+$_['entry_minimum']          = 'crwdns81519:0crwdne81519:0';
+$_['entry_stock_status']     = 'crwdns81521:0crwdne81521:0';
+$_['entry_price']            = 'crwdns81523:0crwdne81523:0';
+$_['entry_tax_class']        = 'crwdns81525:0crwdne81525:0';
+$_['entry_points']           = 'crwdns81527:0crwdne81527:0';
+$_['entry_option_points']    = 'crwdns81529:0crwdne81529:0';
+$_['entry_subtract']         = 'crwdns81531:0crwdne81531:0';
+$_['entry_weight_class']     = 'crwdns81533:0crwdne81533:0';
+$_['entry_weight']           = 'crwdns81535:0crwdne81535:0';
+$_['entry_dimension']        = 'crwdns81537:0crwdne81537:0';
+$_['entry_length_class']     = 'crwdns81539:0crwdne81539:0';
+$_['entry_length']           = 'crwdns81541:0crwdne81541:0';
+$_['entry_width']            = 'crwdns81543:0crwdne81543:0';
+$_['entry_height']           = 'crwdns81545:0crwdne81545:0';
+$_['entry_image']            = 'crwdns81547:0crwdne81547:0';
+$_['entry_additional_image'] = 'crwdns81549:0crwdne81549:0';
+$_['entry_customer_group']   = 'crwdns81551:0crwdne81551:0';
+$_['entry_date_start']       = 'crwdns81553:0crwdne81553:0';
+$_['entry_date_end']         = 'crwdns81555:0crwdne81555:0';
+$_['entry_priority']         = 'crwdns81557:0crwdne81557:0';
+$_['entry_attribute']        = 'crwdns81559:0crwdne81559:0';
+$_['entry_attribute_group']  = 'crwdns81561:0crwdne81561:0';
+$_['entry_text']             = 'crwdns81563:0crwdne81563:0';
+$_['entry_option']           = 'crwdns81565:0crwdne81565:0';
+$_['entry_option_value']     = 'crwdns81567:0crwdne81567:0';
+$_['entry_required']         = 'crwdns81569:0crwdne81569:0';
+$_['entry_status']           = 'crwdns81571:0crwdne81571:0';
+$_['entry_sort_order']       = 'crwdns81573:0crwdne81573:0';
+$_['entry_category']         = 'crwdns81575:0crwdne81575:0';
+$_['entry_filter']           = 'crwdns81577:0crwdne81577:0';
+$_['entry_download']         = 'crwdns81579:0crwdne81579:0';
+$_['entry_related']          = 'crwdns81581:0crwdne81581:0';
+$_['entry_tag']              = 'crwdns81583:0crwdne81583:0';
+$_['entry_reward']           = 'crwdns81585:0crwdne81585:0';
+$_['entry_layout']           = 'crwdns81587:0crwdne81587:0';
+$_['entry_recurring']        = 'crwdns81589:0crwdne81589:0';
+
+// Help
+$_['help_sku']               = 'crwdns81591:0crwdne81591:0';
+$_['help_upc']               = 'crwdns81593:0crwdne81593:0';
+$_['help_ean']               = 'crwdns81595:0crwdne81595:0';
+$_['help_jan']               = 'crwdns81597:0crwdne81597:0';
+$_['help_isbn']              = 'crwdns81599:0crwdne81599:0';
+$_['help_mpn']               = 'crwdns81601:0crwdne81601:0';
+$_['help_manufacturer']      = 'crwdns81603:0crwdne81603:0';
+$_['help_minimum']           = 'crwdns81605:0crwdne81605:0';
+$_['help_stock_status']      = 'crwdns81607:0crwdne81607:0';
+$_['help_points']            = 'crwdns81609:0crwdne81609:0';
+$_['help_category']          = 'crwdns81611:0crwdne81611:0';
+$_['help_filter']            = 'crwdns81613:0crwdne81613:0';
+$_['help_download']          = 'crwdns81615:0crwdne81615:0';
+$_['help_related']           = 'crwdns81617:0crwdne81617:0';
+$_['help_tag']               = 'crwdns81619:0crwdne81619:0';
+
+// Error
+$_['error_warning']          = 'crwdns81621:0crwdne81621:0';
+$_['error_permission']       = 'crwdns81623:0crwdne81623:0';
+$_['error_name']             = 'crwdns81625:0crwdne81625:0';
+$_['error_meta_title']       = 'crwdns81627:0crwdne81627:0';
+$_['error_model']            = 'crwdns81629:0crwdne81629:0';
+$_['error_keyword']          = 'crwdns81631:0crwdne81631:0';
+$_['error_unique']           = 'crwdns81633:0crwdne81633:0';
