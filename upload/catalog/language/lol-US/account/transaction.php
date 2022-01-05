65c62ca6a42e6273479193b5c54bc25eb7d5de13
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:55 +0200

    New translations transaction.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/account/transaction.php
@@ -0,0 +1,14 @@
+<?php
+// Heading
+$_['heading_title']      = 'crwdns96361:0crwdne96361:0';
+
+// Column
+$_['column_date_added']  = 'crwdns96363:0crwdne96363:0';
+$_['column_description'] = 'crwdns96365:0crwdne96365:0';
+$_['column_amount']      = 'crwdns96367:0%scrwdne96367:0';
+
+// Text
+$_['text_account']       = 'crwdns96369:0crwdne96369:0';
+$_['text_transaction']   = 'crwdns96371:0crwdne96371:0';
+$_['text_total']         = 'crwdns96373:0crwdne96373:0';
+$_['text_empty']         = 'crwdns96375:0crwdne96375:0';
