9930becc504cc820456050d3f43bc58fd2a9dde7
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:12 +0200

    New translations return.php (LOLCAT)

---
+++ upload/admin/language/lol-US/mail/return.php
@@ -0,0 +1,8 @@
+<?php
+// Text
+$_['text_subject']       = 'crwdns93825:0%scrwdnd93825:0%scrwdne93825:0';
+$_['text_return_id']     = 'crwdns93827:0crwdne93827:0';
+$_['text_date_added']    = 'crwdns93829:0crwdne93829:0';
+$_['text_return_status'] = 'crwdns93831:0crwdne93831:0';
+$_['text_comment']       = 'crwdns93833:0crwdne93833:0';
+$_['text_footer']        = 'crwdns93835:0crwdne93835:0';
