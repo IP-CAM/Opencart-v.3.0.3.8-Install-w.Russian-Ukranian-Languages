0821f4a2e9c40682e8f767e6224cf37540351ee1
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:01 +0200

    New translations customer.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/api/customer.php
@@ -0,0 +1,12 @@
+<?php
+// Text
+$_['text_success']       = 'crwdns96599:0crwdne96599:0';
+
+// Error
+$_['error_permission']   = 'crwdns96601:0crwdne96601:0';
+$_['error_customer']     = 'crwdns96603:0crwdne96603:0';
+$_['error_firstname']    = 'crwdns96605:0crwdne96605:0';
+$_['error_lastname']     = 'crwdns96607:0crwdne96607:0';
+$_['error_email']        = 'crwdns96609:0crwdne96609:0';
+$_['error_telephone']    = 'crwdns96611:0crwdne96611:0';
+$_['error_custom_field'] = 'crwdns96613:0%scrwdne96613:0';
