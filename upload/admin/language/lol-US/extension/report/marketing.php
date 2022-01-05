83da4af8efdccfe8cfd7851fc963962979c9111f
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:45 +0200

    New translations marketing.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/report/marketing.php
@@ -0,0 +1,26 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns91857:0crwdne91857:0';
+
+// Text
+$_['text_extension']   = 'crwdns91859:0crwdne91859:0';
+$_['text_edit']        = 'crwdns91861:0crwdne91861:0';
+$_['text_success']     = 'crwdns91863:0crwdne91863:0';
+$_['text_filter']      = 'crwdns91865:0crwdne91865:0';
+$_['text_all_status']  = 'crwdns91867:0crwdne91867:0';
+
+// Column
+$_['column_campaign']  = 'crwdns91869:0crwdne91869:0';
+$_['column_code']      = 'crwdns91871:0crwdne91871:0';
+$_['column_clicks']    = 'crwdns91873:0crwdne91873:0';
+$_['column_orders']    = 'crwdns91875:0crwdne91875:0';
+$_['column_total']     = 'crwdns91877:0crwdne91877:0';
+
+// Entry
+$_['entry_date_start'] = 'crwdns91879:0crwdne91879:0';
+$_['entry_date_end']   = 'crwdns91881:0crwdne91881:0';
+$_['entry_status']     = 'crwdns91883:0crwdne91883:0';
+$_['entry_sort_order'] = 'crwdns91885:0crwdne91885:0';
+
+// Error
+$_['error_permission']  = 'crwdns91887:0crwdne91887:0';
