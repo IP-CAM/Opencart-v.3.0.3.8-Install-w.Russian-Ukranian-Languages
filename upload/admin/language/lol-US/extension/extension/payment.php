3c89a455b01d5a5b6ffe7c102cc5581cf08e9e61
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:36 +0200

    New translations payment.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/extension/payment.php
@@ -0,0 +1,17 @@
+<?php
+// Heading
+$_['heading_title']     = 'crwdns83997:0crwdne83997:0';
+
+// Text
+$_['text_success']      = 'crwdns83999:0crwdne83999:0';
+$_['text_list']         = 'crwdns84001:0crwdne84001:0';
+
+
+// Column
+$_['column_name']       = 'crwdns84003:0crwdne84003:0';
+$_['column_status']     = 'crwdns84005:0crwdne84005:0';
+$_['column_sort_order'] = 'crwdns84007:0crwdne84007:0';
+$_['column_action']     = 'crwdns84009:0crwdne84009:0';
+
+// Error
+$_['error_permission']  = 'crwdns84011:0crwdne84011:0';
