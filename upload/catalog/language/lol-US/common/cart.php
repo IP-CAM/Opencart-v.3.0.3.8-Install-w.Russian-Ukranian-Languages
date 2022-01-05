1f73aad72c800432b1de0300b8c004c83d5869d8
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:31 +0200

    New translations cart.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/common/cart.php
@@ -0,0 +1,7 @@
+<?php
+// Text
+$_['text_items']     = 'crwdns96983:0%scrwdnd96983:0%scrwdne96983:0';
+$_['text_empty']     = 'crwdns96985:0crwdne96985:0';
+$_['text_cart']      = 'crwdns96987:0crwdne96987:0';
+$_['text_checkout']  = 'crwdns96989:0crwdne96989:0';
+$_['text_recurring'] = 'crwdns96991:0crwdne96991:0';
