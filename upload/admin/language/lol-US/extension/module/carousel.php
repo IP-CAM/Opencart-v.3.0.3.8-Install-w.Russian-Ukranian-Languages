bffd7cc89ddb0fbbda1e4701b478e775e611ab82
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:11 +0200

    New translations carousel.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/module/carousel.php
@@ -0,0 +1,21 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns84717:0crwdne84717:0';
+
+// Text
+$_['text_extension']   = 'crwdns84719:0crwdne84719:0';
+$_['text_success']     = 'crwdns84721:0crwdne84721:0';
+$_['text_edit']        = 'crwdns84723:0crwdne84723:0';
+
+// Entry
+$_['entry_name']       = 'crwdns84725:0crwdne84725:0';
+$_['entry_banner']     = 'crwdns84727:0crwdne84727:0';
+$_['entry_width']      = 'crwdns84729:0crwdne84729:0';
+$_['entry_height']     = 'crwdns84731:0crwdne84731:0';
+$_['entry_status']     = 'crwdns84733:0crwdne84733:0';
+
+// Error
+$_['error_permission'] = 'crwdns84735:0crwdne84735:0';
+$_['error_name']       = 'crwdns84737:0crwdne84737:0';
+$_['error_width']      = 'crwdns84739:0crwdne84739:0';
+$_['error_height']     = 'crwdns84741:0crwdne84741:0';
