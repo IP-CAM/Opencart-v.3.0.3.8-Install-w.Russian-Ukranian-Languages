2c47b1cb2246b5566b4baf6425492675fd7d3af2
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:11 +0200

    New translations firstdata.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/payment/firstdata.php
@@ -0,0 +1,79 @@
+<?php
+// Heading
+$_['heading_title']					 = 'crwdns86495:0crwdne86495:0';
+
+// Text
+$_['text_extension']				 = 'crwdns86497:0crwdne86497:0';
+$_['text_success']					 = 'crwdns86499:0crwdne86499:0';
+$_['text_edit']                      = 'crwdns86501:0crwdne86501:0';
+$_['text_notification_url']			 = 'crwdns86503:0crwdne86503:0';
+$_['text_live']						 = 'crwdns86505:0crwdne86505:0';
+$_['text_demo']						 = 'crwdns86507:0crwdne86507:0';
+$_['text_enabled']					 = 'crwdns86509:0crwdne86509:0';
+$_['text_merchant_id']				 = 'crwdns86511:0crwdne86511:0';
+$_['text_secret']					 = 'crwdns86513:0crwdne86513:0';
+$_['text_capture_ok']				 = 'crwdns86515:0crwdne86515:0';
+$_['text_capture_ok_order']			 = 'crwdns86517:0crwdne86517:0';
+$_['text_void_ok']					 = 'crwdns86519:0crwdne86519:0';
+$_['text_settle_auto']				 = 'crwdns86521:0crwdne86521:0';
+$_['text_settle_delayed']			 = 'crwdns86523:0crwdne86523:0';
+$_['text_success_void']				 = 'crwdns86525:0crwdne86525:0';
+$_['text_success_capture']			 = 'crwdns86527:0crwdne86527:0';
+$_['text_firstdata']				 = 'crwdns86529:0crwdne86529:0';
+$_['text_payment_info']				 = 'crwdns86531:0crwdne86531:0';
+$_['text_capture_status']			 = 'crwdns86533:0crwdne86533:0';
+$_['text_void_status']				 = 'crwdns86535:0crwdne86535:0';
+$_['text_order_ref']				 = 'crwdns86537:0crwdne86537:0';
+$_['text_order_total']				 = 'crwdns86539:0crwdne86539:0';
+$_['text_total_captured']			 = 'crwdns86541:0crwdne86541:0';
+$_['text_transactions']				 = 'crwdns86543:0crwdne86543:0';
+$_['text_column_amount']			 = 'crwdns86545:0crwdne86545:0';
+$_['text_column_type']				 = 'crwdns86547:0crwdne86547:0';
+$_['text_column_date_added']		 = 'crwdns86549:0crwdne86549:0';
+$_['text_confirm_void']				 = 'crwdns86551:0crwdne86551:0';
+$_['text_confirm_capture']			 = 'crwdns86553:0crwdne86553:0';
+
+// Entry
+$_['entry_merchant_id']				 = 'crwdns86555:0crwdne86555:0';
+$_['entry_secret']					 = 'crwdns86557:0crwdne86557:0';
+$_['entry_total']					 = 'crwdns86559:0crwdne86559:0';
+$_['entry_sort_order']				 = 'crwdns86561:0crwdne86561:0';
+$_['entry_geo_zone']				 = 'crwdns86563:0crwdne86563:0';
+$_['entry_status']					 = 'crwdns86565:0crwdne86565:0';
+$_['entry_debug']					 = 'crwdns86567:0crwdne86567:0';
+$_['entry_live_demo']				 = 'crwdns86569:0crwdne86569:0';
+$_['entry_auto_settle']			  	 = 'crwdns86571:0crwdne86571:0';
+$_['entry_card_select']				 = 'crwdns86573:0crwdne86573:0';
+$_['entry_tss_check']				 = 'crwdns86575:0crwdne86575:0';
+$_['entry_live_url']				 = 'crwdns86577:0crwdne86577:0';
+$_['entry_demo_url']				 = 'crwdns86579:0crwdne86579:0';
+$_['entry_status_success_settled']	 = 'crwdns86581:0crwdne86581:0';
+$_['entry_status_success_unsettled'] = 'crwdns86583:0crwdne86583:0';
+$_['entry_status_decline']		 	 = 'crwdns86585:0crwdne86585:0';
+$_['entry_status_void']				 = 'crwdns86587:0crwdne86587:0';
+$_['entry_enable_card_store']		 = 'crwdns86589:0crwdne86589:0';
+
+// Help
+$_['help_total']					 = 'crwdns86591:0crwdne86591:0';
+$_['help_notification']				 = 'crwdns86593:0crwdne86593:0';
+$_['help_debug']					 = 'crwdns86595:0crwdne86595:0';
+$_['help_settle']					 = 'crwdns86597:0crwdne86597:0'; 
+
+// Tab
+$_['tab_account']					 = 'crwdns86599:0crwdne86599:0';
+$_['tab_order_status']				 = 'crwdns86601:0crwdne86601:0';
+$_['tab_payment']					 = 'crwdns86603:0crwdne86603:0';
+$_['tab_advanced']					 = 'crwdns86605:0crwdne86605:0';
+
+// Button
+$_['button_capture']				 = 'crwdns86607:0crwdne86607:0';
+$_['button_void']					 = 'crwdns86609:0crwdne86609:0';
+
+// Error
+$_['error_merchant_id']				 = 'crwdns86611:0crwdne86611:0';
+$_['error_secret']					 = 'crwdns86613:0crwdne86613:0';
+$_['error_live_url']				 = 'crwdns86615:0crwdne86615:0';
+$_['error_demo_url']				 = 'crwdns86617:0crwdne86617:0';
+$_['error_data_missing']			 = 'crwdns86619:0crwdne86619:0';
+$_['error_void_error']				 = 'crwdns86621:0crwdne86621:0';
+$_['error_capture_error']			 = 'crwdns86623:0crwdne86623:0';
