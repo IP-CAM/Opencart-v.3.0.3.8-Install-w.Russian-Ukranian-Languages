2c2e9d6632ea3fb04fc85bafffaad9a21768b1e5
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:59 +0200

    New translations sagepay_server.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/payment/sagepay_server.php
@@ -0,0 +1,30 @@
+<?php
+// Text
+$_['text_title']				= 'crwdns99525:0crwdne99525:0';
+$_['text_credit_card']			= 'crwdns99527:0crwdne99527:0';
+$_['text_description']			= 'crwdns99529:0%scrwdnd99529:0%scrwdne99529:0';
+$_['text_card_type']			= 'crwdns99531:0crwdne99531:0';
+$_['text_card_name']			= 'crwdns99533:0crwdne99533:0';
+$_['text_card_digits']			= 'crwdns99535:0crwdne99535:0';
+$_['text_card_expiry']			= 'crwdns99537:0crwdne99537:0';
+$_['text_trial']				= 'crwdns99539:0%scrwdnd99539:0%scrwdnd99539:0%scrwdnd99539:0%scrwdne99539:0';
+$_['text_recurring']			= 'crwdns99541:0%scrwdnd99541:0%scrwdnd99541:0%scrwdne99541:0';
+$_['text_length']				= 'crwdns99543:0%scrwdne99543:0';
+$_['text_success']				= 'crwdns99545:0crwdne99545:0';
+$_['text_decline']				= 'crwdns99547:0crwdne99547:0';
+$_['text_bank_error']			= 'crwdns99549:0crwdne99549:0';
+$_['text_transaction_error']	= 'crwdns99551:0crwdne99551:0';
+$_['text_generic_error']		= 'crwdns99553:0crwdne99553:0';
+$_['text_hash_failed']			= 'crwdns99555:0crwdne99555:0';
+$_['text_link']					= 'crwdns99557:0%scrwdne99557:0';
+$_['text_confirm_delete']		= 'crwdns99559:0crwdne99559:0';
+
+// Entry
+$_['entry_card']				= 'crwdns99561:0crwdne99561:0';
+$_['entry_card_existing']		= 'crwdns99563:0crwdne99563:0';
+$_['entry_card_new']			= 'crwdns99565:0crwdne99565:0';
+$_['entry_card_save']			= 'crwdns99567:0crwdne99567:0';
+$_['entry_cc_choice']			= 'crwdns99569:0crwdne99569:0';
+
+// Button
+$_['button_delete_card']		= 'crwdns99571:0crwdne99571:0';
