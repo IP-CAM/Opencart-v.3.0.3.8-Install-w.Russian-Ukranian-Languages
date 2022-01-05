99000c5880bc38b5747fb69550081b4d932dff2b
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:10 +0200

    New translations report.php (LOLCAT)

---
+++ upload/admin/language/lol-US/report/report.php
@@ -0,0 +1,9 @@
+<?php
+// Heading
+$_['heading_title'] = 'crwdns94349:0crwdne94349:0';
+
+// Text
+$_['text_success']  = 'crwdns94351:0crwdne94351:0';
+$_['text_list']     = 'crwdns94353:0crwdne94353:0';
+$_['text_type']     = 'crwdns94355:0crwdne94355:0';
+$_['text_filter']   = 'crwdns94357:0crwdne94357:0';
