3cacbaf7f294ac687d89708a93d297e43ec0b3b1
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:19 +0200

    New translations liqpay.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/payment/liqpay.php
@@ -0,0 +1,29 @@
+<?php
+// Heading
+$_['heading_title']		 = 'crwdns87897:0crwdne87897:0';
+
+// Text
+$_['text_extension']	 = 'crwdns87899:0crwdne87899:0';
+$_['text_success']		 = 'crwdns87901:0crwdne87901:0';
+$_['text_edit']          = 'crwdns87903:0crwdne87903:0';
+$_['text_pay']			 = 'crwdns87905:0crwdne87905:0';
+$_['text_card']			 = 'crwdns87907:0crwdne87907:0';
+$_['text_liqpay']		 = 'crwdns87909:0crwdne87909:0';
+
+// Entry
+$_['entry_merchant']	 = 'crwdns87911:0crwdne87911:0';
+$_['entry_signature']	 = 'crwdns87913:0crwdne87913:0';
+$_['entry_type']		 = 'crwdns87915:0crwdne87915:0';
+$_['entry_total']		 = 'crwdns87917:0crwdne87917:0';
+$_['entry_order_status'] = 'crwdns87919:0crwdne87919:0';
+$_['entry_geo_zone']	 = 'crwdns87921:0crwdne87921:0';
+$_['entry_status']		 = 'crwdns87923:0crwdne87923:0';
+$_['entry_sort_order']	 = 'crwdns87925:0crwdne87925:0';
+
+// Help
+$_['help_total']		 = 'crwdns87927:0crwdne87927:0';
+
+// Error
+$_['error_permission']	 = 'crwdns87929:0crwdne87929:0';
+$_['error_merchant']	 = 'crwdns87931:0crwdne87931:0';
+$_['error_signature']	 = 'crwdns87933:0crwdne87933:0';
