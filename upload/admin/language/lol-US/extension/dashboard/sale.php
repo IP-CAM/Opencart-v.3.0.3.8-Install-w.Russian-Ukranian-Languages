41c1dcacb4a2da95f7f20c905f2340fafdb9f2f7
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:28 +0200

    New translations sale.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/dashboard/sale.php
@@ -0,0 +1,17 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns83823:0crwdne83823:0';
+
+// Text
+$_['text_extension']   = 'crwdns83825:0crwdne83825:0';
+$_['text_success']     = 'crwdns83827:0crwdne83827:0';
+$_['text_edit']        = 'crwdns83829:0crwdne83829:0';
+$_['text_view']        = 'crwdns83831:0crwdne83831:0';
+
+// Entry
+$_['entry_status']     = 'crwdns83833:0crwdne83833:0';
+$_['entry_sort_order'] = 'crwdns83835:0crwdne83835:0';
+$_['entry_width']      = 'crwdns83837:0crwdne83837:0';
+
+// Error
+$_['error_permission'] = 'crwdns83839:0crwdne83839:0';
