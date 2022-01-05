5941bb3da038c1351b21847f8fd1611c92360830
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:11 +0200

    New translations divido.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/payment/divido.php
@@ -0,0 +1,42 @@
+<?php
+// Heading
+$_['heading_title']                    = 'crwdns86113:0crwdne86113:0';
+
+// Text
+$_['text_divido']                      = 'crwdns86115:0crwdne86115:0';
+$_['text_edit']                        = 'crwdns86117:0crwdne86117:0';
+$_['text_extension']                   = 'crwdns86119:0crwdne86119:0';
+$_['text_order_info']                  = 'crwdns86121:0crwdne86121:0';
+$_['text_success']                     = 'crwdns86123:0crwdne86123:0';
+$_['text_proposal_id']                 = 'crwdns86125:0crwdne86125:0';
+$_['text_application_id']              = 'crwdns86127:0crwdne86127:0';
+$_['text_deposit_amount']              = 'crwdns86129:0crwdne86129:0';
+
+// Entry
+$_['entry_order_status']               = 'crwdns86131:0crwdne86131:0';
+$_['entry_sort_order']                 = 'crwdns86133:0crwdne86133:0';
+$_['entry_status']                     = 'crwdns86135:0crwdne86135:0';
+$_['entry_api_key']                    = 'crwdns86137:0crwdne86137:0';
+$_['entry_title']                      = 'crwdns86139:0crwdne86139:0';
+$_['entry_productselection']           = 'crwdns86141:0crwdne86141:0';
+$_['entry_planselection']              = 'crwdns86143:0crwdne86143:0';
+$_['entry_planlist']                   = 'crwdns86145:0crwdne86145:0';
+$_['entry_plans_options_all']          = 'crwdns86147:0crwdne86147:0';
+$_['entry_plans_options_selected']     = 'crwdns86149:0crwdne86149:0';
+$_['entry_products_options_all']       = 'crwdns86151:0crwdne86151:0';
+$_['entry_products_options_selected']  = 'crwdns86153:0crwdne86153:0';
+$_['entry_products_options_threshold'] = 'crwdns86155:0crwdne86155:0';
+$_['entry_price_threshold']            = 'crwdns86157:0crwdne86157:0';
+$_['entry_cart_threshold']             = 'crwdns86159:0crwdne86159:0';
+$_['entry_threshold_list']             = 'crwdns86161:0crwdne86161:0';
+$_['entry_category']                   = 'crwdns86163:0crwdne86163:0';
+
+// Help
+$_['help_api_key']                     = 'crwdns86165:0crwdne86165:0';
+$_['help_status']                      = 'crwdns86167:0crwdne86167:0';
+$_['help_order_status']                = 'crwdns86169:0crwdne86169:0';
+$_['help_title']                       = 'crwdns86171:0crwdne86171:0';
+$_['help_planselection']               = 'crwdns86173:0crwdne86173:0';
+$_['help_productselection']            = 'crwdns86175:0crwdne86175:0';
+$_['help_category']                    = 'crwdns86177:0crwdne86177:0';
+$_['help_cart_threshold']              = 'crwdns86179:0crwdne86179:0';
