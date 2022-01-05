70cda3e48fcbc63675dc9396a9da5e603fbae1af
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:44 +0200

    New translations squareup.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/credit_card/squareup.php
@@ -0,0 +1,11 @@
+<?php
+
+$_['heading_title'] = 'crwdns97289:0crwdne97289:0';
+
+$_['text_account'] = 'crwdns97291:0crwdne97291:0';
+$_['text_back'] = 'crwdns97293:0crwdne97293:0';
+$_['text_delete'] = 'crwdns97295:0crwdne97295:0';
+$_['text_no_cards'] = 'crwdns97297:0crwdne97297:0';
+$_['text_card_ends_in'] = 'crwdns97299:0%scrwdnd97299:0%scrwdne97299:0';
+$_['text_warning_card'] = 'crwdns97301:0crwdne97301:0';
+$_['text_success_card_delete'] = 'crwdns97303:0crwdne97303:0';
