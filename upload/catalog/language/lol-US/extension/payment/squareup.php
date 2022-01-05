f88199a6e54ba010f043102b52e7cdafa01c9348
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:25:04 +0200

    New translations squareup.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/payment/squareup.php
@@ -0,0 +1,57 @@
+<?php
+// Text
+$_['text_new_card']                     = 'crwdns99659:0crwdne99659:0';
+$_['text_loading']                      = 'crwdns99661:0crwdne99661:0';
+$_['text_card_details']                 = 'crwdns99663:0crwdne99663:0';
+$_['text_saved_card']                   = 'crwdns99665:0crwdne99665:0';
+$_['text_card_ends_in']                 = 'crwdns99667:0%scrwdnd99667:0%scrwdne99667:0';
+$_['text_card_number']                  = 'crwdns99669:0crwdne99669:0';
+$_['text_card_expiry']                  = 'crwdns99671:0crwdne99671:0';
+$_['text_card_cvc']                     = 'crwdns99673:0crwdne99673:0';
+$_['text_card_zip']                     = 'crwdns99675:0crwdne99675:0';
+$_['text_card_save']                    = 'crwdns99677:0crwdne99677:0';
+$_['text_trial']                        = 'crwdns99679:0%scrwdnd99679:0%scrwdnd99679:0%scrwdnd99679:0%scrwdne99679:0';
+$_['text_recurring']                    = 'crwdns99681:0%scrwdnd99681:0%scrwdnd99681:0%scrwdne99681:0';
+$_['text_length']                       = 'crwdns99683:0%scrwdne99683:0';
+$_['text_cron_subject']                 = 'crwdns99685:0crwdne99685:0';
+$_['text_cron_message']                 = 'crwdns99687:0crwdne99687:0';
+$_['text_squareup_profile_suspended']   = 'crwdns99689:0crwdne99689:0';
+$_['text_squareup_trial_expired']       = 'crwdns99691:0crwdne99691:0';
+$_['text_squareup_recurring_expired']   = 'crwdns99693:0crwdne99693:0';
+$_['text_cron_summary_token_heading']   = 'crwdns99695:0crwdne99695:0';
+$_['text_cron_summary_token_updated']   = 'crwdns99697:0crwdne99697:0';
+$_['text_cron_summary_error_heading']   = 'crwdns99699:0crwdne99699:0';
+$_['text_cron_summary_fail_heading']    = 'crwdns99701:0crwdne99701:0';
+$_['text_cron_summary_success_heading'] = 'crwdns99703:0crwdne99703:0';
+$_['text_cron_fail_charge']             = 'crwdns99705:0%scrwdnd99705:0%scrwdne99705:0';
+$_['text_cron_success_charge']          = 'crwdns99707:0%scrwdnd99707:0%scrwdne99707:0';
+$_['text_card_placeholder']             = 'crwdns99709:0crwdne99709:0';
+$_['text_cvv']                          = 'crwdns99711:0crwdne99711:0';
+$_['text_expiry']                       = 'crwdns99713:0crwdne99713:0';
+$_['text_default_squareup_name']        = 'crwdns99715:0crwdne99715:0';
+$_['text_token_issue_customer_error']   = 'crwdns99717:0crwdne99717:0';
+$_['text_token_revoked_subject']        = 'crwdns99719:0crwdne99719:0';
+$_['text_token_revoked_message']        = "crwdns99721:0crwdne99721:0";
+$_['text_token_expired_subject']        = 'crwdns99723:0crwdne99723:0';
+$_['text_token_expired_message']        = "crwdns99725:0crwdne99725:0";
+
+// Error
+$_['error_browser_not_supported']       = 'crwdns99727:0crwdne99727:0';
+$_['error_card_invalid']                = 'crwdns99729:0crwdne99729:0';
+$_['error_squareup_cron_token']         = 'crwdns99731:0crwdne99731:0';
+
+// Warning
+$_['warning_test_mode']                 = 'crwdns99733:0crwdne99733:0';
+
+// Statuses
+$_['squareup_status_comment_authorized']    = 'crwdns99735:0crwdne99735:0';
+$_['squareup_status_comment_captured']      = 'crwdns99737:0crwdne99737:0';
+$_['squareup_status_comment_voided']        = 'crwdns99739:0crwdne99739:0';
+$_['squareup_status_comment_failed']        = 'crwdns99741:0crwdne99741:0';
+
+// Override errors
+$_['squareup_override_error_billing_address.country']       = 'crwdns99743:0crwdne99743:0';
+$_['squareup_override_error_shipping_address.country']      = 'crwdns99745:0crwdne99745:0';
+$_['squareup_override_error_email_address']                 = 'crwdns99747:0crwdne99747:0';
+$_['squareup_override_error_phone_number']                  = 'crwdns99749:0crwdne99749:0';
+$_['squareup_error_field']                                  = 'crwdns99751:0%scrwdne99751:0';
