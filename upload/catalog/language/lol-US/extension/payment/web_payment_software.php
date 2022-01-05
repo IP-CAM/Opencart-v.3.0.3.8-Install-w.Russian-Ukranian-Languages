5128784579963a06542b0a0f68c6f4636fcfa9f1
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:57 +0200

    New translations web_payment_software.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/payment/web_payment_software.php
@@ -0,0 +1,10 @@
+<?php
+// Text
+$_['text_title']           = 'crwdns99755:0crwdne99755:0';
+$_['text_credit_card']     = 'crwdns99757:0crwdne99757:0';
+
+// Entry
+$_['entry_cc_owner']       = 'crwdns99759:0crwdne99759:0';
+$_['entry_cc_number']      = 'crwdns99761:0crwdne99761:0';
+$_['entry_cc_expire_date'] = 'crwdns99763:0crwdne99763:0';
+$_['entry_cc_cvv2']        = 'crwdns99765:0crwdne99765:0';
