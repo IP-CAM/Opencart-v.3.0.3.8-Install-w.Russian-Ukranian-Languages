93f2432da4415be66fb6f478060333faf6aa23cb
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:25:21 +0200

    New translations upload.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/tool/upload.php
@@ -0,0 +1,8 @@
+<?php
+// Text
+$_['text_upload']    = 'crwdns100705:0crwdne100705:0';
+
+// Error
+$_['error_filename'] = 'crwdns100707:0crwdne100707:0';
+$_['error_filetype'] = 'crwdns100709:0crwdne100709:0';
+$_['error_upload']   = 'crwdns100711:0crwdne100711:0';
