2f0321a1799702a6409c3f9b4eb3b15edd880f97
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:22 +0200

    New translations weight.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/shipping/weight.php
@@ -0,0 +1,21 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns92937:0crwdne92937:0';
+
+// Text
+$_['text_extension']   = 'crwdns92939:0crwdne92939:0';
+$_['text_success']     = 'crwdns92941:0crwdne92941:0';
+$_['text_edit']        = 'crwdns92943:0crwdne92943:0';
+
+// Entry
+$_['entry_rate']       = 'crwdns92945:0crwdne92945:0';
+$_['entry_tax_class']  = 'crwdns92947:0crwdne92947:0';
+$_['entry_geo_zone']   = 'crwdns92949:0crwdne92949:0';
+$_['entry_status']     = 'crwdns92951:0crwdne92951:0';
+$_['entry_sort_order'] = 'crwdns92953:0crwdne92953:0';
+
+// Help
+$_['help_rate']        = 'crwdns92955:0crwdne92955:0';
+
+// Error
+$_['error_permission'] = 'crwdns92957:0crwdne92957:0';
