3b4aae70b6a4bf292359c07b6dc22112d61bc300
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:31 +0200

    New translations success.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/checkout/success.php
@@ -0,0 +1,10 @@
+<?php
+// Heading
+$_['heading_title']        = 'crwdns96971:0crwdne96971:0';
+
+// Text
+$_['text_basket']          = 'crwdns96973:0crwdne96973:0';
+$_['text_checkout']        = 'crwdns96975:0crwdne96975:0';
+$_['text_success']         = 'crwdns96977:0crwdne96977:0';
+$_['text_customer']        = 'crwdns96979:0%scrwdnd96979:0%scrwdnd96979:0%scrwdnd96979:0%scrwdne96979:0';
+$_['text_guest']           = 'crwdns96981:0%scrwdne96981:0';
