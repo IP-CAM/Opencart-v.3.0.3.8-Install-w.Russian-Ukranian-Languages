88c58292407816cecc8df3f09304f2d0a7d041d2
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:37 +0200

    New translations report.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/extension/report.php
@@ -0,0 +1,16 @@
+<?php
+// Heading
+$_['heading_title']     = 'crwdns84013:0crwdne84013:0';
+
+// Text
+$_['text_success']      = 'crwdns84015:0crwdne84015:0';
+$_['text_list']         = 'crwdns84017:0crwdne84017:0';
+
+// Column
+$_['column_name']       = 'crwdns84019:0crwdne84019:0';
+$_['column_status']     = 'crwdns84021:0crwdne84021:0';
+$_['column_sort_order'] = 'crwdns84023:0crwdne84023:0';
+$_['column_action']     = 'crwdns84025:0crwdne84025:0';
+
+// Error
+$_['error_permission']  = 'crwdns84027:0crwdne84027:0';
