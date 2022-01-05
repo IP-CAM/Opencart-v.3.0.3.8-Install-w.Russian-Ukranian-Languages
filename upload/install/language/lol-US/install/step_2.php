f7bc92874090dd762b4a762174dd4e6a853e5ffa
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:25:17 +0200

    New translations step_2.php (LOLCAT)

---
+++ upload/install/language/lol-US/install/step_2.php
@@ -0,0 +1,62 @@
+<?php
+// Heading
+$_['heading_title']          = 'crwdns100745:0crwdne100745:0';
+
+// Text
+$_['text_step_2']            = 'crwdns100747:0crwdne100747:0';
+$_['text_install_php']       = 'crwdns100749:0crwdne100749:0';
+$_['text_install_extension'] = 'crwdns100751:0crwdne100751:0';
+$_['text_install_db']        = 'crwdns100753:0crwdne100753:0';
+$_['text_install_file']      = 'crwdns100755:0crwdne100755:0';
+$_['text_install_directory'] = 'crwdns100757:0crwdne100757:0';
+$_['text_setting']           = 'crwdns100759:0crwdne100759:0';
+$_['text_current']           = 'crwdns100761:0crwdne100761:0';
+$_['text_required']          = 'crwdns100763:0crwdne100763:0';
+$_['text_extension']         = 'crwdns100765:0crwdne100765:0';
+$_['text_db']                = 'crwdns100767:0crwdne100767:0';
+$_['text_db_driver']         = 'crwdns100769:0crwdne100769:0';
+$_['text_file']              = 'crwdns100771:0crwdne100771:0';
+$_['text_directory']         = 'crwdns100773:0crwdne100773:0';
+$_['text_status']            = 'crwdns100775:0crwdne100775:0';
+$_['text_version']           = 'crwdns100777:0crwdne100777:0';
+$_['text_global']            = 'crwdns100779:0crwdne100779:0';
+$_['text_magic']             = 'crwdns100781:0crwdne100781:0';
+$_['text_file_upload']       = 'crwdns100783:0crwdne100783:0';
+$_['text_session']           = 'crwdns100785:0crwdne100785:0';
+$_['text_gd']                = 'crwdns100787:0crwdne100787:0';
+$_['text_curl']              = 'crwdns100789:0crwdne100789:0';
+$_['text_openssl']           = 'crwdns100791:0crwdne100791:0';
+$_['text_zlib']              = 'crwdns100793:0crwdne100793:0';
+$_['text_zip']               = 'crwdns100795:0crwdne100795:0';
+$_['text_mbstring']          = 'crwdns100797:0crwdne100797:0';
+$_['text_on']                = 'crwdns100799:0crwdne100799:0';
+$_['text_off']               = 'crwdns100801:0crwdne100801:0';
+$_['text_writable']          = 'crwdns100803:0crwdne100803:0';
+$_['text_unwritable']        = 'crwdns100805:0crwdne100805:0';
+//$_['text_missing']           = 'Missing';
+
+// Error
+$_['error_missing']          = 'crwdns100807:0crwdne100807:0';
+$_['error_unwritable']       = 'crwdns100809:0crwdne100809:0';
+$_['error_version']          = 'crwdns100811:0crwdne100811:0';
+$_['error_file_upload']      = 'crwdns100813:0crwdne100813:0';
+$_['error_session']          = 'crwdns100815:0crwdne100815:0';
+$_['error_db']               = 'crwdns100817:0crwdne100817:0';
+$_['error_gd']               = 'crwdns100819:0crwdne100819:0';
+$_['error_curl']             = 'crwdns100821:0crwdne100821:0';
+$_['error_openssl']          = 'crwdns100823:0crwdne100823:0';
+$_['error_zlib']             = 'crwdns100825:0crwdne100825:0';
+$_['error_zip']              = 'crwdns100827:0crwdne100827:0';
+$_['error_mbstring']         = 'crwdns100829:0crwdne100829:0';
+$_['error_catalog_exist']    = 'crwdns100831:0crwdne100831:0';
+$_['error_catalog_writable'] = 'crwdns100833:0crwdne100833:0';
+$_['error_admin_exist']      = 'crwdns100835:0crwdne100835:0';
+$_['error_admin_writable']   = 'crwdns100837:0crwdne100837:0';
+$_['error_image']            = 'crwdns100839:0crwdne100839:0';
+$_['error_image_cache']      = 'crwdns100841:0crwdne100841:0';
+$_['error_image_catalog']    = 'crwdns100843:0crwdne100843:0';
+$_['error_cache']            = 'crwdns100845:0crwdne100845:0';
+$_['error_log']              = 'crwdns100847:0crwdne100847:0';
+$_['error_download']         = 'crwdns100849:0crwdne100849:0';
+$_['error_upload']           = 'crwdns100851:0crwdne100851:0';
+$_['error_modification']     = 'crwdns100853:0crwdne100853:0';
