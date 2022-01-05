f0acbac9036b7b7e83b3852f369b4ad6bf37b34e
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:17 +0200

    New translations latest.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/module/latest.php
@@ -0,0 +1,21 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns84869:0crwdne84869:0';
+
+// Text
+$_['text_extension']   = 'crwdns84871:0crwdne84871:0';
+$_['text_success']     = 'crwdns84873:0crwdne84873:0';
+$_['text_edit']        = 'crwdns84875:0crwdne84875:0';
+
+// Entry
+$_['entry_name']       = 'crwdns84877:0crwdne84877:0';
+$_['entry_limit']      = 'crwdns84879:0crwdne84879:0';
+$_['entry_width']      = 'crwdns84881:0crwdne84881:0';
+$_['entry_height']     = 'crwdns84883:0crwdne84883:0';
+$_['entry_status']     = 'crwdns84885:0crwdne84885:0';
+
+// Error
+$_['error_permission'] = 'crwdns84887:0crwdne84887:0';
+$_['error_name']       = 'crwdns84889:0crwdne84889:0';
+$_['error_width']      = 'crwdns84891:0crwdne84891:0';
+$_['error_height']     = 'crwdns84893:0crwdne84893:0';
