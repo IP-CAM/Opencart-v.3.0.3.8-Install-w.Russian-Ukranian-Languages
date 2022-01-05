1b187f69cf6e9e0ea0981e438e82c1d188336b04
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:25:08 +0200

    New translations squareup.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/recurring/squareup.php
@@ -0,0 +1,13 @@
+<?php
+// Text
+$_['text_title']                = 'crwdns99827:0crwdne99827:0';
+$_['text_canceled']             = 'crwdns99829:0crwdne99829:0';
+$_['text_confirm_cancel']       = 'crwdns99831:0crwdne99831:0';
+$_['text_order_history_cancel'] = 'crwdns99833:0crwdne99833:0';
+
+// Button
+$_['button_cancel']             = 'crwdns99835:0crwdne99835:0';
+
+// Error
+$_['error_not_cancelled']       = 'crwdns99837:0%scrwdne99837:0';
+$_['error_not_found']           = 'crwdns99839:0crwdne99839:0';
