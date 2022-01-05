0d3223b93c2afadd9a567ada9cef7c1d7cf157f5
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:48 +0200

    New translations wishlist.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/account/wishlist.php
@@ -0,0 +1,20 @@
+<?php
+// Heading
+$_['heading_title'] = 'crwdns96421:0crwdne96421:0';
+
+// Text
+$_['text_account']  = 'crwdns96423:0crwdne96423:0';
+$_['text_instock']  = 'crwdns96425:0crwdne96425:0';
+$_['text_wishlist'] = 'crwdns96427:0%scrwdne96427:0';
+$_['text_login']    = 'crwdns96429:0%scrwdnd96429:0%scrwdnd96429:0%scrwdnd96429:0%scrwdnd96429:0%scrwdne96429:0';
+$_['text_success']  = 'crwdns96431:0%scrwdnd96431:0%scrwdnd96431:0%scrwdne96431:0';
+$_['text_remove']   = 'crwdns96433:0crwdne96433:0';
+$_['text_empty']    = 'crwdns96435:0crwdne96435:0';
+
+// Column
+$_['column_image']  = 'crwdns96437:0crwdne96437:0';
+$_['column_name']   = 'crwdns96439:0crwdne96439:0';
+$_['column_model']  = 'crwdns96441:0crwdne96441:0';
+$_['column_stock']  = 'crwdns96443:0crwdne96443:0';
+$_['column_price']  = 'crwdns96445:0crwdne96445:0';
+$_['column_action'] = 'crwdns96447:0crwdne96447:0';
