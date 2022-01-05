921773c39c4c73c833859fd632d56d1a8c455c75
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:53 +0200

    New translations order_edit.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/mail/order_edit.php
@@ -0,0 +1,9 @@
+<?php
+// Text
+$_['text_subject']      = 'crwdns100325:0%scrwdnd100325:0%scrwdne100325:0';
+$_['text_order_id']     = 'crwdns100327:0crwdne100327:0';
+$_['text_date_added']   = 'crwdns100329:0crwdne100329:0';
+$_['text_order_status'] = 'crwdns100331:0crwdne100331:0';
+$_['text_comment']      = 'crwdns100333:0crwdne100333:0';
+$_['text_link']         = 'crwdns100335:0crwdne100335:0';
+$_['text_footer']       = 'crwdns100337:0crwdne100337:0';
