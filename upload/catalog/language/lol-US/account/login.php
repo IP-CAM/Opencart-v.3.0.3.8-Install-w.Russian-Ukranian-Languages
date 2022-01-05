deed24f7553a6b300278ad8af5af7f77cefb0c7e
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:10 +0200

    New translations login.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/account/login.php
@@ -0,0 +1,22 @@
+<?php
+// Heading
+$_['heading_title']                = 'crwdns95943:0crwdne95943:0';
+
+// Text
+$_['text_account']                 = 'crwdns95945:0crwdne95945:0';
+$_['text_login']                   = 'crwdns95947:0crwdne95947:0';
+$_['text_new_customer']            = 'crwdns95949:0crwdne95949:0';
+$_['text_register']                = 'crwdns95951:0crwdne95951:0';
+$_['text_register_account']        = 'crwdns95953:0crwdne95953:0';
+$_['text_returning_customer']      = 'crwdns95955:0crwdne95955:0';
+$_['text_i_am_returning_customer'] = 'crwdns95957:0crwdne95957:0';
+$_['text_forgotten']               = 'crwdns95959:0crwdne95959:0';
+
+// Entry
+$_['entry_email']                  = 'crwdns95961:0crwdne95961:0';
+$_['entry_password']               = 'crwdns95963:0crwdne95963:0';
+
+// Error
+$_['error_login']                  = 'crwdns95965:0crwdne95965:0';
+$_['error_attempts']               = 'crwdns95967:0crwdne95967:0';
+$_['error_approved']               = 'crwdns95969:0crwdne95969:0';
