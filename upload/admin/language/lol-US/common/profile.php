e523dc941f5e926a963a6ac0112c550fce3d70b2
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:20:39 +0200

    New translations profile.php (LOLCAT)

---
+++ upload/admin/language/lol-US/common/profile.php
@@ -0,0 +1,33 @@
+<?php
+// Heading
+$_['heading_title']         = 'crwdns82019:0crwdne82019:0';
+
+// Text
+$_['text_success']          = 'crwdns82021:0crwdne82021:0';
+$_['text_edit']             = 'crwdns82023:0crwdne82023:0';
+
+// Column
+$_['column_username']       = 'crwdns82025:0crwdne82025:0';
+$_['column_status']         = 'crwdns82027:0crwdne82027:0';
+$_['column_date_added']     = 'crwdns82029:0crwdne82029:0';
+$_['column_action']         = 'crwdns82031:0crwdne82031:0';
+
+// Entry
+$_['entry_username']        = 'crwdns82033:0crwdne82033:0';
+$_['entry_password']        = 'crwdns82035:0crwdne82035:0';
+$_['entry_confirm']         = 'crwdns82037:0crwdne82037:0';
+$_['entry_firstname']       = 'crwdns82039:0crwdne82039:0';
+$_['entry_lastname']        = 'crwdns82041:0crwdne82041:0';
+$_['entry_email']           = 'crwdns82043:0crwdne82043:0';
+$_['entry_image']           = 'crwdns82045:0crwdne82045:0';
+
+// Error
+$_['error_permission']      = 'crwdns82047:0crwdne82047:0';
+$_['error_exists_username'] = 'crwdns82049:0crwdne82049:0';
+$_['error_username']        = 'crwdns82051:0crwdne82051:0';
+$_['error_password']        = 'crwdns82053:0crwdne82053:0';
+$_['error_confirm']         = 'crwdns82055:0crwdne82055:0';
+$_['error_firstname']       = 'crwdns82057:0crwdne82057:0';
+$_['error_lastname']        = 'crwdns82059:0crwdne82059:0';
+$_['error_email']           = 'crwdns82061:0crwdne82061:0';
+$_['error_exists_email']    = 'crwdns82063:0crwdne82063:0';
