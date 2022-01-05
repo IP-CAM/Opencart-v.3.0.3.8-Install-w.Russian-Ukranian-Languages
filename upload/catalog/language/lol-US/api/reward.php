93024317afc230532ef8fcb6a651dace1fba946f
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:04 +0200

    New translations reward.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/api/reward.php
@@ -0,0 +1,9 @@
+<?php
+// Text
+$_['text_success']     = 'crwdns96673:0crwdne96673:0';
+
+// Error
+$_['error_permission'] = 'crwdns96675:0crwdne96675:0';
+$_['error_reward']     = 'crwdns96677:0crwdne96677:0';
+$_['error_points']     = 'crwdns96679:0%scrwdne96679:0';
+$_['error_maximum']    = 'crwdns96681:0%scrwdne96681:0';
