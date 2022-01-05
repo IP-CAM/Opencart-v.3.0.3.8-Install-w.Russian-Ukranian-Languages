a9652923fe787a57e391140375defdb4ca54302c
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:40 +0200

    New translations google.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/advertise/google.php
@@ -0,0 +1,6 @@
+<?php
+
+// Text
+$_['text_cron_email_message'] = 'crwdns97199:0%scrwdne97199:0';
+$_['text_cron_email_subject'] = 'crwdns97201:0crwdne97201:0';
+$_['text_per_day']            = 'crwdns97203:0%scrwdne97203:0';
