1b4956d921132952fb26673e17df329511a67e75
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:08 +0200

    New translations edit.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/account/edit.php
@@ -0,0 +1,23 @@
+<?php
+// Heading
+$_['heading_title']      = 'crwdns95887:0crwdne95887:0';
+
+// Text
+$_['text_account']       = 'crwdns95889:0crwdne95889:0';
+$_['text_edit']          = 'crwdns95891:0crwdne95891:0';
+$_['text_your_details']  = 'crwdns95893:0crwdne95893:0';
+$_['text_success']       = 'crwdns95895:0crwdne95895:0';
+
+// Entry
+$_['entry_firstname']    = 'crwdns95897:0crwdne95897:0';
+$_['entry_lastname']     = 'crwdns95899:0crwdne95899:0';
+$_['entry_email']        = 'crwdns95901:0crwdne95901:0';
+$_['entry_telephone']    = 'crwdns95903:0crwdne95903:0';
+
+// Error
+$_['error_exists']       = 'crwdns95905:0crwdne95905:0';
+$_['error_firstname']    = 'crwdns95907:0crwdne95907:0';
+$_['error_lastname']     = 'crwdns95909:0crwdne95909:0';
+$_['error_email']        = 'crwdns95911:0crwdne95911:0';
+$_['error_telephone']    = 'crwdns95913:0crwdne95913:0';
+$_['error_custom_field'] = 'crwdns95915:0%scrwdne95915:0';
