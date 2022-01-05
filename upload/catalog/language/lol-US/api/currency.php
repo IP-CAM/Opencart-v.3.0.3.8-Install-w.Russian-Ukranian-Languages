6b084d8b95cc6320a373093f7f21e8029f727103
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:00 +0200

    New translations currency.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/api/currency.php
@@ -0,0 +1,7 @@
+<?php
+// Text
+$_['text_success']     = 'crwdns96593:0crwdne96593:0';
+
+// Error
+$_['error_permission'] = 'crwdns96595:0crwdne96595:0';
+$_['error_currency']   = 'crwdns96597:0crwdne96597:0';
