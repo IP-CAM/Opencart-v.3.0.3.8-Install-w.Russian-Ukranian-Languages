c951b62bd5414dc7ff9699469c3a8a64f31d105b
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:53 +0200

    New translations reward.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/account/reward.php
@@ -0,0 +1,14 @@
+<?php
+// Heading
+$_['heading_title']      = 'crwdns96321:0crwdne96321:0';
+
+// Column
+$_['column_date_added']  = 'crwdns96323:0crwdne96323:0';
+$_['column_description'] = 'crwdns96325:0crwdne96325:0';
+$_['column_points']      = 'crwdns96327:0crwdne96327:0';
+
+// Text
+$_['text_account']       = 'crwdns96329:0crwdne96329:0';
+$_['text_reward']        = 'crwdns96331:0crwdne96331:0';
+$_['text_total']         = 'crwdns96333:0crwdne96333:0';
+$_['text_empty']         = 'crwdns96335:0crwdne96335:0';
