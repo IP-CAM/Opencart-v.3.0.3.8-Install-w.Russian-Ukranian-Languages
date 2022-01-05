ec3a08d1c001fae7648439683ed50611e97f88e3
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:20:49 +0200

    New translations information.php (LOLCAT)

---
+++ upload/admin/language/lol-US/catalog/information.php
@@ -0,0 +1,46 @@
+<?php
+// Heading
+$_['heading_title']          = 'crwdns81263:0crwdne81263:0';
+
+// Text
+$_['text_success']           = 'crwdns81265:0crwdne81265:0';
+$_['text_list']              = 'crwdns81267:0crwdne81267:0';
+$_['text_add']               = 'crwdns81269:0crwdne81269:0';
+$_['text_edit']              = 'crwdns81271:0crwdne81271:0';
+$_['text_default']           = 'crwdns81273:0crwdne81273:0';
+$_['text_keyword']           = 'crwdns81275:0crwdne81275:0';
+
+// Column
+$_['column_title']           = 'crwdns81277:0crwdne81277:0';
+$_['column_sort_order']      = 'crwdns81279:0crwdne81279:0';
+$_['column_action']          = 'crwdns81281:0crwdne81281:0';
+
+// Entry
+$_['entry_title']            = 'crwdns81283:0crwdne81283:0';
+$_['entry_description']      = 'crwdns81285:0crwdne81285:0';
+$_['entry_meta_title']       = 'crwdns81287:0crwdne81287:0';
+$_['entry_meta_keyword']     = 'crwdns81289:0crwdne81289:0';
+$_['entry_meta_description'] = 'crwdns81291:0crwdne81291:0';
+$_['entry_store']            = 'crwdns81293:0crwdne81293:0';
+$_['entry_keyword']          = 'crwdns81295:0crwdne81295:0';
+$_['entry_bottom']           = 'crwdns81297:0crwdne81297:0';
+$_['entry_status']           = 'crwdns81299:0crwdne81299:0';
+$_['entry_sort_order']       = 'crwdns81301:0crwdne81301:0';
+$_['entry_layout']           = 'crwdns81303:0crwdne81303:0';
+
+// Help
+$_['help_bottom']            = 'crwdns81305:0crwdne81305:0';
+
+// Error
+$_['error_warning']          = 'crwdns81307:0crwdne81307:0';
+$_['error_permission']       = 'crwdns81309:0crwdne81309:0';
+$_['error_title']            = 'crwdns81311:0crwdne81311:0';
+$_['error_description']      = 'crwdns81313:0crwdne81313:0';
+$_['error_meta_title']       = 'crwdns81315:0crwdne81315:0';
+$_['error_keyword']          = 'crwdns81317:0crwdne81317:0';
+$_['error_unique']           = 'crwdns81319:0crwdne81319:0';
+$_['error_account']          = 'crwdns81321:0crwdne81321:0';
+$_['error_checkout']         = 'crwdns81323:0crwdne81323:0';
+$_['error_affiliate']        = 'crwdns81325:0crwdne81325:0';
+$_['error_return']           = 'crwdns81327:0crwdne81327:0';
+$_['error_store']            = 'crwdns81329:0%scrwdne81329:0';
