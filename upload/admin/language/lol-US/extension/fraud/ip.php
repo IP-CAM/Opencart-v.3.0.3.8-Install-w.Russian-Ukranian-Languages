2e5d3119911b307ee7822fa87d555a6b3d0ab1bb
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:42 +0200

    New translations ip.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/fraud/ip.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']      = 'crwdns84261:0crwdne84261:0';
+
+// Text
+$_['text_extension']     = 'crwdns84263:0crwdne84263:0';
+$_['text_success']       = 'crwdns84265:0crwdne84265:0';
+$_['text_edit']          = 'crwdns84267:0crwdne84267:0';
+$_['text_ip_add']        = 'crwdns84269:0crwdne84269:0';
+$_['text_ip_list']       = 'crwdns84271:0crwdne84271:0';
+
+// Column
+$_['column_ip']          = 'crwdns84273:0crwdne84273:0';
+$_['column_total']       = 'crwdns84275:0crwdne84275:0';
+$_['column_date_added']  = 'crwdns84277:0crwdne84277:0';
+$_['column_action']      = 'crwdns84279:0crwdne84279:0';
+
+// Entry
+$_['entry_ip']           = 'crwdns84281:0crwdne84281:0';
+$_['entry_status']       = 'crwdns84283:0crwdne84283:0';
+$_['entry_order_status'] = 'crwdns84285:0crwdne84285:0';
+
+// Help
+$_['help_order_status']  = 'crwdns84287:0crwdne84287:0';
+
+// Error
+$_['error_permission']   = 'crwdns84289:0crwdne84289:0';
