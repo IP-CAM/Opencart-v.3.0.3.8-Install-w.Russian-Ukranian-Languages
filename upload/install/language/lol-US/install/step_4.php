3854f8fb6490676b02608afe6f6b13f75061bdc9
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:25:26 +0200

    New translations step_4.php (LOLCAT)

---
+++ upload/install/language/lol-US/install/step_4.php
@@ -0,0 +1,32 @@
+<?php
+// Heading
+$_['heading_title']               = 'crwdns100911:0crwdne100911:0';
+
+// Text
+$_['text_step_4']                 = 'crwdns100913:0crwdne100913:0';
+$_['text_catalog']                = 'crwdns100915:0crwdne100915:0';
+$_['text_admin']                  = 'crwdns100917:0crwdne100917:0';
+$_['text_loading']                = 'crwdns100919:0crwdne100919:0';
+$_['text_extension']              = 'crwdns100921:0crwdne100921:0';
+$_['text_mail']                   = 'crwdns100923:0crwdne100923:0';
+$_['text_mail_description']       = 'crwdns100925:0crwdne100925:0';
+$_['text_facebook']               = 'crwdns100927:0crwdne100927:0';
+$_['text_facebook_description']   = 'crwdns100929:0crwdne100929:0';
+$_['text_facebook_visit']         = 'crwdns100931:0crwdne100931:0';
+$_['text_forum']                  = 'crwdns100933:0crwdne100933:0';
+$_['text_forum_description']      = 'crwdns100935:0crwdne100935:0';
+$_['text_forum_visit']            = 'crwdns100937:0crwdne100937:0';
+$_['text_commercial']             = 'crwdns100939:0crwdne100939:0';
+$_['text_commercial_description'] = 'crwdns100941:0crwdne100941:0';
+$_['text_commercial_visit']       = 'crwdns100943:0crwdne100943:0';
+$_['text_price']                  = 'crwdns100945:0crwdne100945:0';
+$_['text_view']                   = 'crwdns100947:0crwdne100947:0';
+$_['text_download']               = 'crwdns100949:0crwdne100949:0';
+$_['text_downloads']              = 'crwdns100951:0crwdne100951:0';
+
+// Button
+$_['button_mail']                 = 'crwdns100953:0crwdne100953:0';
+$_['button_setup']                = 'crwdns100955:0crwdne100955:0';
+
+// Error
+$_['error_warning']               = 'crwdns100957:0crwdne100957:0';
