3333d1680da9b751cdfee4200cff19250828f0f4
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:41 +0200

    New translations google_sitemap.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/feed/google_sitemap.php
@@ -0,0 +1,15 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns84105:0crwdne84105:0';
+
+// Text
+$_['text_extension']   = 'crwdns84107:0crwdne84107:0';
+$_['text_success']     = 'crwdns84109:0crwdne84109:0';
+$_['text_edit']        = 'crwdns84111:0crwdne84111:0';
+
+// Entry
+$_['entry_status']     = 'crwdns84113:0crwdne84113:0';
+$_['entry_data_feed']  = 'crwdns84115:0crwdne84115:0';
+
+// Error
+$_['error_permission'] = 'crwdns84117:0crwdne84117:0';
