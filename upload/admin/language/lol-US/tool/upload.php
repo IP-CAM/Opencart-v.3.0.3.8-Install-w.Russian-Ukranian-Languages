4b3d9ef213caec9c2f1839244c22d6dec3c46f30
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:02 +0200

    New translations upload.php (LOLCAT)

---
+++ upload/admin/language/lol-US/tool/upload.php
@@ -0,0 +1,23 @@
+<?php
+// Heading
+$_['heading_title']     = 'crwdns95563:0crwdne95563:0';
+
+// Text
+$_['text_success']      = 'crwdns95565:0crwdne95565:0';
+$_['text_list']         = 'crwdns95567:0crwdne95567:0';
+
+// Column
+$_['column_name']       = 'crwdns95569:0crwdne95569:0';
+$_['column_filename']   = 'crwdns95571:0crwdne95571:0';
+$_['column_date_added'] = 'crwdns95573:0crwdne95573:0';
+$_['column_action']     = 'crwdns95575:0crwdne95575:0';
+
+// Entry
+$_['entry_name']        = 'crwdns95577:0crwdne95577:0';
+$_['entry_filename']    = 'crwdns95579:0crwdne95579:0';
+$_['entry_date_added']  = 'crwdns95581:0crwdne95581:0';
+
+// Error
+$_['error_permission']  = 'crwdns95583:0crwdne95583:0';
+$_['error_upload']      = 'crwdns95585:0crwdne95585:0';
+$_['error_file']        = 'crwdns95587:0crwdne95587:0';
