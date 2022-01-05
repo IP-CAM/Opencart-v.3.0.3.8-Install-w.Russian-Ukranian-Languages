fbb137f8517f8041b976ee74e6827cce6ffa349b
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:10 +0200

    New translations cod.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/payment/cod.php
@@ -0,0 +1,21 @@
+<?php
+// Heading
+$_['heading_title']      = 'crwdns86091:0crwdne86091:0';
+
+// Text
+$_['text_extension']     = 'crwdns86093:0crwdne86093:0';
+$_['text_success']       = 'crwdns86095:0crwdne86095:0';
+$_['text_edit']          = 'crwdns86097:0crwdne86097:0';
+
+// Entry
+$_['entry_total']        = 'crwdns86099:0crwdne86099:0';
+$_['entry_order_status'] = 'crwdns86101:0crwdne86101:0';
+$_['entry_geo_zone']     = 'crwdns86103:0crwdne86103:0';
+$_['entry_status']       = 'crwdns86105:0crwdne86105:0';
+$_['entry_sort_order']   = 'crwdns86107:0crwdne86107:0';
+
+// Help
+$_['help_total']         = 'crwdns86109:0crwdne86109:0';
+
+// Error
+$_['error_permission']   = 'crwdns86111:0crwdne86111:0';
