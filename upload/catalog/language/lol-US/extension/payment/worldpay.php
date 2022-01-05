bda0ab7c6b345377b895d09fd7424b29265eced9
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:25:07 +0200

    New translations worldpay.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/payment/worldpay.php
@@ -0,0 +1,28 @@
+<?php
+// Text
+$_['text_title']			 = 'crwdns99777:0crwdne99777:0';
+$_['text_credit_card']		 = 'crwdns99779:0crwdne99779:0';
+$_['text_card_type']		 = 'crwdns99781:0crwdne99781:0';
+$_['text_card_name']		 = 'crwdns99783:0crwdne99783:0';
+$_['text_card_digits']		 = 'crwdns99785:0crwdne99785:0';
+$_['text_card_expiry']		 = 'crwdns99787:0crwdne99787:0';
+$_['text_trial']			 = 'crwdns99789:0%scrwdnd99789:0%scrwdnd99789:0%scrwdnd99789:0%scrwdne99789:0';
+$_['text_recurring']		 = 'crwdns99791:0%scrwdnd99791:0%scrwdnd99791:0%scrwdne99791:0';
+$_['text_length']			 = 'crwdns99793:0%scrwdne99793:0';
+$_['text_confirm_delete']	 = 'crwdns99795:0crwdne99795:0';
+$_['text_card_success']		 = 'crwdns99797:0crwdne99797:0';
+$_['text_card_error']		 = 'crwdns99799:0crwdne99799:0';
+
+// Entry
+$_['entry_card']			 = 'crwdns99801:0crwdne99801:0';
+$_['entry_card_existing']	 = 'crwdns99803:0crwdne99803:0';
+$_['entry_card_new']		 = 'crwdns99805:0crwdne99805:0';
+$_['entry_card_save']		 = 'crwdns99807:0crwdne99807:0';
+$_['entry_cc_cvc']			 = 'crwdns99809:0crwdne99809:0';
+$_['entry_cc_choice']		 = 'crwdns99811:0crwdne99811:0';
+
+// Button
+$_['button_delete_card']	 = 'crwdns99813:0crwdne99813:0';
+
+// Error
+$_['error_process_order']	 = 'crwdns99815:0crwdne99815:0';
