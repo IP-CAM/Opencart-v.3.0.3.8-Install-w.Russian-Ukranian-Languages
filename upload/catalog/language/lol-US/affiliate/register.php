8026ac1e694804b933dd8d3ad94500050a12126d
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:57 +0200

    New translations register.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/affiliate/register.php
@@ -0,0 +1,52 @@
+<?php
+// Heading
+$_['heading_title']             = 'crwdns96477:0crwdne96477:0';
+
+// Text
+$_['text_account']              = 'crwdns96479:0crwdne96479:0';
+$_['text_register']             = 'crwdns96481:0crwdne96481:0';
+$_['text_account_already']      = 'crwdns96483:0%scrwdne96483:0';
+$_['text_signup']               = 'crwdns96485:0crwdne96485:0';
+$_['text_your_details']         = 'crwdns96487:0crwdne96487:0';
+$_['text_your_address']         = 'crwdns96489:0crwdne96489:0';
+$_['text_your_affiliate']       = 'crwdns96491:0crwdne96491:0';
+$_['text_your_password']        = 'crwdns96493:0crwdne96493:0';
+$_['text_cheque']               = 'crwdns96495:0crwdne96495:0';
+$_['text_paypal']               = 'crwdns96497:0crwdne96497:0';
+$_['text_bank']                 = 'crwdns96499:0crwdne96499:0';
+$_['text_agree']                = 'crwdns96501:0%scrwdnd96501:0%scrwdne96501:0';
+
+// Entry
+$_['entry_customer_group']      = 'crwdns96503:0crwdne96503:0';
+$_['entry_firstname']           = 'crwdns96505:0crwdne96505:0';
+$_['entry_lastname']            = 'crwdns96507:0crwdne96507:0';
+$_['entry_email']               = 'crwdns96509:0crwdne96509:0';
+$_['entry_telephone']           = 'crwdns96511:0crwdne96511:0';
+$_['entry_company']             = 'crwdns96513:0crwdne96513:0';
+$_['entry_website']             = 'crwdns96515:0crwdne96515:0';
+$_['entry_tax']                 = 'crwdns96517:0crwdne96517:0';
+$_['entry_payment']             = 'crwdns96519:0crwdne96519:0';
+$_['entry_cheque']              = 'crwdns96521:0crwdne96521:0';
+$_['entry_paypal']              = 'crwdns96523:0crwdne96523:0';
+$_['entry_bank_name']           = 'crwdns96525:0crwdne96525:0';
+$_['entry_bank_branch_number']  = 'crwdns96527:0crwdne96527:0';
+$_['entry_bank_swift_code']     = 'crwdns96529:0crwdne96529:0';
+$_['entry_bank_account_name']   = 'crwdns96531:0crwdne96531:0';
+$_['entry_bank_account_number'] = 'crwdns96533:0crwdne96533:0';
+$_['entry_password']            = 'crwdns96535:0crwdne96535:0';
+$_['entry_confirm']             = 'crwdns96537:0crwdne96537:0';
+
+// Error
+$_['error_exists']              = 'crwdns96539:0crwdne96539:0';
+$_['error_firstname']           = 'crwdns96541:0crwdne96541:0';
+$_['error_lastname']            = 'crwdns96543:0crwdne96543:0';
+$_['error_email']               = 'crwdns96545:0crwdne96545:0';
+$_['error_telephone']           = 'crwdns96547:0crwdne96547:0';
+$_['error_custom_field']        = 'crwdns96549:0%scrwdne96549:0';
+$_['error_cheque']              = 'crwdns96551:0crwdne96551:0';
+$_['error_paypal']              = 'crwdns96553:0crwdne96553:0';
+$_['error_bank_account_name']   = 'crwdns96555:0crwdne96555:0';
+$_['error_bank_account_number'] = 'crwdns96557:0crwdne96557:0';
+$_['error_password']            = 'crwdns96559:0crwdne96559:0';
+$_['error_confirm']             = 'crwdns96561:0crwdne96561:0';
+$_['error_agree']               = 'crwdns96563:0%scrwdne96563:0';
