56cdbef2991e84c89d8cc18bceb210ae798c8005
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:25:02 +0200

    New translations securetrading_ws.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/payment/securetrading_ws.php
@@ -0,0 +1,29 @@
+<?php
+$_['text_title'] = 'crwdns99607:0crwdne99607:0';
+$_['text_card_details'] = 'crwdns99609:0crwdne99609:0';
+$_['text_wait'] = 'crwdns99611:0crwdne99611:0';
+$_['text_auth_code'] = 'crwdns99613:0%scrwdne99613:0';
+$_['text_postcode_check'] = 'crwdns99615:0%scrwdne99615:0';
+$_['text_security_code_check'] = 'crwdns99617:0%scrwdne99617:0';
+$_['text_address_check'] = 'crwdns99619:0%scrwdne99619:0';
+$_['text_3d_secure_check'] = 'crwdns99621:0%scrwdne99621:0';
+$_['text_not_given'] = 'crwdns99623:0crwdne99623:0';
+$_['text_not_checked'] = 'crwdns99625:0crwdne99625:0';
+$_['text_match'] = 'crwdns99627:0crwdne99627:0';
+$_['text_not_match'] = 'crwdns99629:0crwdne99629:0';
+$_['text_authenticated'] = 'crwdns99631:0crwdne99631:0';
+$_['text_not_authenticated'] = 'crwdns99633:0crwdne99633:0';
+$_['text_authentication_not_completed'] = 'crwdns99635:0crwdne99635:0';
+$_['text_unable_to_perform'] = 'crwdns99637:0crwdne99637:0';
+$_['text_transaction_declined'] = 'crwdns99639:0crwdne99639:0';
+$_['text_transaction_failed'] = 'crwdns99641:0crwdne99641:0';
+$_['text_connection_error'] = 'crwdns99643:0crwdne99643:0';
+
+$_['entry_type'] = 'crwdns99645:0crwdne99645:0';
+$_['entry_number'] = 'crwdns99647:0crwdne99647:0';
+$_['entry_expire_date'] = 'crwdns99649:0crwdne99649:0';
+$_['entry_cvv2'] = 'crwdns99651:0crwdne99651:0';
+
+$_['button_confirm'] = 'crwdns99653:0crwdne99653:0';
+
+$_['error_failure'] = 'crwdns99655:0crwdne99655:0';
