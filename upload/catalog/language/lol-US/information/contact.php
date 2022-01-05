d0d5edbef25c61d51d18e78eaa00df4a137d7dc9
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:48 +0200

    New translations contact.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/information/contact.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']  = 'crwdns100159:0crwdne100159:0';
+
+// Text
+$_['text_location']  = 'crwdns100161:0crwdne100161:0';
+$_['text_store']     = 'crwdns100163:0crwdne100163:0';
+$_['text_contact']   = 'crwdns100165:0crwdne100165:0';
+$_['text_address']   = 'crwdns100167:0crwdne100167:0';
+$_['text_telephone'] = 'crwdns100169:0crwdne100169:0';
+$_['text_fax']       = 'crwdns100171:0crwdne100171:0';
+$_['text_open']      = 'crwdns100173:0crwdne100173:0';
+$_['text_comment']   = 'crwdns100175:0crwdne100175:0';
+$_['text_message']   = 'crwdns100177:0crwdne100177:0'; 
+
+// Entry
+$_['entry_name']     = 'crwdns100179:0crwdne100179:0';
+$_['entry_email']    = 'crwdns100181:0crwdne100181:0';
+$_['entry_enquiry']  = 'crwdns100183:0crwdne100183:0';
+
+// Email
+$_['email_subject']  = 'crwdns100185:0%scrwdne100185:0';
+
+// Errors
+$_['error_name']     = 'crwdns100187:0crwdne100187:0';
+$_['error_email']    = 'crwdns100189:0crwdne100189:0';
+$_['error_enquiry']  = 'crwdns100191:0crwdne100191:0';
