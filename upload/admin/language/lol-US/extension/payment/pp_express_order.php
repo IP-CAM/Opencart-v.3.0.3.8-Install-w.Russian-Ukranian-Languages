10408d66618607dbea3aca3aabc9a8f03e4c3c5d
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:50 +0200

    New translations pp_express_order.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/payment/pp_express_order.php
@@ -0,0 +1,52 @@
+<?php
+// Text
+$_['text_extension']		 = 'crwdns88995:0crwdne88995:0';
+$_['text_capture_status']	 = 'crwdns88997:0crwdne88997:0';
+$_['text_amount_authorised'] = 'crwdns88999:0crwdne88999:0';
+$_['text_amount_captured']	 = 'crwdns89001:0crwdne89001:0';
+$_['text_amount_refunded']	 = 'crwdns89003:0crwdne89003:0';
+$_['text_transaction']		 = 'crwdns89005:0crwdne89005:0';
+$_['text_complete']			 = 'crwdns89007:0crwdne89007:0';
+$_['text_confirm_void']		 = 'crwdns89009:0crwdne89009:0';
+$_['text_view']				 = 'crwdns89011:0crwdne89011:0';
+$_['text_refund']			 = 'crwdns89013:0crwdne89013:0';
+$_['text_resend']			 = 'crwdns89015:0crwdne89015:0';
+$_['text_success']           = 'crwdns89017:0crwdne89017:0';
+$_['text_full_refund']		 = 'crwdns89019:0crwdne89019:0';
+$_['text_partial_refund']	 = 'crwdns89021:0crwdne89021:0';
+$_['text_payment']		 	 = 'crwdns89023:0crwdne89023:0';
+$_['text_current_refunds']   = 'crwdns89025:0crwdne89025:0';
+
+// Column
+$_['column_transaction']	 = 'crwdns89027:0crwdne89027:0';
+$_['column_amount']			 = 'crwdns89029:0crwdne89029:0';
+$_['column_type']			 = 'crwdns89031:0crwdne89031:0';
+$_['column_status']			 = 'crwdns89033:0crwdne89033:0';
+$_['column_pending_reason']	 = 'crwdns89035:0crwdne89035:0';
+$_['column_date_added']		 = 'crwdns89037:0crwdne89037:0';
+$_['column_action']			 = 'crwdns89039:0crwdne89039:0';
+
+// Entry
+$_['entry_capture_amount']	 = 'crwdns89041:0crwdne89041:0';
+$_['entry_capture_complete'] = 'crwdns89043:0crwdne89043:0';
+$_['entry_full_refund']		 = 'crwdns89045:0crwdne89045:0';
+$_['entry_amount']			 = 'crwdns89047:0crwdne89047:0';
+$_['entry_note']             = 'crwdns89049:0crwdne89049:0';
+
+// Help
+$_['help_capture_complete']  = 'crwdns89051:0crwdne89051:0';
+
+// Tab
+$_['tab_capture']		     = 'crwdns89053:0crwdne89053:0';
+$_['tab_refund']             = 'crwdns89055:0crwdne89055:0';
+
+// Button
+$_['button_void']			 = 'crwdns89057:0crwdne89057:0';
+$_['button_capture']		 = 'crwdns89059:0crwdne89059:0';
+$_['button_refund']		     = 'crwdns89061:0crwdne89061:0';
+
+// Error
+$_['error_capture']		     = 'crwdns89063:0crwdne89063:0';
+$_['error_transaction']	     = 'crwdns89065:0crwdne89065:0';
+$_['error_not_found']	     = 'crwdns89067:0crwdne89067:0';
+$_['error_partial_amt']		 = 'crwdns89069:0crwdne89069:0';
