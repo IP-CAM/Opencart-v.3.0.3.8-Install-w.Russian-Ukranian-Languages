f9c4954a6e74f931eebcd980cd06d7bd5d0af8da
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:29 +0200

    New translations advertise.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/extension/advertise.php
@@ -0,0 +1,17 @@
+<?php
+
+// Heading
+$_['heading_title'] = "crwdns83841:0crwdne83841:0";
+
+
+// Columns
+$_['column_name']   = "crwdns83843:0crwdne83843:0";
+$_['column_status'] = "crwdns83845:0crwdne83845:0";
+$_['column_action'] = "crwdns83847:0crwdne83847:0";
+
+
+// Text
+$_['text_success']   = "crwdns83849:0crwdne83849:0";
+
+// Error
+$_['error_adblock'] = "crwdns83851:0crwdne83851:0";
