3ec375504ea6106a3082c60906bf561cbaa7f1fe
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:49 +0200

    New translations recurring.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/account/recurring.php
@@ -0,0 +1,52 @@
+<?php
+// Heading
+$_['heading_title']                        = 'crwdns96063:0crwdne96063:0';
+
+// Text
+$_['text_account']                         = 'crwdns96065:0crwdne96065:0';
+$_['text_recurring']                       = 'crwdns96067:0crwdne96067:0';
+$_['text_recurring_detail']                = 'crwdns96069:0crwdne96069:0';
+$_['text_order_recurring_id']              = 'crwdns96071:0crwdne96071:0';
+$_['text_date_added']                      = 'crwdns96073:0crwdne96073:0';
+$_['text_status']                          = 'crwdns96075:0crwdne96075:0';
+$_['text_payment_method']                  = 'crwdns96077:0crwdne96077:0';
+$_['text_order_id']                        = 'crwdns96079:0crwdne96079:0';
+$_['text_product']                         = 'crwdns96081:0crwdne96081:0';
+$_['text_quantity']                        = 'crwdns96083:0crwdne96083:0';
+$_['text_description']                     = 'crwdns96085:0crwdne96085:0';
+$_['text_reference']                       = 'crwdns96087:0crwdne96087:0';
+$_['text_transaction']                     = 'crwdns96089:0crwdne96089:0';
+$_['text_status_1']                        = 'crwdns96091:0crwdne96091:0';
+$_['text_status_2']                        = 'crwdns96093:0crwdne96093:0';
+$_['text_status_3']                        = 'crwdns96095:0crwdne96095:0';
+$_['text_status_4']                        = 'crwdns96097:0crwdne96097:0';
+$_['text_status_5']                        = 'crwdns96099:0crwdne96099:0';
+$_['text_status_6']                        = 'crwdns96101:0crwdne96101:0';
+$_['text_transaction_date_added']          = 'crwdns96103:0crwdne96103:0';
+$_['text_transaction_payment']             = 'crwdns96105:0crwdne96105:0';
+$_['text_transaction_outstanding_payment'] = 'crwdns96107:0crwdne96107:0';
+$_['text_transaction_skipped']             = 'crwdns96109:0crwdne96109:0';
+$_['text_transaction_failed']              = 'crwdns96111:0crwdne96111:0';
+$_['text_transaction_cancelled']           = 'crwdns96113:0crwdne96113:0';
+$_['text_transaction_suspended']           = 'crwdns96115:0crwdne96115:0';
+$_['text_transaction_suspended_failed']    = 'crwdns96117:0crwdne96117:0';
+$_['text_transaction_outstanding_failed']  = 'crwdns96119:0crwdne96119:0';
+$_['text_transaction_expired']             = 'crwdns96121:0crwdne96121:0';
+$_['text_empty']                           = 'crwdns96123:0crwdne96123:0';
+$_['text_error']                           = 'crwdns96125:0crwdne96125:0';
+$_['text_cancelled']                       = 'crwdns96127:0crwdne96127:0';
+
+// Column
+$_['column_date_added']                    = 'crwdns96129:0crwdne96129:0';
+$_['column_type']                          = 'crwdns96131:0crwdne96131:0';
+$_['column_amount']                        = 'crwdns96133:0crwdne96133:0';
+$_['column_status']                        = 'crwdns96135:0crwdne96135:0';
+$_['column_product']                       = 'crwdns96137:0crwdne96137:0';
+$_['column_order_recurring_id']            = 'crwdns96139:0crwdne96139:0';
+
+// Error
+$_['error_not_cancelled']                  = 'crwdns96141:0%scrwdne96141:0';
+$_['error_not_found']                      = 'crwdns96143:0crwdne96143:0';
+
+// Button
+$_['button_return']                        = 'crwdns96145:0crwdne96145:0';
