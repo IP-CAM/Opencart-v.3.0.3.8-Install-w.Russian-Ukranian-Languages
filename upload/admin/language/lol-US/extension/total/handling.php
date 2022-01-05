f6dddec281d3905dce38b53145d81a4957614f98
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:25 +0200

    New translations handling.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/total/handling.php
@@ -0,0 +1,21 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns93063:0crwdne93063:0';
+
+// Text
+$_['text_extension']   = 'crwdns93065:0crwdne93065:0';
+$_['text_success']     = 'crwdns93067:0crwdne93067:0';
+$_['text_edit']        = 'crwdns93069:0crwdne93069:0';
+
+// Entry
+$_['entry_total']      = 'crwdns93071:0crwdne93071:0';
+$_['entry_fee']        = 'crwdns93073:0crwdne93073:0';
+$_['entry_tax_class']  = 'crwdns93075:0crwdne93075:0';
+$_['entry_status']     = 'crwdns93077:0crwdne93077:0';
+$_['entry_sort_order'] = 'crwdns93079:0crwdne93079:0';
+
+// Help
+$_['help_total']       = 'crwdns93081:0crwdne93081:0';
+
+// Error
+$_['error_permission'] = 'crwdns93083:0crwdne93083:0';
