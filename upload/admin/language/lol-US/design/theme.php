2be981abc09811b6df652884bf8fa5931730f647
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:20:38 +0200

    New translations theme.php (LOLCAT)

---
+++ upload/admin/language/lol-US/design/theme.php
@@ -0,0 +1,23 @@
+<?php
+// Heading
+$_['heading_title']     = 'crwdns82609:0crwdne82609:0';
+
+// Text
+$_['text_success']      = 'crwdns82611:0crwdne82611:0';
+$_['text_edit']         = 'crwdns82613:0crwdne82613:0';
+$_['text_store']        = 'crwdns82615:0crwdne82615:0';
+$_['text_template']     = 'crwdns82617:0crwdne82617:0';
+$_['text_default']      = 'crwdns82619:0crwdne82619:0';
+$_['text_history']      = 'crwdns82621:0crwdne82621:0';
+$_['text_twig']         = 'crwdns82623:0crwdne82623:0';
+
+// Column
+$_['column_store']      = 'crwdns82625:0crwdne82625:0';
+$_['column_route']      = 'crwdns82627:0crwdne82627:0';
+$_['column_theme']      = 'crwdns82629:0crwdne82629:0';
+$_['column_date_added'] = 'crwdns82631:0crwdne82631:0';
+$_['column_action']     = 'crwdns82633:0crwdne82633:0';
+
+// Error
+$_['error_permission']  = 'crwdns82635:0crwdne82635:0';
+$_['error_twig']        = 'crwdns82637:0crwdne82637:0';
