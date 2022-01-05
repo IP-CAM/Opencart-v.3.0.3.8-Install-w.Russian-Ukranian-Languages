1b63521f7a88323a8b23f4c28a602305227bfd8e
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:18 +0200

    New translations laybuy_layout.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/module/laybuy_layout.php
@@ -0,0 +1,14 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns84895:0crwdne84895:0';
+
+// Text
+$_['text_extension']   = 'crwdns84897:0crwdne84897:0';
+$_['text_success']     = 'crwdns84899:0crwdne84899:0';
+$_['text_edit']        = 'crwdns84901:0crwdne84901:0';
+
+// Entry
+$_['entry_status']     = 'crwdns84903:0crwdne84903:0';
+
+// Error
+$_['error_permission'] = 'crwdns84905:0crwdne84905:0';
