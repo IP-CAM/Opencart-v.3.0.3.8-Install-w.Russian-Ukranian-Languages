a6bc67ec0889f614fe59897c2b89d1d3c1db7730
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:20 +0200

    New translations skrill.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/payment/skrill.php
@@ -0,0 +1,29 @@
+<?php
+// Heading
+$_['heading_title']			  = 'crwdns90989:0crwdne90989:0';
+
+// Text
+$_['text_extension']		  = 'crwdns90991:0crwdne90991:0';
+$_['text_success']			  = 'crwdns90993:0crwdne90993:0';
+$_['text_edit']               = 'crwdns90995:0crwdne90995:0';
+$_['text_skrill']	     	  = 'crwdns90997:0crwdne90997:0';
+
+// Entry
+$_['entry_email']			  = 'crwdns90999:0crwdne90999:0';
+$_['entry_secret']		      = 'crwdns91001:0crwdne91001:0';
+$_['entry_total']			  = 'crwdns91003:0crwdne91003:0';
+$_['entry_order_status']	  = 'crwdns91005:0crwdne91005:0';
+$_['entry_pending_status']	  = 'crwdns91007:0crwdne91007:0';
+$_['entry_canceled_status']	  = 'crwdns91009:0crwdne91009:0';
+$_['entry_failed_status']	  = 'crwdns91011:0crwdne91011:0';
+$_['entry_chargeback_status'] = 'crwdns91013:0crwdne91013:0';
+$_['entry_geo_zone']		  = 'crwdns91015:0crwdne91015:0';
+$_['entry_status']			  = 'crwdns91017:0crwdne91017:0';
+$_['entry_sort_order']		  = 'crwdns91019:0crwdne91019:0';
+
+// Help
+$_['help_total']			  = 'crwdns91021:0crwdne91021:0';
+
+// Error
+$_['error_permission']		  = 'crwdns91023:0crwdne91023:0';
+$_['error_email']			  = 'crwdns91025:0crwdne91025:0';
