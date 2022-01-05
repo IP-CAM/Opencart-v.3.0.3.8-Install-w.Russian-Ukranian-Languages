c9bcf2ea8ee4af1f347844b51d7cf0719ed44c0a
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:25 +0200

    New translations api.php (LOLCAT)

---
+++ upload/admin/language/lol-US/marketplace/api.php
@@ -0,0 +1,16 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns94057:0crwdne94057:0';
+
+// Text
+$_['text_success']     = 'crwdns94059:0crwdne94059:0';
+$_['text_signup']      = 'crwdns94061:0crwdne94061:0';
+
+// Entry
+$_['entry_username']   = 'crwdns94063:0crwdne94063:0';
+$_['entry_secret']     = 'crwdns94065:0crwdne94065:0';
+
+// Error
+$_['error_permission'] = 'crwdns94067:0crwdne94067:0';
+$_['error_username']   = 'crwdns94069:0crwdne94069:0';
+$_['error_secret']     = 'crwdns94071:0crwdne94071:0';
