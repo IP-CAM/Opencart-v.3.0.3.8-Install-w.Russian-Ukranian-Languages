7e779363a315661a48741fb64e1ed44830f17210
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:09 +0200

    New translations cheque.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/payment/cheque.php
@@ -0,0 +1,23 @@
+<?php
+// Heading
+$_['heading_title']      = 'crwdns86065:0crwdne86065:0';
+
+// Text
+$_['text_extension']     = 'crwdns86067:0crwdne86067:0';
+$_['text_success']       = 'crwdns86069:0crwdne86069:0';
+$_['text_edit']          = 'crwdns86071:0crwdne86071:0';
+
+// Entry
+$_['entry_payable']      = 'crwdns86073:0crwdne86073:0';
+$_['entry_total']        = 'crwdns86075:0crwdne86075:0';
+$_['entry_order_status'] = 'crwdns86077:0crwdne86077:0';
+$_['entry_geo_zone']     = 'crwdns86079:0crwdne86079:0';
+$_['entry_status']       = 'crwdns86081:0crwdne86081:0';
+$_['entry_sort_order']   = 'crwdns86083:0crwdne86083:0';
+
+// Help
+$_['help_total']         = 'crwdns86085:0crwdne86085:0';
+
+// Error
+$_['error_permission']   = 'crwdns86087:0crwdne86087:0';
+$_['error_payable']      = 'crwdns86089:0crwdne86089:0';
