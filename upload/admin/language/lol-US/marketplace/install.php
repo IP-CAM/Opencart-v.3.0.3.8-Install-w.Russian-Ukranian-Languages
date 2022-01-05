6227ee0fc52c0b91d0db9e27d4778546161d9ff7
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:27 +0200

    New translations install.php (LOLCAT)

---
+++ upload/admin/language/lol-US/marketplace/install.php
@@ -0,0 +1,18 @@
+<?php
+// Text
+$_['text_success']     = 'crwdns94107:0crwdne94107:0';
+$_['text_unzip']       = 'crwdns94109:0crwdne94109:0';
+$_['text_move']        = 'crwdns94111:0crwdne94111:0';
+$_['text_xml']         = 'crwdns94113:0crwdne94113:0';
+$_['text_remove']      = 'crwdns94115:0crwdne94115:0';
+
+// Error
+$_['error_permission'] = 'crwdns94117:0crwdne94117:0';
+$_['error_install']    = 'crwdns94119:0crwdne94119:0';
+$_['error_unzip']      = 'crwdns94121:0crwdne94121:0';
+$_['error_file']       = 'crwdns94123:0crwdne94123:0';
+$_['error_directory']  = 'crwdns94125:0crwdne94125:0';
+$_['error_code']       = 'crwdns94127:0crwdne94127:0';
+$_['error_xml']        = 'crwdns94129:0%scrwdne94129:0';
+$_['error_exists']     = 'crwdns94131:0%scrwdne94131:0';
+$_['error_allowed']    = 'crwdns94133:0%scrwdne94133:0';
