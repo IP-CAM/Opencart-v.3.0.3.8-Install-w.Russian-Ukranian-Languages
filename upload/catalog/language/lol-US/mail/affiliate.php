c736c41fe60c6796320010fab079a5e76925e84c
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:51 +0200

    New translations affiliate.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/mail/affiliate.php
@@ -0,0 +1,17 @@
+<?php
+// Text
+$_['text_subject']        = 'crwdns100221:0%scrwdne100221:0';
+$_['text_welcome']        = 'crwdns100223:0%scrwdne100223:0';
+$_['text_login']          = 'crwdns100225:0crwdne100225:0';
+$_['text_approval']       = 'crwdns100227:0crwdne100227:0';
+$_['text_service']        = 'crwdns100229:0crwdne100229:0';
+$_['text_thanks']         = 'crwdns100231:0crwdne100231:0';
+$_['text_new_affiliate']  = 'crwdns100233:0crwdne100233:0';
+$_['text_signup']         = 'crwdns100235:0crwdne100235:0';
+$_['text_website']        = 'crwdns100237:0crwdne100237:0';
+$_['text_customer_group'] = 'crwdns100239:0crwdne100239:0';
+$_['text_firstname']      = 'crwdns100241:0crwdne100241:0';
+$_['text_lastname']       = 'crwdns100243:0crwdne100243:0';
+$_['text_company']        = 'crwdns100245:0crwdne100245:0';
+$_['text_email']          = 'crwdns100247:0crwdne100247:0';
+$_['text_telephone']      = 'crwdns100249:0crwdne100249:0';
