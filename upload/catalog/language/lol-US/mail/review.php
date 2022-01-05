810abd4f4725bfe9a8d22cb02ee6a6a6f960b691
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:25:14 +0200

    New translations review.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/mail/review.php
@@ -0,0 +1,8 @@
+<?php
+// Text
+$_['text_subject']  = 'crwdns100365:0%scrwdne100365:0';
+$_['text_waiting']  = 'crwdns100367:0crwdne100367:0';
+$_['text_product']  = 'crwdns100369:0%scrwdne100369:0';
+$_['text_reviewer'] = 'crwdns100371:0%scrwdne100371:0';
+$_['text_rating']   = 'crwdns100373:0%scrwdne100373:0';
+$_['text_review']   = 'crwdns100375:0crwdne100375:0';
