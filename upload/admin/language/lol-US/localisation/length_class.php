40ca3351dcf1e2340ee696da48a1874262eb0c80
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:34 +0200

    New translations length_class.php (LOLCAT)

---
+++ upload/admin/language/lol-US/localisation/length_class.php
@@ -0,0 +1,30 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns93409:0crwdne93409:0';
+
+// Text
+$_['text_success']     = 'crwdns93411:0crwdne93411:0';
+$_['text_list']        = 'crwdns93413:0crwdne93413:0';
+$_['text_add']         = 'crwdns93415:0crwdne93415:0';
+$_['text_edit']        = 'crwdns93417:0crwdne93417:0';
+
+// Column
+$_['column_title']     = 'crwdns93419:0crwdne93419:0';
+$_['column_unit']      = 'crwdns93421:0crwdne93421:0';
+$_['column_value']     = 'crwdns93423:0crwdne93423:0';
+$_['column_action']    = 'crwdns93425:0crwdne93425:0';
+
+// Entry
+$_['entry_title']      = 'crwdns93427:0crwdne93427:0';
+$_['entry_unit']       = 'crwdns93429:0crwdne93429:0';
+$_['entry_value']      = 'crwdns93431:0crwdne93431:0';
+
+// Help
+$_['help_value']       = 'crwdns93433:0crwdne93433:0';
+
+// Error
+$_['error_permission'] = 'crwdns93435:0crwdne93435:0';
+$_['error_title']      = 'crwdns93437:0crwdne93437:0';
+$_['error_unit']       = 'crwdns93439:0crwdne93439:0';
+$_['error_default']    = 'crwdns93441:0crwdne93441:0';
+$_['error_product']    = 'crwdns93443:0%scrwdne93443:0';
