7e84e3897c906b6ecee970b31b1bccc4595cc23d
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:16 +0200

    New translations zone.php (LOLCAT)

---
+++ upload/admin/language/lol-US/localisation/zone.php
@@ -0,0 +1,30 @@
+<?php
+// Heading
+$_['heading_title']          = 'crwdns93741:0crwdne93741:0';
+
+// Text
+$_['text_success']           = 'crwdns93743:0crwdne93743:0';
+$_['text_list']              = 'crwdns93745:0crwdne93745:0';
+$_['text_add']               = 'crwdns93747:0crwdne93747:0';
+$_['text_edit']              = 'crwdns93749:0crwdne93749:0';
+
+// Column
+$_['column_name']            = 'crwdns93751:0crwdne93751:0';
+$_['column_code']            = 'crwdns93753:0crwdne93753:0';
+$_['column_country']         = 'crwdns93755:0crwdne93755:0';
+$_['column_action']          = 'crwdns93757:0crwdne93757:0';
+
+// Entry
+$_['entry_name']             = 'crwdns93759:0crwdne93759:0';
+$_['entry_code']             = 'crwdns93761:0crwdne93761:0';
+$_['entry_country']          = 'crwdns93763:0crwdne93763:0';
+$_['entry_status']           = 'crwdns93765:0crwdne93765:0';
+
+// Error
+$_['error_permission']       = 'crwdns93767:0crwdne93767:0';
+$_['error_name']             = 'crwdns93769:0crwdne93769:0';
+$_['error_default']          = 'crwdns93771:0crwdne93771:0';
+$_['error_store']            = 'crwdns93773:0%scrwdne93773:0';
+$_['error_address']          = 'crwdns93775:0%scrwdne93775:0';
+$_['error_affiliate']        = 'crwdns93777:0%scrwdne93777:0';
+$_['error_zone_to_geo_zone'] = 'crwdns93779:0%scrwdne93779:0';
