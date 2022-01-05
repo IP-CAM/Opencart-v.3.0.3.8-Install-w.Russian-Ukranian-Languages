d4bdd195df43f309ea0d76f2fc536352da24e334
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:26 +0200

    New translations information.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/module/information.php
@@ -0,0 +1,7 @@
+<?php
+// Heading
+$_['heading_title'] = 'crwdns97479:0crwdne97479:0';
+
+// Text
+$_['text_contact']  = 'crwdns97481:0crwdne97481:0';
+$_['text_sitemap']  = 'crwdns97483:0crwdne97483:0';
