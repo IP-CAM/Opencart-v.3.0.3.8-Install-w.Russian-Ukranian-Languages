7d0adb317ea5b1a0801193f4e0cdfaa8f7a7cac2
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:20:59 +0200

    New translations option.php (LOLCAT)

---
+++ upload/admin/language/lol-US/catalog/option.php
@@ -0,0 +1,41 @@
+<?php
+// Heading
+$_['heading_title']      = 'crwdns81377:0crwdne81377:0';
+
+// Text
+$_['text_success']       = 'crwdns81379:0crwdne81379:0';
+$_['text_list']          = 'crwdns81381:0crwdne81381:0';
+$_['text_add']           = 'crwdns81383:0crwdne81383:0';
+$_['text_edit']          = 'crwdns81385:0crwdne81385:0';
+$_['text_choose']        = 'crwdns81387:0crwdne81387:0';
+$_['text_select']        = 'crwdns81389:0crwdne81389:0';
+$_['text_radio']         = 'crwdns81391:0crwdne81391:0';
+$_['text_checkbox']      = 'crwdns81393:0crwdne81393:0';
+$_['text_input']         = 'crwdns81395:0crwdne81395:0';
+$_['text_text']          = 'crwdns81397:0crwdne81397:0';
+$_['text_textarea']      = 'crwdns81399:0crwdne81399:0';
+$_['text_file']          = 'crwdns81401:0crwdne81401:0';
+$_['text_date']          = 'crwdns81403:0crwdne81403:0';
+$_['text_datetime']      = 'crwdns81405:0crwdne81405:0';
+$_['text_time']          = 'crwdns81407:0crwdne81407:0';
+$_['text_option']        = 'crwdns81409:0crwdne81409:0';
+$_['text_value']         = 'crwdns81411:0crwdne81411:0';
+
+// Column
+$_['column_name']        = 'crwdns81413:0crwdne81413:0';
+$_['column_sort_order']  = 'crwdns81415:0crwdne81415:0';
+$_['column_action']      = 'crwdns81417:0crwdne81417:0';
+
+// Entry
+$_['entry_name']         = 'crwdns81419:0crwdne81419:0';
+$_['entry_type']         = 'crwdns81421:0crwdne81421:0';
+$_['entry_option_value'] = 'crwdns81423:0crwdne81423:0';
+$_['entry_image']        = 'crwdns81425:0crwdne81425:0';
+$_['entry_sort_order']   = 'crwdns81427:0crwdne81427:0';
+
+// Error
+$_['error_permission']   = 'crwdns81429:0crwdne81429:0';
+$_['error_name']         = 'crwdns81431:0crwdne81431:0';
+$_['error_type']         = 'crwdns81433:0crwdne81433:0';
+$_['error_option_value'] = 'crwdns81435:0crwdne81435:0';
+$_['error_product']      = 'crwdns81437:0%scrwdne81437:0';
