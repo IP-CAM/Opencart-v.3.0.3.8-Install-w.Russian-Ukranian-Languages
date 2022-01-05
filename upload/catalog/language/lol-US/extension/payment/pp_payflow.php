89c58147c206ff5230687fd38e5722126d823eb1
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:20 +0200

    New translations pp_payflow.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/payment/pp_payflow.php
@@ -0,0 +1,24 @@
+<?php
+// Text
+$_['text_title']				= 'crwdns99231:0crwdne99231:0';
+$_['text_credit_card']			= 'crwdns99233:0crwdne99233:0';
+$_['text_start_date']			= 'crwdns99235:0crwdne99235:0';
+$_['text_issue']				= 'crwdns99237:0crwdne99237:0';
+$_['text_wait']					= 'crwdns99239:0crwdne99239:0';
+
+// Entry
+$_['entry_cc_owner']			= 'crwdns99241:0crwdne99241:0';
+$_['entry_cc_type']				= 'crwdns99243:0crwdne99243:0';
+$_['entry_cc_number']			= 'crwdns99245:0crwdne99245:0';
+$_['entry_cc_start_date']		= 'crwdns99247:0crwdne99247:0';
+$_['entry_cc_expire_date']		= 'crwdns99249:0crwdne99249:0';
+$_['entry_cc_cvv2']				= 'crwdns99251:0crwdne99251:0';
+$_['entry_cc_issue']			= 'crwdns99253:0crwdne99253:0';
+
+// Error
+$_['error_required']			= 'crwdns99255:0crwdne99255:0';
+$_['error_general']				= 'crwdns99257:0crwdne99257:0';
+$_['error_config']				= 'crwdns99259:0crwdne99259:0';
+$_['error_address']				= 'crwdns99261:0crwdne99261:0';
+$_['error_declined']			= 'crwdns99263:0crwdne99263:0';
+$_['error_invalid']				= 'crwdns99265:0crwdne99265:0';
