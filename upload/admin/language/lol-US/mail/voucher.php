46ec6e785c97ee820818cb83aafb3a44a7261866
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:22 +0200

    New translations voucher.php (LOLCAT)

---
+++ upload/admin/language/lol-US/mail/voucher.php
@@ -0,0 +1,10 @@
+<?php
+// Text
+$_['text_success']  = 'crwdns93851:0crwdne93851:0';
+$_['text_subject']  = 'crwdns93853:0%scrwdne93853:0';
+$_['text_greeting'] = 'crwdns93855:0%scrwdne93855:0';
+$_['text_from']     = 'crwdns93857:0%scrwdne93857:0';
+$_['text_message']  = 'crwdns93859:0crwdne93859:0';
+$_['text_redeem']   = 'crwdns93861:0%scrwdne93861:0';
+$_['text_footer']   = 'crwdns93863:0crwdne93863:0';
+$_['text_sent']     = 'crwdns93865:0crwdne93865:0';
