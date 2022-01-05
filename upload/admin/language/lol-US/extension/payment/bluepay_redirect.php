37180b46b5bd21872ecb1ee8fbaf05dcd145298a
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:06 +0200

    New translations bluepay_redirect.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/payment/bluepay_redirect.php
@@ -0,0 +1,66 @@
+<?php
+// Heading
+$_['heading_title']			 = 'crwdns85699:0crwdne85699:0';
+
+// Text
+$_['text_extension']		 = 'crwdns85701:0crwdne85701:0';
+$_['text_success']			 = 'crwdns85703:0crwdne85703:0';
+$_['text_edit']              = 'crwdns85705:0crwdne85705:0';
+$_['text_bluepay_redirect']	 = 'crwdns85707:0crwdne85707:0';
+$_['text_sim']				 = 'crwdns85709:0crwdne85709:0';
+$_['text_test']				 = 'crwdns85711:0crwdne85711:0';
+$_['text_live']				 = 'crwdns85713:0crwdne85713:0';
+$_['text_sale']				 = 'crwdns85715:0crwdne85715:0';
+$_['text_authenticate']		 = 'crwdns85717:0crwdne85717:0';
+$_['text_release_ok']		 = 'crwdns85719:0crwdne85719:0';
+$_['text_release_ok_order']	 = 'crwdns85721:0crwdne85721:0';
+$_['text_rebate_ok']		 = 'crwdns85723:0crwdne85723:0';
+$_['text_rebate_ok_order']	 = 'crwdns85725:0crwdne85725:0';
+$_['text_void_ok']			 = 'crwdns85727:0crwdne85727:0';
+$_['text_payment_info']		 = 'crwdns85729:0crwdne85729:0';
+$_['text_release_status']	 = 'crwdns85731:0crwdne85731:0';
+$_['text_void_status']		 = 'crwdns85733:0crwdne85733:0';
+$_['text_rebate_status']	 = 'crwdns85735:0crwdne85735:0';
+$_['text_order_ref']		 = 'crwdns85737:0crwdne85737:0';
+$_['text_order_total']		 = 'crwdns85739:0crwdne85739:0';
+$_['text_total_released']	 = 'crwdns85741:0crwdne85741:0';
+$_['text_transactions']		 = 'crwdns85743:0crwdne85743:0';
+$_['text_column_amount']	 = 'crwdns85745:0crwdne85745:0';
+$_['text_column_type']		 = 'crwdns85747:0crwdne85747:0';
+$_['text_column_date_added'] = 'crwdns85749:0crwdne85749:0';
+$_['text_confirm_void']		 = 'crwdns85751:0crwdne85751:0';
+$_['text_confirm_release']	 = 'crwdns85753:0crwdne85753:0';
+$_['text_confirm_rebate']	 = 'crwdns85755:0crwdne85755:0';
+$_['text_void']	    		 = 'crwdns85757:0crwdne85757:0';
+$_['text_payment']           = 'crwdns85759:0crwdne85759:0';
+$_['text_rebate']            = 'crwdns85761:0crwdne85761:0';
+
+// Entry
+$_['entry_vendor']			 = 'crwdns85763:0crwdne85763:0';
+$_['entry_secret_key']		 = 'crwdns85765:0crwdne85765:0';
+$_['entry_test']			 = 'crwdns85767:0crwdne85767:0';
+$_['entry_transaction']		 = 'crwdns85769:0crwdne85769:0';
+$_['entry_total']			 = 'crwdns85771:0crwdne85771:0';
+$_['entry_order_status']	 = 'crwdns85773:0crwdne85773:0';
+$_['entry_geo_zone']		 = 'crwdns85775:0crwdne85775:0';
+$_['entry_status']			 = 'crwdns85777:0crwdne85777:0';
+$_['entry_sort_order']		 = 'crwdns85779:0crwdne85779:0';
+$_['entry_debug']			 = 'crwdns85781:0crwdne85781:0';
+$_['entry_card']			 = 'crwdns85783:0crwdne85783:0';
+
+// Help
+$_['help_total']			 = 'crwdns85785:0crwdne85785:0';
+$_['help_debug']			 = 'crwdns85787:0crwdne85787:0';
+$_['help_transaction']		 = 'crwdns85789:0crwdne85789:0';
+$_['help_cron_job_token']	 = 'crwdns85791:0crwdne85791:0';
+$_['help_cron_job_url']		 = 'crwdns85793:0crwdne85793:0';
+
+// Button
+$_['button_release']		 = 'crwdns85795:0crwdne85795:0';
+$_['button_rebate']			 = 'crwdns85797:0crwdne85797:0';
+$_['button_void']			 = 'crwdns85799:0crwdne85799:0';
+
+// Error
+$_['error_permission']		 = 'crwdns85801:0crwdne85801:0';
+$_['error_account_id']		 = 'crwdns85803:0crwdne85803:0';
+$_['error_secret_key']		 = 'crwdns85805:0crwdne85805:0';
