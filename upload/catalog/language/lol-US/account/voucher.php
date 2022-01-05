3ef20b0e979b7b28d392cdb3f2f1d9fd26da2f9f
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:55 +0200

    New translations voucher.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/account/voucher.php
@@ -0,0 +1,32 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns96377:0crwdne96377:0';
+
+// Text
+$_['text_account']     = 'crwdns96379:0crwdne96379:0';
+$_['text_voucher']     = 'crwdns96381:0crwdne96381:0';
+$_['text_description'] = 'crwdns96383:0crwdne96383:0';
+$_['text_agree']       = 'crwdns96385:0crwdne96385:0';
+$_['text_message']     = 'crwdns96387:0crwdne96387:0';
+$_['text_for']         = 'crwdns96389:0%scrwdnd96389:0%scrwdne96389:0';
+
+// Entry
+$_['entry_to_name']    = 'crwdns96391:0crwdne96391:0';
+$_['entry_to_email']   = 'crwdns96393:0crwdne96393:0';
+$_['entry_from_name']  = 'crwdns96395:0crwdne96395:0';
+$_['entry_from_email'] = 'crwdns96397:0crwdne96397:0';
+$_['entry_theme']      = 'crwdns96399:0crwdne96399:0';
+$_['entry_message']    = 'crwdns96401:0crwdne96401:0';
+$_['entry_amount']     = 'crwdns96403:0crwdne96403:0';
+
+// Help
+$_['help_message']     = 'crwdns96405:0crwdne96405:0';
+$_['help_amount']      = 'crwdns96407:0%scrwdnd96407:0%scrwdne96407:0';
+
+// Error
+$_['error_to_name']    = 'crwdns96409:0crwdne96409:0';
+$_['error_from_name']  = 'crwdns96411:0crwdne96411:0';
+$_['error_email']      = 'crwdns96413:0crwdne96413:0';
+$_['error_theme']      = 'crwdns96415:0crwdne96415:0';
+$_['error_amount']     = 'crwdns96417:0%scrwdnd96417:0%scrwdne96417:0';
+$_['error_agree']      = 'crwdns96419:0crwdne96419:0';
