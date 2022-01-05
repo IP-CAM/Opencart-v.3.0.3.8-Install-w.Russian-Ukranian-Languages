77716799b6596a86a909dedf3402b01cc941f561
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:52 +0200

    New translations flat.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/shipping/flat.php
@@ -0,0 +1,18 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns92387:0crwdne92387:0';
+
+// Text
+$_['text_extension']   = 'crwdns92389:0crwdne92389:0';
+$_['text_success']     = 'crwdns92391:0crwdne92391:0';
+$_['text_edit']        = 'crwdns92393:0crwdne92393:0';
+
+// Entry
+$_['entry_cost']       = 'crwdns92395:0crwdne92395:0';
+$_['entry_tax_class']  = 'crwdns92397:0crwdne92397:0';
+$_['entry_geo_zone']   = 'crwdns92399:0crwdne92399:0';
+$_['entry_status']     = 'crwdns92401:0crwdne92401:0';
+$_['entry_sort_order'] = 'crwdns92403:0crwdne92403:0';
+
+// Error
+$_['error_permission'] = 'crwdns92405:0crwdne92405:0';
