572a895851e456fda9b8ee9d0234afc01ed58366
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:20:36 +0200

    New translations layout.php (LOLCAT)

---
+++ upload/admin/language/lol-US/design/layout.php
@@ -0,0 +1,36 @@
+<?php
+// Heading
+$_['heading_title']       = 'crwdns82515:0crwdne82515:0';
+
+// Text
+$_['text_success']        = 'crwdns82517:0crwdne82517:0';
+$_['text_list']           = 'crwdns82519:0crwdne82519:0';
+$_['text_add']            = 'crwdns82521:0crwdne82521:0';
+$_['text_edit']           = 'crwdns82523:0crwdne82523:0';
+$_['text_remove']         = 'crwdns82525:0crwdne82525:0';
+$_['text_route']          = 'crwdns82527:0crwdne82527:0';
+$_['text_module']         = 'crwdns82529:0crwdne82529:0';
+$_['text_default']        = 'crwdns82531:0crwdne82531:0';
+$_['text_content_top']    = 'crwdns82533:0crwdne82533:0';
+$_['text_content_bottom'] = 'crwdns82535:0crwdne82535:0';
+$_['text_column_left']    = 'crwdns82537:0crwdne82537:0';
+$_['text_column_right']   = 'crwdns82539:0crwdne82539:0';
+
+// Column
+$_['column_name']         = 'crwdns82541:0crwdne82541:0';
+$_['column_action']       = 'crwdns82543:0crwdne82543:0';
+
+// Entry
+$_['entry_name']          = 'crwdns82545:0crwdne82545:0';
+$_['entry_store']         = 'crwdns82547:0crwdne82547:0';
+$_['entry_route']         = 'crwdns82549:0crwdne82549:0';
+$_['entry_module']        = 'crwdns82551:0crwdne82551:0';
+
+// Error
+$_['error_permission']    = 'crwdns82553:0crwdne82553:0';
+$_['error_name']          = 'crwdns82555:0crwdne82555:0';
+$_['error_default']       = 'crwdns82557:0crwdne82557:0';
+$_['error_store']         = 'crwdns82559:0%scrwdne82559:0';
+$_['error_product']       = 'crwdns82561:0%scrwdne82561:0';
+$_['error_category']      = 'crwdns82563:0%scrwdne82563:0';
+$_['error_information']   = 'crwdns82565:0%scrwdne82565:0';
