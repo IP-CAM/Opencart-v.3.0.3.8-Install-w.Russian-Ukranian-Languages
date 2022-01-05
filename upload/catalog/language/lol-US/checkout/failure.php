f650f415828426a53c4821fa30bca9c97940c625
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:30 +0200

    New translations failure.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/checkout/failure.php
@@ -0,0 +1,9 @@
+<?php
+// Heading
+$_['heading_title'] = 'crwdns96961:0crwdne96961:0';
+
+// Text
+$_['text_basket']   = 'crwdns96963:0crwdne96963:0';
+$_['text_checkout'] = 'crwdns96965:0crwdne96965:0';
+$_['text_failure']  = 'crwdns96967:0crwdne96967:0';
+$_['text_message']  = 'crwdns96969:0%scrwdne96969:0';
