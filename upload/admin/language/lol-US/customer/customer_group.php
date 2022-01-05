4b37efbd652a23029e0b47c8c21fa7b7a3346558
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:20:35 +0200

    New translations customer_group.php (LOLCAT)

---
+++ upload/admin/language/lol-US/customer/customer_group.php
@@ -0,0 +1,30 @@
+<?php
+// Heading
+$_['heading_title']     = 'crwdns82443:0crwdne82443:0';
+
+// Text
+$_['text_success']      = 'crwdns82445:0crwdne82445:0';
+$_['text_list']         = 'crwdns82447:0crwdne82447:0';
+$_['text_add']          = 'crwdns82449:0crwdne82449:0';
+$_['text_edit']         = 'crwdns82451:0crwdne82451:0';
+
+// Column
+$_['column_name']       = 'crwdns82453:0crwdne82453:0';
+$_['column_sort_order'] = 'crwdns82455:0crwdne82455:0';
+$_['column_action']     = 'crwdns82457:0crwdne82457:0';
+
+// Entry
+$_['entry_name']        = 'crwdns82459:0crwdne82459:0';
+$_['entry_description'] = 'crwdns82461:0crwdne82461:0';
+$_['entry_approval']    = 'crwdns82463:0crwdne82463:0';
+$_['entry_sort_order']  = 'crwdns82465:0crwdne82465:0';
+
+// Help
+$_['help_approval']     = 'crwdns82467:0crwdne82467:0';
+
+// Error
+$_['error_permission']  = 'crwdns82469:0crwdne82469:0';
+$_['error_name']        = 'crwdns82471:0crwdne82471:0';
+$_['error_default']     = 'crwdns82473:0crwdne82473:0';
+$_['error_store']       = 'crwdns82475:0%scrwdne82475:0';
+$_['error_customer']    = 'crwdns82477:0%scrwdne82477:0';
