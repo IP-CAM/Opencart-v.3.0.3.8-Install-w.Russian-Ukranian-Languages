de6b24f26e155733b0dc7fb42c6ef2fca8a788c0
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:53 +0200

    New translations pp_pro.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/payment/pp_pro.php
@@ -0,0 +1,33 @@
+<?php
+// Heading
+$_['heading_title']		 = 'crwdns89619:0crwdne89619:0';
+
+// Text
+$_['text_extension']     = 'crwdns89621:0crwdne89621:0';
+$_['text_success']		 = 'crwdns89623:0crwdne89623:0';
+$_['text_edit']          = 'crwdns89625:0crwdne89625:0';
+$_['text_pp_pro']		 = 'crwdns89627:0crwdne89627:0';
+$_['text_authorization'] = 'crwdns89629:0crwdne89629:0';
+$_['text_sale']			 = 'crwdns89631:0crwdne89631:0';
+
+// Entry
+$_['entry_username']	 = 'crwdns89633:0crwdne89633:0';
+$_['entry_password']	 = 'crwdns89635:0crwdne89635:0';
+$_['entry_signature']	 = 'crwdns89637:0crwdne89637:0';
+$_['entry_test']		 = 'crwdns89639:0crwdne89639:0';
+$_['entry_transaction']	 = 'crwdns89641:0crwdne89641:0';
+$_['entry_total']		 = 'crwdns89643:0crwdne89643:0';
+$_['entry_order_status'] = 'crwdns89645:0crwdne89645:0';
+$_['entry_geo_zone']	 = 'crwdns89647:0crwdne89647:0';
+$_['entry_status']		 = 'crwdns89649:0crwdne89649:0';
+$_['entry_sort_order']	 = 'crwdns89651:0crwdne89651:0';
+
+// Help
+$_['help_test']			 = 'crwdns89653:0crwdne89653:0';
+$_['help_total']		 = 'crwdns89655:0crwdne89655:0';
+
+// Error
+$_['error_permission']	 = 'crwdns89657:0crwdne89657:0';
+$_['error_username']	 = 'crwdns89659:0crwdne89659:0';
+$_['error_password']	 = 'crwdns89661:0crwdne89661:0';
+$_['error_signature']	 = 'crwdns89663:0crwdne89663:0';
