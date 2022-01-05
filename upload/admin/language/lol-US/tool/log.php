e51e3067f21cfe614708c51a96094fcb17215db6
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:56 +0200

    New translations log.php (LOLCAT)

---
+++ upload/admin/language/lol-US/tool/log.php
@@ -0,0 +1,11 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns95553:0crwdne95553:0';
+
+// Text
+$_['text_success']     = 'crwdns95555:0crwdne95555:0';
+$_['text_list']        = 'crwdns95557:0crwdne95557:0';
+
+// Error
+$_['error_warning']    = 'crwdns95559:0%scrwdnd95559:0%scrwdne95559:0';
+$_['error_permission'] = 'crwdns95561:0crwdne95561:0';
