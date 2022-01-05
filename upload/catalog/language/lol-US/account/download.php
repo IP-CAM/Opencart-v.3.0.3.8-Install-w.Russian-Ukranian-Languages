8ba36950c27b7db8f0800daeb8031f5f91a66932
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:08 +0200

    New translations download.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/account/download.php
@@ -0,0 +1,14 @@
+<?php
+// Heading
+$_['heading_title']     = 'crwdns95871:0crwdne95871:0';
+
+// Text
+$_['text_account']      = 'crwdns95873:0crwdne95873:0';
+$_['text_downloads']    = 'crwdns95875:0crwdne95875:0';
+$_['text_empty']        = 'crwdns95877:0crwdne95877:0';
+
+// Column
+$_['column_order_id']   = 'crwdns95879:0crwdne95879:0';
+$_['column_name']       = 'crwdns95881:0crwdne95881:0';
+$_['column_size']       = 'crwdns95883:0crwdne95883:0';
+$_['column_date_added'] = 'crwdns95885:0crwdne95885:0';
