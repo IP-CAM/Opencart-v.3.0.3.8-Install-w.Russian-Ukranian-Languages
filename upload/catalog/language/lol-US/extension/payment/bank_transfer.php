f3b8e6d431f21b6f73f0ccbb3d4f9ef79e57e08c
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:34 +0200

    New translations bank_transfer.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/payment/bank_transfer.php
@@ -0,0 +1,6 @@
+<?php
+// Text
+$_['text_title']       = 'crwdns97777:0crwdne97777:0';
+$_['text_instruction'] = 'crwdns97779:0crwdne97779:0';
+$_['text_description'] = 'crwdns97781:0crwdne97781:0';
+$_['text_payment']     = 'crwdns97783:0crwdne97783:0';
