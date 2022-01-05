0be12a46155d0b4c738c1c736458b120443337cf
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:17 +0200

    New translations perpetual_payments.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/payment/perpetual_payments.php
@@ -0,0 +1,20 @@
+<?php
+// Text
+$_['text_title']				= 'crwdns99085:0crwdne99085:0';
+$_['text_credit_card']			= 'crwdns99087:0crwdne99087:0';
+$_['text_transaction']			= 'crwdns99089:0crwdne99089:0';
+$_['text_avs']					= 'crwdns99091:0crwdne99091:0';
+$_['text_avs_full_match']		= 'crwdns99093:0crwdne99093:0';
+$_['text_avs_not_match']		= 'crwdns99095:0crwdne99095:0';
+$_['text_authorisation']		= 'crwdns99097:0crwdne99097:0';
+
+// Entry
+$_['entry_cc_number']			= 'crwdns99099:0crwdne99099:0';
+$_['entry_cc_start_date']		= 'crwdns99101:0crwdne99101:0';
+$_['entry_cc_expire_date']		= 'crwdns99103:0crwdne99103:0';
+$_['entry_cc_cvv2']				= 'crwdns99105:0crwdne99105:0';
+$_['entry_cc_issue']			= 'crwdns99107:0crwdne99107:0';
+
+// Help
+$_['help_start_date']			= 'crwdns99109:0crwdne99109:0';
+$_['help_issue']				= 'crwdns99111:0crwdne99111:0';
