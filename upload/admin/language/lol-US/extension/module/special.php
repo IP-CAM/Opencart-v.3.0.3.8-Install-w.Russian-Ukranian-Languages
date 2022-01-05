2b94f3721f5fb0c8c0d59dbb71867b67be0d66f9
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:23 +0200

    New translations special.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/module/special.php
@@ -0,0 +1,21 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns85103:0crwdne85103:0';
+
+// Text
+$_['text_extension']   = 'crwdns85105:0crwdne85105:0';
+$_['text_success']     = 'crwdns85107:0crwdne85107:0';
+$_['text_edit']        = 'crwdns85109:0crwdne85109:0';
+
+// Entry
+$_['entry_name']       = 'crwdns85111:0crwdne85111:0';
+$_['entry_limit']      = 'crwdns85113:0crwdne85113:0';
+$_['entry_width']      = 'crwdns85115:0crwdne85115:0';
+$_['entry_height']     = 'crwdns85117:0crwdne85117:0';
+$_['entry_status']     = 'crwdns85119:0crwdne85119:0';
+
+// Error
+$_['error_permission'] = 'crwdns85121:0crwdne85121:0';
+$_['error_name']       = 'crwdns85123:0crwdne85123:0';
+$_['error_width']      = 'crwdns85125:0crwdne85125:0';
+$_['error_height']     = 'crwdns85127:0crwdne85127:0';
