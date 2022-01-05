4bce549746a16e328019f54efc2ff0b3028143e8
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:42 +0200

    New translations google.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/captcha/google.php
@@ -0,0 +1,9 @@
+<?php
+// Text
+$_['text_captcha']  = 'crwdns97211:0crwdne97211:0';
+
+// Entry
+$_['entry_captcha'] = 'crwdns97213:0crwdne97213:0';
+
+// Error
+$_['error_captcha'] = 'crwdns97215:0crwdne97215:0';
