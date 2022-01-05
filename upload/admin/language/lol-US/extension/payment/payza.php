caf8d9aae96b9a63e28fba36ad23af65de6c14ac
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:46 +0200

    New translations payza.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/payment/payza.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']		 = 'crwdns88301:0crwdne88301:0';
+
+// Text
+$_['text_extension']	 = 'crwdns88303:0crwdne88303:0';
+$_['text_success']		 = 'crwdns88305:0crwdne88305:0';
+$_['text_edit']          = 'crwdns88307:0crwdne88307:0';
+
+// Entry
+$_['entry_merchant']	 = 'crwdns88309:0crwdne88309:0';
+$_['entry_security']	 = 'crwdns88311:0crwdne88311:0';
+$_['entry_callback']	 = 'crwdns88313:0crwdne88313:0';
+$_['entry_total']		 = 'crwdns88315:0crwdne88315:0';
+$_['entry_order_status'] = 'crwdns88317:0crwdne88317:0';
+$_['entry_geo_zone']	 = 'crwdns88319:0crwdne88319:0';
+$_['entry_status']		 = 'crwdns88321:0crwdne88321:0';
+$_['entry_sort_order']	 = 'crwdns88323:0crwdne88323:0';
+
+// Help
+$_['help_callback']		 = 'crwdns88325:0crwdne88325:0';
+$_['help_total']		 = 'crwdns88327:0crwdne88327:0';
+
+// Error
+$_['error_permission']	 = 'crwdns88329:0crwdne88329:0';
+$_['error_merchant']	 = 'crwdns88331:0crwdne88331:0';
+$_['error_security']	 = 'crwdns88333:0crwdne88333:0';
