778d68c7c765e17404a0c765a5b30626a3ec85a1
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:58 +0200

    New translations sagepay_us.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/payment/sagepay_us.php
@@ -0,0 +1,25 @@
+<?php
+// Heading
+$_['heading_title']		 = 'crwdns90601:0crwdne90601:0';
+
+// Text
+$_['text_extension']	 = 'crwdns90603:0crwdne90603:0';
+$_['text_success']		 = 'crwdns90605:0crwdne90605:0';
+$_['text_edit']          = 'crwdns90607:0crwdne90607:0';
+
+// Entry
+$_['entry_merchant_id']	 = 'crwdns90609:0crwdne90609:0';
+$_['entry_merchant_key'] = 'crwdns90611:0crwdne90611:0';
+$_['entry_total']		 = 'crwdns90613:0crwdne90613:0';
+$_['entry_order_status'] = 'crwdns90615:0crwdne90615:0';
+$_['entry_geo_zone']	 = 'crwdns90617:0crwdne90617:0';
+$_['entry_status']		 = 'crwdns90619:0crwdne90619:0';
+$_['entry_sort_order']	 = 'crwdns90621:0crwdne90621:0';
+
+// Help
+$_['help_total']		 = 'crwdns90623:0crwdne90623:0';
+
+// Error
+$_['error_permission']	 = 'crwdns90625:0crwdne90625:0';
+$_['error_merchant_id']	 = 'crwdns90627:0crwdne90627:0';
+$_['error_merchant_key'] = 'crwdns90629:0crwdne90629:0';
