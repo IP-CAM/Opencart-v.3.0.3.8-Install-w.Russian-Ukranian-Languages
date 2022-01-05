2d61210192ce6df356f23042cbb77b8678936ffc
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:38 +0200

    New translations pp_pro.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/payment/pp_pro.php
@@ -0,0 +1,17 @@
+<?php
+// Text
+$_['text_title']				= 'crwdns99273:0crwdne99273:0';
+$_['text_wait']					= 'crwdns99275:0crwdne99275:0';
+$_['text_credit_card']			= 'crwdns99277:0crwdne99277:0';
+
+// Entry
+$_['entry_cc_type']				= 'crwdns99279:0crwdne99279:0';
+$_['entry_cc_number']			= 'crwdns99281:0crwdne99281:0';
+$_['entry_cc_start_date']		= 'crwdns99283:0crwdne99283:0';
+$_['entry_cc_expire_date']		= 'crwdns99285:0crwdne99285:0';
+$_['entry_cc_cvv2']				= 'crwdns99287:0crwdne99287:0';
+$_['entry_cc_issue']			= 'crwdns99289:0crwdne99289:0';
+
+// Help
+$_['help_start_date']			= 'crwdns99291:0crwdne99291:0';
+$_['help_issue']				= 'crwdns99293:0crwdne99293:0';
