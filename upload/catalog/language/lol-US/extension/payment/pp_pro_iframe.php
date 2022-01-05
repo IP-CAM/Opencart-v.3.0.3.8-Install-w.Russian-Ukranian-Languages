6973e6e2d287e6902652e3b858f910611f83eef2
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:39 +0200

    New translations pp_pro_iframe.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/payment/pp_pro_iframe.php
@@ -0,0 +1,7 @@
+<?php
+// Text
+$_['text_title']				= 'crwdns99295:0crwdne99295:0';
+$_['text_secure_connection']	= 'crwdns99297:0crwdne99297:0';
+
+// Error
+$_['error_connection']			= 'crwdns99299:0crwdne99299:0';
