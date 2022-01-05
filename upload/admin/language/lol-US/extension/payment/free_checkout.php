5756233b93dc06e3205a1e4310faf1dda086934b
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:13 +0200

    New translations free_checkout.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/payment/free_checkout.php
@@ -0,0 +1,16 @@
+<?php
+// Heading
+$_['heading_title']      = 'crwdns86781:0crwdne86781:0';
+
+// Text
+$_['text_extension']     = 'crwdns86783:0crwdne86783:0';
+$_['text_success']       = 'crwdns86785:0crwdne86785:0';
+$_['text_edit']          = 'crwdns86787:0crwdne86787:0';
+
+// Entry
+$_['entry_order_status'] = 'crwdns86789:0crwdne86789:0';
+$_['entry_status']       = 'crwdns86791:0crwdne86791:0';
+$_['entry_sort_order']   = 'crwdns86793:0crwdne86793:0';
+
+// Error
+$_['error_permission']   = 'crwdns86795:0crwdne86795:0';
