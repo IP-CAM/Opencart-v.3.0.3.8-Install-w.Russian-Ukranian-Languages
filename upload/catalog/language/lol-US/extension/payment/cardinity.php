0f1af6b6e28ad3c867102cd203f38a761ebbe2f2
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:37 +0200

    New translations cardinity.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/payment/cardinity.php
@@ -0,0 +1,24 @@
+<?php
+// Text
+$_['text_title']						= 'crwdns97953:0crwdne97953:0';
+$_['text_payment_success']				= 'crwdns97955:0crwdne97955:0';
+$_['text_payment_failed']				= 'crwdns97957:0crwdne97957:0';
+
+// Entry
+$_['entry_holder']						= 'crwdns97959:0crwdne97959:0';
+$_['entry_pan']							= 'crwdns97961:0crwdne97961:0';
+$_['entry_expires']						= 'crwdns97963:0crwdne97963:0';
+$_['entry_exp_month']					= 'crwdns97965:0crwdne97965:0';
+$_['entry_exp_year']					= 'crwdns97967:0crwdne97967:0';
+$_['entry_cvc']							= 'crwdns97969:0crwdne97969:0';
+
+// Error
+$_['error_process_order']				= 'crwdns97971:0crwdne97971:0';
+$_['error_invalid_currency']			= 'crwdns97973:0crwdne97973:0';
+$_['error_finalizing_payment']			= 'crwdns97975:0crwdne97975:0';
+$_['error_unknown_order_id']			= 'crwdns97977:0crwdne97977:0';
+$_['error_invalid_hash']				= 'crwdns97979:0crwdne97979:0';
+$_['error_payment_declined']			= 'crwdns97981:0crwdne97981:0';
+
+// Button
+$_['button_confirm']					= 'crwdns97983:0crwdne97983:0';
