48f1ce63f6cd1781efe2ee37db91a67f31d49fde
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:25:23 +0200

    New translations search.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/product/search.php
@@ -0,0 +1,34 @@
+<?php
+// Heading
+$_['heading_title']     = 'crwdns100609:0crwdne100609:0';
+$_['heading_tag']       = 'crwdns100611:0crwdne100611:0';
+
+// Text
+$_['text_search']       = 'crwdns100613:0crwdne100613:0';
+$_['text_keyword']      = 'crwdns100615:0crwdne100615:0';
+$_['text_category']     = 'crwdns100617:0crwdne100617:0';
+$_['text_sub_category'] = 'crwdns100619:0crwdne100619:0';
+$_['text_empty']        = 'crwdns100621:0crwdne100621:0';
+$_['text_quantity']     = 'crwdns100623:0crwdne100623:0';
+$_['text_manufacturer'] = 'crwdns100625:0crwdne100625:0';
+$_['text_model']        = 'crwdns100627:0crwdne100627:0';
+$_['text_points']       = 'crwdns100629:0crwdne100629:0';
+$_['text_price']        = 'crwdns100631:0crwdne100631:0';
+$_['text_tax']          = 'crwdns100633:0crwdne100633:0';
+$_['text_reviews']      = 'crwdns100635:0%scrwdne100635:0';
+$_['text_compare']      = 'crwdns100637:0%scrwdne100637:0';
+$_['text_sort']         = 'crwdns100639:0crwdne100639:0';
+$_['text_default']      = 'crwdns100641:0crwdne100641:0';
+$_['text_name_asc']     = 'crwdns100643:0crwdne100643:0';
+$_['text_name_desc']    = 'crwdns100645:0crwdne100645:0';
+$_['text_price_asc']    = 'crwdns100647:0crwdne100647:0';
+$_['text_price_desc']   = 'crwdns100649:0crwdne100649:0';
+$_['text_rating_asc']   = 'crwdns100651:0crwdne100651:0';
+$_['text_rating_desc']  = 'crwdns100653:0crwdne100653:0';
+$_['text_model_asc']    = 'crwdns100655:0crwdne100655:0';
+$_['text_model_desc']   = 'crwdns100657:0crwdne100657:0';
+$_['text_limit']        = 'crwdns100659:0crwdne100659:0';
+
+// Entry
+$_['entry_search']      = 'crwdns100661:0crwdne100661:0';
+$_['entry_description'] = 'crwdns100663:0crwdne100663:0';
