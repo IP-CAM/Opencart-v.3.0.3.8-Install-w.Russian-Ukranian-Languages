add3dce2be1d9af4b2258eca2dd51d1bac5a7788
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:12 +0200

    New translations klarna_invoice.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/payment/klarna_invoice.php
@@ -0,0 +1,36 @@
+<?php
+// Text
+$_['text_title']				= 'crwdns98907:0crwdne98907:0';
+$_['text_terms_fee']			= 'crwdns98909:0%scrwdnd98909:0%scrwdnd98909:0%scrwdnd98909:0%scrwdne98909:0';
+$_['text_terms_no_fee']			= 'crwdns98911:0%scrwdnd98911:0%scrwdne98911:0';
+$_['text_additional']			= 'crwdns98913:0crwdne98913:0';
+$_['text_male']					= 'crwdns98915:0crwdne98915:0';
+$_['text_female']				= 'crwdns98917:0crwdne98917:0';
+$_['text_year']					= 'crwdns98919:0crwdne98919:0';
+$_['text_month']				= 'crwdns98921:0crwdne98921:0';
+$_['text_day']					= 'crwdns98923:0crwdne98923:0';
+$_['text_comment']				= 'crwdns98925:0%scrwdne98925:0' . "crwdns98927:0crwdne98927:0" . 'crwdns98929:0%scrwdnd98929:0%scrwdnd98929:0%.4fcrwdne98929:0';
+$_['text_trems_description']    = 'crwdns98931:0crwdne98931:0';
+
+// Entry
+$_['entry_gender']				= 'crwdns98933:0crwdne98933:0';
+$_['entry_pno']					= 'crwdns98935:0crwdne98935:0';
+$_['entry_dob']					= 'crwdns98937:0crwdne98937:0';
+$_['entry_phone_no']			= 'crwdns98939:0crwdne98939:0';
+$_['entry_street']				= 'crwdns98941:0crwdne98941:0';
+$_['entry_house_no']			= 'crwdns98943:0crwdne98943:0';
+$_['entry_house_ext']			= 'crwdns98945:0crwdne98945:0';
+$_['entry_company']				= 'crwdns98947:0crwdne98947:0';
+
+// Help
+$_['help_pno']					= 'crwdns98949:0crwdne98949:0';
+$_['help_phone_no']				= 'crwdns98951:0crwdne98951:0';
+$_['help_street']				= 'crwdns98953:0crwdne98953:0';
+$_['help_house_no']				= 'crwdns98955:0crwdne98955:0';
+$_['help_house_ext']			= 'crwdns98957:0crwdne98957:0';
+$_['help_company']				= 'crwdns98959:0crwdne98959:0';
+
+// Error
+$_['error_deu_terms']			= 'crwdns98961:0crwdne98961:0';
+$_['error_address_match']		= 'crwdns98963:0crwdne98963:0';
+$_['error_network']				= 'crwdns98965:0crwdne98965:0';
