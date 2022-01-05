0c4c4365a09bfcdd40d2c921bcda014de95442ff
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:24 +0200

    New translations divido_calculator.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/module/divido_calculator.php
@@ -0,0 +1,15 @@
+<?php
+// Calculator
+$_['text_checkout_title']      = 'crwdns97445:0crwdne97445:0';
+$_['text_choose_plan']         = 'crwdns97447:0crwdne97447:0';
+$_['text_choose_deposit']      = 'crwdns97449:0crwdne97449:0';
+$_['text_monthly_payments']    = 'crwdns97451:0crwdne97451:0';
+$_['text_months']              = 'crwdns97453:0crwdne97453:0';
+$_['text_term']                = 'crwdns97455:0crwdne97455:0';
+$_['text_deposit']             = 'crwdns97457:0crwdne97457:0';
+$_['text_credit_amount']       = 'crwdns97459:0crwdne97459:0';
+$_['text_amount_payable']      = 'crwdns97461:0crwdne97461:0';
+$_['text_total_interest']      = 'crwdns97463:0crwdne97463:0';
+$_['text_monthly_installment'] = 'crwdns97465:0crwdne97465:0';
+$_['text_redirection']         = 'crwdns97467:0crwdne97467:0';
+$_['text_divido']              = 'crwdns97469:0crwdne97469:0';
