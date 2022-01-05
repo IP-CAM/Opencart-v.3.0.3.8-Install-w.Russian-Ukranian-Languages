f0853570db373194b97432db584dfdfac0a516b9
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:28 +0200

    New translations logout.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/account/logout.php
@@ -0,0 +1,8 @@
+<?php
+// Heading
+$_['heading_title'] = 'crwdns95971:0crwdne95971:0';
+
+// Text
+$_['text_message']  = 'crwdns95973:0crwdne95973:0';
+$_['text_account']  = 'crwdns95975:0crwdne95975:0';
+$_['text_logout']   = 'crwdns95977:0crwdne95977:0';
