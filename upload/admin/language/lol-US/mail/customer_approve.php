6f134396db6d538573f435db666b6e9d48d30500
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:18 +0200

    New translations customer_approve.php (LOLCAT)

---
+++ upload/admin/language/lol-US/mail/customer_approve.php
@@ -0,0 +1,7 @@
+<?php
+// Text
+$_['text_subject'] = 'crwdns93799:0%scrwdne93799:0';
+$_['text_welcome'] = 'crwdns93801:0%scrwdne93801:0';
+$_['text_login']   = 'crwdns93803:0crwdne93803:0';
+$_['text_service'] = 'crwdns93805:0crwdne93805:0';
+$_['text_thanks']  = 'crwdns93807:0crwdne93807:0';
