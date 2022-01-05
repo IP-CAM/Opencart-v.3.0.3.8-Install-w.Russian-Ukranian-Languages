ec25422153a728a6c382eb74e229c1d077121e58
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:13 +0200

    New translations featured.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/module/featured.php
@@ -0,0 +1,25 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns84767:0crwdne84767:0';
+
+// Text
+$_['text_extension']   = 'crwdns84769:0crwdne84769:0';
+$_['text_success']     = 'crwdns84771:0crwdne84771:0';
+$_['text_edit']        = 'crwdns84773:0crwdne84773:0';
+
+// Entry
+$_['entry_name']       = 'crwdns84775:0crwdne84775:0';
+$_['entry_product']    = 'crwdns84777:0crwdne84777:0';
+$_['entry_limit']      = 'crwdns84779:0crwdne84779:0';
+$_['entry_width']      = 'crwdns84781:0crwdne84781:0';
+$_['entry_height']     = 'crwdns84783:0crwdne84783:0';
+$_['entry_status']     = 'crwdns84785:0crwdne84785:0';
+
+// Help
+$_['help_product']     = 'crwdns84787:0crwdne84787:0';
+
+// Error
+$_['error_permission'] = 'crwdns84789:0crwdne84789:0';
+$_['error_name']       = 'crwdns84791:0crwdne84791:0';
+$_['error_width']      = 'crwdns84793:0crwdne84793:0';
+$_['error_height']     = 'crwdns84795:0crwdne84795:0';
