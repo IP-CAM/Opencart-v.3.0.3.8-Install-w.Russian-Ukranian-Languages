f256a24b0a80315b14192749c3346acad956affd
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:21 +0200

    New translations shipping.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/total/shipping.php
@@ -0,0 +1,16 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns93153:0crwdne93153:0';
+
+// Text
+$_['text_extension']   = 'crwdns93155:0crwdne93155:0';
+$_['text_success']     = 'crwdns93157:0crwdne93157:0';
+$_['text_edit']        = 'crwdns93159:0crwdne93159:0';
+
+// Entry
+$_['entry_estimator']  = 'crwdns93161:0crwdne93161:0';
+$_['entry_status']     = 'crwdns93163:0crwdne93163:0';
+$_['entry_sort_order'] = 'crwdns93165:0crwdne93165:0';
+
+// Error
+$_['error_permission'] = 'crwdns93167:0crwdne93167:0';
