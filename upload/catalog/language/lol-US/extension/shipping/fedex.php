b0ae16be868bf53b0ed73435a2db8f7ce781d543
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:25:11 +0200

    New translations fedex.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/shipping/fedex.php
@@ -0,0 +1,26 @@
+<?php
+// Text
+$_['text_title']                               = 'crwdns99873:0crwdne99873:0';
+$_['text_weight']                              = 'crwdns99875:0crwdne99875:0';
+$_['text_eta']                                 = 'crwdns99877:0crwdne99877:0';
+$_['text_europe_first_international_priority'] = 'crwdns99879:0crwdne99879:0';
+$_['text_fedex_1_day_freight']                 = 'crwdns99881:0crwdne99881:0';
+$_['text_fedex_2_day']                         = 'crwdns99883:0crwdne99883:0';
+$_['text_fedex_2_day_am']                      = 'crwdns99885:0crwdne99885:0';
+$_['text_fedex_2_day_freight']                 = 'crwdns99887:0crwdne99887:0';
+$_['text_fedex_3_day_freight']                 = 'crwdns99889:0crwdne99889:0';
+$_['text_fedex_express_saver']                 = 'crwdns99891:0crwdne99891:0';
+$_['text_fedex_first_freight']                 = 'crwdns99893:0crwdne99893:0';
+$_['text_fedex_freight_economy']               = 'crwdns99895:0crwdne99895:0';
+$_['text_fedex_freight_priority']              = 'crwdns99897:0crwdne99897:0';
+$_['text_fedex_ground']                        = 'crwdns99899:0crwdne99899:0';
+$_['text_first_overnight']                     = 'crwdns99901:0crwdne99901:0';
+$_['text_ground_home_delivery']                = 'crwdns99903:0crwdne99903:0';
+$_['text_international_economy']               = 'crwdns99905:0crwdne99905:0';
+$_['text_international_economy_freight']       = 'crwdns99907:0crwdne99907:0';
+$_['text_international_first']                 = 'crwdns99909:0crwdne99909:0';
+$_['text_international_priority']              = 'crwdns99911:0crwdne99911:0';
+$_['text_international_priority_freight']      = 'crwdns99913:0crwdne99913:0';
+$_['text_priority_overnight']                  = 'crwdns99915:0crwdne99915:0';
+$_['text_smart_post']                          = 'crwdns99917:0crwdne99917:0';
+$_['text_standard_overnight']                  = 'crwdns99919:0crwdne99919:0';
