81d7cc1d725105bb3dbbf7a3bb6897f988a1b470
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:52 +0200

    New translations reset.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/account/reset.php
@@ -0,0 +1,17 @@
+<?php
+// Heading
+$_['heading_title']  = 'crwdns96197:0crwdne96197:0';
+
+// Text
+$_['text_account']   = 'crwdns96199:0crwdne96199:0';
+$_['text_password']  = 'crwdns96201:0crwdne96201:0';
+$_['text_success']   = 'crwdns96203:0crwdne96203:0';
+
+// Entry
+$_['entry_password'] = 'crwdns96205:0crwdne96205:0';
+$_['entry_confirm']  = 'crwdns96207:0crwdne96207:0';
+
+// Error
+$_['error_password'] = 'crwdns96209:0crwdne96209:0';
+$_['error_confirm']  = 'crwdns96211:0crwdne96211:0';
+$_['error_code']     = 'crwdns96213:0crwdne96213:0';
