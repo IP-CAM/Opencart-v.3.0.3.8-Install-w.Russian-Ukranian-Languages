6e022701634ed7ad37a756bfa19c2c85451d62ec
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:25 +0200

    New translations amazon_pay.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/module/amazon_pay.php
@@ -0,0 +1,34 @@
+<?php
+// Heading
+$_['heading_title'] = 'crwdns84609:0crwdne84609:0';
+
+//Text
+$_['text_module'] = 'crwdns84611:0crwdne84611:0';
+$_['text_extension']               = 'crwdns84613:0crwdne84613:0';
+$_['text_success'] = 'crwdns84615:0crwdne84615:0';
+$_['text_content_top'] = 'crwdns84617:0crwdne84617:0';
+$_['text_content_bottom'] = 'crwdns84619:0crwdne84619:0';
+$_['text_column_left'] = 'crwdns84621:0crwdne84621:0';
+$_['text_column_right'] = 'crwdns84623:0crwdne84623:0';
+$_['text_pwa_button'] = 'crwdns84625:0crwdne84625:0';
+$_['text_pay_button'] = 'crwdns84627:0crwdne84627:0';
+$_['text_a_button'] = 'crwdns84629:0crwdne84629:0';
+$_['text_gold_button'] = 'crwdns84631:0crwdne84631:0';
+$_['text_darkgray_button'] = 'crwdns84633:0crwdne84633:0';
+$_['text_lightgray_button'] = 'crwdns84635:0crwdne84635:0';
+$_['text_small_button'] = 'crwdns84637:0crwdne84637:0';
+$_['text_medium_button'] = 'crwdns84639:0crwdne84639:0';
+$_['text_large_button'] = 'crwdns84641:0crwdne84641:0';
+$_['text_x_large_button'] = 'crwdns84643:0crwdne84643:0';
+
+//Entry
+$_['entry_button_type'] = 'crwdns84645:0crwdne84645:0';
+$_['entry_button_colour'] = 'crwdns84647:0crwdne84647:0';
+$_['entry_button_size'] = 'crwdns84649:0crwdne84649:0';
+$_['entry_layout'] = 'crwdns84651:0crwdne84651:0';
+$_['entry_position'] = 'crwdns84653:0crwdne84653:0';
+$_['entry_status'] = 'crwdns84655:0crwdne84655:0';
+$_['entry_sort_order'] = 'crwdns84657:0crwdne84657:0';
+
+//Error
+$_['error_permission'] = 'crwdns84659:0crwdne84659:0';
