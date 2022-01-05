cd942c185263f985109ad511ef5c7082a9fc7714
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:03 +0200

    New translations payment.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/api/payment.php
@@ -0,0 +1,18 @@
+<?php
+// Text
+$_['text_address']       = 'crwdns96645:0crwdne96645:0';
+$_['text_method']        = 'crwdns96647:0crwdne96647:0';
+
+// Error
+$_['error_permission']   = 'crwdns96649:0crwdne96649:0';
+$_['error_firstname']    = 'crwdns96651:0crwdne96651:0';
+$_['error_lastname']     = 'crwdns96653:0crwdne96653:0';
+$_['error_address_1']    = 'crwdns96655:0crwdne96655:0';
+$_['error_city']         = 'crwdns96657:0crwdne96657:0';
+$_['error_postcode']     = 'crwdns96659:0crwdne96659:0';
+$_['error_country']      = 'crwdns96661:0crwdne96661:0';
+$_['error_zone']         = 'crwdns96663:0crwdne96663:0';
+$_['error_custom_field'] = 'crwdns96665:0%scrwdne96665:0';
+$_['error_address']      = 'crwdns96667:0crwdne96667:0';
+$_['error_method']       = 'crwdns96669:0crwdne96669:0';
+$_['error_no_payment']   = 'crwdns96671:0crwdne96671:0';
