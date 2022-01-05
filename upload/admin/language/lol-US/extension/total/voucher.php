a25c21ad9b48bd142334d6430fbdd5ffd2badd17
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:31 +0200

    New translations voucher.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/total/voucher.php
@@ -0,0 +1,15 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns93211:0crwdne93211:0';
+
+// Text
+$_['text_extension']   = 'crwdns93213:0crwdne93213:0';
+$_['text_success']     = 'crwdns93215:0crwdne93215:0';
+$_['text_edit']        = 'crwdns93217:0crwdne93217:0';
+
+// Entry
+$_['entry_status']     = 'crwdns93219:0crwdne93219:0';
+$_['entry_sort_order'] = 'crwdns93221:0crwdne93221:0';
+
+// Error
+$_['error_permission'] = 'crwdns93223:0crwdne93223:0';
