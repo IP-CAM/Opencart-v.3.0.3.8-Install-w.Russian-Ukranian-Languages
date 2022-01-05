6bab87a49249d4eb2fab470278fe856bf49b263f
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:55 +0200

    New translations pp_standard.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/payment/pp_standard.php
@@ -0,0 +1,44 @@
+<?php
+// Heading
+$_['heading_title']					 = 'crwdns89981:0crwdne89981:0';
+
+// Text
+$_['text_extension']				 = 'crwdns89983:0crwdne89983:0';
+$_['text_success']					 = 'crwdns89985:0crwdne89985:0';
+$_['text_edit']                      = 'crwdns89987:0crwdne89987:0';
+$_['text_pp_standard']				 = 'crwdns89989:0crwdne89989:0';
+$_['text_authorization']			 = 'crwdns89991:0crwdne89991:0';
+$_['text_sale']						 = 'crwdns89993:0crwdne89993:0'; 
+
+// Entry
+$_['entry_email']					 = 'crwdns89995:0crwdne89995:0';
+$_['entry_test']					 = 'crwdns89997:0crwdne89997:0';
+$_['entry_transaction']				 = 'crwdns89999:0crwdne89999:0';
+$_['entry_debug']					 = 'crwdns90001:0crwdne90001:0';
+$_['entry_total']					 = 'crwdns90003:0crwdne90003:0';
+$_['entry_canceled_reversal_status'] = 'crwdns90005:0crwdne90005:0';
+$_['entry_completed_status']		 = 'crwdns90007:0crwdne90007:0';
+$_['entry_denied_status']			 = 'crwdns90009:0crwdne90009:0';
+$_['entry_expired_status']			 = 'crwdns90011:0crwdne90011:0';
+$_['entry_failed_status']			 = 'crwdns90013:0crwdne90013:0';
+$_['entry_pending_status']			 = 'crwdns90015:0crwdne90015:0';
+$_['entry_processed_status']		 = 'crwdns90017:0crwdne90017:0';
+$_['entry_refunded_status']			 = 'crwdns90019:0crwdne90019:0';
+$_['entry_reversed_status']			 = 'crwdns90021:0crwdne90021:0';
+$_['entry_voided_status']			 = 'crwdns90023:0crwdne90023:0';
+$_['entry_geo_zone']				 = 'crwdns90025:0crwdne90025:0';
+$_['entry_status']					 = 'crwdns90027:0crwdne90027:0';
+$_['entry_sort_order']				 = 'crwdns90029:0crwdne90029:0';
+
+// Tab
+$_['tab_general']					 = 'crwdns90031:0crwdne90031:0';
+$_['tab_order_status']       		 = 'crwdns90033:0crwdne90033:0';
+
+// Help
+$_['help_test']						 = 'crwdns90035:0crwdne90035:0';
+$_['help_debug']			    	 = 'crwdns90037:0crwdne90037:0';
+$_['help_total']					 = 'crwdns90039:0crwdne90039:0';
+
+// Error
+$_['error_permission']				 = 'crwdns90041:0crwdne90041:0';
+$_['error_email']					 = 'crwdns90043:0crwdne90043:0';
