2fe2546db19e6bd0fd58b9c911d750d175afa3d7
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:33 +0200

    New translations footer.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/common/footer.php
@@ -0,0 +1,17 @@
+<?php
+// Text
+$_['text_information']  = 'crwdns96995:0crwdne96995:0';
+$_['text_service']      = 'crwdns96997:0crwdne96997:0';
+$_['text_extra']        = 'crwdns96999:0crwdne96999:0';
+$_['text_contact']      = 'crwdns97001:0crwdne97001:0';
+$_['text_return']       = 'crwdns97003:0crwdne97003:0';
+$_['text_sitemap']      = 'crwdns97005:0crwdne97005:0';
+$_['text_manufacturer'] = 'crwdns97007:0crwdne97007:0';
+$_['text_voucher']      = 'crwdns97009:0crwdne97009:0';
+$_['text_affiliate']    = 'crwdns97011:0crwdne97011:0';
+$_['text_special']      = 'crwdns97013:0crwdne97013:0';
+$_['text_account']      = 'crwdns97015:0crwdne97015:0';
+$_['text_order']        = 'crwdns97017:0crwdne97017:0';
+$_['text_wishlist']     = 'crwdns97019:0crwdne97019:0';
+$_['text_newsletter']   = 'crwdns97021:0crwdne97021:0';
+$_['text_powered']      = 'crwdns97023:0%scrwdnd97023:0%scrwdne97023:0';
