15efab5d822a768b39b41a44ca04089bac7c4a7e
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:20:57 +0200

    New translations download.php (LOLCAT)

---
+++ upload/admin/language/lol-US/catalog/download.php
@@ -0,0 +1,34 @@
+<?php
+// Heading
+$_['heading_title']     = 'crwdns81187:0crwdne81187:0';
+
+// Text
+$_['text_success']      = 'crwdns81189:0crwdne81189:0';
+$_['text_list']         = 'crwdns81191:0crwdne81191:0';
+$_['text_add']          = 'crwdns81193:0crwdne81193:0';
+$_['text_edit']         = 'crwdns81195:0crwdne81195:0';
+$_['text_upload']       = 'crwdns81197:0crwdne81197:0';
+
+// Column
+$_['column_name']       = 'crwdns81199:0crwdne81199:0';
+$_['column_date_added'] = 'crwdns81201:0crwdne81201:0';
+$_['column_action']     = 'crwdns81203:0crwdne81203:0';
+
+// Entry
+$_['entry_name']        = 'crwdns81205:0crwdne81205:0';
+$_['entry_filename']    = 'crwdns81207:0crwdne81207:0';
+$_['entry_mask']        = 'crwdns81209:0crwdne81209:0';
+
+// Help
+$_['help_filename']     = 'crwdns81211:0crwdne81211:0';
+$_['help_mask']         = 'crwdns81213:0crwdne81213:0';
+
+// Error
+$_['error_permission']  = 'crwdns81215:0crwdne81215:0';
+$_['error_name']        = 'crwdns81217:0crwdne81217:0';
+$_['error_upload']      = 'crwdns81219:0crwdne81219:0';
+$_['error_filename']    = 'crwdns81221:0crwdne81221:0';
+$_['error_exists']      = 'crwdns81223:0crwdne81223:0';
+$_['error_mask']        = 'crwdns81225:0crwdne81225:0';
+$_['error_filetype']    = 'crwdns81227:0crwdne81227:0';
+$_['error_product']     = 'crwdns81229:0%scrwdne81229:0';
