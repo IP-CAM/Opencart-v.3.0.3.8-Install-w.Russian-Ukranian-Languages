510eefdf70c9db748c970948c686551dcb6e118f
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:26 +0200

    New translations extension.php (LOLCAT)

---
+++ upload/admin/language/lol-US/marketplace/extension.php
@@ -0,0 +1,9 @@
+<?php
+// Heading
+$_['heading_title'] = 'crwdns94097:0crwdne94097:0';
+
+// Text
+$_['text_success']  = 'crwdns94099:0crwdne94099:0';
+$_['text_list']     = 'crwdns94101:0crwdne94101:0';
+$_['text_type']     = 'crwdns94103:0crwdne94103:0';
+$_['text_filter']   = 'crwdns94105:0crwdne94105:0';
