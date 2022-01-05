f704dfd599163f9fcdf44cc8c7afd8d05b38f56e
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:25:14 +0200

    New translations parcelforce_48.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/shipping/parcelforce_48.php
@@ -0,0 +1,7 @@
+<?php
+// Text
+$_['text_title']       = 'crwdns99933:0crwdne99933:0';
+$_['text_description'] = 'crwdns99935:0crwdne99935:0';
+$_['text_weight']      = 'crwdns99937:0crwdne99937:0';
+$_['text_insurance']   = 'crwdns99939:0crwdne99939:0';
+$_['text_time']        = 'crwdns99941:0crwdne99941:0';
