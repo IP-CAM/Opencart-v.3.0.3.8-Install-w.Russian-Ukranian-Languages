2d576b3b6abea86876f82b65ca56c4972cfc164a
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:28 +0200

    New translations laybuy_layout.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/module/laybuy_layout.php
@@ -0,0 +1,26 @@
+<?php
+// Heading
+$_['heading_title']            = 'crwdns97489:0crwdne97489:0';
+
+// Text
+$_['text_reference_info']      = 'crwdns97491:0crwdne97491:0';
+$_['text_laybuy_ref_no']       = 'crwdns97493:0crwdne97493:0';
+$_['text_paypal_profile_id']   = 'crwdns97495:0crwdne97495:0';
+$_['text_payment_plan']        = 'crwdns97497:0crwdne97497:0';
+$_['text_status']              = 'crwdns97499:0crwdne97499:0';
+$_['text_amount']              = 'crwdns97501:0crwdne97501:0';
+$_['text_downpayment_percent'] = 'crwdns97503:0crwdne97503:0';
+$_['text_months']              = 'crwdns97505:0crwdne97505:0';
+$_['text_downpayment_amount']  = 'crwdns97507:0crwdne97507:0';
+$_['text_payment_amounts']     = 'crwdns97509:0crwdne97509:0';
+$_['text_first_payment_due']   = 'crwdns97511:0crwdne97511:0';
+$_['text_last_payment_due']    = 'crwdns97513:0crwdne97513:0';
+$_['text_downpayment']         = 'crwdns97515:0crwdne97515:0';
+$_['text_month']               = 'crwdns97517:0crwdne97517:0';
+
+// Column
+$_['column_instalment']        = 'crwdns97519:0crwdne97519:0';
+$_['column_amount']            = 'crwdns97521:0crwdne97521:0';
+$_['column_date']              = 'crwdns97523:0crwdne97523:0';
+$_['column_pp_trans_id']       = 'crwdns97525:0crwdne97525:0';
+$_['column_status']            = 'crwdns97527:0crwdne97527:0';
