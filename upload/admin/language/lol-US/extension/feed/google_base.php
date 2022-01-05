91789666025cfdcb37ae536c11667fc873bf0ace
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:40 +0200

    New translations google_base.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/feed/google_base.php
@@ -0,0 +1,25 @@
+<?php
+// Heading
+$_['heading_title']          = 'crwdns84075:0crwdne84075:0';
+
+// Text
+$_['text_extension']         = 'crwdns84077:0crwdne84077:0';
+$_['text_success']           = 'crwdns84079:0crwdne84079:0';
+$_['text_edit']              = 'crwdns84081:0crwdne84081:0';
+$_['text_import']            = 'crwdns84083:0crwdne84083:0';
+
+// Column
+$_['column_google_category'] = 'crwdns84085:0crwdne84085:0';
+$_['column_category']        = 'crwdns84087:0crwdne84087:0';
+$_['column_action']          = 'crwdns84089:0crwdne84089:0';
+
+// Entry
+$_['entry_google_category']  = 'crwdns84091:0crwdne84091:0';
+$_['entry_category']         = 'crwdns84093:0crwdne84093:0';
+$_['entry_data_feed']        = 'crwdns84095:0crwdne84095:0';
+$_['entry_status']           = 'crwdns84097:0crwdne84097:0';
+
+// Error
+$_['error_permission']       = 'crwdns84099:0crwdne84099:0';
+$_['error_upload']           = 'crwdns84101:0crwdne84101:0';
+$_['error_filetype']         = 'crwdns84103:0crwdne84103:0';
