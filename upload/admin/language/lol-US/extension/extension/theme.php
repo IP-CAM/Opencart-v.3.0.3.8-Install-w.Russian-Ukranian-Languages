22d0927468950ceee66a41f521a7c55d691584bc
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:39 +0200

    New translations theme.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/extension/theme.php
@@ -0,0 +1,15 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns84045:0crwdne84045:0';
+
+// Text
+$_['text_success']     = 'crwdns84047:0crwdne84047:0';
+$_['text_list']        = 'crwdns84049:0crwdne84049:0';
+
+// Column
+$_['column_name']      = 'crwdns84051:0crwdne84051:0';
+$_['column_status']    = 'crwdns84053:0crwdne84053:0';
+$_['column_action']    = 'crwdns84055:0crwdne84055:0';
+
+// Error
+$_['error_permission'] = 'crwdns84057:0crwdne84057:0';
