720dac96551a02a6f43ee1bf36389501828e52e3
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:04 +0200

    New translations authorizenet_sim.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/payment/authorizenet_sim.php
@@ -0,0 +1,31 @@
+<?php
+// Heading
+$_['heading_title']			= 'crwdns85521:0crwdne85521:0';
+
+// Text
+$_['text_extension']		= 'crwdns85523:0crwdne85523:0';
+$_['text_success']			= 'crwdns85525:0crwdne85525:0';
+$_['text_edit']             = 'crwdns85527:0crwdne85527:0';
+$_['text_authorizenet_sim']	= 'crwdns85529:0crwdne85529:0';
+
+// Entry
+$_['entry_merchant']		= 'crwdns85531:0crwdne85531:0';
+$_['entry_key']				= 'crwdns85533:0crwdne85533:0';
+$_['entry_callback']		= 'crwdns85535:0crwdne85535:0';
+$_['entry_md5']				= 'crwdns85537:0crwdne85537:0';
+$_['entry_test']			= 'crwdns85539:0crwdne85539:0';
+$_['entry_total']			= 'crwdns85541:0crwdne85541:0';
+$_['entry_order_status']	= 'crwdns85543:0crwdne85543:0';
+$_['entry_geo_zone']		= 'crwdns85545:0crwdne85545:0';
+$_['entry_status']			= 'crwdns85547:0crwdne85547:0';
+$_['entry_sort_order']		= 'crwdns85549:0crwdne85549:0';
+
+// Help
+$_['help_callback']			= 'crwdns85551:0crwdne85551:0';
+$_['help_md5']				= 'crwdns85553:0crwdne85553:0';
+$_['help_total']			= 'crwdns85555:0crwdne85555:0';
+
+// Error
+$_['error_permission']		= 'crwdns85557:0crwdne85557:0';
+$_['error_merchant']		= 'crwdns85559:0crwdne85559:0';
+$_['error_key']				= 'crwdns85561:0crwdne85561:0';
