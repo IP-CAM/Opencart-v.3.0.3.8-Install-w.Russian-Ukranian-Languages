ee33801fee84e58b2e6a5a8268ec71723b6423c0
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:41 +0200

    New translations basic.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/captcha/basic.php
@@ -0,0 +1,9 @@
+<?php
+// Text
+$_['text_captcha']  = 'crwdns97205:0crwdne97205:0';
+
+// Entry
+$_['entry_captcha'] = 'crwdns97207:0crwdne97207:0';
+
+// Error
+$_['error_captcha'] = 'crwdns97209:0crwdne97209:0';
