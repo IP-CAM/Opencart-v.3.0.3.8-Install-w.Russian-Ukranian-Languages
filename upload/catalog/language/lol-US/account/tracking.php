49f350f476e16f7adbdd4d9e2af20c6607109a07
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:54 +0200

    New translations tracking.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/account/tracking.php
@@ -0,0 +1,15 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns96347:0crwdne96347:0';
+
+// Text
+$_['text_account']     = 'crwdns96349:0crwdne96349:0';
+$_['text_description'] = 'crwdns96351:0%scrwdne96351:0';
+
+// Entry
+$_['entry_code']       = 'crwdns96353:0crwdne96353:0';
+$_['entry_generator']  = 'crwdns96355:0crwdne96355:0';
+$_['entry_link']       = 'crwdns96357:0crwdne96357:0';
+
+// Help
+$_['help_generator']  = 'crwdns96359:0crwdne96359:0';
