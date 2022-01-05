017bfd59b71659f3f5179dc04b763ac8f19ea631
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:14 +0200

    New translations g2apay.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/payment/g2apay.php
@@ -0,0 +1,63 @@
+<?php
+// Heading
+$_['heading_title']				 	  = 'crwdns86797:0crwdne86797:0';
+
+// Text
+$_['text_extension']				  = 'crwdns86799:0crwdne86799:0';
+$_['text_success']				 	  = 'crwdns86801:0crwdne86801:0';
+$_['text_edit']					 	  = 'crwdns86803:0crwdne86803:0';
+$_['text_g2apay']				 	  = 'crwdns86805:0crwdne86805:0';
+$_['text_payment_info']			 	  = 'crwdns86807:0crwdne86807:0';
+$_['text_refund_status']		 	  = 'crwdns86809:0crwdne86809:0';
+$_['text_order_ref']			 	  = 'crwdns86811:0crwdne86811:0';
+$_['text_order_total']			 	  = 'crwdns86813:0crwdne86813:0';
+$_['text_total_released']		 	  = 'crwdns86815:0crwdne86815:0';
+$_['text_transactions']			 	  = 'crwdns86817:0crwdne86817:0';
+$_['text_column_amount']		 	  = 'crwdns86819:0crwdne86819:0';
+$_['text_column_type']			 	  = 'crwdns86821:0crwdne86821:0';
+$_['text_column_date_added']	 	  = 'crwdns86823:0crwdne86823:0';
+$_['text_refund_ok']			 	  = 'crwdns86825:0crwdne86825:0';
+$_['text_refund_ok_order']		 	  = 'crwdns86827:0crwdne86827:0';
+$_['text_refund']                     = 'crwdns86829:0crwdne86829:0';
+
+// Entry
+$_['entry_username']			 	  = 'crwdns86831:0crwdne86831:0';
+$_['entry_secret']				 	  = 'crwdns86833:0crwdne86833:0';
+$_['entry_api_hash']		     	  = 'crwdns86835:0crwdne86835:0';
+$_['entry_environment']			 	  = 'crwdns86837:0crwdne86837:0';
+$_['entry_secret_token']		 	  = 'crwdns86839:0crwdne86839:0';
+$_['entry_ipn_url']				 	  = 'crwdns86841:0crwdne86841:0';
+$_['entry_total']				 	  = 'crwdns86843:0crwdne86843:0';
+$_['entry_geo_zone']			 	  = 'crwdns86845:0crwdne86845:0';
+$_['entry_status']				 	  = 'crwdns86847:0crwdne86847:0';
+$_['entry_sort_order']			 	  = 'crwdns86849:0crwdne86849:0';
+$_['entry_debug']				 	  = 'crwdns86851:0crwdne86851:0';
+$_['entry_order_status']			  = 'crwdns86853:0crwdne86853:0';
+$_['entry_complete_status']			  = 'crwdns86855:0crwdne86855:0';
+$_['entry_rejected_status']			  = 'crwdns86857:0crwdne86857:0';
+$_['entry_cancelled_status']		  = 'crwdns86859:0crwdne86859:0';
+$_['entry_pending_status']            = 'crwdns86861:0crwdne86861:0';
+$_['entry_refunded_status']			  = 'crwdns86863:0crwdne86863:0';
+$_['entry_partially_refunded_status'] = 'crwdns86865:0crwdne86865:0';
+
+// Help
+$_['help_username']					  = 'crwdns86867:0crwdne86867:0';
+$_['help_total']				 	  = 'crwdns86869:0crwdne86869:0';
+$_['help_debug']				 	  = 'crwdns86871:0crwdne86871:0';
+
+// Tab
+$_['tab_settings']				 	  = 'crwdns86873:0crwdne86873:0';
+$_['tab_order_status']				  = 'crwdns86875:0crwdne86875:0';
+
+// Error
+$_['error_permission']			 	  = 'crwdns86877:0crwdne86877:0';
+$_['error_email']				 	  = 'crwdns86879:0crwdne86879:0';
+$_['error_secret']				 	  = 'crwdns86881:0crwdne86881:0';
+$_['error_api_hash']			 	  = 'crwdns86883:0crwdne86883:0';
+$_['entry_status']				 	  = 'crwdns86885:0crwdne86885:0';
+
+//Button
+$_['btn_refund']				 	  = 'crwdns86887:0crwdne86887:0';
+
+$_['g2apay_environment_live']	 	  = 'crwdns86889:0crwdne86889:0';
+$_['g2apay_environment_test']	 	  = 'crwdns86891:0crwdne86891:0';
