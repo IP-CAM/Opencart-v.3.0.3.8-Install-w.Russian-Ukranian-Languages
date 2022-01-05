30dd86e87d02cda6c2d9788606fea15064bee41c
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:52 +0200

    New translations order_add.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/mail/order_add.php
@@ -0,0 +1,27 @@
+<?php
+// Text
+$_['text_subject']          = 'crwdns100259:0%scrwdnd100259:0%scrwdne100259:0';
+$_['text_greeting']         = 'crwdns100261:0%scrwdne100261:0';
+$_['text_link']             = 'crwdns100263:0crwdne100263:0';
+$_['text_order_detail']     = 'crwdns100265:0crwdne100265:0';
+$_['text_instruction']      = 'crwdns100267:0crwdne100267:0';
+$_['text_order_id']         = 'crwdns100269:0crwdne100269:0';
+$_['text_date_added']       = 'crwdns100271:0crwdne100271:0';
+$_['text_order_status']     = 'crwdns100273:0crwdne100273:0';
+$_['text_payment_method']   = 'crwdns100275:0crwdne100275:0';
+$_['text_shipping_method']  = 'crwdns100277:0crwdne100277:0';
+$_['text_email']            = 'crwdns100279:0crwdne100279:0';
+$_['text_telephone']        = 'crwdns100281:0crwdne100281:0';
+$_['text_ip']               = 'crwdns100283:0crwdne100283:0';
+$_['text_payment_address']  = 'crwdns100285:0crwdne100285:0';
+$_['text_shipping_address'] = 'crwdns100287:0crwdne100287:0';
+$_['text_products']         = 'crwdns100289:0crwdne100289:0';
+$_['text_product']          = 'crwdns100291:0crwdne100291:0';
+$_['text_model']            = 'crwdns100293:0crwdne100293:0';
+$_['text_quantity']         = 'crwdns100295:0crwdne100295:0';
+$_['text_price']            = 'crwdns100297:0crwdne100297:0';
+$_['text_order_total']      = 'crwdns100299:0crwdne100299:0';
+$_['text_total']            = 'crwdns100301:0crwdne100301:0';
+$_['text_download']         = 'crwdns100303:0crwdne100303:0';
+$_['text_comment']          = 'crwdns100305:0crwdne100305:0';
+$_['text_footer']           = 'crwdns100307:0crwdne100307:0';
