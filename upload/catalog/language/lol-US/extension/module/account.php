fce02fb4c8f93db6a5bb677485be0a8b6e4cfae8
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:44 +0200

    New translations account.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/module/account.php
@@ -0,0 +1,21 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns97305:0crwdne97305:0';
+
+// Text
+$_['text_register']    = 'crwdns97307:0crwdne97307:0';
+$_['text_login']       = 'crwdns97309:0crwdne97309:0';
+$_['text_logout']      = 'crwdns97311:0crwdne97311:0';
+$_['text_forgotten']   = 'crwdns97313:0crwdne97313:0';
+$_['text_account']     = 'crwdns97315:0crwdne97315:0';
+$_['text_edit']        = 'crwdns97317:0crwdne97317:0';
+$_['text_password']    = 'crwdns97319:0crwdne97319:0';
+$_['text_address']     = 'crwdns97321:0crwdne97321:0';
+$_['text_wishlist']    = 'crwdns97323:0crwdne97323:0';
+$_['text_order']       = 'crwdns97325:0crwdne97325:0';
+$_['text_download']    = 'crwdns97327:0crwdne97327:0';
+$_['text_reward']      = 'crwdns97329:0crwdne97329:0';
+$_['text_return']      = 'crwdns97331:0crwdne97331:0';
+$_['text_transaction'] = 'crwdns97333:0crwdne97333:0';
+$_['text_newsletter']  = 'crwdns97335:0crwdne97335:0';
+$_['text_recurring']   = 'crwdns97337:0crwdne97337:0';
