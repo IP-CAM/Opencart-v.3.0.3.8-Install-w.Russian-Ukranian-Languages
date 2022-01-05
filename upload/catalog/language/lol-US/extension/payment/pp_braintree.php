13e4bdd38237b0bf0739806d9d46174895ffef17
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:18 +0200

    New translations pp_braintree.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/payment/pp_braintree.php
@@ -0,0 +1,48 @@
+<?php
+// Text
+$_['text_title']						= 'crwdns99117:0crwdne99117:0';
+$_['text_express_title']      			= 'crwdns99119:0crwdne99119:0';
+$_['text_vaulted_payment_method_name']	= 'crwdns99121:0%scrwdnd99121:0%scrwdnd99121:0%scrwdne99121:0';
+$_['text_remember']						= 'crwdns99123:0crwdne99123:0';
+$_['text_remove']						= 'crwdns99125:0crwdne99125:0';
+$_['text_remove_confirm']				= 'crwdns99127:0crwdne99127:0';
+$_['text_month']						= 'crwdns99129:0crwdne99129:0';
+$_['text_year']							= 'crwdns99131:0crwdne99131:0';
+$_['text_loading']						= 'crwdns99133:0crwdne99133:0';
+$_['text_new_method']					= 'crwdns99135:0crwdne99135:0';
+$_['text_saved_method']					= 'crwdns99137:0crwdne99137:0';
+$_['text_paypal']						= 'crwdns99139:0crwdne99139:0';
+$_['text_pay_by_paypal']				= 'crwdns99141:0crwdne99141:0';
+$_['text_method_removed']				= 'crwdns99143:0crwdne99143:0';
+$_['text_method_not_removed']			= 'crwdns99145:0crwdne99145:0';
+$_['text_authentication']				= 'crwdns99147:0crwdne99147:0';
+$_['text_cart']               			= 'crwdns99149:0crwdne99149:0';
+$_['text_shipping_updated']   			= 'crwdns99151:0crwdne99151:0';
+
+// Entry
+$_['entry_new']							= 'crwdns99153:0crwdne99153:0';
+$_['entry_saved_methods']				= 'crwdns99155:0crwdne99155:0';
+$_['entry_card']						= 'crwdns99157:0crwdne99157:0';
+$_['entry_expires']						= 'crwdns99159:0crwdne99159:0';
+$_['entry_cvv']							= 'crwdns99161:0crwdne99161:0';
+$_['entry_remember_card_method']		= 'crwdns99163:0crwdne99163:0';
+$_['entry_remember_paypal_method']		= 'crwdns99165:0crwdne99165:0';
+$_['entry_card_placeholder']			= 'crwdns99167:0crwdne99167:0';
+$_['entry_month_placeholder']			= 'crwdns99169:0crwdne99169:0';
+$_['entry_year_placeholder']			= 'crwdns99171:0crwdne99171:0';
+$_['entry_cvv_placeholder']				= 'crwdns99173:0crwdne99173:0';
+
+// Error
+$_['error_process_order']				= 'crwdns99175:0crwdne99175:0';
+$_['error_alert_fields_empty']			= 'crwdns99177:0crwdne99177:0';
+$_['error_alert_fields_invalid']		= 'crwdns99179:0crwdne99179:0';
+$_['error_alert_failed_token']			= 'crwdns99181:0crwdne99181:0';
+$_['error_alert_failed_network']		= 'crwdns99183:0crwdne99183:0';
+$_['error_alert_unknown']				= 'crwdns99185:0crwdne99185:0';
+$_['error_unavailable'] 	  			= 'crwdns99187:0crwdne99187:0';
+$_['error_no_shipping']    				= 'crwdns99189:0crwdne99189:0';
+
+// Button
+$_['button_confirm']					= 'crwdns99191:0crwdne99191:0';
+$_['button_express_confirm']  			= 'crwdns99193:0crwdne99193:0';
+$_['button_express_shipping'] 			= 'crwdns99195:0crwdne99195:0';
