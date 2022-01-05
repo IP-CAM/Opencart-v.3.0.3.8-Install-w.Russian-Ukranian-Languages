7ffa4370d69acdcb94c23877db7c142b1603044d
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:47 +0200

    New translations order.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/account/order.php
@@ -0,0 +1,37 @@
+<?php
+// Heading
+$_['heading_title']         = 'crwdns95989:0crwdne95989:0';
+
+// Text
+$_['text_account']          = 'crwdns95991:0crwdne95991:0';
+$_['text_order']            = 'crwdns95993:0crwdne95993:0';
+$_['text_order_detail']     = 'crwdns95995:0crwdne95995:0';
+$_['text_invoice_no']       = 'crwdns95997:0crwdne95997:0';
+$_['text_order_id']         = 'crwdns95999:0crwdne95999:0';
+$_['text_date_added']       = 'crwdns96001:0crwdne96001:0';
+$_['text_shipping_address'] = 'crwdns96003:0crwdne96003:0';
+$_['text_shipping_method']  = 'crwdns96005:0crwdne96005:0';
+$_['text_payment_address']  = 'crwdns96007:0crwdne96007:0';
+$_['text_payment_method']   = 'crwdns96009:0crwdne96009:0';
+$_['text_comment']          = 'crwdns96011:0crwdne96011:0';
+$_['text_history']          = 'crwdns96013:0crwdne96013:0';
+$_['text_success']          = 'crwdns96015:0%scrwdnd96015:0%scrwdnd96015:0%scrwdne96015:0';
+$_['text_empty']            = 'crwdns96017:0crwdne96017:0';
+$_['text_error']            = 'crwdns96019:0crwdne96019:0';
+
+// Column
+$_['column_order_id']       = 'crwdns96021:0crwdne96021:0';
+$_['column_customer']       = 'crwdns96023:0crwdne96023:0';
+$_['column_product']        = 'crwdns96025:0crwdne96025:0';
+$_['column_name']           = 'crwdns96027:0crwdne96027:0';
+$_['column_model']          = 'crwdns96029:0crwdne96029:0';
+$_['column_quantity']       = 'crwdns96031:0crwdne96031:0';
+$_['column_price']          = 'crwdns96033:0crwdne96033:0';
+$_['column_total']          = 'crwdns96035:0crwdne96035:0';
+$_['column_action']         = 'crwdns96037:0crwdne96037:0';
+$_['column_date_added']     = 'crwdns96039:0crwdne96039:0';
+$_['column_status']         = 'crwdns96041:0crwdne96041:0';
+$_['column_comment']        = 'crwdns96043:0crwdne96043:0';
+
+// Error
+$_['error_reorder']         = 'crwdns96045:0%scrwdne96045:0';
