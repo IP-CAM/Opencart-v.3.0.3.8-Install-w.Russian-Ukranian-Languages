14e61e1eaddc3d0da4b29a24ae8872d5c2cf233d
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:20:46 +0200

    New translations chart.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/dashboard/chart.php
@@ -0,0 +1,22 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns83693:0crwdne83693:0';
+
+// Text
+$_['text_extension']   = 'crwdns83695:0crwdne83695:0';
+$_['text_success']     = 'crwdns83697:0crwdne83697:0';
+$_['text_edit']        = 'crwdns83699:0crwdne83699:0';
+$_['text_order']       = 'crwdns83701:0crwdne83701:0';
+$_['text_customer']    = 'crwdns83703:0crwdne83703:0';
+$_['text_day']         = 'crwdns83705:0crwdne83705:0';
+$_['text_week']        = 'crwdns83707:0crwdne83707:0';
+$_['text_month']       = 'crwdns83709:0crwdne83709:0';
+$_['text_year']        = 'crwdns83711:0crwdne83711:0';
+
+// Entry
+$_['entry_status']     = 'crwdns83713:0crwdne83713:0';
+$_['entry_sort_order'] = 'crwdns83715:0crwdne83715:0';
+$_['entry_width']      = 'crwdns83717:0crwdne83717:0';
+
+// Error
+$_['error_permission'] = 'crwdns83719:0crwdne83719:0';
