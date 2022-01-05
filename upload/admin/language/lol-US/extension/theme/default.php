992d2f875c7b9b00c8f150939f36fe7c92e01e0a
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:23 +0200

    New translations default.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/theme/default.php
@@ -0,0 +1,48 @@
+<?php
+// Heading
+$_['heading_title']                    = 'crwdns92959:0crwdne92959:0';
+
+// Text
+$_['text_extension']                   = 'crwdns92961:0crwdne92961:0';
+$_['text_success']                     = 'crwdns92963:0crwdne92963:0';
+$_['text_edit']                        = 'crwdns92965:0crwdne92965:0';
+$_['text_general']                     = 'crwdns92967:0crwdne92967:0';
+$_['text_product']                     = 'crwdns92969:0crwdne92969:0';
+$_['text_image']                       = 'crwdns92971:0crwdne92971:0';
+
+// Entry
+$_['entry_directory']                  = 'crwdns92973:0crwdne92973:0';
+$_['entry_status']                     = 'crwdns92975:0crwdne92975:0';
+$_['entry_product_limit']              = 'crwdns92977:0crwdne92977:0';
+$_['entry_product_description_length'] = 'crwdns92979:0crwdne92979:0';
+$_['entry_image_category']             = 'crwdns92981:0crwdne92981:0';
+$_['entry_image_thumb']                = 'crwdns92983:0crwdne92983:0';
+$_['entry_image_popup']                = 'crwdns92985:0crwdne92985:0';
+$_['entry_image_product']              = 'crwdns92987:0crwdne92987:0';
+$_['entry_image_additional']           = 'crwdns92989:0crwdne92989:0';
+$_['entry_image_related']              = 'crwdns92991:0crwdne92991:0';
+$_['entry_image_compare']              = 'crwdns92993:0crwdne92993:0';
+$_['entry_image_wishlist']             = 'crwdns92995:0crwdne92995:0';
+$_['entry_image_cart']                 = 'crwdns92997:0crwdne92997:0';
+$_['entry_image_location']             = 'crwdns92999:0crwdne92999:0';
+$_['entry_width']                      = 'crwdns93001:0crwdne93001:0';
+$_['entry_height']                     = 'crwdns93003:0crwdne93003:0';
+
+// Help
+$_['help_directory']                   = 'crwdns93005:0crwdne93005:0';
+$_['help_product_limit']               = 'crwdns93007:0crwdne93007:0';
+$_['help_product_description_length']  = 'crwdns93009:0crwdne93009:0';
+
+// Error
+$_['error_permission']                 = 'crwdns93011:0crwdne93011:0';
+$_['error_limit']                      = 'crwdns93013:0crwdne93013:0';
+$_['error_image_thumb']                = 'crwdns93015:0crwdne93015:0';
+$_['error_image_popup']                = 'crwdns93017:0crwdne93017:0';
+$_['error_image_product']              = 'crwdns93019:0crwdne93019:0';
+$_['error_image_category']             = 'crwdns93021:0crwdne93021:0';
+$_['error_image_additional']           = 'crwdns93023:0crwdne93023:0';
+$_['error_image_related']              = 'crwdns93025:0crwdne93025:0';
+$_['error_image_compare']              = 'crwdns93027:0crwdne93027:0';
+$_['error_image_wishlist']             = 'crwdns93029:0crwdne93029:0';
+$_['error_image_cart']                 = 'crwdns93031:0crwdne93031:0';
+$_['error_image_location']             = 'crwdns93033:0crwdne93033:0';
