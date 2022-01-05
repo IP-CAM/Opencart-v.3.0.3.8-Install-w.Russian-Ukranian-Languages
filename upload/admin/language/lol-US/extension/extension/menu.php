a3131f1f6f5cdb40e863c2b94a7beb79cffbf80c
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:34 +0200

    New translations menu.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/extension/menu.php
@@ -0,0 +1,15 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns83941:0crwdne83941:0';
+
+// Text
+$_['text_success']     = 'crwdns83943:0crwdne83943:0';
+$_['text_list']        = 'crwdns83945:0crwdne83945:0';
+
+// Column
+$_['column_name']      = 'crwdns83947:0crwdne83947:0';
+$_['column_status']    = 'crwdns83949:0crwdne83949:0';
+$_['column_action']    = 'crwdns83951:0crwdne83951:0';
+
+// Error
+$_['error_permission'] = 'crwdns83953:0crwdne83953:0';
