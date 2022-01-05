14eb99ab35ac3374f51a3fb8016c58a0cd7a27d5
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:16 +0200

    New translations affiliate_approve.php (LOLCAT)

---
+++ upload/admin/language/lol-US/mail/affiliate_approve.php
@@ -0,0 +1,7 @@
+<?php
+// Text
+$_['text_subject']  = 'crwdns93781:0%scrwdne93781:0';
+$_['text_welcome']  = 'crwdns93783:0%scrwdne93783:0';
+$_['text_login']    = 'crwdns93785:0crwdne93785:0';
+$_['text_services'] = 'crwdns93787:0crwdne93787:0';
+$_['text_thanks']   = 'crwdns93789:0crwdne93789:0';
