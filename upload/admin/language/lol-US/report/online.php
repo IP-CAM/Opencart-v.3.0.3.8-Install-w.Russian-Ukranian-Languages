c059bad55530a873955c6443eac831c5992a15b8
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:03 +0200

    New translations online.php (LOLCAT)

---
+++ upload/admin/language/lol-US/report/online.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']     = 'crwdns94315:0crwdne94315:0';
+
+// Text
+$_['text_extension']    = 'crwdns94317:0crwdne94317:0';
+$_['text_success']      = 'crwdns94319:0crwdne94319:0';
+$_['text_list']         = 'crwdns94321:0crwdne94321:0';
+$_['text_filter']       = 'crwdns94323:0crwdne94323:0';
+$_['text_guest']        = 'crwdns94325:0crwdne94325:0';
+
+// Column
+$_['column_ip']         = 'crwdns94327:0crwdne94327:0';
+$_['column_customer']   = 'crwdns94329:0crwdne94329:0';
+$_['column_url']        = 'crwdns94331:0crwdne94331:0';
+$_['column_referer']    = 'crwdns94333:0crwdne94333:0';
+$_['column_date_added'] = 'crwdns94335:0crwdne94335:0';
+$_['column_action']     = 'crwdns94337:0crwdne94337:0';
+
+// Entry
+$_['entry_ip']          = 'crwdns94339:0crwdne94339:0';
+$_['entry_customer']    = 'crwdns94341:0crwdne94341:0';
+$_['entry_status']      = 'crwdns94343:0crwdne94343:0';
+$_['entry_sort_order']  = 'crwdns94345:0crwdne94345:0';
+
+// Error
+$_['error_permission']  = 'crwdns94347:0crwdne94347:0';
