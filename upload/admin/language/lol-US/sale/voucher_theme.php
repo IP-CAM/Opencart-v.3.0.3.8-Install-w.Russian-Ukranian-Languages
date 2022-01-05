fea60a3fe80bc26162fcf18849148147574f2875
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:23:00 +0200

    New translations voucher_theme.php (LOLCAT)

---
+++ upload/admin/language/lol-US/sale/voucher_theme.php
@@ -0,0 +1,24 @@
+<?php
+// Heading
+$_['heading_title']     = 'crwdns94943:0crwdne94943:0';
+
+// Text
+$_['text_success']      = 'crwdns94945:0crwdne94945:0';
+$_['text_list']         = 'crwdns94947:0crwdne94947:0';
+$_['text_add']          = 'crwdns94949:0crwdne94949:0';
+$_['text_edit']         = 'crwdns94951:0crwdne94951:0';
+
+// Column
+$_['column_name']       = 'crwdns94953:0crwdne94953:0';
+$_['column_action']     = 'crwdns94955:0crwdne94955:0';
+
+// Entry
+$_['entry_name']        = 'crwdns94957:0crwdne94957:0';
+$_['entry_description'] = 'crwdns94959:0crwdne94959:0';
+$_['entry_image']       = 'crwdns94961:0crwdne94961:0';
+
+// Error
+$_['error_permission']  = 'crwdns94963:0crwdne94963:0';
+$_['error_name']        = 'crwdns94965:0crwdne94965:0';
+$_['error_image']       = 'crwdns94967:0crwdne94967:0';
+$_['error_voucher']     = 'crwdns94969:0%scrwdne94969:0';
