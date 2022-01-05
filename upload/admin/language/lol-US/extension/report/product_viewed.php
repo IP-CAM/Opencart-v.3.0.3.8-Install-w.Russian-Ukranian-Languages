7cd10686db513ec847a4006dd2f4b67add962c67
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:38 +0200

    New translations product_viewed.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/report/product_viewed.php
@@ -0,0 +1,21 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns91925:0crwdne91925:0';
+
+// Text
+$_['text_extension']   = 'crwdns91927:0crwdne91927:0';
+$_['text_edit']        = 'crwdns91929:0crwdne91929:0';
+$_['text_success']     = 'crwdns91931:0crwdne91931:0';
+
+// Column
+$_['column_name']      = 'crwdns91933:0crwdne91933:0';
+$_['column_model']     = 'crwdns91935:0crwdne91935:0';
+$_['column_viewed']    = 'crwdns91937:0crwdne91937:0';
+$_['column_percent']   = 'crwdns91939:0crwdne91939:0';
+
+// Entry
+$_['entry_status']     = 'crwdns91941:0crwdne91941:0';
+$_['entry_sort_order'] = 'crwdns91943:0crwdne91943:0';
+
+// Error
+$_['error_permission'] = 'crwdns91945:0crwdne91945:0';
