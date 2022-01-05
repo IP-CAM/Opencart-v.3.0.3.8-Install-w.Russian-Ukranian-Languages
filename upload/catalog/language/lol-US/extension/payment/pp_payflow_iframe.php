05681539d92925aec660f633d13c9cec08b1c4ae
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:20 +0200

    New translations pp_payflow_iframe.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/payment/pp_payflow_iframe.php
@@ -0,0 +1,7 @@
+<?php
+// Text
+$_['text_title']				= 'crwdns99267:0crwdne99267:0';
+$_['text_secure_connection']	= 'crwdns99269:0crwdne99269:0';
+
+// Error
+$_['error_connection']			= 'crwdns99271:0crwdne99271:0';
