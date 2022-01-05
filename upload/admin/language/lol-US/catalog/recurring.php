b64b1514c98f6df07c9f48eaebc366c729d3d6e8
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:01 +0200

    New translations recurring.php (LOLCAT)

---
+++ upload/admin/language/lol-US/catalog/recurring.php
@@ -0,0 +1,42 @@
+<?php
+// Heading
+$_['heading_title']         = 'crwdns81635:0crwdne81635:0';
+
+// Text
+$_['text_success']          = 'crwdns81637:0crwdne81637:0';
+$_['text_list']             = 'crwdns81639:0crwdne81639:0';
+$_['text_add']              = 'crwdns81641:0crwdne81641:0';
+$_['text_edit']             = 'crwdns81643:0crwdne81643:0';
+$_['text_day']              = 'crwdns81645:0crwdne81645:0';
+$_['text_week']             = 'crwdns81647:0crwdne81647:0';
+$_['text_semi_month']       = 'crwdns81649:0crwdne81649:0';
+$_['text_month']            = 'crwdns81651:0crwdne81651:0';
+$_['text_year']             = 'crwdns81653:0crwdne81653:0';
+$_['text_recurring']        = 'crwdns81655:0crwdne81655:0';
+$_['text_profile']          = 'crwdns81657:0crwdne81657:0';
+$_['text_trial']            = 'crwdns81659:0crwdne81659:0';
+
+// Entry
+$_['entry_name']            = 'crwdns81661:0crwdne81661:0';
+$_['entry_price']           = 'crwdns81663:0crwdne81663:0';
+$_['entry_duration']        = 'crwdns81665:0crwdne81665:0';
+$_['entry_cycle']           = 'crwdns81667:0crwdne81667:0';
+$_['entry_frequency']       = 'crwdns81669:0crwdne81669:0';
+$_['entry_trial_price']     = 'crwdns81671:0crwdne81671:0';
+$_['entry_trial_duration']  = 'crwdns81673:0crwdne81673:0';
+$_['entry_trial_status']    = 'crwdns81675:0crwdne81675:0';
+$_['entry_trial_cycle']     = 'crwdns81677:0crwdne81677:0';
+$_['entry_trial_frequency'] = 'crwdns81679:0crwdne81679:0';
+$_['entry_status']          = 'crwdns81681:0crwdne81681:0';
+$_['entry_sort_order']      = 'crwdns81683:0crwdne81683:0';
+
+// Column
+$_['column_name']           = 'crwdns81685:0crwdne81685:0';
+$_['column_sort_order']     = 'crwdns81687:0crwdne81687:0';
+$_['column_action']         = 'crwdns81689:0crwdne81689:0';
+
+// Error
+$_['error_warning']         = 'crwdns81691:0crwdne81691:0';
+$_['error_permission']      = 'crwdns81693:0crwdne81693:0';
+$_['error_name']            = 'crwdns81695:0crwdne81695:0';
+$_['error_product']         = 'crwdns81697:0%scrwdne81697:0';
