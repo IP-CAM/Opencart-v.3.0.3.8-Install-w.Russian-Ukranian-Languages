39c7ac37cbaada5ee218f846aa49698615be3d93
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:15 +0200

    New translations globalpay.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/payment/globalpay.php
@@ -0,0 +1,101 @@
+<?php
+// Heading
+$_['heading_title']					 = 'crwdns86893:0crwdne86893:0';
+
+// Text
+$_['text_extension']				  	 = 'crwdns86895:0crwdne86895:0';
+$_['text_success']					 = 'crwdns86897:0crwdne86897:0';
+$_['text_edit']                      = 'crwdns86899:0crwdne86899:0';
+$_['text_live']						 = 'crwdns86901:0crwdne86901:0';
+$_['text_demo']						 = 'crwdns86903:0crwdne86903:0';
+$_['text_card_type']				 = 'crwdns86905:0crwdne86905:0';
+$_['text_enabled']					 = 'crwdns86907:0crwdne86907:0';
+$_['text_use_default']				 = 'crwdns86909:0crwdne86909:0';
+$_['text_merchant_id']				 = 'crwdns86911:0crwdne86911:0';
+$_['text_subaccount']				 = 'crwdns86913:0crwdne86913:0';
+$_['text_secret']					 = 'crwdns86915:0crwdne86915:0';
+$_['text_card_visa']				 = 'crwdns86917:0crwdne86917:0';
+$_['text_card_master']				 = 'crwdns86919:0crwdne86919:0';
+$_['text_card_amex']				 = 'crwdns86921:0crwdne86921:0';
+$_['text_card_switch']				 = 'crwdns86923:0crwdne86923:0';
+$_['text_card_laser']				 = 'crwdns86925:0crwdne86925:0';
+$_['text_card_diners']				 = 'crwdns86927:0crwdne86927:0';
+$_['text_capture_ok']				 = 'crwdns86929:0crwdne86929:0';
+$_['text_capture_ok_order']			 = 'crwdns86931:0crwdne86931:0';
+$_['text_rebate_ok']				 = 'crwdns86933:0crwdne86933:0';
+$_['text_rebate_ok_order']			 = 'crwdns86935:0crwdne86935:0';
+$_['text_void_ok']					 = 'crwdns86937:0crwdne86937:0';
+$_['text_settle_auto']				 = 'crwdns86939:0crwdne86939:0';
+$_['text_settle_delayed']			 = 'crwdns86941:0crwdne86941:0';
+$_['text_settle_multi']				 = 'crwdns86943:0crwdne86943:0';
+$_['text_url_message']				 = 'crwdns86945:0crwdne86945:0';
+$_['text_payment_info']				 = 'crwdns86947:0crwdne86947:0';
+$_['text_capture_status']			 = 'crwdns86949:0crwdne86949:0';
+$_['text_void_status']				 = 'crwdns86951:0crwdne86951:0';
+$_['text_rebate_status']			 = 'crwdns86953:0crwdne86953:0';
+$_['text_order_ref']				 = 'crwdns86955:0crwdne86955:0';
+$_['text_order_total']				 = 'crwdns86957:0crwdne86957:0';
+$_['text_total_captured']			 = 'crwdns86959:0crwdne86959:0';
+$_['text_transactions']				 = 'crwdns86961:0crwdne86961:0';
+$_['text_column_amount']			 = 'crwdns86963:0crwdne86963:0';
+$_['text_column_type']				 = 'crwdns86965:0crwdne86965:0';
+$_['text_column_date_added']		 = 'crwdns86967:0crwdne86967:0';
+$_['text_confirm_void']				 = 'crwdns86969:0crwdne86969:0';
+$_['text_confirm_capture']			 = 'crwdns86971:0crwdne86971:0';
+$_['text_confirm_rebate']			 = 'crwdns86973:0crwdne86973:0';
+$_['text_globalpay']                 = 'crwdns86975:0crwdne86975:0';
+$_['text_void']                      = 'crwdns86977:0crwdne86977:0'; 
+$_['text_payment']                   = 'crwdns86979:0crwdne86979:0';
+$_['text_rebate']                    = 'crwdns86981:0crwdne86981:0';
+
+// Entry
+$_['entry_merchant_id']				 = 'crwdns86983:0crwdne86983:0';
+$_['entry_secret']					 = 'crwdns86985:0crwdne86985:0';
+$_['entry_rebate_password']			 = 'crwdns86987:0crwdne86987:0';
+$_['entry_total']					 = 'crwdns86989:0crwdne86989:0';
+$_['entry_sort_order']				 = 'crwdns86991:0crwdne86991:0';
+$_['entry_geo_zone']				 = 'crwdns86993:0crwdne86993:0';
+$_['entry_status']					 = 'crwdns86995:0crwdne86995:0';
+$_['entry_debug']					 = 'crwdns86997:0crwdne86997:0';
+$_['entry_live_demo']				 = 'crwdns86999:0crwdne86999:0';
+$_['entry_auto_settle']				 = 'crwdns87001:0crwdne87001:0';
+$_['entry_card_select']				 = 'crwdns87003:0crwdne87003:0';
+$_['entry_tss_check']				 = 'crwdns87005:0crwdne87005:0';
+$_['entry_live_url']				 = 'crwdns87007:0crwdne87007:0';
+$_['entry_demo_url']				 = 'crwdns87009:0crwdne87009:0';
+$_['entry_status_success_settled']	 = 'crwdns87011:0crwdne87011:0';
+$_['entry_status_success_unsettled'] = 'crwdns87013:0crwdne87013:0';
+$_['entry_status_decline']			 = 'crwdns87015:0crwdne87015:0';
+$_['entry_status_decline_pending']	 = 'crwdns87017:0crwdne87017:0';
+$_['entry_status_decline_stolen']	 = 'crwdns87019:0crwdne87019:0';
+$_['entry_status_decline_bank']		 = 'crwdns87021:0crwdne87021:0';
+$_['entry_status_void']				 = 'crwdns87023:0crwdne87023:0';
+$_['entry_status_rebate']			 = 'crwdns87025:0crwdne87025:0';
+$_['entry_notification_url']		 = 'crwdns87027:0crwdne87027:0';
+
+// Help
+$_['help_total']					 = 'crwdns87029:0crwdne87029:0';
+$_['help_card_select']				 = 'crwdns87031:0crwdne87031:0';
+$_['help_notification']				 = 'crwdns87033:0crwdne87033:0';
+$_['help_debug']					 = 'crwdns87035:0crwdne87035:0';
+$_['help_dcc_settle']				 = 'crwdns87037:0crwdne87037:0';
+
+// Tab
+$_['tab_api']					     = 'crwdns87039:0crwdne87039:0';
+$_['tab_account']		     		 = 'crwdns87041:0crwdne87041:0';
+$_['tab_order_status']				 = 'crwdns87043:0crwdne87043:0';
+$_['tab_payment']					 = 'crwdns87045:0crwdne87045:0';
+$_['tab_advanced']					 = 'crwdns87047:0crwdne87047:0';
+
+// Button
+$_['button_capture']				 = 'crwdns87049:0crwdne87049:0';
+$_['button_rebate']					 = 'crwdns87051:0crwdne87051:0';
+$_['button_void']					 = 'crwdns87053:0crwdne87053:0';
+
+// Error
+$_['error_merchant_id']				 = 'crwdns87055:0crwdne87055:0';
+$_['error_secret']					 = 'crwdns87057:0crwdne87057:0';
+$_['error_live_url']				 = 'crwdns87059:0crwdne87059:0';
+$_['error_demo_url']				 = 'crwdns87061:0crwdne87061:0';
+$_['error_data_missing']			 = 'crwdns87063:0crwdne87063:0';
+$_['error_use_select_card']			 = 'crwdns87065:0crwdne87065:0';
