b6fd635cc19eee30ddda0c0826c10a75252ab75a
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:19 +0200

    New translations pp_express.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/payment/pp_express.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['express_text_title']      = 'crwdns99197:0crwdne99197:0';
+
+// Text
+$_['text_title']              = 'crwdns99199:0crwdne99199:0';
+$_['text_cart']               = 'crwdns99201:0crwdne99201:0';
+$_['text_shipping_updated']   = 'crwdns99203:0crwdne99203:0';
+$_['text_trial']              = 'crwdns99205:0%scrwdnd99205:0%scrwdnd99205:0%scrwdnd99205:0%scrwdne99205:0';
+$_['text_recurring']          = 'crwdns99207:0%scrwdnd99207:0%scrwdnd99207:0%scrwdne99207:0';
+$_['text_recurring_item']     = 'crwdns99209:0crwdne99209:0';
+$_['text_length']             = 'crwdns99211:0%scrwdne99211:0';
+
+// Entry
+$_['express_entry_coupon']    = 'crwdns99213:0crwdne99213:0';
+
+// Button
+$_['button_express_coupon']   = 'crwdns99215:0crwdne99215:0';
+$_['button_express_confirm']  = 'crwdns99217:0crwdne99217:0';
+$_['button_express_login']    = 'crwdns99219:0crwdne99219:0';
+$_['button_express_shipping'] = 'crwdns99221:0crwdne99221:0';
+
+// Error
+$_['error_heading_title']	  = 'crwdns99223:0crwdne99223:0';
+$_['error_too_many_failures'] = 'crwdns99225:0crwdne99225:0';
+$_['error_unavailable'] 	  = 'crwdns99227:0crwdne99227:0';
+$_['error_no_shipping']    	  = 'crwdns99229:0%scrwdne99229:0';
