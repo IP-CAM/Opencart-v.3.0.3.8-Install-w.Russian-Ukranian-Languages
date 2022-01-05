5b0b28a5a47da7307df9471d2236a6d15b993f6d
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:22 +0200

    New translations slideshow.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/module/slideshow.php
@@ -0,0 +1,21 @@
+<?php
+// Heading
+$_['heading_title']    = 'crwdns85077:0crwdne85077:0';
+
+// Text
+$_['text_extension']   = 'crwdns85079:0crwdne85079:0';
+$_['text_success']     = 'crwdns85081:0crwdne85081:0';
+$_['text_edit']        = 'crwdns85083:0crwdne85083:0';
+
+// Entry
+$_['entry_name']       = 'crwdns85085:0crwdne85085:0';
+$_['entry_banner']     = 'crwdns85087:0crwdne85087:0';
+$_['entry_width']      = 'crwdns85089:0crwdne85089:0';
+$_['entry_height']     = 'crwdns85091:0crwdne85091:0';
+$_['entry_status']     = 'crwdns85093:0crwdne85093:0';
+
+// Error
+$_['error_permission'] = 'crwdns85095:0crwdne85095:0';
+$_['error_name']       = 'crwdns85097:0crwdne85097:0';
+$_['error_width']      = 'crwdns85099:0crwdne85099:0';
+$_['error_height']     = 'crwdns85101:0crwdne85101:0';
