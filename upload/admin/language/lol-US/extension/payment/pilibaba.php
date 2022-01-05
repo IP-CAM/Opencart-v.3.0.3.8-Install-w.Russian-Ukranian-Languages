8f2395f76893bf2404d8680e155a449f210127cc
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:48 +0200

    New translations pilibaba.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/payment/pilibaba.php
@@ -0,0 +1,79 @@
+<?php
+// Heading
+$_['heading_title']         = 'crwdns88369:0crwdne88369:0';
+
+// Tab
+$_['tab_register']          = 'crwdns88371:0crwdne88371:0';
+$_['tab_settings']          = 'crwdns88373:0crwdne88373:0';
+
+// Text
+$_['text_extension']        = 'crwdns88375:0crwdne88375:0';
+$_['text_success']          = 'crwdns88377:0crwdne88377:0';
+$_['text_edit']             = 'crwdns88379:0crwdne88379:0';
+$_['text_pilibaba']         = 'crwdns88381:0crwdne88381:0';
+$_['text_live']             = 'crwdns88383:0crwdne88383:0';
+$_['text_test']             = 'crwdns88385:0crwdne88385:0';
+$_['text_payment_info']     = 'crwdns88387:0crwdne88387:0';
+$_['text_order_id']         = 'crwdns88389:0crwdne88389:0';
+$_['text_amount']           = 'crwdns88391:0crwdne88391:0';
+$_['text_fee']              = 'crwdns88393:0crwdne88393:0';
+$_['text_date_added']       = 'crwdns88395:0crwdne88395:0';
+$_['text_tracking']         = 'crwdns88397:0crwdne88397:0';
+$_['text_barcode']          = 'crwdns88399:0crwdne88399:0';
+$_['text_barcode_info']     = 'crwdns88401:0crwdne88401:0';
+$_['text_confirm']          = 'crwdns88403:0crwdne88403:0';
+$_['text_register_success'] = 'crwdns88405:0crwdne88405:0';
+$_['text_tracking_success'] = 'crwdns88407:0crwdne88407:0';
+$_['text_other']            = 'crwdns88409:0crwdne88409:0';
+$_['text_email']            = 'crwdns88411:0%scrwdne88411:0';
+
+// Entry
+$_['entry_email_address']   = 'crwdns88413:0crwdne88413:0';
+$_['entry_password']        = 'crwdns88415:0crwdne88415:0';
+$_['entry_currency']        = 'crwdns88417:0crwdne88417:0';
+$_['entry_warehouse']       = 'crwdns88419:0crwdne88419:0';
+$_['entry_country']         = 'crwdns88421:0crwdne88421:0';
+$_['entry_merchant_number'] = 'crwdns88423:0crwdne88423:0';
+$_['entry_secret_key']      = 'crwdns88425:0crwdne88425:0';
+$_['entry_environment']     = 'crwdns88427:0crwdne88427:0';
+$_['entry_shipping_fee']    = 'crwdns88429:0crwdne88429:0';
+$_['entry_order_status']    = 'crwdns88431:0crwdne88431:0';
+$_['entry_status']          = 'crwdns88433:0crwdne88433:0';
+$_['entry_logging']         = 'crwdns88435:0crwdne88435:0';
+$_['entry_sort_order']      = 'crwdns88437:0crwdne88437:0';
+
+// Help
+$_['help_email_address']    = 'crwdns88439:0crwdne88439:0';
+$_['help_password']         = 'crwdns88441:0crwdne88441:0';
+$_['help_currency']         = 'crwdns88443:0crwdne88443:0';
+$_['help_warehouse']        = 'crwdns88445:0crwdne88445:0';
+$_['help_country']          = 'crwdns88447:0crwdne88447:0';
+$_['help_merchant_number']  = 'crwdns88449:0crwdne88449:0';
+$_['help_secret_key']       = 'crwdns88451:0crwdne88451:0';
+$_['help_shipping_fee']     = 'crwdns88453:0crwdne88453:0';
+$_['help_order_status']     = 'crwdns88455:0crwdne88455:0';
+$_['help_total']            = 'crwdns88457:0crwdne88457:0';
+$_['help_logging']          = 'crwdns88459:0crwdne88459:0';
+
+// Error
+$_['error_warning']         = 'crwdns88461:0crwdne88461:0';
+$_['error_permission']      = 'crwdns88463:0crwdne88463:0';
+$_['error_merchant_number'] = 'crwdns88465:0crwdne88465:0';
+$_['error_secret_key']      = 'crwdns88467:0crwdne88467:0';
+$_['error_shipping_fee']    = 'crwdns88469:0crwdne88469:0';
+$_['error_not_enabled']     = 'crwdns88471:0crwdne88471:0';
+$_['error_data_missing']    = 'crwdns88473:0crwdne88473:0';
+$_['error_tracking_length'] = 'crwdns88475:0crwdne88475:0';
+$_['error_email_address']   = 'crwdns88477:0crwdne88477:0';
+$_['error_email_invalid']   = 'crwdns88479:0crwdne88479:0';
+$_['error_password']        = 'crwdns88481:0crwdne88481:0';
+$_['error_currency']        = 'crwdns88483:0crwdne88483:0';
+$_['error_warehouse']       = 'crwdns88485:0crwdne88485:0';
+$_['error_country']         = 'crwdns88487:0crwdne88487:0';
+$_['error_weight']          = 'crwdns88489:0%scrwdne88489:0';
+$_['error_bad_response']    = 'crwdns88491:0crwdne88491:0';
+
+// Button
+$_['button_register']       = 'crwdns88493:0crwdne88493:0';
+$_['button_tracking']       = 'crwdns88495:0crwdne88495:0';
+$_['button_barcode']        = 'crwdns88497:0crwdne88497:0';
