e5c1e98208e9e776cb10171f4fb8706dccdbfa4e
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:25:08 +0200

    New translations pp_express.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/recurring/pp_express.php
@@ -0,0 +1,11 @@
+<?php
+// Text
+$_['text_title']          = 'crwdns99817:0crwdne99817:0';
+$_['text_canceled']       = 'crwdns99819:0crwdne99819:0';
+
+// Button
+$_['button_cancel']       = 'crwdns99821:0crwdne99821:0';
+
+// Error
+$_['error_not_cancelled'] = 'crwdns99823:0%scrwdne99823:0';
+$_['error_not_found']     = 'crwdns99825:0crwdne99825:0';
