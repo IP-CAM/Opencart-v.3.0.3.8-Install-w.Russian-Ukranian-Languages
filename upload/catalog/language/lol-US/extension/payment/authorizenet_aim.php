7b639a2b1e27b4f9fb2f4bc03564133720065792
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:33 +0200

    New translations authorizenet_aim.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/payment/authorizenet_aim.php
@@ -0,0 +1,10 @@
+<?php
+// Text
+$_['text_title']           = 'crwdns97763:0crwdne97763:0';
+$_['text_credit_card']     = 'crwdns97765:0crwdne97765:0';
+
+// Entry
+$_['entry_cc_owner']       = 'crwdns97767:0crwdne97767:0';
+$_['entry_cc_number']      = 'crwdns97769:0crwdne97769:0';
+$_['entry_cc_expire_date'] = 'crwdns97771:0crwdne97771:0';
+$_['entry_cc_cvv2']        = 'crwdns97773:0crwdne97773:0';
