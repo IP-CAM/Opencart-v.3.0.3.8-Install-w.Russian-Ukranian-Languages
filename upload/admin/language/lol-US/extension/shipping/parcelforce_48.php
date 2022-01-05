89ba4a38d7446d3d8e59a358ddfbc94468982c66
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:22:47 +0200

    New translations parcelforce_48.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/shipping/parcelforce_48.php
@@ -0,0 +1,29 @@
+<?php
+// Heading
+$_['heading_title']           = 'crwdns92447:0crwdne92447:0';
+
+// Text
+$_['text_extension']          = 'crwdns92449:0crwdne92449:0';
+$_['text_success']            = 'crwdns92451:0crwdne92451:0';
+$_['text_edit']               = 'crwdns92453:0crwdne92453:0';
+
+// Entry
+$_['entry_rate']              = 'crwdns92455:0crwdne92455:0';
+$_['entry_insurance']         = 'crwdns92457:0crwdne92457:0';
+$_['entry_display_weight']    = 'crwdns92459:0crwdne92459:0';
+$_['entry_display_insurance'] = 'crwdns92461:0crwdne92461:0';
+$_['entry_display_time']      = 'crwdns92463:0crwdne92463:0';
+$_['entry_tax_class']         = 'crwdns92465:0crwdne92465:0';
+$_['entry_geo_zone']          = 'crwdns92467:0crwdne92467:0';
+$_['entry_status']            = 'crwdns92469:0crwdne92469:0';
+$_['entry_sort_order']        = 'crwdns92471:0crwdne92471:0';
+
+// Help
+$_['help_rate']               = 'crwdns92473:0crwdne92473:0';
+$_['help_insurance']          = 'crwdns92475:0crwdne92475:0';
+$_['help_display_weight']     = 'crwdns92477:0crwdne92477:0';
+$_['help_display_insurance']  = 'crwdns92479:0crwdne92479:0';
+$_['help_display_time']       = 'crwdns92481:0crwdne92481:0';
+
+// Error
+$_['error_permission']        = 'crwdns92483:0crwdne92483:0';
