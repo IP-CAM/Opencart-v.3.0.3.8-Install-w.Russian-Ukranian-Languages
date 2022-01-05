8c29979924f1e541f7567bc7d02107025b579e87
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:50 +0200

    New translations pp_express_refund.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/payment/pp_express_refund.php
@@ -0,0 +1,21 @@
+<?php
+// Heading
+$_['heading_title']		   = 'crwdns89071:0crwdne89071:0';
+
+// Text
+$_['text_pp_express']	   = 'crwdns89073:0crwdne89073:0';
+$_['text_current_refunds'] = 'crwdns89075:0crwdne89075:0';
+$_['text_refund']		   = 'crwdns89077:0crwdne89077:0';
+
+// Entry
+$_['entry_transaction_id'] = 'crwdns89079:0crwdne89079:0';
+$_['entry_full_refund']	   = 'crwdns89081:0crwdne89081:0';
+$_['entry_amount']		   = 'crwdns89083:0crwdne89083:0';
+$_['entry_message']		   = 'crwdns89085:0crwdne89085:0';
+
+// Button
+$_['button_refund']		   = 'crwdns89087:0crwdne89087:0';
+
+// Error
+$_['error_partial_amt']	   = 'crwdns89089:0crwdne89089:0';
+$_['error_data']		   = 'crwdns89091:0crwdne89091:0';
