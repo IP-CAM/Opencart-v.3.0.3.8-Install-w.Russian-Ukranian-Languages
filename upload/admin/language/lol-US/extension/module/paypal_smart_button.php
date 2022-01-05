ac50287e35e1f21276336358b177d042704ddcf4
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:19 +0200

    New translations paypal_smart_button.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/module/paypal_smart_button.php
@@ -0,0 +1,67 @@
+<?php
+// Heading
+$_['heading_title']    					= 'crwdns84907:0crwdne84907:0';
+
+// Text
+$_['text_extensions']   				= 'crwdns84909:0crwdne84909:0';
+$_['text_edit']        					= 'crwdns84911:0crwdne84911:0';
+$_['text_general']				 	 	= 'crwdns84913:0crwdne84913:0';
+$_['text_product_page']					= 'crwdns84915:0crwdne84915:0';
+$_['text_cart_page']					= 'crwdns84917:0crwdne84917:0';
+$_['text_insert_prepend']				= 'crwdns84919:0crwdne84919:0';
+$_['text_insert_append']				= 'crwdns84921:0crwdne84921:0';
+$_['text_insert_before']				= 'crwdns84923:0crwdne84923:0';
+$_['text_insert_after']					= 'crwdns84925:0crwdne84925:0';
+$_['text_align_left']					= 'crwdns84927:0crwdne84927:0';
+$_['text_align_center']					= 'crwdns84929:0crwdne84929:0';
+$_['text_align_right']					= 'crwdns84931:0crwdne84931:0';
+$_['text_small']			 			= 'crwdns84933:0crwdne84933:0';
+$_['text_medium']			 	 		= 'crwdns84935:0crwdne84935:0';
+$_['text_large']			 	 		= 'crwdns84937:0crwdne84937:0';
+$_['text_responsive']			 	 	= 'crwdns84939:0crwdne84939:0';
+$_['text_gold']			 	 			= 'crwdns84941:0crwdne84941:0';
+$_['text_blue']			 	 			= 'crwdns84943:0crwdne84943:0';
+$_['text_silver']			 	 		= 'crwdns84945:0crwdne84945:0';
+$_['text_white']			 	 		= 'crwdns84947:0crwdne84947:0';
+$_['text_black']			 	 		= 'crwdns84949:0crwdne84949:0';
+$_['text_pill']			 	 			= 'crwdns84951:0crwdne84951:0';
+$_['text_rect']			 	 			= 'crwdns84953:0crwdne84953:0';
+$_['text_checkout']			 	 		= 'crwdns84955:0crwdne84955:0';
+$_['text_pay']			 	 			= 'crwdns84957:0crwdne84957:0';
+$_['text_buy_now']			 	 		= 'crwdns84959:0crwdne84959:0';
+$_['text_pay_pal']			 	 		= 'crwdns84961:0crwdne84961:0';
+$_['text_installment']			 	 	= 'crwdns84963:0crwdne84963:0';
+$_['text_text']							= 'crwdns84965:0crwdne84965:0';
+$_['text_flex']							= 'crwdns84967:0crwdne84967:0';
+$_['text_yes']			 	 			= 'crwdns84969:0crwdne84969:0';
+$_['text_no']			 	 			= 'crwdns84971:0crwdne84971:0';
+
+// Entry
+$_['entry_status']     					= 'crwdns84973:0crwdne84973:0';
+$_['entry_product_page_status']     	= 'crwdns84975:0crwdne84975:0';
+$_['entry_cart_page_status']     		= 'crwdns84977:0crwdne84977:0';
+$_['entry_insert_tag']     				= 'crwdns84979:0crwdne84979:0';
+$_['entry_insert_type']     			= 'crwdns84981:0crwdne84981:0';
+$_['entry_button_align']     			= 'crwdns84983:0crwdne84983:0';
+$_['entry_button_size'] 				= 'crwdns84985:0crwdne84985:0';
+$_['entry_button_color'] 				= 'crwdns84987:0crwdne84987:0';
+$_['entry_button_shape'] 				= 'crwdns84989:0crwdne84989:0';
+$_['entry_button_label'] 				= 'crwdns84991:0crwdne84991:0';
+$_['entry_button_tagline'] 				= 'crwdns84993:0crwdne84993:0';
+$_['entry_message_status']     			= 'crwdns84995:0crwdne84995:0';
+$_['entry_message_align']     			= 'crwdns84997:0crwdne84997:0';
+$_['entry_message_size'] 				= 'crwdns84999:0crwdne84999:0';
+$_['entry_message_layout'] 				= 'crwdns85001:0crwdne85001:0';
+$_['entry_message_text_color'] 			= 'crwdns85003:0crwdne85003:0';
+$_['entry_message_text_size'] 			= 'crwdns85005:0crwdne85005:0';
+$_['entry_message_flex_color'] 			= 'crwdns85007:0crwdne85007:0';
+$_['entry_message_flex_ratio'] 			= 'crwdns85009:0crwdne85009:0';
+
+// Help
+$_['help_message_status']				= 'crwdns85011:0crwdne85011:0';
+
+// Success
+$_['success_save']						= 'crwdns85013:0crwdne85013:0';
+
+// Error
+$_['error_permission'] 					= 'crwdns85015:0crwdne85015:0';
