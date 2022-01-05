77c57dd00188ceec5188ddfb9e49350a0dba62dd
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:24:11 +0200

    New translations klarna_checkout.php (LOLCAT)

---
+++ upload/catalog/language/lol-US/extension/payment/klarna_checkout.php
@@ -0,0 +1,14 @@
+<?php
+// Heading
+$_['heading_title']				   = 'crwdns98887:0crwdne98887:0';
+$_['heading_title_success']		   = 'crwdns98889:0crwdne98889:0';
+
+// Text
+$_['text_title']				   = 'crwdns98891:0crwdne98891:0';
+$_['text_basket']				   = 'crwdns98893:0crwdne98893:0';
+$_['text_checkout']				   = 'crwdns98895:0crwdne98895:0';
+$_['text_success']				   = 'crwdns98897:0crwdne98897:0';
+$_['text_choose_shipping_method']  = 'crwdns98899:0crwdne98899:0';
+$_['text_sales_tax']			   = 'crwdns98901:0crwdne98901:0';
+$_['text_newsletter']			   = 'crwdns98903:0crwdne98903:0';
+$_['text_list']                    = 'crwdns98905:0crwdne98905:0';
