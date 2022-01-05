615b17592d63656c87686ac6360f72767e36459a
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:34 +0200

    New translations other.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/extension/other.php
@@ -0,0 +1,15 @@
+<?php
+// Heading
+$_['heading_title']     = 'crwdns83983:0crwdne83983:0';
+
+// Text
+$_['text_success']      = 'crwdns83985:0crwdne83985:0';
+$_['text_list']         = 'crwdns83987:0crwdne83987:0';
+
+// Column
+$_['column_name']       = 'crwdns83989:0crwdne83989:0';
+$_['column_status']     = 'crwdns83991:0crwdne83991:0';
+$_['column_action']     = 'crwdns83993:0crwdne83993:0';
+
+// Error
+$_['error_permission']  = 'crwdns83995:0crwdne83995:0';
