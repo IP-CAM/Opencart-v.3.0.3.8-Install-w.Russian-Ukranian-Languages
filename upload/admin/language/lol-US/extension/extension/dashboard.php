17acb37570491a13c04094b8320b50e5797e9218
Dmytro Pavlovskyi <nerodocinn@gmail.com>
2022-01-05 15:21:31 +0200

    New translations dashboard.php (LOLCAT)

---
+++ upload/admin/language/lol-US/extension/extension/dashboard.php
@@ -0,0 +1,17 @@
+<?php
+// Heading
+$_['heading_title']     = 'crwdns83881:0crwdne83881:0';
+
+// Text
+$_['text_success']      = 'crwdns83883:0crwdne83883:0';
+$_['text_list']         = 'crwdns83885:0crwdne83885:0';
+
+// Column
+$_['column_name']       = 'crwdns83887:0crwdne83887:0';
+$_['column_width']      = 'crwdns83889:0crwdne83889:0';
+$_['column_status']     = 'crwdns83891:0crwdne83891:0';
+$_['column_sort_order'] = 'crwdns83893:0crwdne83893:0';
+$_['column_action']     = 'crwdns83895:0crwdne83895:0';
+
+// Error
+$_['error_permission']  = 'crwdns83897:0crwdne83897:0';
